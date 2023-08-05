37d1ce409032937f2b44a5fad7587fd02cb597b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:03 +0000

    Add package pyhiapi/0.1.5/pyhiapi-0.1.5.tar.gz

---
+++ packages/pyhiapi/pyhiapi-0.1.5.tar.gz/pyhiapi-0.1.5/setup.py
@@ -0,0 +1,24 @@
+"""Set up hiapi
+"""
+
+from setuptools import setup
+
+setup(
+    name='pyhiapi',
+    version='0.1.5',
+    author='Ryan Jung',
+    author_email='ryanjjung@gmail.com',
+    packages=['hiapi'],
+    url='https://github.com/ryanjjung/pyhiapi',
+    license='LICENCE',
+    description='PyHiAPI - Simple test API',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'flask'
+    ],
+    entry_points={
+        'console_scripts': [
+            'hiapi = hiapi.hi:main'
+        ]
+    }
+)
