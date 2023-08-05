f69b04b285187fa7059512ddc755d810a5399cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:48 +0000

    Add package simple-python-couch-db/0.0.1/simple_python_couch_db-0.0.1.tar.gz

---
+++ packages/simple-python-couch-db/simple_python_couch_db-0.0.1.tar.gz/simple_python_couch_db-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simple_python_couch_db",
+    version="0.0.1",
+    author="Oliver Schwamb",
+    author_email="mail@oliverschwamb.de",
+    description="Simple adapter to use CouchDB with Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bjoerndot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Database :: Front-Ends',
+    ],
+    python_requires='>=3.6',
+)
