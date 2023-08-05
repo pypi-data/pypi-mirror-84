b92e733da58808ecabbf4b3bb791619e6be3b46f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:00 +0000

    Add package smaths/0.0.3/smaths-0.0.3.tar.gz

---
+++ packages/smaths/smaths-0.0.3.tar.gz/smaths-0.0.3/README.md
@@ -0,0 +1,15 @@
+# Square Root Simplification algorithm
+
+
+
+Smath stands for SquareMATH. Algorithm written exclusively in Python and used to simplify square roots
+
+
+
+# How To Use
+
+
+Firstly, import the spl function from the file (from smath import spl). Then, call the spl() function and parse in a string argument; for example if you want the square root of 125, you would parse in "!125"; note this is using the ! instead of the √ (that char is not on the keyboard). If you want the simplification of 5√125, you would parse "5!125".
+
+
+Also note the function returns a string value.
---
+++ packages/smaths/smaths-0.0.3.tar.gz/smaths-0.0.3/smaths/__init__.py
@@ -0,0 +1,70 @@
+__author__ = 'miha_focsa'
+
+from math import sqrt
+
+def spl(*root):
+    foundSolution = False
+    primes = []
+    nums = []
+    start, end = 0, 100000
+    sep = root[0].split('!')
+    if sep[0] == '':
+        root = sep[1]
+        multp = None
+    else:
+        root = sep[1]
+        multp = sep[0]
+    for i in range(start, end + 1):
+        if (i**(.5) == int(i**(.5))):
+            nums.append(i)
+    else:
+        for i in range(100000):
+            primes.append(i)
+        else:
+            for num in nums:
+                if root == num:
+                    foundSolution = True
+                    return str(int(sqrt(num)))
+                else:
+                    pass
+            else:
+                for prime in primes:
+                    if prime != 0:
+                        if int(root) % int(prime) == 0:
+                            r = int(root) / prime
+                            s = sqrt(r)
+                            items = str(s).split(".")
+                            if foundSolution == True:
+                                pass
+                            else:
+                                if len(str(float(items[1]))) > 3:
+                                    pass
+                                else:
+                                    if multp == None:
+                                        if str(float(items[1])) == '0.0':
+                                            foundSolution = True
+                                            mod_s = int(s)
+                                            if mod_s != 1:
+                                                if prime != 1:
+                                                    return str(f"{mod_s}√{prime}")
+                                                else:
+                                                    mod_s = int(s)
+                                                    return str(f"{mod_s}")
+                                            else:
+                                                return str(f"{prime}")
+                                    else:
+                                        if str(float(items[1])) == '0.0':
+                                            foundSolution = True
+                                            mod_s = int(s) * int(multp)
+                                            if mod_s != 1:
+                                                if prime != 1:
+                                                    mod_s = int(s) * int(multp)
+                                                    return str(f"{mod_s}√{prime}")
+                                                else:
+                                                    mod_s = int(s) * int(multp)
+                                                    mods_s = mod_s * prime
+                                                    return str(f"{mods_s}") 
+                                            else:
+                                                return str(f"{prime}")
+                    else:
+                        foundSolution = False
