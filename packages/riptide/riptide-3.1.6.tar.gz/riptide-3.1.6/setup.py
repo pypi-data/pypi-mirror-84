d23f2d4b017c00ffc858530a2e4c0fcf1bb57f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:11 +0000

    Add package riptide/3.1.6/riptide-3.1.6.tar.gz

---
+++ packages/riptide/riptide-3.1.6.tar.gz/riptide-3.1.6/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setup(name='riptide',
+	version='3.1.6', 
+	description='Reaction Inclusion by Parsimony and Transcript Distribution (RIPTiDe)',
+	author='Matthew Jenior',
+	author_email='mattjenior@gmail.com',
+	url='https://github.com/mjenior/riptide',
+	packages=find_packages(),
+    install_requires=['cobra','symengine','scipy'],
+    license='MIT',
+    long_description_content_type='text/markdown',
+    long_description=long_description)
