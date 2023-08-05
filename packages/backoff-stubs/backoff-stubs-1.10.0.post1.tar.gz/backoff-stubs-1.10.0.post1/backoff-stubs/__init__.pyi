a4491a87d1844aabf0c638c22b06cf9e577f4608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:41 +0000

    Add package backoff-stubs/1.10.0.post1/backoff-stubs-1.10.0.post1.tar.gz

---
+++ packages/backoff-stubs/backoff-stubs-1.10.0.post1.tar.gz/backoff-stubs-1.10.0.post1/backoff-stubs/__init__.pyi
@@ -0,0 +1,52 @@
+import logging
+from typing import (Any, Callable, Dict, Generator, Iterable, Optional,
+                    Sequence, Tuple, Type, TypeVar, Union)
+
+_FuncT = TypeVar('_FuncT', bound=Callable)
+_HandlerFunction = Callable[[Dict], None]
+_EventHandler = Union[_HandlerFunction, Iterable[_HandlerFunction]]
+
+def on_predicate(
+    wait_gen: Callable[..., Generator[float, None, None]],
+    predicate: Callable[[Any], bool],
+    max_tries: Optional[int] = ...,
+    max_time: Optional[int] = ...,
+    jitter: Callable[[float], float] = ...,
+    on_success: Optional[_EventHandler] = ...,
+    on_backoff: Optional[_EventHandler] = ...,
+    on_giveup: Optional[_EventHandler] = ...,
+    logger: Union[str, logging.Logger] = ...,
+    **wait_gen_kwargs: Any
+) -> Callable[[_FuncT], _FuncT]: ...
+
+def on_exception(
+    wait_gen: Callable[..., Generator[float, None, None]],
+    exception: Union[Type[Exception], Tuple[Type[Exception], ...]],
+    max_tries: Optional[int] = ...,
+    max_time: Optional[int] = ...,
+    jitter: Callable[[float], float] = ...,
+    giveup: Callable[[Exception], bool] = ...,
+    on_success: Optional[_EventHandler] = ...,
+    on_backoff: Optional[_EventHandler] = ...,
+    on_giveup: Optional[_EventHandler] = ...,
+    logger: Union[str, logging.Logger] = ...,
+    **wait_gen_kwargs: Any
+) -> Callable[[_FuncT], _FuncT]: ...
+
+def expo(
+    base: float = ...,
+    factor: float = ...,
+    max_value: Optional[float] = ...
+) -> Generator[float, None, None]: ...
+
+def fibo(
+    max_value: Optional[float] = ...
+) -> Generator[float, None, None]: ...
+
+def constant(
+    interval: Union[float, Iterable[float], Sequence[float]] = ...
+) -> Generator[float, None, None]: ...
+
+def full_jitter(value: float) -> float: ...
+
+def random_jitter(value: float) -> float: ...
