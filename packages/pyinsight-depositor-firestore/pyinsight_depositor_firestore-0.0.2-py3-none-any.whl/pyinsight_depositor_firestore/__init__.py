d83755e1170202c6683b3385a4a87bf70a5acde9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:40 +0000

    Add package pyinsight-depositor-firestore/0.0.2/pyinsight_depositor_firestore-0.0.2-py3-none-any.whl

---
+++ packages/pyinsight-depositor-firestore/pyinsight_depositor_firestore-0.0.2-py3-none-any.whl/pyinsight_depositor_firestore/__init__.py
@@ -0,0 +1,10 @@
+# Module Import
+from pyinsight_depositor_firestore import firestore_depositor
+
+# Object Import
+from pyinsight_depositor_firestore.firestore_depositor import FirestoreDepositor
+
+# Element Listing
+__all__ = ['FirestoreDepositor']
+
+__version__ = "0.0.2"
