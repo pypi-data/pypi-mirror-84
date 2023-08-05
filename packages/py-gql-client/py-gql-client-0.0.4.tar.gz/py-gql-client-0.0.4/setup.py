5a5b04cd7cc2bb384ec80ee73cfc61e5954ee44a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:16 +0000

    Add package py-gql-client/0.0.4/py-gql-client-0.0.4.tar.gz

---
+++ packages/py-gql-client/py-gql-client-0.0.4.tar.gz/py-gql-client-0.0.4/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+    name="py-gql-client",
+    version="0.0.4",
+    author="Facebook Inc.",
+    description="Python GraphQL generated code client",
+    packages=setuptools.find_packages(include=["gql_client", "gql_client*"]),
+    license="BSD License",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.7",
+    ],
+    python_requires=">=3.7",
+    include_package_data=True,
+    scripts=["bin/gql-compiler"],
+    install_requires=[
+        "graphql-core>=3",
+        "unicodecsv>=0.14.1",
+        "requests>=2.22.0",
+        "dataclasses-json==0.3.2",
+    ],
+)
