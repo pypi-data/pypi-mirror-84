488215cb4fc679343c39e90629bd0afbda693d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:44 +0000

    Add package ocw-data-parser/0.14.1/ocw_data_parser-0.14.1-py3-none-any.whl

---
+++ packages/ocw-data-parser/ocw_data_parser-0.14.1-py3-none-any.whl/ocw_data_parser/test_constants.py
@@ -0,0 +1,4 @@
+COURSE_DIR = "ocw_data_parser/test_json/course_dir/"
+SINGLE_COURSE_DIR = "{}course-1".format(COURSE_DIR)
+STATIC_PREFIX = "static_files/"
+S3_TEST_COURSE_ROOT = "QA/Department/CourseNumber/Term/"
