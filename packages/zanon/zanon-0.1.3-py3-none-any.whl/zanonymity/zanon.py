567a879d9380abc27074609b26b0822b05900d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:38 +0000

    Add package zanon/0.1.3/zanon-0.1.3-py3-none-any.whl

---
+++ packages/zanon/zanon-0.1.3-py3-none-any.whl/zanonymity/zanon.py
@@ -0,0 +1,32 @@
+from .utils import *
+
+class zanon(object):
+
+    def __init__(self, deltat, z): 
+        super(zanon, self).__init__()
+        self.deltat = deltat
+        self.z = z
+        self.H = {}
+        self.LRU = {}
+        self.c = {}
+
+	
+    def anonymize(self, line, f):
+        t, u, a = read_next_visit(line)
+            
+        #manage data structure
+        manage_data_structure(t, u, a, self.H, self.LRU, self.c)
+        #evict old users
+        evict(t, self.H, self.LRU, self.c, self.deltat)
+        #possibly outputs the data
+        output = check_and_output(t, u, a, self.c, self.z)
+
+        if output != None:
+            print(output)
+            f.write("\t".join(str(x) for x in output) + "\n")
+
+
+
+
+
+		
