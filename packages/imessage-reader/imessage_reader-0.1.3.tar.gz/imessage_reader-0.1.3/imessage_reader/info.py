f844356398d669f88570c585f8e3239cb8d4d71f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:21 +0000

    Add package imessage-reader/0.1.3/imessage_reader-0.1.3.tar.gz

---
+++ packages/imessage-reader/imessage_reader-0.1.3.tar.gz/imessage_reader-0.1.3/imessage_reader/info.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+App info, license, version
+Python 3.8+
+Author: niftycode
+Date created: June 14th, 2020
+Date modified: October 24th, 2020
+"""
+
+VERSION = '0.1.3'
+
+
+def app_info():
+    """
+    Show app infos (Version & License)
+    """
+    print('\n\n    ##### A Python script to read iMessage data #####')
+    print('    #              Created by niftycode             #')
+    print(f'    #                 Version {VERSION}                 #')
+    print('    #                  MIT License                  #')
+    print('    #################################################\n\n')
+    print()
---
+++ packages/imessage-reader/imessage_reader-0.1.3.tar.gz/imessage_reader-0.1.3/setup.py
@@ -0,0 +1,35 @@
+import io
+from setuptools import setup, find_packages
+
+with io.open('README.md', 'rt', encoding='utf8') as f:
+    LONG_DESC = f.read()
+
+VERSION = '0.1.3'
+
+setup(
+    name='imessage_reader',
+    version=VERSION,
+    license='MIT',
+    description='Fetch recipients and chat messages from the chat.db database.',
+    long_description=LONG_DESC,
+    long_description_content_type='text/markdown',
+    author='Bodo Schönfeld',
+    author_email='bodo.schoenfeld@niftycode.de',
+    url='https://github.com/niftycode/imessage_reader',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+    packages=find_packages(exclude=('tests', 'docs')),
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': ['imessage_reader=imessage_reader.cli:main'],
+    },
+    install_requires=['openpyxl'],
+    tests_require=['pytest'],
+)
