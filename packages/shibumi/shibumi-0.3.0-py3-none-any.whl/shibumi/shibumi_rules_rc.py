0c92145846a151d63875765b4d8e0f56c59a8fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:57 +0000

    Add package shibumi/0.3.0/shibumi-0.3.0-py3-none-any.whl

---
+++ packages/shibumi/shibumi-0.3.0-py3-none-any.whl/shibumi/shibumi_rules_rc.py
@@ -0,0 +1,208 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 5.15.0
+# WARNING! All changes made in this file will be lost!
+
+from PySide2 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x02\x02\
+-\
+--\x0atitle: Spline\
+ Rules\x0a\x0a---\x0a\x0a* d\
+esigned by N\xc3\xa9st\
+or Romeral Andr\xc3\
+\xa9s\x0a* 2 players\x0a\x0a\
+### Start\x0aThe bo\
+ard starts empty\
+.\x0a\x0a### Play\x0aPlay\
+ers take turns a\
+dding a piece of\
+ their colour to\
+ any\x0aplayable po\
+int (empty hole \
+or platform).\x0a\x0a#\
+## End\x0aA player \
+wins by making a\
+ flat line of th\
+eir colour spann\
+ing\x0aside to side\
+, or corner to c\
+orner, on any le\
+vel.\x0a\x0a### Strate\
+gy\x0aMost wins wil\
+l occur on the 2\
+\xc3\x972 level, by th\
+e first player t\
+o reach that hig\
+h.\x0a\x0aEvery game i\
+s guaranteed to \
+produce a winner\
+ before the last\
+ ball is played.\
+\x0a\
+\x00\x00\x029\
+-\
+--\x0atitle: Spaiji\
+ Rules\x0a\x0a---\x0a\x0a* d\
+esigned by N\xc3\xa9st\
+or Romeral Andr\xc3\
+\xa9s\x0a* 2 players\x0a\x0a\
+Spaiji is the Sh\
+ibumi version of\
+ Taiji, which in\
+volves the place\
+ment of pieces\x0ao\
+f both colours p\
+er turn.\x0a\x0a### St\
+art\x0aThe board st\
+arts empty.\x0a\x0a###\
+ Play\x0aPlayers ta\
+ke turns adding \
+a white ball and\
+ a black ball ea\
+ch turn, in any \
+order,\x0aat any pl\
+ayable points, p\
+rovided that bot\
+h balls touch.\x0a\x0a\
+### End\x0aThe game\
+ ends when the p\
+yramid is comple\
+te, or when a pl\
+ayer cannot play\
+ two\x0aballs that \
+touch. The owner\
+ of the largest \
+visibly connecte\
+d group wins.\x0a\x0aI\
+f tied, the seco\
+nd player (Black\
+) wins.\x0a\
+\x00\x00\x00\xae\
+<\
+!DOCTYPE RCC><RC\
+C version=\x221.0\x22>\
+\x0a<qresource>\x0a<fi\
+le>./shibumi_rul\
+es.qrc</file>\x0a<f\
+ile>./spaiji.md<\
+/file>\x0a<file>./s\
+pargo.md</file>\x0a\
+<file>./spline.m\
+d</file>\x0a</qreso\
+urce>\x0a</RCC>\x0a\
+\x00\x00\x03\xfd\
+-\
+--\x0atitle: Spargo\
+ / Margo Rules\x0a\x0a\
+---\x0a\x0a* designed \
+by Cameron Brown\
+e\x0a* 2 players\x0a\x0aS\
+pargo is a 3D ex\
+tension of Go, i\
+n which pinned p\
+ieces remain act\
+ive in the game\x0a\
+following captur\
+e. Spargo uses a\
+ 4x4 board, and \
+Margo uses 6x6.\x0a\
+\x0a### Start\x0aThe b\
+oard starts empt\
+y.\x0a\x0a### Play\x0aPla\
+yers take turns \
+adding a ball of\
+ their colour to\
+ a playable\x0apoin\
+t. The ball must\
+ have freedom (i\
+.e. it must be v\
+isibly connected\
+\x0ato at least one\
+ empty board hol\
+e by a chain of \
+visibly touching\
+\x0afriendly balls)\
+ following the m\
+ove.\x0a\x0aEnemy grou\
+ps with no freed\
+om are captured \
+after each move,\
+\x0aexcept that bal\
+ls supporting on\
+e or more enemy \
+pieces are not\x0ar\
+emoved. Such bal\
+ls survive captu\
+re and remain ac\
+tive in the\x0agame\
+ as zombies.\x0a\x0aPa\
+ssing is not all\
+owed.\x0a\x0aOverpasse\
+s cut underpasse\
+s.\x0a\x0aThe superko \
+rule applies: it\
+ is not allowed \
+to repeat the bo\
+ard position of\x0a\
+any previous tur\
+n with the same \
+player to move.\x0a\
+\x0a### End\x0aThe gam\
+e ends when the \
+current player h\
+as no legal move\
+s,\x0aand is won by\
+ the player with\
+ the most balls \
+in play (countin\
+g zombies).\x0a\
+"
+
+qt_resource_name = b"\
+\x00\x0d\
+\x0c\xb6\xbbC\
+\x00s\
+\x00h\x00i\x00b\x00u\x00m\x00i\x00_\x00r\x00u\x00l\x00e\x00s\
+\x00\x09\
+\x03\x04\xb1\xd4\
+\x00s\
+\x00p\x00l\x00i\x00n\x00e\x00.\x00m\x00d\
+\x00\x09\
+\x08\x00q\xf4\
+\x00s\
+\x00p\x00a\x00i\x00j\x00i\x00.\x00m\x00d\
+\x00\x11\
+\x0b_5\xe3\
+\x00s\
+\x00h\x00i\x00b\x00u\x00m\x00i\x00_\x00r\x00u\x00l\x00e\x00s\x00.\x00q\x00r\x00c\
+\
+\x00\x09\
+\x08\x8f\x11\xf4\
+\x00s\
+\x00p\x00a\x00r\x00g\x00o\x00.\x00m\x00d\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x04\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00 \x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01t\x95i\x8e\x9e\
+\x00\x00\x008\x00\x00\x00\x00\x00\x01\x00\x00\x02\x06\
+\x00\x00\x01uM\xf0c\x04\
+\x00\x00\x00x\x00\x00\x00\x00\x00\x01\x00\x00\x04\xf5\
+\x00\x00\x01uM\xf9\xf0\xc6\
+\x00\x00\x00P\x00\x00\x00\x00\x00\x01\x00\x00\x04C\
+\x00\x00\x01uM\xfa\x1d\xbe\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
