8d34165359895be563f4c45389869a23c763183e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:04 +0000

    Add package imghst/0.5.2/imghst-0.5.2-py3-none-any.whl

---
+++ packages/imghst/imghst-0.5.2-py3-none-any.whl/imghst/app/configuration/configuration.py
@@ -0,0 +1,13 @@
+from imghst.app.configuration.unique_generators.uuid_based import generate_unique_id
+from pathlib import Path
+
+
+class Configuration:
+    """ Do Not Touch Anything Here. Configure with arguments. """
+
+    api_request_key = "DEFAULTCONFIGURATION"
+    image_unique_file_name = generate_unique_id
+    image_hosting_folder = Path(".")
+
+    port_number_to_run_on = 5000
+    current_domain_url = "https://changedomainhere.com/"
