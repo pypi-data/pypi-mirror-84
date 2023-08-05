8b0c1d54f9e30823ada111afa5cb03c9a69aa855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:54 +0000

    Add package backoff-stubs/1.10.0.post3/backoff-stubs-1.10.0.post3.tar.gz

---
+++ packages/backoff-stubs/backoff-stubs-1.10.0.post3.tar.gz/backoff-stubs-1.10.0.post3/setup.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+from setuptools import setup
+
+DIR = Path(__file__).parent
+
+setup(
+    name='backoff-stubs',
+    author='Gleb Chipiga',
+    version='1.10.0.post3',
+    description='External type annotations for the backoff library',
+    long_description=(DIR / 'README.rst').read_text('utf-8').strip(),
+    long_description_content_type="text/x-rst",
+    url='https://github.com/gleb-chipiga/backoff-stubs',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    license='MIT',
+    keywords=['retry', 'backoff', 'decorators', 'stubs', 'mypy'],
+    packages=['backoff-stubs'],
+    package_data={'backoff-stubs': ['__init__.pyi']},
+    python_requires='>=3.8'
+)
