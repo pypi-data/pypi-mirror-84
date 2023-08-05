0ee661e02176cf0487ef20351f3f9e650379fae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:02 +0000

    Add package video-razor/0.0.2/video-razor-0.0.2.tar.gz

---
+++ packages/video-razor/video-razor-0.0.2.tar.gz/video-razor-0.0.2/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="video-razor",
+    packages=['video-razor'],
+    version="0.0.2",
+    license='MIT',
+    author="David Alford",
+    author_email="firstlast678@gmail.com",
+    description="A package for slicing a video into N sections",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/5starkarma/video-razor",
+    download_url='https://github.com/5starkarma/video-razor/archive/0.0.2.tar.gz',
+    keywords=['OPENCV', 'VIDEO', 'CUT', 'SLICE', 'RAZOR'],
+    install_requires=[
+        'numpy',
+        'opencv-python',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Build Tools',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
