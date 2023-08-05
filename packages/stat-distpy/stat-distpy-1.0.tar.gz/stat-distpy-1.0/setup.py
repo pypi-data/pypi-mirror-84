70933dbf93dbd012377e8842873a7d481963f751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:47 +0000

    Add package stat-distpy/1.0/stat-distpy-1.0.tar.gz

---
+++ packages/stat-distpy/stat-distpy-1.0.tar.gz/stat-distpy-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stat-distpy',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['stat-distpy'],
+      author='Luiz Alberto',
+      author_email='gomes.luiz@gmail.com',
+      zip_safe=False)
