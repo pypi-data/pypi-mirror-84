2e65d07ae91508a77e6c376aa2a38f9b8269f97e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:34 +0000

    Add package ledfx-dev/0.1.1/ledfx_dev-0.1.1-py3-none-any.whl

---
+++ packages/ledfx-dev/ledfx_dev-0.1.1-py3-none-any.whl/ledfx/consts.py
@@ -0,0 +1,17 @@
+import pathlib
+
+__author__ = "Austin Hodges"
+__copyright__ = "Austin Hodges"
+__license__ = "mit"
+
+REQUIRED_PYTHON_VERSION = (3, 6, 0)
+REQUIRED_PYTHON_STRING = '>={}.{}.{}'.format(REQUIRED_PYTHON_VERSION[0],
+                                             REQUIRED_PYTHON_VERSION[1],
+                                             REQUIRED_PYTHON_VERSION[2])
+
+MAJOR_VERSION = 0
+MINOR_VERSION = 1
+MICRO_VERSION = 1
+PROJECT_VERSION = '{}.{}.{}'.format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
+
+__version__ = PROJECT_VERSION
