16821b94d77db004c46b7a77e4a47435f664c41b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:10 +0000

    Add package dvpipe/0.0.4/dvpipe-0.0.4-py3-none-any.whl

---
+++ packages/dvpipe/dvpipe-0.0.4-py3-none-any.whl/dpipe/__init__.py
@@ -0,0 +1,26 @@
+""" dpipe """
+
+
+from functools import reduce, partial
+
+
+__all__ = ['pipe']
+
+
+def pipe(seed, *funcs):
+    """
+    Data Pipe utility that pipes data from function to function in sequencial order.
+    Attributes:
+        seed (object): Input to apply to functions.
+        *funcs (func): Functions for the input to be passed into.
+                        Use tuples for functions with parameters.
+    Example:
+        pipe({'foo': 'bar'}, func1, func2, (func3, 'test'))
+    """
+    def return_partial(arg, func):
+        args = list(func)
+        args.insert(1, arg)
+        return partial(*tuple(args))()
+
+    return reduce(lambda arg, func: return_partial(arg, func)
+        if isinstance(func, tuple) else func(arg), funcs, seed)
