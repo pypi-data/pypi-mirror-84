bf5615ea71ddd802b09c53fee0fa01cc1f3b3372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:20 +0000

    Add package xlcalculator/0.2.10/xlcalculator-0.2.10-py3-none-any.whl

---
+++ packages/xlcalculator/xlcalculator-0.2.10-py3-none-any.whl/tests/xlfunctions/test_text.py
@@ -0,0 +1,79 @@
+import unittest
+
+from xlcalculator.xlfunctions import text, xl, xlerrors, func_xltypes
+
+
+class TextModuleTest(unittest.TestCase):
+
+    def test_CONCAT(self):
+        self.assertEqual(
+            text.CONCAT("SPAM", " ", "SPAM"), 'SPAM SPAM')
+        self.assertEqual(
+            text.CONCAT(
+                "SPAM",
+                " ",
+                func_xltypes.Array([[1, 2]]),
+                4
+            ),
+            'SPAM 124'
+        )
+
+    def test_CONCAT_with_too_many_items(self):
+        self.assertIsInstance(
+            text.CONCAT(*[0] * 300),
+            xlerrors.ValueExcelError
+        )
+
+    def test_CONCAT_MS_cases(self):
+
+        self.assertEqual(
+            text.CONCAT(
+                "The",
+                " ",
+                "sun",
+                " ",
+                "will",
+                " ",
+                "come",
+                " ",
+                "up",
+                " ",
+                "tomorrow."
+            ),
+            "The sun will come up tomorrow.")
+
+        range0 = func_xltypes.Array(
+            ["A's", 'a1', 'a2', 'a4', 'a5', 'a6', 'a7'])
+        range1 = func_xltypes.Array(
+            ["B's", 'b1', 'b2', 'b4', 'b5', 'b6', 'b7'])
+
+        self.assertEqual(text.CONCAT(range0, range1),
+                         "A'sa1a2a4a5a6a7B'sb1b2b4b5b6b7")
+
+    def test_MID(self):
+        self.assertEqual(text.MID('Romain', 3, 4), 'main')
+        self.assertEqual(text.MID('Romain', 1, 2), 'Ro')
+        self.assertEqual(text.MID('Romain', 3, 6), 'main')
+
+    def test_MID_start_num_converted_to_integer(self):
+        self.assertEqual(text.MID('Romain', 1.1, 2), 'Ro')
+
+    def test_MID_num_chars_converted_to_integer(self):
+        self.assertEqual(text.MID('Romain', 1, 2.1), 'Ro')
+
+    def test_MID_start_num_must_be_superior_or_equal_to_1(self):
+        self.assertIsInstance(
+            text.MID('Romain', 0, 3), xlerrors.NumExcelError)
+
+    def test_MID_num_chars_must_be_positive(self):
+        self.assertIsInstance(
+            text.MID('Romain', 1, -1), xlerrors.NumExcelError)
+
+    def test_MID_with_too_large_text(self):
+        self.assertIsInstance(
+            text.MID('foo' + ' ' * xl.CELL_CHARACTER_LIMIT, 1, 3),
+            xlerrors.ValueExcelError)
+
+    def test_RIGHT(self):
+        self.assertEqual(text.RIGHT("Sale Price", 5), "Price")
+        self.assertEqual(text.RIGHT("Stock Number"), "r")
