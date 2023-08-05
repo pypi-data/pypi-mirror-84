effd3611b66096c278a34861949e633fc929d783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:59 +0000

    Add package power-ups/0.0.0/power-ups-0.0.0.tar.gz

---
+++ packages/power-ups/power-ups-0.0.0.tar.gz/power-ups-0.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="power-ups",
+    version="0.0.0",
+    author="ambientnuance",
+    author_email="ambientnuance@gmail.com",
+    description="(name-squatting placeholder)",
+    packages=setuptools.find_packages(),
+)
