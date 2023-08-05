89ecf49a6dd3a606d1f6f793a3d86061e95af936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:19 +0000

    Add package amulog/0.1.5/amulog-0.1.5.tar.gz

---
+++ packages/amulog/amulog-0.1.5.tar.gz/amulog-0.1.5/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+from setuptools import setup
+
+
+def load_readme():
+    with open('README.rst', 'r') as f:
+        return f.read()
+
+def load_requirements():
+    """Parse requirements.txt"""
+    reqs_path = os.path.join('.', 'requirements.txt')
+    with open(reqs_path, 'r') as f:
+        requirements = [line.rstrip() for line in f]
+    return requirements
+
+
+sys.path.append("./tests")
+package_name = 'amulog'
+data_dir = "/".join((package_name, "data"))
+data_files = ["/".join((data_dir, fn)) for fn in os.listdir(data_dir)]
+
+init_path = os.path.join(os.path.dirname(__file__), package_name, '__init__.py')
+with open(init_path) as f:
+    version = re.search("__version__ = '([^']+)'", f.read()).group(1)
+
+setup(name=package_name,
+      version=version,
+      description='A system log management tool with automatically generated log templates.',
+      long_description=load_readme(),
+      #long_description_content_type='text/markdown',
+      author='Satoru Kobayashi',
+      author_email='sat@nii.ac.jp',
+      url='https://github.com/cpflat/amulog/',
+      install_requires=load_requirements(),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Topic :: Software Development :: Libraries :: Python Modules'],
+      license='The 3-Clause BSD License',
+
+      packages=['amulog'],
+      package_data={'amulog': data_files},
+      test_suite="tests"
+      )
