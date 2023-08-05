76368b266c650022a44fd2cc78378cc1c5565e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:55 +0000

    Add package matrix-path/0.0.1/matrix_path-0.0.1.tar.gz

---
+++ packages/matrix-path/matrix_path-0.0.1.tar.gz/matrix_path-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='matrix_path',
+    version='0.0.1',
+    description='Navigating and Visualizing paths in a 2D Matrix.',
+    py_modules=["matrix"],
+    package_dir={'': 'src'},
+)
