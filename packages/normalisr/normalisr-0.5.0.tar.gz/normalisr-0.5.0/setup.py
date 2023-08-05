4126c84a358b1cbb6f4b135139ec5d35ba3cce93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:02 +0000

    Add package normalisr/0.5.0/normalisr-0.5.0.tar.gz

---
+++ packages/normalisr/normalisr-0.5.0.tar.gz/normalisr-0.5.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+
+pkgname="normalisr"
+pkgnamefull="Normalisr Offers Robust Modelling of Associations Linearly In Single-cell RNA-seq"
+version=[0,5,0]
+license="BSD-3-Clause"
+url="https://github.com/lingfeiwang/"+pkgname
+author="Lingfei Wang"
+author_email="Lingfei.Wang.github@outlook.com"
+
+
+
+def pkg_setup():
+	from setuptools import setup
+	from os import path
+	pkgnameu=pkgname[0].upper()+pkgname[1:].lower()
+	with open(path.join(path.abspath(path.dirname(__file__)),'README.rst'),encoding='utf-8') as f:
+		long_description=f.read()
+	setup(name=pkgname,
+		version='.'.join(map(str,version)),
+		author=author,
+		author_email=author_email,
+		description=pkgnamefull,
+		long_description=long_description,
+		long_description_content_type='text/x-rst',
+		url=url,
+		# download_url=url,
+		scripts=['bin/normalisr'],
+		# include_package_data=True,
+		install_requires=['numpy','scipy','pandas','scikit-learn','biothings_client','goatools>=0.7.11'],
+		classifiers=['Development Status :: 4 - Beta',
+			'License :: OSI Approved :: BSD License',
+			'Environment :: Console',
+			# 'Framework :: Pytest',
+			'Intended Audience :: Science/Research',
+			'Intended Audience :: Developers',
+			'Operating System :: OS Independent',
+			'Programming Language :: Python :: 3',
+			'Topic :: Scientific/Engineering :: Bio-Informatics'],
+		license=license,
+		packages=[pkgname],
+		package_dir={pkgname:path.join('src',pkgname)},
+	)
+
+pkg_setup()
---
+++ packages/normalisr/normalisr-0.5.0.tar.gz/normalisr-0.5.0/src/normalisr/coex.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+def coex(dt,dc,**ka):
+	"""Performs co-expression analyses for all gene pairs.
+
+	Performs parallel computation with multiple processes on the same machine.
+
+	Model for co-expression between genes i & j:
+		X_i=gamma*X_j+alpha*C+epsilon,
+
+		epsilon~i.i.d. N(0,sigma**2).
+
+	Test statistic: conditional R**2 (or proportion of variance explained) between X_i and X_j.
+
+	Null hypothesis: gamma=0.
+
+	Parameters
+	----------
+	dt:		numpy.ndarray(shape=(n_gene,n_cell),dtype=float)
+		Normalized expression matrix X.
+	dc:		numpy.ndarray(shape=(n_cov,n_cell),dtype=float)
+		Normalized covariate matrix C.
+	ka:		dict
+		Keyword arguments passed to normalisr.association.association_test_1. See below.
+
+	Returns
+	-------
+	P-values:	numpy.ndarray(shape=(n_gene,n_gene))
+		Co-expression P-value matrix.
+	dot:		numpy.ndarray(shape=(n_gene,n_gene))
+		Inner product of expression between gene pairs, after removing covariates.
+	var:		numpy.ndarray(shape=(n_gene))
+		Variance of gene expression after removing covariates. Pearson R=(((dot/numpy.sqrt(var)).T)/numpy.sqrt(var)).T.
+
+	Keyword arguments
+	-----------------
+	bs:		int
+		Batch size, i.e. number of genes in each computing batch. Use 0 for default: Data transfer limited to 1GB, capped at bs=500.
+	nth:	int
+		Number of parallel processes. Set to 0 for using automatically detected CPU counts.
+	dimreduce:	numpy.ndarray(shape=(n_gene,),dtype=int) or int
+		If dt doesn't have full rank, such as due to prior covariate removal (although the recommended method is to leave covariates in dc), this parameter allows to specify the loss of ranks/degrees of freedom to allow for accurate P-value computation. Default is 0, indicating no rank loss.
+		
+	"""
+	from .association import association_tests_1
+	ans=association_tests_1(dt,None,dc,lowmem=True,**ka)
+	
+	return (ans[0],ans[1],ans[3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+assert __name__ != "__main__"
