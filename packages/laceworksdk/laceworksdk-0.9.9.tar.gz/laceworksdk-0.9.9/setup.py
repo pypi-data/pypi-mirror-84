a91374a4c9ed7840a41de50c581bd804c62acafc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:25 +0000

    Add package laceworksdk/0.9.9/laceworksdk-0.9.9.tar.gz

---
+++ packages/laceworksdk/laceworksdk-0.9.9.tar.gz/laceworksdk-0.9.9/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+# read the contents of README.md file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='laceworksdk',
+    packages=find_packages(include=["laceworksdk", "laceworksdk.*"]),
+    version='0.9.9',
+    license='MIT',
+    description='Community-developed Python SDK for the Lacework APIs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Alan Nix',
+    author_email='alan.nix@lacework.net',
+    url='https://github.com/alannix-lw/python-sdk',
+    download_url='https://pypi.python.org/pypi/laceworksdk',
+    keywords=['lacework', 'api', 'sdk', 'python', 'api'],
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
