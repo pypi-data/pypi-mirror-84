7e7a636cdfb725e6144f837e3f1237e8ea218333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:50 +0000

    Add package radiko.py/3.0.5/radiko.py-3.0.5.tar.gz

---
+++ packages/radiko.py/radiko.py-3.0.5.tar.gz/radiko.py-3.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="radiko.py",
+    version="3.0.5",
+    author="akatonbo1011",
+    author_email="akatonbobook@gmail.com",
+    description="radiko rapper",
+    install_requires=["requests", "m3u8"],
+    tests_require=["requests", "m3u8"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="Radiko",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        'License :: OSI Approved :: BSD License',
+    ]
+)
