ce407eab71f0a6ba2423ccf788aa364af6a35c97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:13 +0000

    Add package FinAvn/0.6/FinAvn-0.6.tar.gz

---
+++ packages/FinAvn/FinAvn-0.6.tar.gz/FinAvn-0.6/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from distutils.core import setup
+setup(
+  name = 'FinAvn',
+  packages = ['FinAvn'],
+  version = '0.6',
+  license='MIT',
+  description = 'Performing Financial Time series forecasting using Machine Learning',
+  author = 'Avneesh Dubey',
+  author_email = 'avneesh.d01@gmail.com',
+  url = 'https://github.com/avneeshdubey99/FinAvn',
+  download_url = 'https://github.com/avneeshdubey99/FinAvn.git',
+  keywords = ['Machine Learning', 'Pandas', 'Numpy', 'Analysis', 'Regression', 'Classification', 'Dimensionality', 'Classifiers'],
+  install_requires=[
+          'numpy',
+          'sklearn',
+          'pandas'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
+    
