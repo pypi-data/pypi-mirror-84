391019594b69b3bf23aaeaa4500157e42ef8785d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:38 +0000

    Add package clickplc/0.3.1/clickplc-0.3.1.tar.gz

---
+++ packages/clickplc/clickplc-0.3.1.tar.gz/clickplc-0.3.1/setup.py
@@ -0,0 +1,46 @@
+"""Python driver for Koyo Ethernet ClickPLCs."""
+from platform import python_version
+from setuptools import setup
+
+if python_version() < '3.6':
+    raise ImportError("This module requires Python >=3.6")
+
+with open('README.md', 'r') as in_file:
+    long_description = in_file.read()
+
+setup(
+    name='clickplc',
+    version='0.3.1',
+    description="Python driver for Koyo Ethernet ClickPLCs.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://github.com/numat/clickplc/',
+    author='Patrick Fuller',
+    author_email='pat@numat-tech.com',
+    packages=['clickplc'],
+    entry_points={
+        'console_scripts': [('clickplc = clickplc:command_line')]
+    },
+    install_requires=[
+        'pymodbus==2.2.0rc1'
+    ],
+    extras_require={
+        'test': [
+            'pytest',
+            'pytest-cov',
+            'pytest-asyncio',
+        ],
+    },
+    license='GPLv2',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces'
+    ]
+)
