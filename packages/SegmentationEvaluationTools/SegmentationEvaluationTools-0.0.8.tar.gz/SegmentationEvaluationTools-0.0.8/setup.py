badcb4a5cfa59b11f5956b60c52fb61bb9d43913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:55 +0000

    Add package SegmentationEvaluationTools/0.0.8/SegmentationEvaluationTools-0.0.8.tar.gz

---
+++ packages/SegmentationEvaluationTools/SegmentationEvaluationTools-0.0.8.tar.gz/SegmentationEvaluationTools-0.0.8/setup.py
@@ -0,0 +1,29 @@
+__author__ = 'Brian M Anderson'
+# Created on 9/15/2020
+
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='SegmentationEvaluationTools',
+    author='Brian Mark Anderson',
+    author_email='bmanderson@mdanderson.org',
+    version='0.0.8',
+    description='Tools for evaluating predictions on Simple-ITK Images',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={'SegmentationEvaluationTools': 'src/SegmentationEvaluationTools'},
+    packages=['SegmentationEvaluationTools'],
+    include_package_data=True,
+    url='https://github.com/brianmanderson/Segmentation_Evaluation_Tools',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+    ],
+    install_requires=required,
+)
