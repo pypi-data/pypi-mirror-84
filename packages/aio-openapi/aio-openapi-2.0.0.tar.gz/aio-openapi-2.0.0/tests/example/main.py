3dfc4feefd52cb58737dbe2bc2dc2f61649e4155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:15 +0000

    Add package aio-openapi/2.0.0/aio-openapi-2.0.0.tar.gz

---
+++ packages/aio-openapi/aio-openapi-2.0.0.tar.gz/aio-openapi-2.0.0/tests/example/main.py
@@ -0,0 +1,58 @@
+import uuid
+
+from aiohttp import web
+
+from openapi import sentry
+from openapi.db.commands import db as db_command
+from openapi.middleware import json_error
+from openapi.rest import rest
+from openapi.spec import Redoc
+from openapi.ws import LocalBroker, Sockets
+
+from . import db
+from .endpoints import routes
+from .endpoints_additional import additional_routes
+from .endpoints_base import base_routes
+from .endpoints_form import form_routes
+from .ws import ws_routes
+
+
+def create_app():
+    return rest(
+        openapi=dict(title="Test API"),
+        security=dict(
+            auth_key={
+                "type": "apiKey",
+                "name": "X-Meta-Api-Key",
+                "description": (
+                    "The authentication key is required to access most "
+                    "endpoints of the API"
+                ),
+                "in": "header",
+            }
+        ),
+        setup_app=setup_app,
+        commands=[db_command],
+        redoc=Redoc(),
+    )
+
+
+def setup_app(app: web.Application) -> None:
+    db.setup(app)
+    app.middlewares.append(json_error())
+    app.middlewares.append(
+        sentry.middleware(app, f"https://{uuid.uuid4().hex}@sentry.io/1234567", "test")
+    )
+    app.router.add_routes(base_routes)
+    app.router.add_routes(routes)
+    #
+    # Additional routes for testing
+    app.router.add_routes(additional_routes)
+    app.router.add_routes(form_routes)
+    app["broker"] = LocalBroker()
+    app["web_sockets"] = Sockets(app)
+    app.router.add_routes(ws_routes)
+
+
+if __name__ == "__main__":
+    create_app().main()
---
+++ packages/aio-openapi/aio-openapi-2.0.0.tar.gz/aio-openapi-2.0.0/tests/example/ws.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from aiohttp import web
+
+from openapi import ws
+from openapi.spec.path import ApiPath
+from openapi.ws import pubsub
+
+ws_routes = web.RouteTableDef()
+
+
+@ws_routes.view("/stream")
+class StreamPath(ws.WsPathMixin, pubsub.Publish, pubsub.Subscribe, ApiPath):
+    """
+    ---
+    summary: Create and query Tasks
+    tags:
+        - Task
+    """
+
+    async def ws_rpc_echo(self, payload):
+        """Echo parameters"""
+        return payload
+
+    async def ws_rpc_cancel(self, payload):
+        """Echo parameters"""
+        raise asyncio.CancelledError
+
+    async def ws_rpc_badjson(self, payload):
+        """Echo parameters"""
+        return ApiPath
