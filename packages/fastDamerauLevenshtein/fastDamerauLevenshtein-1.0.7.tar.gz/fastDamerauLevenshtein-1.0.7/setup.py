604ea1d17cf9ebe3b150c53109c1e87b52f02495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:10 +0000

    Add package fastDamerauLevenshtein/1.0.7/fastDamerauLevenshtein-1.0.7.tar.gz

---
+++ packages/fastDamerauLevenshtein/fastDamerauLevenshtein-1.0.7.tar.gz/fastDamerauLevenshtein-1.0.7/setup.py
@@ -0,0 +1,56 @@
+'''
+-------
+License
+-------
+
+It is released under the MIT license.
+
+    Copyright (c) 2019 Robert Grigoroiu
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+
+try:
+    from setuptools import setup, Extension
+except Exception:
+    from distutils import setup, Extension
+
+README = open("./README.md").read()
+
+metadata = dict(
+    name='fastDamerauLevenshtein',
+    version='1.0.7',
+    description='Cython implementation of true Damerau-Levenshtein algorithm.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Robert Grigoroiu',
+    author_email='robertgr991@yahoo.ro',
+    url='https://github.com/robertgr991/fastDamerauLevenshtein',
+    license='MIT License',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Cython',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ]
+)
+
+setup(
+    ext_modules=[Extension('fastDamerauLevenshtein', ['fastDamerauLevenshtein/fastDamerauLevenshtein.c'])],
+    **metadata
+)
