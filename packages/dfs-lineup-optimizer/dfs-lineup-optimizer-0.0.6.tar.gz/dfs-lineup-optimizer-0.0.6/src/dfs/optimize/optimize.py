1e728497a74f0be3164920492c96e5de94242fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:47 +0000

    Add package dfs-lineup-optimizer/0.0.6/dfs-lineup-optimizer-0.0.6.tar.gz

---
+++ packages/dfs-lineup-optimizer/dfs-lineup-optimizer-0.0.6.tar.gz/dfs-lineup-optimizer-0.0.6/src/dfs/optimize/optimize.py
@@ -0,0 +1,14 @@
+from argparse import Namespace
+
+from src.dfs.command import Command
+
+
+class Optimize(Command):
+    def __init__(self):
+        super().__init__('optimize')
+
+    def add_parser(self, subparsers):
+        optimize_parser = subparsers.add_parser(self._name, help='Generate an optimal lineup')
+
+    def execute(self, args=Namespace):
+        print('optimize: ', args)
