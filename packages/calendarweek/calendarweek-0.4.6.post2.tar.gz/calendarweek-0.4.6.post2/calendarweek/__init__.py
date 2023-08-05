2ea4fe0709398795fef16563ea1a1aa0652f71fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:49 +0000

    Add package calendarweek/0.4.6.post2/calendarweek-0.4.6.post2.tar.gz

---
+++ packages/calendarweek/calendarweek-0.4.6.post2.tar.gz/calendarweek-0.4.6.post2/calendarweek/__init__.py
@@ -0,0 +1,1 @@
+from .calendarweek import CalendarWeek
---
+++ packages/calendarweek/calendarweek-0.4.6.post2.tar.gz/calendarweek-0.4.6.post2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['calendarweek']
+
+package_data = \
+{'': ['*']}
+
+extras_require = \
+{'django': ['Django>=2.2,<4.0']}
+
+setup_kwargs = {
+    'name': 'calendarweek',
+    'version': '0.4.6.post2',
+    'description': 'Utilities for working with calendar weeks in Python and Django',
+    'long_description': None,
+    'author': 'Dominik George',
+    'author_email': 'nik@naturalnet.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://edugit.org/AlekSIS/libs/python-calendarweek',
+    'packages': packages,
+    'package_data': package_data,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
