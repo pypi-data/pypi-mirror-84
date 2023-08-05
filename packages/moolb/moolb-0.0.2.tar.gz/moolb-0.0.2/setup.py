f7f2bde6059ead349e3c63031b6ef33459a5c0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:34 +0000

    Add package moolb/0.0.2/moolb-0.0.2.tar.gz

---
+++ packages/moolb/moolb-0.0.2.tar.gz/moolb-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+f = open('./README.md', 'r')
+long_description = f.read()
+f.close()
+
+setup(
+        name='moolb',
+        version='0.0.2',
+        description='Simple bloom filter with pluggable hash backend',
+        author='Louis Holbrook',
+        author_email='dev@holbrook.no',
+        license='GPL3',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        install_requires=[
+            'numpy>=1.19.0',
+            ],
+        packages=[
+            'moolb',
+        ],
+        url='https://gitlab.com/nolash/python-moolb',
+        )
