cf45981e0b91db510bd3a6c88eb51b3169cbd882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:44 +0000

    Add package datamart-materialize/0.8.1/datamart_materialize-0.8.1.tar.gz

---
+++ packages/datamart-materialize/datamart_materialize-0.8.1.tar.gz/datamart_materialize-0.8.1/setup.py
@@ -0,0 +1,57 @@
+import io
+import os
+from setuptools import setup
+
+
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+
+req = [
+    'requests',
+    'xlrd',
+    'pyreadstat>=1.0,<2.0',
+]
+with io.open('README.rst', encoding='utf-8') as fp:
+    description = fp.read()
+setup(name='datamart_materialize',
+      version='0.8.1',
+      packages=['datamart_materialize'],
+      entry_points={
+          'datamart_materialize': [
+              'datamart.noaa = datamart_materialize.noaa:noaa_materializer',
+          ],
+          'datamart_materialize.writer': [
+              'csv = datamart_materialize:CsvWriter',
+              'd3m = datamart_materialize.d3m:D3mWriter',
+              'pandas = datamart_materialize:PandasWriter',
+          ],
+          'datamart_materialize.converter': [
+              'xls = datamart_materialize.excel:ExcelConverter',
+              'pivot = datamart_materialize.pivot:PivotConverter',
+              'spss = datamart_materialize.spss:SpssConverter',
+              'stata = datamart_materialize.stata:StataConverter',
+              'tsv = datamart_materialize.tsv:TsvConverter',
+          ],
+      },
+      install_requires=req,
+      description="Materialization library for Datamart",
+      long_description=description,
+      author="Remi Rampin",
+      author_email='remi.rampin@nyu.edu',
+      maintainer="Remi Rampin",
+      maintainer_email='remi.rampin@nyu.edu',
+      url='https://gitlab.com/ViDA-NYU/datamart/datamart',
+      project_urls={
+          'Homepage': 'https://gitlab.com/ViDA-NYU/datamart/datamart',
+          'Source': 'https://gitlab.com/ViDA-NYU/datamart/datamart',
+          'Tracker': 'https://gitlab.com/ViDA-NYU/datamart/datamart/issues',
+      },
+      license='Apache-2.0',
+      keywords=['datamart'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Scientific/Engineering :: Information Analysis'])
