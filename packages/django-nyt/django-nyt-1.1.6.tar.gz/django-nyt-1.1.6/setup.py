38323b44daa8d0a3fa120d42ee9dbd6c6f9dd99a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:05 +0000

    Add package django-nyt/1.1.6/django-nyt-1.1.6.tar.gz

---
+++ packages/django-nyt/django-nyt-1.1.6.tar.gz/django-nyt-1.1.6/setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages
+from setuptools import setup
+
+from django_nyt import __version__
+
+
+packages = find_packages()
+
+setup(
+    name="django-nyt",
+    version=__version__,
+    author="Benjamin Bach",
+    author_email="benjamin@overtag.dk",
+    url="https://github.com/benjaoming/django-nyt",
+    description="A pluggable notification system written for the Django framework.",
+    license="Apache License 2.0",
+    keywords=["django", "notification" "alerts"],
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=["django>=1.11,<3.2"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    include_package_data=True,
+)
