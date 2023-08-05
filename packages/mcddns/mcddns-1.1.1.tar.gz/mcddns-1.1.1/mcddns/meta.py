a383cd9a18b9c00f819af4f1aa01f170d5bba602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:50 +0000

    Add package mcddns/1.1.1/mcddns-1.1.1.tar.gz

---
+++ packages/mcddns/mcddns-1.1.1.tar.gz/mcddns-1.1.1/mcddns/meta.py
@@ -0,0 +1,6 @@
+NAME = __package__
+DESCRIPTION = "Industrial-strength dynamic DNS client"
+VERSION = "1.1.1"
+AUTHOR = "Mike Conigliaro"
+CONTACT = "http://conigliaro.org"
+COPYRIGHT = f"""Copyright (c) 2020 {AUTHOR}"""
