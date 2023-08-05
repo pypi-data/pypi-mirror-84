63818c857698737c940f20d25ac268ef3c1a5798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:46 +0000

    Add package Vlue/0.1.0/Vlue-0.1.0.tar.gz

---
+++ packages/Vlue/Vlue-0.1.0.tar.gz/Vlue-0.1.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="Vlue",
+    version="0.1.0",
+    license = "MIT",
+    author="jhlee838",
+    author_email="jhlee@xzx.kr",
+    description="Programming language",
+    long_description=open('../readme.md').read(),
+    url="https://github.com/jhlee838/blue-lang",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
