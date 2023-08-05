2b5062e295af2c40537af31788cf3bc60bfbccda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:15 +0000

    Add package falocalrepo-server/1.6.2/falocalrepo-server-1.6.2.tar.gz

---
+++ packages/falocalrepo-server/falocalrepo-server-1.6.2.tar.gz/falocalrepo-server-1.6.2/falocalrepo_server/__main__.py
@@ -0,0 +1,15 @@
+from os.path import isfile
+from sys import argv
+
+from .server import server
+
+
+def main():
+    assert isfile(argv[1])
+
+    host, port = argv[2].split(":") if argv[2:] else ("0.0.0.0", 8080)
+    server(argv[1], host=host, port=int(port))
+
+
+if __name__ == '__main__':
+    main()
