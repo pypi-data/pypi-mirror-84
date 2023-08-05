5b24f91e5d195dd3ab3075c8db0c823f7eb04e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:44 +0000

    Add package datastore-viewer/0.3.1a1/datastore-viewer-0.3.1a1.tar.gz

---
+++ packages/datastore-viewer/datastore-viewer-0.3.1a1.tar.gz/datastore-viewer-0.3.1a1/datastore_viewer/presentation/template/datastore_viewer/precache-manifest.d13dc4d558e78a9aad33201fd07b7aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7779aae939b0cf853fd1db01e98bbf91",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bf62d320683adf9ee985",
+    "url": "./static/css/main.a295f0a2.chunk.css"
+  },
+  {
+    "revision": "0b15b962cc43541e23da",
+    "url": "./static/js/2.8520b7fc.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "./static/js/2.8520b7fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf62d320683adf9ee985",
+    "url": "./static/js/main.6f94282b.chunk.js"
+  },
+  {
+    "revision": "9670db271a1236c1b415",
+    "url": "./static/js/runtime-main.84107d9c.js"
+  }
+]);
---
+++ packages/datastore-viewer/datastore-viewer-0.3.1a1.tar.gz/datastore-viewer-0.3.1a1/setup.py
@@ -0,0 +1,46 @@
+import setuptools
+import os
+
+name = 'datastore-viewer'
+version = '0.3.1a1'
+description = 'Datastore Viewer for emulator'
+dependencies = [
+    'google-cloud-datastore >=1.7.0, >= 1.8.0',
+    'Flask >= 1.0.2',
+    'flasgger >= 0.9.1',
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+packages = [
+    package for package in setuptools.find_packages()
+    if package.startswith('datastore_viewer')
+]
+
+namespaces = ['datastore_viewer']
+
+scripts = [
+    os.path.join('bin', 'datastore-viewer')
+]
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author="Gumo Project Team",
+    author_email="gumo-py@googlegroups.com",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gumo-py/datastore-viewer",
+    packages=packages,
+    namespaces=namespaces,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=scripts,
+    install_requires=dependencies,
+    include_package_data=True,
+)
