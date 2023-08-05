d363da6383620af7516e359a149c820f1275d34c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:42 +0000

    Add package pandas-dedupe/1.4.0/pandas_dedupe-1.4.0.tar.gz

---
+++ packages/pandas-dedupe/pandas_dedupe-1.4.0.tar.gz/pandas_dedupe-1.4.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='pandas_dedupe',
+      version='1.4.0',
+      description='The Dedupe library made easy with Pandas.',
+      url='https://github.com/Lyonk71/pandas-dedupe',
+      author='Keith Lyons',
+      author_email='lyonk71@gmail.com',
+      license='MIT',
+      packages=['pandas_dedupe'],
+      install_requires=[
+          'dedupe>=2.0.0',
+          'unidecode',
+          'pandas',
+      ],
+      zip_safe=False,
+      
+      #Enable pypi description
+      long_description=long_description,
+      long_description_content_type="text/markdown")
