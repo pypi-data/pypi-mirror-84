e47d47bff06fa218dc652b1e6756c64c9331193c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:22 +0000

    Add package pss-marta-danilo/0.0.13/pss_marta_danilo-0.0.13.tar.gz

---
+++ packages/pss-marta-danilo/pss_marta_danilo-0.0.13.tar.gz/pss_marta_danilo-0.0.13/pss-marta-danilo/main.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import media
+import scambia
+import MySQLdb as sql
+
+
+# per far funzionare su gitlab usare questi valori
+# Host: mysql
+# User: runner
+# Password: Databasepss20/21 #noqa
+# Database: assignment1
+def get_im_from_db(indice):
+    db = sql.connect(host="mysql", user="runner", passwd="Databasepss20/21", db="assignment1") # nosec
+    #db = sql.connect(host="127.0.0.1", user="root", passwd=MYSQL_PASS, db=MYSQL_DB) # noqa
+    cursor = db.cursor()
+    percorso = "SELECT percorso FROM metadata WHERE id =" + str(indice)
+    cursor.execute(percorso)
+    # results = cursor.fetchall()
+    results = cursor.fetchone()
+    image = plt.imread(str(results[0]))
+    return image
+
+
+if __name__ == "__main__":
+    im = get_im_from_db(2)
+
+    array_medie = media.get_media(im)
+
+    pos = array_medie.index(max(array_medie))
+
+    new_im = scambia.scambia(im, pos)
+    print("sto per stampare le immagini")
+    plt.subplot(1, 2, 1)
+    plt.imshow(im)
+    plt.subplot(1, 2, 2)
+    plt.imshow(new_im)
+    plt.show()
