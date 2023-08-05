c88210de839869af932915fcead4bea07f8f0fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:28 +0000

    Add package datasette-upload-csvs/0.6/datasette-upload-csvs-0.6.tar.gz

---
+++ packages/datasette-upload-csvs/datasette-upload-csvs-0.6.tar.gz/datasette-upload-csvs-0.6/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+import os
+
+VERSION = "0.6"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="datasette-upload-csvs",
+    description="Datasette plugin for uploading CSV files and converting them to database tables",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Simon Willison",
+    url="https://github.com/simonw/datasette-upload-csvs",
+    license="Apache License, Version 2.0",
+    version=VERSION,
+    packages=["datasette_upload_csvs"],
+    entry_points={"datasette": ["upload_csvs = datasette_upload_csvs"]},
+    install_requires=[
+        "datasette>=0.51",
+        "asgi-csrf>=0.7",
+        "starlette",
+        "aiofiles",
+        "python-multipart",
+        "sqlite-utils",
+    ],
+    extras_require={
+        "test": ["pytest", "pytest-asyncio", "asgiref", "httpx", "asgi-lifespan"]
+    },
+    package_data={"datasette_upload_csvs": ["templates/*.html"]},
+)
