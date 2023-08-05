b5cb704966d1671cee7615a095300e2bf704ea2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:34 +0000

    Add package kora/0.8.22/kora-0.8.22.tar.gz

---
+++ packages/kora/kora-0.8.22.tar.gz/kora-0.8.22/kora/streamlit.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+os.system("pip install streamlit pyngrok")
+from pyngrok import ngrok
+
+
+def start(script=None):
+    """ run the script and connect with ngrok"""
+    if script is None:
+        script = guess_file()
+    url = ngrok.connect(8501)
+    url = url.replace('http:', 'https:')
+    print(url)
+    os.system(f"streamlit run {script} --server.runOnSave=true &")
+
+
+def stop():
+    ngrok.kill()
+    os.system("pkill streamlit")
+
+
+def guess_file():
+    for fn in ["main.py", "app.py", "hello.py"]:
+        if Path(fn).exists():
+            return fn
+    # the first .py in current directory
+    return next(Path().glob("*.py"))
