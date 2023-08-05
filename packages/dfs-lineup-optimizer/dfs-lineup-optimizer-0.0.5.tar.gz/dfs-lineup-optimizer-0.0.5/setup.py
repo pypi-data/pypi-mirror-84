5cce397ff67878e0cd42811cda9660628fddfb39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:40 +0000

    Add package dfs-lineup-optimizer/0.0.5/dfs-lineup-optimizer-0.0.5.tar.gz

---
+++ packages/dfs-lineup-optimizer/dfs-lineup-optimizer-0.0.5.tar.gz/dfs-lineup-optimizer-0.0.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    install_requires=[
+        "amply==0.1.4; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+        "certifi==2020.6.20",
+        "chardet==3.0.4",
+        "docutils==0.16; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+        "idna==2.10; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+        "pulp==2.3.1",
+        "pyparsing==2.4.7; python_version >= '2.6' and python_version not in '3.0, 3.1, 3.2'",
+        "requests==2.24.0",
+        "urllib3==1.25.11; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4' and python_version < '4'",
+    ],
+    name="dfs-lineup-optimizer",
+    version="0.0.5",
+    author="Aaron Mamparo",
+    author_email="aaronmamparo@gmail.com",
+    description="Generate optimal DFS lineups",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amamparo/dfs-lineup-optimizer",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    entry_points={"console_scripts": ["dfs = dfs.main:main"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">= 3.8",
+)
