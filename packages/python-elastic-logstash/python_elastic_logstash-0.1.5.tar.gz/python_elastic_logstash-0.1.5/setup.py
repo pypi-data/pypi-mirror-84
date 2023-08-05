ccec8b206fffa4d3c7c1cbf1c6b50bed0a843a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:55 +0000

    Add package python-elastic-logstash/0.1.5/python_elastic_logstash-0.1.5.tar.gz

---
+++ packages/python-elastic-logstash/python_elastic_logstash-0.1.5.tar.gz/python_elastic_logstash-0.1.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+setup(
+    name='python_elastic_logstash',
+    packages=['python_elastic_logstash'],
+    version='0.1.5',
+    description='Python logging handler for elastic search.',
+    long_description=open('README.rst').read(),
+    license='MIT',
+    author='Washim Ahmed',
+    author_email='washim.ahmed@gmail.com',
+    url='https://github.com/washim/python-elastic-logstash',
+    setup_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Logging',
+    ]
+)
