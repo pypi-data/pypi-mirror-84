cebdb4e75eaaeb138fb0e96575633da317a0f2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:13 +0000

    Add package alibabacloud-tea/0.1.5/alibabacloud-tea-0.1.5.tar.gz

---
+++ packages/alibabacloud-tea/alibabacloud-tea-0.1.5.tar.gz/alibabacloud-tea-0.1.5/Tea/exceptions.py
@@ -0,0 +1,28 @@
+class TeaException(Exception):
+    def __init__(self, dic):
+        self.code = dic.get("code")
+        self.message = dic.get("message")
+        self.data = dic.get("data")
+
+
+class RequiredArgumentException(TeaException):
+    def __init__(self, arg):
+        self.arg = arg
+
+    def __str__(self):
+        return '"%s" is required.' % self.arg
+
+
+class RetryError(TeaException):
+    def __init__(self, message):
+        self.message = message
+        self.data = None
+
+
+class UnretryableException(TeaException):
+    def __init__(self, request, ex):
+        self.last_request = request
+        self.inner_exception = ex
+        self.message = "Retry failed: %s" % ex.message
+
+
