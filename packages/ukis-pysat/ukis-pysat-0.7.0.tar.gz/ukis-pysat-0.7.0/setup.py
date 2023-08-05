548c60faead596c29fda19296b1081be008ece82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:03 +0000

    Add package ukis-pysat/0.7.0/ukis-pysat-0.7.0.tar.gz

---
+++ packages/ukis-pysat/ukis-pysat-0.7.0.tar.gz/ukis-pysat-0.7.0/setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+
+with open(r"README.md", encoding="utf8") as f:
+    long_description = f.read()
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+# please also update requirements.txt for testing. Make sure package versions are the same in each subset.
+extras_require = {
+    "data": [
+        "fiona~=1.8.11",
+        "landsatxplore==0.8",
+        "numpy~=1.19.2",
+        "pyproj~=2.6.1.post1",
+        "sentinelsat~=0.14",
+        "requests~=2.24.0",
+        "pillow~=7.2.0",
+        "pyfields~=1.6.0",
+        "pylandsat==0.4",
+        "python-dateutil~=2.8.1",
+        "shapely~=1.7.1",
+    ],
+    "raster": ["numpy~=1.19.2", "rasterio~=1.1.6", "rio_toa==0.3.0", "shapely~=1.7.1",],
+}
+extras_require["complete"] = list(set([v for req in extras_require.values() for v in req]))
+
+extras_require["dev"] = sorted(
+    extras_require["complete"]
+    + ["sphinx >= 1.3", "sphinx_rtd_theme", "pandas~=1.1.2", "dask[array]~=2.28.0", "utm==0.6.0",]
+)
+
+setup(
+    name="ukis-pysat",
+    version=get_version(os.path.join("ukis_pysat", "__init__.py")),
+    url="https://github.com/dlr-eoc/ukis-pysat",
+    author="German Aerospace Center (DLR)",
+    author_email="ukis-helpdesk@dlr.de",
+    license="Apache 2.0",
+    description="generic classes and functions to query, access and process multi-spectral and SAR satellite images",
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=[],
+    extras_require=extras_require,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Scientific/Engineering :: GIS",
+    ],
+    python_requires=">=3.6",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
