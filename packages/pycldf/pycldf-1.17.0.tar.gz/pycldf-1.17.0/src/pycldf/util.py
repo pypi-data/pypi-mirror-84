90e69ab51c7d29098da980fcb929e6b99b392bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:21 +0000

    Add package pycldf/1.17.0/pycldf-1.17.0.tar.gz

---
+++ packages/pycldf/pycldf-1.17.0.tar.gz/pycldf-1.17.0/src/pycldf/util.py
@@ -0,0 +1,35 @@
+import pathlib
+import itertools
+
+import pycldf
+
+
+def pkg_path(*comps):
+    return pathlib.Path(pycldf.__file__).resolve().parent.joinpath(*comps)
+
+
+def multislice(sliceable, *slices):
+    res = type(sliceable)()
+    for sl in slices:
+        if isinstance(sl, str):
+            if ':' in sl:
+                sl = [int(s) - (1 if i == 0 else 0) for i, s in enumerate(sl.split(':'))]
+            else:
+                sl = [int(sl) - 1, int(sl)]
+        res += sliceable[slice(*sl)]
+    return res
+
+
+def resolve_slices(row, ds, slice_spec, target_spec, fk, target_row=None):
+    # 1. Determine the slice column:
+    slices = ds[slice_spec]
+
+    # 2. Determine the to-be-sliced column:
+    morphemes = ds[target_spec]
+
+    # 3. Retrieve the matching row in the target table:
+    target_row = target_row or ds.get_row(target_spec[0], row[fk])
+
+    # 4. Slice the segments
+    return list(itertools.chain(*[
+        s.split() for s in multislice(target_row[morphemes.name], *row[slices.name])]))
