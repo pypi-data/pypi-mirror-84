72ff43e2b34a511e4e951416e1f3f75f9720b244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:36 +0000

    Add package backend.ai-manager/20.9.0a2/backend.ai_manager-20.9.0a2-py3-none-any.whl

---
+++ packages/backend.ai-manager/backend.ai_manager-20.9.0a2-py3-none-any.whl/ai/backend/manager/defs.py
@@ -0,0 +1,17 @@
+"""
+Common definitions/constants used throughout the manager.
+"""
+
+from typing import Final
+
+from ai.backend.common.types import SlotName, SlotTypes
+
+
+INTRINSIC_SLOTS: Final = {
+    SlotName('cpu'): SlotTypes('count'),
+    SlotName('mem'): SlotTypes('bytes'),
+}
+
+
+# The default container role name for multi-container sessions
+DEFAULT_ROLE: Final = "main"
