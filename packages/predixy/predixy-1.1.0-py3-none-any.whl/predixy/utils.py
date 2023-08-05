f19cb18863c096b29fa62b7d2a842b66f271938e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:56 +0000

    Add package predixy/1.1.0/predixy-1.1.0-py3-none-any.whl

---
+++ packages/predixy/predixy-1.1.0-py3-none-any.whl/predixy/utils.py
@@ -0,0 +1,20 @@
+__BANNER__ = '''=========================================================================================================
+
+PREDI-XY Version {version}
+
+=========================================================================================================
+
+A program for automated generation of NICS-XY-Scans for 
+cata-condensed polycyclic aromatic systems using additivty
+
+Developed by:       Alexandra Wahab, Felix Fleckenstein, Stefan Feusi, and Renana Gershoni-Poranne
+
+If you use this program, please cite:
+                    R. Gershoni-Poranne and A. Stanger, Chem. Eur. J. 2014, 20, 6573-5688
+                    R. Gershoni-Poranne, Chem. Eur. J. 2018, 24, 4165-4172
+                    P. Finkelstein and R. Gershoni-Poranne, ChemPhysChem 2019, 20, 1508-1520
+
+=========================================================================================================
+'''
+
+__SEPARATOR__ = "========================================================================================================="
