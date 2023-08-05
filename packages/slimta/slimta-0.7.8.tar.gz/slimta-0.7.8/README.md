06266ea54adcbfd4e7c7b764e7ea1a27fb421fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:36 +0000

    Add package slimta/0.7.8/slimta-0.7.8.tar.gz

---
+++ packages/slimta/slimta-0.7.8.tar.gz/slimta-0.7.8/README.md
@@ -0,0 +1,73 @@
+#### [Project Homepage][1]
+#### [Usage Manual][2]
+
+--------------------
+
+About
+=====
+
+`slimta` is a configurable MTA based on the `python-slimta` libraries. While
+the purpose of the `python-slimta` library is to avoid configuration files and
+allow full control via Python code, the `slimta` project recognizes that not
+everyone will want or need that level of control. Setup, configuration, and
+execution of `slimta` is designed to be familiar to non-programmers.
+
+The `slimta` project is released under the [MIT License][3].
+
+Getting Started
+===============
+
+Install `slimta` from [PyPi][4]:
+
+```console
+$ pip install slimta
+```
+
+Pip should pull in all the required dependencies. Next, we create the basic
+configuration files:
+
+```console
+$ slimta-setup config
+```
+
+This creates 3 files, in `~/.slimta/` or wherever you specified. The sample
+configs are designed to work out of the box, so lets give it a shot:
+
+```console
+$ slimta --no-daemon
+```
+
+In another terminal, let's connect to port 1025 to see if it's working. After
+the banner (the line beginning with `220 `), type in `QUIT` to end the session:
+
+```console
+$ telnet localhost 1025
+Trying 127.0.0.1...
+Connected to localhost.
+Escape character is '^]'.
+220 localhost.localdomain ESMTP example.com Mail Delivery Agent
+QUIT
+221 2.0.0 Bye
+Connection closed by foreign host.
+```
+
+Port 1025 is fully capable of accepting mail in the SMTP session, but is
+configured by default with the `blackhole` relay to silently discard messages.
+You can also try it out with the built-in Python SMTP libraries:
+
+```pycon
+>>> import smtplib
+>>> smtplib.SMTP('localhost', 1025).sendmail('test@example.com',
+                                             ['postmaster@example.com'],
+                                             'test message')
+```
+
+At this point, we're still a little ways off from where you'd probably like to
+be: actually sending and receiving email to the Internet. Please check out the
+[Usage Manual][2] for information on configuring `slimta` to your liking,
+including more advanced and custom setups.
+
+[1]: http://slimta.org/
+[2]: http://slimta.org/latest/manual/slimta.html
+[3]: http://opensource.org/licenses/MIT
+[4]: https://pypi.python.org/pypi/slimta/
---
+++ packages/slimta/slimta-0.7.8.tar.gz/slimta-0.7.8/slimta/app/main.py
@@ -0,0 +1,67 @@
+# Copyright (c) 2013 Ian C. Good
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+import os
+import os.path
+import sys
+from argparse import ArgumentParser
+
+os.environ['GEVENT_RESOLVER'] = 'ares'
+from gevent import monkey; monkey.patch_all()
+
+from . import __version__
+from .state import SlimtaState
+
+
+def parse_args():
+    argparser = ArgumentParser(description='Configurable MTA based on the python-slimta libraries.')
+    argparser.add_argument('--version', action='version', version='%(prog)s '+__version__)
+    argparser.add_argument('-a', '--no-daemon', dest='attached', action='store_true',
+                           help='Force the process to remain attached to the terminal.')
+    argparser.add_argument('-d', '--daemon', dest='attached', action='store_false',
+                           help='Force the process to daemonize.')
+    argparser.add_argument('-p', '--pid-file', metavar='FILE', default=None,
+                           help='Store process ID in FILE during execution.')
+
+    default_process_name = os.path.basename(sys.argv[0])
+    group = argparser.add_argument_group('config options')
+    group.add_argument('-c', '--config', metavar='FILE', default=None,
+                       help='Specifies a configuration file to read. If not given, the default locations ($HOME/.slimta/slimta.yaml, /etc/slimta/slimta.yaml) are checked.')
+    group.add_argument('-n', '--process-name', metavar='NAME', default=default_process_name,
+                       help='Use the process sub-section NAME for configuration. (default: %(default)s)')
+    group.add_argument('--no-edge', action='store_true',
+                       help='Ignore all configured edges.')
+    group.add_argument('--no-relay', action='store_true',
+                       help='Ignore all configured relays.')
+
+    argparser.set_defaults(attached=None)
+    return argparser, argparser.parse_args()
+
+
+def main():
+    argparser, args = parse_args()
+    state = SlimtaState(args)
+    state.load_config(argparser=argparser)
+
+    state.loop()
+
+
+# vim:et:fdm=marker:sts=4:sw=4:ts=4
