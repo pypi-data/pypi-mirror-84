aa59c33e7d336bd2903d7a13fb86c99159a0d221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:15:08 +0000

    Add package tensorflow-rocm-enhanced/1.15.3/tensorflow_rocm_enhanced-1.15.3-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/tensorflow-rocm-enhanced/tensorflow_rocm_enhanced-1.15.3-cp36-cp36m-manylinux1_x86_64.whl/tensorflow_core/include/external/local_config_rocm/rocm/rocm/include/hipblas-version.h
@@ -0,0 +1,17 @@
+/* ************************************************************************
+ * Copyright 2016-2020 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+#ifndef HIPBLAS_VERSION_H_
+#define HIPBLAS_VERSION_H_
+
+/* the configured version and settings
+ */
+// clang-format off
+#define hipblasVersionMajor 0
+#define hipblaseVersionMinor 36
+#define hipblasVersionPatch 0
+#define hipblasVersionTweak 0-rocm-rel-3.9-17-e4d9e7b
+// clang-format on
+
+#endif
