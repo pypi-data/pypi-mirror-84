899e54f07775ffeb585bd7418766c85c7fb05b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:02 +0000

    Add package python-gerrit-api/1.0.1/python-gerrit-api-1.0.1.tar.gz

---
+++ packages/python-gerrit-api/python-gerrit-api-1.0.1.tar.gz/python-gerrit-api-1.0.1/gerrit/accounts/emails.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author: Jialiang Shi
+from gerrit.utils.models import BaseModel
+
+
+class Email(BaseModel):
+    def __init__(self, **kwargs):
+        super(Email, self).__init__(**kwargs)
+        self.attributes = ["email", "preferred", "pending_confirmation", "username", "gerrit"]
+
+    def delete(self):
+        """
+        Deletes an email address of an account.
+
+        :return:
+        """
+        endpoint = "/accounts/%s/emails/%s" % (self.username, self.email)
+        self.gerrit.requester.delete(self.gerrit.get_endpoint_url(endpoint))
+
+    def set_preferred(self):
+        """
+        Sets an email address as preferred email address for an account.
+
+        :return:
+        """
+        endpoint = "/accounts/%s/emails/%s/preferred" % (self.username, self.email)
+        self.gerrit.requester.put(self.gerrit.get_endpoint_url(endpoint))
+
+
+class Emails:
+    def __init__(self, username, gerrit):
+        self.username = username
+        self.gerrit = gerrit
+
+    def list(self) -> list:
+        """
+        Returns the email addresses that are configured for the specified user.
+
+        :return:
+        """
+        endpoint = "/accounts/%s/emails" % self.username
+        response = self.gerrit.requester.get(self.gerrit.get_endpoint_url(endpoint))
+        result = self.gerrit.decode_response(response)
+        return Email.parse_list(result, username=self.username, gerrit=self.gerrit)
+
+    def get(self, email: str) -> Email:
+        """
+        Retrieves an email address of a user.
+
+        :return:
+        """
+        endpoint = "/accounts/%s/emails/%s" % (self.username, email)
+        response = self.gerrit.requester.get(self.gerrit.get_endpoint_url(endpoint))
+        result = self.gerrit.decode_response(response)
+        return Email.parse(result, username=self.username, gerrit=self.gerrit)
+
+    def set_preferred(self, email: str):
+        """
+        Sets an email address as preferred email address for an account.
+
+        :param email: account email
+        :return:
+        """
+        endpoint = "/accounts/%s/emails/%s/preferred" % (self.username, email)
+        self.gerrit.requester.put(self.gerrit.get_endpoint_url(endpoint))
+
+    def delete(self, email: str):
+        """
+        Deletes an email address of an account.
+
+        :param email: account email
+        :return:
+        """
+        endpoint = "/accounts/%s/emails/%s" % (self.username, email)
+        self.gerrit.requester.delete(self.gerrit.get_endpoint_url(endpoint))
---
+++ packages/python-gerrit-api/python-gerrit-api-1.0.1.tar.gz/python-gerrit-api-1.0.1/gerrit/projects/commit.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author: Jialiang Shi
+from urllib.parse import quote
+from gerrit.utils.common import check
+from gerrit.utils.models import BaseModel
+
+
+class Commit(BaseModel):
+    def __init__(self, **kwargs):
+        super(Commit, self).__init__(**kwargs)
+        self.attributes = [
+            "commit",
+            "author",
+            "committer",
+            "message",
+            "parents",
+            "subject",
+            "web_links",
+            "project",
+            "gerrit",
+        ]
+
+    def get_include_in(self) -> dict:
+        """
+        Retrieves the branches and tags in which a change is included.
+
+        :return:
+        """
+        endpoint = "/projects/%s/commits/%s/in" % (self.project, self.commit)
+        response = self.gerrit.requester.get(self.gerrit.get_endpoint_url(endpoint))
+        result = self.gerrit.decode_response(response)
+        return result
+
+    def get_file_content(self, file: str) -> str:
+        """
+        Gets the content of a file from a certain commit.
+
+        :param file: the file path
+        :return:
+        """
+        endpoint = "/projects/%s/commits/%s/files/%s/content" % (
+            self.project,
+            self.commit,
+            quote(file, safe=""),
+        )
+        response = self.gerrit.requester.get(self.gerrit.get_endpoint_url(endpoint))
+        result = self.gerrit.decode_response(response)
+        return result
+
+    @check
+    def cherry_pick(self, input_: dict) -> dict:
+        """
+        Cherry-picks a commit of a project to a destination branch.
+
+        .. code-block:: python
+
+            input_ = {
+                "message": "Implementing Feature X",
+                "destination": "release-branch"
+            }
+            result = commit.cherry_pick(input_)
+
+        :param input_: the CherryPickInput entity,
+          https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html#cherrypick-input
+        :return:  the resulting cherry-picked change
+        """
+        endpoint = "/projects/%s/commits/%s/cherrypick" % (self.project, self.commit)
+        base_url = self.gerrit.get_endpoint_url(endpoint)
+        response = self.gerrit.requester.post(
+            base_url, json=input_, headers=self.gerrit.default_headers
+        )
+        result = self.gerrit.decode_response(response)
+        return self.gerrit.changes.get(result.get("id"))
+
+    def list_change_files(self) -> dict:
+        """
+        Lists the files that were modified, added or deleted in a commit.
+
+        :return:
+        """
+        endpoint = "/projects/%s/commits/%s/files/" % (self.project, self.commit)
+        response = self.gerrit.requester.get(self.gerrit.get_endpoint_url(endpoint))
+        result = self.gerrit.decode_response(response)
+        return result
