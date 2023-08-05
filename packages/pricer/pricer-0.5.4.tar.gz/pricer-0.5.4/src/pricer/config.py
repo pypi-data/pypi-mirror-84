4d9f8641bb6f7b76727a40bd25643c3569384214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:23 +0000

    Add package pricer/0.5.4/pricer-0.5.4.tar.gz

---
+++ packages/pricer/pricer-0.5.4.tar.gz/pricer-0.5.4/src/pricer/config.py
@@ -0,0 +1,72 @@
+"""It collates and loads user specified configuration for data pipeline."""
+import logging
+from typing import Any, Dict
+
+import tqdm
+
+from pricer import io
+
+logger = logging.getLogger(__name__)
+
+
+class TqdmStream(object):
+    """Allows for writing tqdm alongside logging."""
+
+    @classmethod
+    def write(cls: Any, msg: Any) -> Any:
+        """Writer method."""
+        tqdm.tqdm.write(msg, end="")
+
+
+def set_loggers(
+    base_logger: Any = None, v: bool = False, vv: bool = False, test: bool = False
+) -> None:
+    """Sets up logging across modules in project.
+
+    Uses arguments originally specified from command line to set
+    streaming logging level. Log files are written in debug mode.
+
+    Args:
+        base_logger: base logger object instantiated at program run time.
+            Used to find other log objects across modules.
+        v: Run in verbose mode (INFO)
+        vv: Run in very verbose mode (DEBUG)
+        test: Override / runs in verbose mode (INFO), in case not
+            user specified.
+    """
+    if vv:
+        log_level = 10
+    elif v or test:
+        log_level = 20
+    else:
+        log_level = 30
+
+    loggers = [base_logger] + [
+        logging.getLogger(name)  # type: ignore
+        for name in logging.root.manager.loggerDict  # type: ignore
+        if "pricer." in name
+    ]
+    for logger in loggers:
+        logger.setLevel(log_level)  # type: ignore
+        formatter = logging.Formatter("%(asctime)s:%(name)s:%(message)s")
+
+        file_handler = logging.FileHandler(f"logs/{logger.name}.log")  # type: ignore
+        file_handler.setLevel(10)
+        file_handler.setFormatter(formatter)
+
+        stream_handler = logging.StreamHandler(stream=TqdmStream)  # type: ignore
+        stream_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)  # type: ignore
+        logger.addHandler(stream_handler)  # type: ignore
+
+
+us = io.reader("config", "user_settings", "yaml")
+ui = io.reader("config", "user_items", "yaml")
+gs = io.reader("config", "general_settings", "yaml")
+try:
+    secrets = io.reader(name="SECRETS", ftype="yaml")
+except FileNotFoundError:
+    secrets = {"username": None, "password": None}
+
+env: Dict[str, str] = {"basepath": "data"}
