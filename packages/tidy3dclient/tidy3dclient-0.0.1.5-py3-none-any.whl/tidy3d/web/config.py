7e5c5bdd6269a8bd54352256b2a02bc5ab78e3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:06 +0000

    Add package tidy3dclient/0.0.1.5/tidy3dclient-0.0.1.5-py3-none-any.whl

---
+++ packages/tidy3dclient/tidy3dclient-0.0.1.5-py3-none-any.whl/tidy3d/web/config.py
@@ -0,0 +1,26 @@
+
+class Config:
+    S3_REGION="us-east-1"
+    STUDIO_BUCKET="flow360-studio-v1"
+    WEB_API_ENDPONT="https://webapi-dev.flexcompute.com"
+
+    # auth info
+    auth = None
+    user = None
+
+    #other
+    auth_retry = 0
+#
+# class Config:
+#     S3_REGION="us-gov-west-1"
+#     STUDIO_BUCKET="flow360studio"
+#     WEB_API_ENDPONT="https://webapi.flexcompute.com"
+#
+#     # auth info
+#     auth = None
+#     user = None
+#
+#     #other
+#     auth_retry = 0
+#     VERSION_FDTD = 'beta-20.4.1.0'
+#
