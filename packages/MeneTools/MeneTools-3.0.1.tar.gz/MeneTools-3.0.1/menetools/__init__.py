679c0860faabe8a518ca516be6573ef03ccefdcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:53 +0000

    Add package MeneTools/3.0.1/MeneTools-3.0.1.tar.gz

---
+++ packages/MeneTools/MeneTools-3.0.1.tar.gz/MeneTools-3.0.1/menetools/__init__.py
@@ -0,0 +1,10 @@
+from menetools.menescope import run_menescope
+from menetools.meneacti import run_meneacti
+from menetools.menecheck import run_menecheck
+from menetools.menecof import run_menecof
+from menetools.menepath import run_menepath
+
+
+__version__="3.0.1"
+"""Define the version of the package.
+"""
