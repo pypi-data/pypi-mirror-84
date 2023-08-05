76b680ff741397e8e68e04b7dcf4efb75005b85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:28 +0000

    Add package gym-mapf/0.2.0/gym-mapf-0.2.0.tar.gz

---
+++ packages/gym-mapf/gym-mapf-0.2.0.tar.gz/gym-mapf-0.2.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="gym-mapf",
+    version="0.2.0",
+    author="LevyvoNet",
+    author_email="eladlevy2@gmail.com",
+    description="Multi-Agent Path Finding gym environment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LevyvoNet/gym-mapf",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    test_suite="gym_mapf.tests",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+    ],
+)
