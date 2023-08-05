f861bfbca11d187aaf974d1fdb288a60343b4083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:21 +0000

    Add package blackboxpackage/0.0.5/blackboxpackage-0.0.5.tar.gz

---
+++ packages/blackboxpackage/blackboxpackage-0.0.5.tar.gz/blackboxpackage-0.0.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(
+    name='blackboxpackage',
+    version='0.0.5',
+    url='https://bitbucket.org/ainstec/black-box-package',
+    license='MIT License',
+    author='Ravi Jose Fiori',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='ravi.fiori@concil.com.br',
+    keywords='Pacote',
+    description=u'Exemplo de pacote PyPI',
+    packages=['blackboxpackage'],
+    py_modules=['aws'],
+    install_requires=['boto3==1.11.0', 'pandas==0.25.1', ' numpy==1.16.5', 'pyarrow==0.15.1', 'SQLAlchemy==1.3.15', 'psycopg2==2.8.4', 'psycopg2-binary==2.8.5', 'workalendar==10.4.0', 'cx-Oracle==8.0.1'],
+)
+
+
+# cria a pasta dist com o pacote
+#python setup.py sdist bdist_wheel
+
+# subir no ambiente de teste 
+#twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+# subir no pipy
+#twine upload dist/*
+
+
+#install
+#pip3 install blackboxpackage==0.0.5
