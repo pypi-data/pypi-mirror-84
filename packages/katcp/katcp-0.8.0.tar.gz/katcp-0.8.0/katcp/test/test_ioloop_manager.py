f70b3984854b7e64db7276a4c1c9957ea6e52f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:58 +0000

    Add package katcp/0.8.0/katcp-0.8.0.tar.gz

---
+++ packages/katcp/katcp-0.8.0.tar.gz/katcp-0.8.0/katcp/test/test_ioloop_manager.py
@@ -0,0 +1,66 @@
+# Copyright 2017 National Research Foundation (South African Radio Astronomy Observatory)
+# BSD license - see LICENSE for details
+from __future__ import absolute_import, division, print_function
+from future import standard_library
+standard_library.install_aliases()  # noqa: E402
+
+import unittest
+
+from builtins import object
+from concurrent.futures import Future
+
+from _thread import get_ident as get_thread_ident
+
+# Module under test
+from katcp import ioloop_manager
+from katcp.testutils import start_thread_with_cleanup
+
+
+class test_ThreadsafeMethodAttrWrapper(unittest.TestCase):
+    def setUp(self):
+        self.ioloop_manager = ioloop_manager.IOLoopManager(managed_default=True)
+        self.ioloop = self.ioloop_manager.get_ioloop()
+        self.ioloop_thread_wrapper = ioloop_manager.IOLoopThreadWrapper(self.ioloop)
+        start_thread_with_cleanup(self, self.ioloop_manager, start_timeout=1)
+
+    def test_wrapping(self):
+        test_inst = self
+        class Wrappee(object):
+            def __init__(self, ioloop_thread_id):
+                self.thread_id = ioloop_thread_id
+
+            def a_callable(self, arg, kwarg='abc'):
+                test_inst.assertEqual(get_thread_ident(), self.thread_id)
+                return (arg * 2, kwarg * 3)
+
+            @property
+            def not_in_ioloop(self):
+                test_inst.assertNotEqual(get_thread_ident(), self.thread_id)
+                return 'not_in'
+
+            @property
+            def only_in_ioloop(self):
+                test_inst.assertEqual(get_thread_ident(), self.thread_id)
+                return 'only_in'
+
+        class TestWrapper(ioloop_manager.ThreadSafeMethodAttrWrapper):
+            @property
+            def only_in_ioloop(self):
+                return self._getattr('only_in_ioloop')
+
+
+        id_future = Future()
+        self.ioloop.add_callback(lambda : id_future.set_result(get_thread_ident()))
+        wrappee = Wrappee(id_future.result(timeout=1))
+        wrapped = TestWrapper(wrappee, self.ioloop_thread_wrapper)
+        # First test our assumptions about Wrappee
+        with self.assertRaises(AssertionError):
+            wrappee.a_callable(3, 'a')
+        with self.assertRaises(AssertionError):
+            wrappee.only_in_ioloop
+        self.assertEqual(wrappee.not_in_ioloop, 'not_in')
+
+        # Now test the wrapped version
+        self.assertEqual(wrapped.a_callable(5, kwarg='bcd'), (10, 'bcd'*3))
+        self.assertEqual(wrapped.only_in_ioloop, 'only_in')
+        self.assertEqual(wrapped.not_in_ioloop, 'not_in')
