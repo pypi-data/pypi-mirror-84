8f343f57269f3529310aecafe546268cfa887d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:38 +0000

    Add package minet/0.32.2/minet-0.32.2-py3-none-any.whl

---
+++ packages/minet/minet-0.32.2-py3-none-any.whl/minet/defaults.py
@@ -0,0 +1,14 @@
+# =============================================================================
+# Minet Default Values Collection
+# =============================================================================
+#
+# Listing sane default values used throughout the whole library.
+#
+
+# Fetch-related
+DEFAULT_GROUP_PARALLELISM = 1
+DEFAULT_GROUP_BUFFER_SIZE = 25
+DEFAULT_THROTTLE = 0.2
+DEFAULT_CONNECT_TIMEOUT = 5
+DEFAULT_READ_TIMEOUT = 25
+DEFAULT_SPOOFED_UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:69.0) Gecko/20100101 Firefox/69.0'
