631c2621d7611a855fa4fdb9f2f2b337f049d221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:35 +0000

    Add package pyarmor/6.5.1/pyarmor-6.5.1.zip

---
+++ packages/pyarmor/pyarmor-6.5.1.zip/pyarmor-6.5.1/src/config.py
@@ -0,0 +1,45 @@
+from sys import platform
+
+version = '6.5.1'
+
+# The corresponding version of pytransform.so
+core_version = 'r36.10'
+
+version_info = '''
+PyArmor is a command line tool used to obfuscate python scripts, bind
+obfuscated scripts to fixed machine or expire obfuscated scripts.
+
+For more information, refer to https://pyarmor.readthedocs.io
+'''
+
+purchase_info = '''
+
+If there is no registration code yet, please purchase one by visiting
+
+https://order.shareit.com/cart/add?vendorid=200089125&PRODUCT[300871197]=1
+
+'''
+
+dll_name = '_pytransform'
+dll_ext = '.dylib' if platform == 'darwin' \
+    else '.dll' if platform in ('win32', 'cygwin') else '.so'
+
+
+entry_lines = 'from %spytransform%s import pyarmor_runtime\n', \
+              'pyarmor_runtime(%s)\n'
+protect_code_template = 'protect_code%s.pt'
+
+config_filename = '.pyarmor_config'
+capsule_filename = '.pyarmor_capsule.zip'
+license_filename = 'license.lic'
+default_output_path = 'dist'
+default_manifest_template = 'global-include *.py'
+
+platform_urls = [
+    'https://github.com/dashingsoft/pyarmor-core/raw/{version}/platforms',
+    'https://pyarmor.dashingsoft.com/downloads/{version}'
+]
+platform_config = 'index.json'
+
+key_url = 'https://api.dashingsoft.com/product/key/%s/query'
+reg_url = 'https://api.dashingsoft.com/product/key/activate/%s/'
