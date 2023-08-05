7efbbb41948bbf110ed6badb27825fbb7b782ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:08 +0000

    Add package experiment-collection/0.1.7/experiment_collection-0.1.7-py3-none-any.whl

---
+++ packages/experiment-collection/experiment_collection-0.1.7-py3-none-any.whl/experiment_collection/experiments/collection_abc.py
@@ -0,0 +1,27 @@
+import abc
+import typing
+
+from .experiment import Experiment
+
+
+class ExperimentCollectionABC(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def close(self):
+        pass
+
+    @abc.abstractmethod
+    def add_experiment(self, exp: Experiment, *, ignore_included=False):
+        pass
+
+    @abc.abstractmethod
+    def delete_experiment(self, exp: typing.Union[Experiment, str]):
+        pass
+
+    @abc.abstractmethod
+    def check_experiment(self, exp: typing.Union[Experiment, str]) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def get_experiments(self, normalize=True):
+        pass
