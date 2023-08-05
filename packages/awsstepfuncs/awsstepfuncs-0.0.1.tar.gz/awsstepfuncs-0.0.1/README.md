2a702eec3b9c6bc4deae6e3a148b5b4a47c67082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:15 +0000

    Add package awsstepfuncs/0.0.1/awsstepfuncs-0.0.1.tar.gz

---
+++ packages/awsstepfuncs/awsstepfuncs-0.0.1.tar.gz/awsstepfuncs-0.0.1/README.md
@@ -0,0 +1,155 @@
+# AWS Step Functions SDK
+
+[![python](https://img.shields.io/static/v1?label=python&message=3.8%2B&color=informational&logo=python&logoColor=white)](https://github.com/suzil/awsstepfuncs/releases/latest)
+[![black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/python/black)
+[![Actions Status](https://github.com/suzil/awsstepfuncs/workflows/GH/badge.svg)](https://github.com/suzil/awsstepfuncs/actions)
+[![pre-commit](https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit&logoColor=white)](https://github.com/pre-commit/pre-commit)
+[![Checked with mypy](http://www.mypy-lang.org/static/mypy_badge.svg)](http://mypy-lang.org/)
+[![codecov](https://codecov.io/gh/suzil/awsstepfuncs/branch/master/graph/badge.svg?token=PF990VH0YU)](https://codecov.io/gh/suzil/awsstepfuncs)
+[![Documentation Status](https://readthedocs.org/projects/awsstepfuncs/badge/?version=latest)](https://awsstepfuncs.readthedocs.io/en/latest/?badge=latest)
+
+WARNING: This project is still a work-in-progress. Buyer beware.
+
+The Python SDK `awsstepfuncs` can compile to [Amazon States Machine](https://states-language.net/) to use in a real AWS Step Functions application and can run simulations with mocked functions to debug state machines.
+
+```py
+resource = "arn:aws:lambda:ap-southeast-2:710187714096:function:DummyResource"
+task_state = TaskState("My task", resource=resource)
+succeed_state = SucceedState("Success")
+pass_state = PassState("Just passing")
+fail_state = FailState("Failure", error="IFailed", cause="I failed!")
+
+task_state >> succeed_state
+pass_state >> fail_state
+task_state.add_catcher(["States.ALL"], next_state=pass_state)
+
+state_machine = StateMachine(start_state=task_state)
+
+def failure_mock_fn(_):
+    assert False
+
+state_machine.simulate(
+    resource_to_mock_fn={resource: failure_mock_fn}, show_visualization=True
+)
+```
+
+<p align="center">
+  <img src="assets/state_machine.gif">
+</p>
+
+## Installation
+
+~~This package is available on PyPI:~~
+
+```sh
+$ pip install awsstepfuncs
+```
+
+To create visualizations, you need to have [GraphViz](https://graphviz.org/) installed on your system.
+
+## API coverage
+
+### States compilation and simulation
+
+| State        | Compile Coverage                                                                                            | Simulation Coverage                                                                                         |
+| ------------ | ----------------------------------------------------------------------------------------------------------- | ----------------------------------------------------------------------------------------------------------- |
+| **Fail**     | :heavy_check_mark:                                                                                          | :heavy_multiplication_x: Missing error handling                                                             |
+| **Succeed**  | :heavy_check_mark:                                                                                          | :heavy_multiplication_x: Should return its input instead of nothing                                         |
+| **Choice**   | :heavy_multiplication_x:                                                                                    | :heavy_multiplication_x:                                                                                    |
+| **Wait**     | :heavy_check_mark:                                                                                          | :heavy_check_mark:                                                                                          |
+| **Pass**     | :heavy_check_mark:                                                                                          | :heavy_check_mark:                                                                                          |
+| **Map**      | :heavy_check_mark:                                                                                          | :heavy_check_mark:                                                                                          |
+| **Parallel** | :heavy_multiplication_x:                                                                                    | :heavy_multiplication_x:                                                                                    |
+| **Task**     | :heavy_multiplication_x: Missing TimeoutSeconds, TimeoutSecondsPath, HeartbeatSeconds, HeartbeatSecondsPath | :heavy_multiplication_x: Missing TimeoutSeconds, TimeoutSecondsPath, HeartbeatSeconds, HeartbeatSecondsPath |
+
+### Input and output processing
+
+| Field              | Support                  |
+| ------------------ | ------------------------ |
+| **InputPath**      | :heavy_check_mark:       |
+| **OutputPath**     | :heavy_check_mark:       |
+| **Parameters**     | :heavy_multiplication_x: |
+| **ResultSelector** | :heavy_check_mark:       |
+| **ResultPath**     | :heavy_check_mark:       |
+
+
+### Errors
+
+TODO
+
+
+### Extra fields
+
+Currently lacking support for Context Objects, Payload Templates, and Parameters. When reporting coverage for states above, these fields are ignored.
+
+
+## Usage
+
+TODO: Add a better tutorial
+
+```py
+from awsstepfuncs import TaskState, PassState, StateMachine
+
+# Define some states
+pass_state = PassState(
+    "My Pass", comment="Passes its input to its output without performing work"
+)
+times_two_resource = "arn:aws:lambda:ap-southeast-2:710187714096:function:DivideNumbers"
+task_state = TaskState(
+    "My Task",
+    comment="Times two task",
+    resource=times_two_resource,
+)
+
+# Define a state machine that orchestrates the states
+pass_state >> task_state
+state_machine = StateMachine(start_state=pass_state)
+
+# Compile the state machine to Amazon States Language
+state_machine.to_json("state_machine.json")
+
+# Simulate the state machine by executing it, use mock functions for tasks
+
+
+def mock_times_two(data):
+    data["foo"] *= 2
+    return data
+
+
+state_output = state_machine.simulate(
+    state_input={"foo": 5, "bar": 1},
+    resource_to_mock_fn={
+        times_two_resource: mock_times_two,
+    },
+)
+
+assert state_output == {"foo": 10, "bar": 1}
+```
+
+
+## Development
+
+Create a virtual environment:
+
+```sh
+$ python -m venv .venv
+$ source .venv/bin/activate
+```
+
+Install all dependencies:
+
+```sh
+$ make install
+```
+
+Run lint with:
+
+```sh
+$ make lint
+```
+
+Run tests with:
+
+```sh
+$ make test
+```
---
+++ packages/awsstepfuncs/awsstepfuncs-0.0.1.tar.gz/awsstepfuncs-0.0.1/src/awsstepfuncs/state_machine.py
@@ -0,0 +1,217 @@
+from __future__ import annotations
+
+import json
+from pathlib import Path
+from typing import Any, Dict, Optional, Set, Tuple, Union
+
+from awsstepfuncs.state import AbstractRetryCatchState, AbstractState
+from awsstepfuncs.types import ResourceToMockFn
+from awsstepfuncs.visualization import Visualization
+
+CompiledState = Dict[str, Union[str, bool, Dict[str, str], None]]
+
+
+class StateMachine:
+    """An AWS Step Functions state machine."""
+
+    def __init__(
+        self,
+        *,
+        start_state: AbstractState,
+        comment: Optional[str] = None,
+        version: Optional[str] = None,
+    ):
+        """Initialize a state machine.
+
+        A state machine will contain a reference to the start state. All states
+        in the state machine must have a unique name.
+
+        Args:
+            start_state: The starting state.
+            comment: A human-readable description of the state machine.
+            version: The verison of the state machine. If omitted, defaults to
+                "1.0".
+
+        Raises:
+            ValueError: Raised when there are duplicate state names.
+        """
+        self.start_state = start_state
+
+        if not self._has_unique_names():
+            raise ValueError(
+                "Duplicate names detected in state machine. Names must be unique"
+            )
+
+        self.comment = comment
+        self.version = version
+
+    @property
+    def all_states(self) -> Set[AbstractState]:
+        """Return all states in the state machine.
+
+        Returns:
+            A set of all possible states in the state machine.
+        """
+        return self._all_states_recursive(self.start_state)
+
+    def _all_states_recursive(self, start_state: AbstractState) -> Set[AbstractState]:
+        """Return all states from the given starting state.
+
+        Args:
+            start_state: The starting state.
+
+        Returns:
+            All possible states from the given starting state.
+        """
+        all_states = set()
+        for state in start_state:
+            all_states.add(state)
+            if isinstance(state, AbstractRetryCatchState):
+                for catcher in state.catchers:
+                    all_states |= self._all_states_recursive(catcher.next_state)
+        return all_states
+
+    def _has_unique_names(self) -> bool:
+        """Check if all states have unique names.
+
+        All state names must be unique in a state machine.
+
+        Returns:
+            Whether all states have unique names.
+        """
+        all_state_names = [state.name for state in self.all_states]
+        return len(all_state_names) == len(set(all_state_names))
+
+    def compile(self) -> Dict[str, Any]:  # noqa: A003
+        """Compile a state machine to Amazon States Language.
+
+        Returns:
+            A dictionary of the compiled state in Amazon States Language.
+        """
+        compiled = {
+            "StartAt": self.start_state.name,
+            "States": {state.name: state.compile() for state in self.all_states},
+        }
+
+        if comment := self.comment:
+            compiled["Comment"] = comment
+
+        if version := self.version:
+            compiled["Version"] = version
+
+        return compiled
+
+    def to_json(self, filename: Union[str, Path]) -> None:
+        """Compile to Amazon States Language and then output to JSON.
+
+        Args:
+            filename: The name of the file to write the JSON to.
+        """
+        filename = Path(filename)
+        with filename.open("w") as fp:
+            json.dump(self.compile(), fp)
+
+    def simulate(  # noqa: CCR001
+        self,
+        *,
+        state_input: dict = None,
+        resource_to_mock_fn: ResourceToMockFn = None,
+        show_visualization: bool = False,
+    ) -> Any:
+        """Simulate the state machine by executing all of the states.
+
+        Args:
+            state_input: Data to pass to the first state.
+            resource_to_mock_fn: A dictionary mapping Resource URI to a mock
+                function to use in the simulation.
+            show_visualization: Whether or not to create an animated GIF
+                visualization of the state machine when simulating. Outputs to
+                `state_machine.gif`.
+
+        Returns:
+            The final output state from simulating the state machine.
+        """
+        if state_input is None:
+            state_input = {}
+
+        if resource_to_mock_fn is None:
+            resource_to_mock_fn = {}
+
+        visualization = None
+        if show_visualization:
+            visualization = Visualization(self.start_state)
+
+        current_data = state_input
+        current_state: Optional[AbstractState] = self.start_state
+        print("Starting simulation of state machine")
+
+        while current_state is not None:
+            print(f"Running {current_state}")
+            print("State input:", current_data)
+            if visualization:
+                visualization.highlight_state(current_state)
+
+            next_state, next_data = self._simulate_state(
+                current_state, current_data, resource_to_mock_fn
+            )
+
+            if visualization and next_state:
+                visualization.highlight_state_transition(current_state, next_state)
+
+            current_state, current_data = next_state, next_data
+            print("State output:", current_data)
+
+        if visualization:
+            visualization.render()
+
+        print("Terminating simulation of state machine")
+
+        return current_data
+
+    def _simulate_state(
+        self,
+        state: AbstractState,
+        state_input: Any,
+        resource_to_mock_fn: ResourceToMockFn,
+    ) -> Tuple[Optional[AbstractState], Any]:
+        """Simulate a state, while handling input and output data and errors.
+
+        Args:
+            state: The current state.
+            state_input: The current data (passed as state input).
+            resource_to_mock_fn: A dictionary mapping Resource URI to a mock
+                function to use in the simulation.
+
+        Returns:
+            The next state and the output data.
+        """
+        try:
+            state_output = state.simulate(state_input, resource_to_mock_fn) or {}
+        except Exception:
+            print("Error encountered in state, checking for catchers")
+            next_state = self._check_for_catchers(state)
+            state_output = {}
+        else:
+            next_state = state.next_state
+
+        return next_state, state_output
+
+    @staticmethod
+    def _check_for_catchers(state: AbstractState) -> Optional[AbstractState]:
+        """Check for any failed state catchers.
+
+        Currently only checks for the "catch-all" catcher of error name
+        States.ALL.
+
+        Args:
+            state: The state to check for catchers.
+
+        Returns:
+            The state to transition to if a catcher can be applied.
+        """
+        if isinstance(state, AbstractRetryCatchState):
+            for catcher in state.catchers:
+                if "States.ALL" in catcher.error_equals:
+                    print(f"Found catcher, transitioning to {catcher.next_state}")
+                    return catcher.next_state
+        return None
