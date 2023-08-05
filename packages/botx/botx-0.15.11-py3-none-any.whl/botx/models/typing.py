42937dd6f6235ee1f797e271ec8a205ea3535d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:53 +0000

    Add package botx/0.15.11/botx-0.15.11-py3-none-any.whl

---
+++ packages/botx/botx-0.15.11-py3-none-any.whl/botx/models/typing.py
@@ -0,0 +1,21 @@
+"""Aliases for complex types from `typing` for models."""
+
+from typing import List, Union
+from uuid import UUID
+
+from botx.models.buttons import BubbleElement, KeyboardElement
+
+try:
+    from typing import Literal  # noqa: WPS433
+except ImportError:
+    from typing_extensions import (  # type: ignore  # noqa: WPS433, WPS440, F401
+        Literal,
+    )
+
+BubblesRow = List[BubbleElement]
+BubbleMarkup = List[BubblesRow]
+
+KeyboardRow = List[KeyboardElement]
+KeyboardMarkup = List[KeyboardRow]
+
+AvailableRecipients = Union[List[UUID], Literal["all"]]
