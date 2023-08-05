1aab48683dc75094c517e96cd095cdc493cf6b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:04 +0000

    Add package pymap-admin/0.5.2/pymap-admin-0.5.2.tar.gz

---
+++ packages/pymap-admin/pymap-admin-0.5.2.tar.gz/pymap-admin-0.5.2/pymapadmin/commands/system.py
@@ -0,0 +1,94 @@
+
+from __future__ import annotations
+
+import getpass
+from argparse import ArgumentParser, FileType
+from contextlib import closing
+from typing import Any, Optional, TextIO
+
+from .base import ClientCommand
+from ..local import get_token_file
+from ..typing import RequestT, ResponseT, MethodProtocol
+from ..grpc.admin_grpc import SystemStub
+from ..grpc.admin_pb2 import LoginRequest, LoginResponse, \
+    PingRequest, PingResponse
+
+__all__ = ['LoginCommand', 'PingCommand']
+
+
+class SystemCommand(ClientCommand[SystemStub, RequestT, ResponseT]):
+
+    @property
+    def client(self) -> SystemStub:
+        return SystemStub(self.channel)
+
+
+class LoginCommand(SystemCommand[LoginRequest, LoginResponse]):
+    """Login as a user for future requests."""
+
+    @classmethod
+    def add_subparser(cls, name: str, subparsers: Any) \
+            -> ArgumentParser:  # pragma: no cover
+        subparser = subparsers.add_parser(
+            name, description=cls.__doc__,
+            help='login as a user')
+        subparser.add_argument('-s', '--save', action='store_true',
+                               help='save the login token')
+        subparser.add_argument('-z', '--authzid', metavar='NAME',
+                               help='authorization identity name')
+        subparser.add_argument('--expiration', metavar='TIMESTAMP', type=float,
+                               help='token expiration timestamp')
+        password = subparser.add_mutually_exclusive_group(required=True)
+        password.add_argument('--password', metavar='VAL',
+                              help='login password')
+        password.add_argument('--password-file', metavar='PATH',
+                              type=FileType(),
+                              help='file containing login password')
+        password.add_argument('-i', '--ask-password', action='store_true',
+                              help='read login password from terminal')
+        subparser.add_argument('user', help='login username')
+        return subparser
+
+    @property
+    def method(self) -> MethodProtocol[LoginRequest, LoginResponse]:
+        return self.client.Login
+
+    def build_request(self) -> LoginRequest:
+        username: str = self.args.user
+        authzid: Optional[str] = self.args.authzid
+        expiration: Optional[float] = self.args.expiration
+        if self.args.ask_password:
+            password = getpass.getpass(f'{username} Password: ')
+        elif self.args.password_file is not None:
+            with closing(self.args.password_file) as pw_file:
+                password = pw_file.read()
+        else:
+            password = self.args.password
+        return LoginRequest(authcid=username, secret=password, authzid=authzid,
+                            token_expiration=expiration)
+
+    def handle_success(self, response: LoginResponse, outfile: TextIO) -> None:
+        super().handle_success(response, outfile)
+        token = response.bearer_token
+        if token and self.args.save:
+            token_file = get_token_file(self.args.token_file, user=True)
+            token_file.write_text(token)
+            token_file.chmod(0o600)
+
+
+class PingCommand(SystemCommand[PingRequest, PingResponse]):
+    """Ping the server."""
+
+    @classmethod
+    def add_subparser(cls, name: str, subparsers: Any) \
+            -> ArgumentParser:  # pragma: no cover
+        return subparsers.add_parser(
+            name, description=cls.__doc__,
+            help='ping the server')
+
+    @property
+    def method(self) -> MethodProtocol[PingRequest, PingResponse]:
+        return self.client.Ping
+
+    def build_request(self) -> PingRequest:
+        return PingRequest()
---
+++ packages/pymap-admin/pymap-admin-0.5.2.tar.gz/pymap-admin-0.5.2/pymapadmin/main.py
@@ -0,0 +1,84 @@
+"""Admin functions for a running pymap server.
+
+Many arguments may also be given with a $PYMAP_ADMIN_* environment variable.
+
+"""
+
+from __future__ import annotations
+
+import os
+import sys
+import asyncio
+from argparse import ArgumentParser, Namespace, SUPPRESS
+from ssl import create_default_context, CERT_NONE
+from typing import Type, Optional
+
+from grpclib.client import Channel
+from pymapadmin import __version__
+
+from .commands import load_commands
+from .commands.base import Command
+from .local import get_admin_socket
+
+
+def main() -> int:
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s ' + __version__)
+    parser.add_argument('--host', metavar='HOST',
+                        default=_def('HOST'), help='server host')
+    parser.add_argument('--port', metavar='PORT',
+                        default=_def('PORT'), help='server port')
+    parser.add_argument('--path', metavar='FILE',
+                        default=_def('PATH'), help=SUPPRESS)
+
+    token = parser.add_mutually_exclusive_group()
+    token.add_argument('--token-file', metavar='FILE', help='auth token file')
+    token.add_argument('--token', metavar='STR',
+                       default=_def('TOKEN'), help='auth token')
+
+    group = parser.add_argument_group('tls options')
+    group.add_argument('--cert', metavar='FILE', default=_def('CERT'),
+                       help='client certificate')
+    group.add_argument('--key', metavar='FILE', default=_def('KEY'),
+                       help='client private key')
+    group.add_argument('--cafile', metavar='FILE', default=_def('CAFILE'),
+                       help='CA cert file')
+    group.add_argument('--capath', metavar='PATH', default=_def('CAPATH'),
+                       help='CA cert path')
+    group.add_argument('--no-verify-cert', action='store_true',
+                       help='skip TLS certificate verification')
+
+    subparsers = parser.add_subparsers(dest='command', metavar='COMMAND')
+    commands = load_commands('pymapadmin.commands')
+    for command_name, command_cls in commands.items():
+        command_cls.add_subparser(command_name, subparsers)
+    args = parser.parse_args()
+
+    if not args.command:
+        parser.error('Expected command name.')
+    command = commands[args.command]
+
+    return asyncio.run(run(parser, args, command), debug=False)
+
+
+async def run(parser: ArgumentParser, args: Namespace,
+              command_cls: Type[Command]) -> int:
+    ssl = create_default_context(cafile=args.cafile, capath=args.capath)
+    if args.no_verify_cert:
+        ssl.check_hostname = False
+        ssl.verify_mode = CERT_NONE
+    if args.host is not None or args.port is not None:
+        channel = Channel(host=args.host, port=args.port, ssl=ssl)
+    else:
+        path = get_admin_socket(args.path)
+        channel = Channel(path=str(path))
+    command = command_cls(args, channel)
+    try:
+        return await command(sys.stdout)
+    finally:
+        channel.close()
+
+
+def _def(name: str, val: str = None) -> Optional[str]:
+    return os.environ.get(f'PYMAP_ADMIN_{name}', val)
