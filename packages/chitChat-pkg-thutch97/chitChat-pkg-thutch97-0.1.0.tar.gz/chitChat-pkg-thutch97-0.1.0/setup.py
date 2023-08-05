377bef4c88b7c9da4b1361d5ebb759f5b3153dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:19 +0000

    Add package chitChat-pkg-thutch97/0.1.0/chitChat-pkg-thutch97-0.1.0.tar.gz

---
+++ packages/chitChat-pkg-thutch97/chitChat-pkg-thutch97-0.1.0.tar.gz/chitChat-pkg-thutch97-0.1.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='chitChat-pkg-thutch97',
+      version='0.1.0',
+      author='Tom Hutchinson',
+      author_email='thomas.3.hutchinson@bt.com',
+      description='A python chat server package.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      install_requires = [
+            'Flask==1.1.2',
+		'Flask-RESTful==0.3.8',
+		'requests==2.24.0',
+		'bcrypt==3.2.0'
+      ],
+      packages=setuptools.find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6'
+      # package_data={
+      #       'chitChat-pkg-thutch97':['users.db']
+      # }
+     )
