5d3f57e461bcfb17ab1f1aff374d933774e79eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:03 +0000

    Add package nflfastpy/0.0.5/nflfastpy-0.0.5.tar.gz

---
+++ packages/nflfastpy/nflfastpy-0.0.5.tar.gz/nflfastpy-0.0.5/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="nflfastpy",
+    version="0.0.5",
+    author="Ben Dominguez",
+    author_email="bendominguez011@gmail.com",
+    description="A Python package for loading NFL play by play data from nflfastR",
+    long_description="A Python package for loading NFL play by play data from nflfastR",
+    url="https://github.com/fantasydatapros/nflfast_py",
+    license="MIT",
+    install_requires=['pandas'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6"
+)
