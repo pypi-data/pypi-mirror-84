c86ebd92c42b9f08bca24e013d7211a62f710603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:00 +0000

    Add package pss-marta-danilo/0.0.14/pss_marta_danilo-0.0.14.tar.gz

---
+++ packages/pss-marta-danilo/pss_marta_danilo-0.0.14.tar.gz/pss_marta_danilo-0.0.14/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pss_marta_danilo",
+    version = "0.0.14",
+    author = "Danilo Marta",
+    description = ("..."),
+    license = "BSD",
+    packages = ['pss-marta-danilo'],
+    keywords = "example documentation tutorial",
+    url = "http://packages.python.org/pss_marta_danilo",
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
