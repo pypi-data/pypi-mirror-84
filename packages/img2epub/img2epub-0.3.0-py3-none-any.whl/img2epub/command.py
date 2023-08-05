ccfd14addc89e459dbb281390e7b93f3646775b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:45 +0000

    Add package img2epub/0.3.0/img2epub-0.3.0-py3-none-any.whl

---
+++ packages/img2epub/img2epub-0.3.0-py3-none-any.whl/img2epub/command.py
@@ -0,0 +1,73 @@
+# encoding: utf-8
+
+
+from datetime import datetime, timezone
+import os
+import shutil
+
+import click
+
+from img2epub import functions as func
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(here, "__init__.py")) as f:
+    exec(f.read(), about)
+VERSION = about["__version__"]
+
+
+def main():
+    cmd(obj={})
+
+
+@click.group()
+@click.pass_context
+@click.version_option(version=VERSION, message="v%(version)s")
+def cmd(ctx):
+    pass
+
+
+@cmd.command(help="Build EPUB book.")
+@click.pass_context
+@click.option("--title", type=str, help="Specify book title.")
+@click.option("--output", type=str, metavar="FILENAME", help="Specify filename output to.")
+@click.option("--keep", is_flag=True, help="Keep temporary directory.")
+@click.argument("src_dir")
+def build(ctx, title, output, keep, src_dir):
+    now = datetime.now(timezone.utc)
+    tmp_dir_name = "tmp.epub.{time}".format(time=now.strftime("%Y%m%d%H%M%S"))
+
+    func.make_dirs(tmp_dir_name)
+    images = func.copy_images(src_dir, tmp_dir_name)
+    images = [s.replace("\\","/") for s in sorted(images)]
+    func.gen_mimetype(tmp_dir_name)
+    func.copy_container(tmp_dir_name)
+    if not title:
+        title = src_dir
+    book_opf_context = {
+        "title": title,
+        "time": now.isoformat(),
+        "images": images
+    }
+    func.gen_book_opf(tmp_dir_name, book_opf_context)
+    func.copy_nav(tmp_dir_name)
+    func.gen_chap1_xhtml(tmp_dir_name, book_opf_context)
+    func.zip_epub(tmp_dir_name, func.build_epub_filename(output, title))
+
+    if not keep:
+        shutil.rmtree(tmp_dir_name)
+
+
+@cmd.command(help="Remove any temporary directories.")
+@click.pass_context
+def clean(ctx):
+    dirs = [f for f in os.listdir(".") if os.path.isdir(f)]
+    tmp_dirs = [d for d in dirs if d.startswith("tmp.epub.")]
+    for tmp_dir in tmp_dirs:
+        shutil.rmtree(tmp_dir)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/img2epub/img2epub-0.3.0-py3-none-any.whl/img2epub/functions.py
@@ -0,0 +1,75 @@
+# encoding: utf-8
+
+
+from datetime import datetime, timezone
+import glob
+import os
+import re
+import shutil
+import subprocess
+import uuid
+
+from jinja2 import Template, Environment, FileSystemLoader
+
+
+data_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data")
+
+
+def make_dirs(tmp_dir_name):
+    os.makedirs(os.path.join(tmp_dir_name, "META-INF"))
+    os.makedirs(os.path.join(tmp_dir_name, "EPUB"))
+
+
+def copy_images(src_dir, tmp_dir_name):
+    images_dir = os.path.join(tmp_dir_name, "EPUB/images")
+    shutil.copytree(src_dir, images_dir)
+    return glob.glob("{dir}/*".format(dir=images_dir))
+
+
+def gen_mimetype(tmp_dir_name):
+    with open(os.path.join(tmp_dir_name, "mimetype"), "w") as f:
+        f.write("application/epub+zip")
+
+
+def copy_container(tmp_dir_name):
+    src = os.path.join(data_dir, "container.xml")
+    dest = os.path.join(tmp_dir_name, "META-INF/container.xml")
+    shutil.copyfile(src, dest)
+
+
+def gen_book_opf(tmp_dir_name, context):
+    env = Environment(loader=FileSystemLoader(data_dir))
+    template = env.get_template("book.opf.template")
+    context["images"] = [s.replace("{tmp}/EPUB".format(tmp=tmp_dir_name), ".") for s in context["images"]]
+    context["cover"] = context["images"][0]
+    context["uuid"] = str(uuid.uuid4())
+    with open(os.path.join(tmp_dir_name, "EPUB/book.opf"), "w", encoding="utf-8") as f:
+        f.write(template.render(context))
+
+
+def copy_nav(tmp_dir_name):
+    src = os.path.join(data_dir, "nav.xhtml")
+    dest = os.path.join(tmp_dir_name, "EPUB/nav.xhtml")
+    shutil.copyfile(src, dest)
+
+
+def gen_chap1_xhtml(tmp_dir_name, context):
+    env = Environment(loader=FileSystemLoader(data_dir))
+    template = env.get_template("chap1.xhtml.template")
+    images = [s.replace("{tmp}/EPUB".format(tmp=tmp_dir_name), ".") for s in context["images"]]
+    with open(os.path.join(tmp_dir_name, "EPUB/chap1.xhtml"), "w", encoding="utf-8") as f:
+        f.write(template.render(images=images, title=context["title"]))
+
+
+def zip_epub(tmp_dir_name, filename):
+    epub_file_name = "../{filename}".format(filename=filename)
+    os.chdir(tmp_dir_name)
+    subprocess.run(["zip", "-X0", epub_file_name, "mimetype"], stdout=subprocess.DEVNULL)
+    subprocess.run(["zip", "-r9", epub_file_name, "*", "-x", "mimetype"], stdout=subprocess.DEVNULL)
+    os.chdir("..")
+
+
+def build_epub_filename(output, title):
+    if not output:
+        output = title
+    return re.sub("\.epub\Z", "", output, flags=re.IGNORECASE) + ".epub"
