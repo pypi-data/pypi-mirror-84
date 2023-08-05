aa2a8428b4e584f401a3469dfb8c7dee79a28829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:01 +0000

    Add package generalizedtrees/0.0.6/generalizedtrees-0.0.6.tar.gz

---
+++ packages/generalizedtrees/generalizedtrees-0.0.6.tar.gz/generalizedtrees-0.0.6/generalizedtrees/recipes.py
@@ -0,0 +1,162 @@
+# Our implementation of standard decision tree classifiers
+#
+# Copyright 2020 Yuriy Sverchkov
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import field
+from generalizedtrees.composing import greedy_classification_tree_learner, greedy_binary_classification_tree_learner, compose_split_constructor_mk2
+from generalizedtrees.fitters import supervised_data_fit, fit_with_data_and_oracle
+from generalizedtrees.splitters import information_gain, information_gain_p, \
+    make_split_candidates, make_split_candidates_p, ijcai19_lr_gradient_slow
+from generalizedtrees.queues import Stack, Heap
+from generalizedtrees.stopping import never, node_depth, tree_size_limit
+import generalizedtrees.node_building as nb
+from generalizedtrees.data_generators import trepan_generator, trepan_generator_n, smearing
+
+DecisionTreeClassifier = greedy_classification_tree_learner(
+    name="DecisionTreeClassifier",
+    parameters=[
+        ('max_depth', int, field(default=10))
+    ],
+    fitter=supervised_data_fit,
+    node_building=nb.SupCferNodeBuilderMixin,
+    split_candidate_generator=make_split_candidates,
+    split_score=information_gain,
+    queue=Stack,
+    global_stop=never,
+    local_stop=node_depth
+)
+
+# Version of Trepan that uses hard target estimation for split scoring
+TrepanV1 = greedy_classification_tree_learner(
+    name="Trepan",
+    parameters=[
+        ('use_m_of_n', bool, field(default=False)),
+        ('max_tree_size', int, field(default=20)),
+        ('min_samples', int, field(default=100)),
+        ('dist_test_alpha', float, field(default=0.05))
+    ],
+    fitter=fit_with_data_and_oracle,
+    node_building=nb.OGCferNodeBuilderMixin,
+    split_candidate_generator=make_split_candidates,
+    split_score=information_gain,
+    data_generator=trepan_generator,
+    queue=Heap,
+    global_stop=tree_size_limit,
+    local_stop=never,
+    use_proba=False
+)
+
+# Version of Trepan that uses target probability estimates for split scoring
+Trepan = greedy_classification_tree_learner(
+    name="Trepan",
+    parameters=[
+        ('use_m_of_n', bool, field(default=False)),
+        ('max_tree_size', int, field(default=20)),
+        ('min_samples', int, field(default=100)),
+        ('dist_test_alpha', float, field(default=0.05))
+    ],
+    fitter=fit_with_data_and_oracle,
+    node_building=nb.OGCferNodeBuilderMixin,
+    split_candidate_generator=make_split_candidates_p,
+    split_score=information_gain_p,
+    data_generator=trepan_generator_n,
+    queue=Heap,
+    global_stop=tree_size_limit,
+    local_stop=never,
+    use_proba=True
+)
+
+# Born again trees
+BornAgain = greedy_classification_tree_learner(
+    name="BornAgain",
+    parameters=[
+        ('max_tree_size', int, field(default=20)),
+        ('min_samples', int, field(default=100))
+    ],
+    fitter=fit_with_data_and_oracle,
+    node_building=nb.BATNodeBuilderMixin,
+    split_candidate_generator=make_split_candidates_p,
+    split_score=information_gain_p,
+    data_generator=smearing,
+    queue=Stack,
+    global_stop=tree_size_limit,
+    local_stop=never,
+    use_proba=True
+)
+
+# Trepan with logistic regression leaves
+TLL = greedy_classification_tree_learner(
+        name="TLL",
+    parameters=[
+        ('node_cls', type, field(default=nb.TLLNode)),
+        ('use_m_of_n', bool, field(default=False)),
+        ('max_tree_size', int, field(default=20)),
+        ('min_samples', int, field(default=100)),
+        ('dist_test_alpha', float, field(default=0.05))
+    ],
+    fitter=fit_with_data_and_oracle,
+    node_building=nb.OGCferNodeBuilderMixin,
+    split_candidate_generator=make_split_candidates_p,
+    split_score=information_gain_p,
+    data_generator=trepan_generator_n,
+    queue=Heap,
+    global_stop=tree_size_limit,
+    local_stop=never,
+    use_proba=True
+)
+
+# Trepan with logistic regression leaves
+TLL2 = greedy_classification_tree_learner(
+        name="TLL",
+    parameters=[
+        ('node_cls', type, field(default=nb.TLLNode)),
+        ('use_m_of_n', bool, field(default=False)),
+        ('max_tree_size', int, field(default=20)),
+        ('min_samples', int, field(default=100)),
+        ('dist_test_alpha', float, field(default=0.05))
+    ],
+    fitter=fit_with_data_and_oracle,
+    node_building=nb.OGCferNodeBuilderMixin,
+    split_candidate_generator=make_split_candidates_p,
+    split_score=ijcai19_lr_gradient_slow,
+    data_generator=trepan_generator_n,
+    queue=Heap,
+    global_stop=tree_size_limit,
+    local_stop=never,
+    use_proba=True,
+    splits_composer=compose_split_constructor_mk2
+)
+
+# Trepan with logistic regression leaves and a 1-d estimator
+TLL3 = greedy_binary_classification_tree_learner(
+        name="TLL",
+    parameters=[
+        ('node_cls', type, field(default=nb.BTLLNode)),
+        ('use_m_of_n', bool, field(default=False)),
+        ('max_tree_size', int, field(default=20)),
+        ('min_samples', int, field(default=100)),
+        ('dist_test_alpha', float, field(default=0.05))
+    ],
+    fitter=fit_with_data_and_oracle,
+    node_building=nb.OGCferNodeBuilderMixin,
+    split_candidate_generator=make_split_candidates_p,
+    split_score=ijcai19_lr_gradient_slow,
+    data_generator=trepan_generator_n,
+    queue=Heap,
+    global_stop=tree_size_limit,
+    local_stop=never,
+    use_proba=True,
+    splits_composer=compose_split_constructor_mk2
+)
---
+++ packages/generalizedtrees/generalizedtrees-0.0.6.tar.gz/generalizedtrees-0.0.6/generalizedtrees/vis/vis.py
@@ -0,0 +1,123 @@
+# Visualization functions for our trees
+#
+# Copyright 2020 Yuriy Sverchkov
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pkgutil
+import json
+from logging import getLogger
+from generalizedtrees.queues import Stack
+from generalizedtrees.vis.models import model_to_simplified
+import generalizedtrees.constraints as cons
+
+logger = getLogger()
+
+DATA_TEMPLATE_STR = '/***DATA***/'
+ARTIST_TEMPLATE_STR = '/**DRAWING SCRIPT**/'
+
+def _load_html_template():
+    return pkgutil.get_data('generalizedtrees.vis', 'template.html').decode('utf-8')
+
+def _load_dt_js():
+    return pkgutil.get_data('generalizedtrees.vis', 'decision_tree.js').decode('utf-8')
+
+def explanation_to_html(explanation, out_file, feature_annotations = None):
+
+    json_str = explanation_to_JSON(explanation, feature_annotations)
+    # We anticipate possibly using different templates depending on model type
+    html_template = _load_html_template()
+    dt_artist = _load_dt_js()
+
+    if hasattr(out_file, 'write'):
+        _frankenstein(json_str, dt_artist, html_template, out_file)
+    else:
+        with open(out_file, 'wt') as f:
+            _frankenstein(json_str, dt_artist, html_template, f)
+
+def _frankenstein(data_str, artist_str, template_str, out_file):
+    
+    out_file.write(template_str
+        .replace(DATA_TEMPLATE_STR, 'data = ' + data_str + ';')
+        .replace(ARTIST_TEMPLATE_STR, artist_str))
+
+
+def _get_constraint_type_as_html_string(constraint):
+    if isinstance(constraint, cons.SimpleConstraint):
+        return f'{constraint.operator.value} {constraint.value}'
+    # Fall through catch-all
+    return str(constraint)
+
+
+def explanation_to_JSON(explanation, feature_annotations = None):
+    simplified = explanation_to_simplified(explanation, feature_annotations)
+    try:
+        return json.dumps(simplified)
+    except:
+        logger.debug(f'Simplified model: {simplified}')
+        raise
+
+
+def explanation_to_simplified(explanation, feature_annotations = None):
+
+    root = dict()
+
+    stack = Stack()
+    stack.push((root, explanation.tree.node('root')))
+
+    while (stack):
+
+        out_node, in_node = stack.pop()
+
+        # Record training set counts (and target distributions)
+        if hasattr(in_node.item, 'training_target_proba'):
+            out_node['training_samples'] = [
+                {'label':str(k), 'count': int(v)} for v, k in
+                zip(in_node.item.training_target_proba.sum(axis=0), explanation.classes_)]
+
+        if hasattr(in_node.item, 'gen_target_proba'):
+            out_node['generated_samples'] = [
+                {'label': str(k), 'count': int(v)} for v, k in
+                zip(in_node.item.gen_target_proba.sum(axis=0), explanation.classes_)]
+
+        # Record split
+        if hasattr(in_node.item, 'split') and in_node.item.split is not None:
+            out_node['split'] = str(in_node.item.split)
+
+            if feature_annotations is not None:
+                try:
+                    f = in_node.item.split.feature
+                    annotation = feature_annotations.loc[f]
+                    out_node['feature_annotation'] = [{'annotation': 'feature id', 'value': f}]
+                    out_node['feature_annotation'].extend([
+                        {'annotation': str(i), 'value': v} for i, v in annotation.iteritems()])
+                
+                except Exception as e:
+                    logger.warning(
+                        f"Could not bind feature annotation to {in_node.item.split}",
+                        exc_info=e)
+        
+        # Record children
+        if not in_node.is_leaf:
+            out_node['children'] = [
+                {'branch': _get_constraint_type_as_html_string(c.item.local_constraint)}
+                for c in in_node]
+            for pair in zip(out_node['children'], in_node):
+                stack.push(pair)
+        
+        # Node-model specific conversions should be implemented elsewhere.
+        if hasattr(in_node.item, 'model') and in_node.item.model is not None:
+            # Currently a stump
+            out_node['model'] = model_to_simplified(in_node.item.model, explanation)
+    
+    return root
