f303cfc29c1e0787d16df7e19555281cf6a06fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:30 +0000

    Add package robotframework-browser/2.0.0/robotframework_browser-2.0.0-py3-none-any.whl

---
+++ packages/robotframework-browser/robotframework_browser-2.0.0-py3-none-any.whl/Browser/utils/data_types.py
@@ -0,0 +1,199 @@
+# Copyright 2020-     Robot Framework Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from enum import Enum, auto
+
+from typing_extensions import TypedDict
+
+BoundingBox = TypedDict(
+    "BoundingBox",
+    {"x": float, "y": float, "width": float, "height": float},
+    total=False,
+)
+
+Coordinates = TypedDict("Coordinates", {"x": float, "y": float}, total=False)
+
+MouseOptionsDict = TypedDict(
+    "MouseOptionsDict", {"x": float, "y": float, "options": dict}, total=False
+)
+
+ViewportDimensions = TypedDict("ViewportDimensions", {"width": int, "height": int})
+
+
+class SelectionType(Enum):
+    ACTIVE = auto()
+    CURRENT = ACTIVE
+    ALL = auto()
+    ANY = ALL
+
+
+class DialogAction(Enum):
+    accept = auto()
+    dismiss = auto()
+
+
+class CookieType(Enum):
+    dictionary = auto()
+    dict = dictionary
+    string = auto()
+    str = string
+
+
+CookieSameSite = Enum(
+    "CookieSameSite", {"Strict": "Strict", "Lax": "Lax", "None": "None"}
+)
+
+
+class RequestMethod(Enum):
+    HEAD = auto()
+    GET = auto()
+    POST = auto()
+    PUT = auto()
+    PATCH = auto()
+    DELETE = auto()
+
+
+class MouseButtonAction(Enum):
+    click = auto()
+    down = auto()
+    up = auto()
+
+
+class MouseButton(Enum):
+    left = auto()
+    middle = auto()
+    right = auto()
+
+
+class KeyAction(Enum):
+    down = auto()
+    up = auto()
+    press = auto()
+
+
+class KeyboardInputAction(Enum):
+    insertText = auto()
+    type = auto()
+
+
+class KeyboardModifier(Enum):
+    Alt = auto()
+    Control = auto()
+    Meta = auto()
+    Shift = auto()
+
+
+class SelectAttribute(Enum):
+    value = auto()
+    label = auto()
+    text = label
+    index = auto()
+
+
+class SupportedBrowsers(Enum):
+    chromium = auto()
+    firefox = auto()
+    webkit = auto()
+
+
+ColorScheme = Enum("ColorScheme", ["dark", "light", "no-preference"])
+
+
+ScrollBehavior = Enum("ScrollBehavior", ["auto", "smooth"])
+
+
+class SizeFields(Enum):
+    width = auto()
+    height = auto()
+    ALL = auto()
+
+
+class AreaFields(Enum):
+    top = auto()
+    left = auto()
+    bottom = auto()
+    right = auto()
+    ALL = auto()
+
+
+class BoundingBoxFields(Enum):
+    width = auto()
+    height = auto()
+    x = auto()
+    y = auto()
+    ALL = auto()
+
+
+class AutoClosingLevel(Enum):
+    SUITE = auto()
+    TEST = auto()
+    MANUAL = auto()
+
+
+class ElementState(Enum):
+    attached = auto()
+    detached = auto()
+    visible = auto()
+    hidden = auto()
+    enabled = auto()
+    disabled = auto()
+    editable = auto()
+    readonly = auto()
+    selected = auto()
+    deselected = auto()
+    focused = auto()
+    defocused = auto()
+    checked = auto()
+    unchecked = auto()
+
+
+class ElementStateKey(Enum):
+    attached = auto()
+    visible = auto()
+    disabled = auto()
+    readonly = auto()
+    selected = auto()
+    focused = auto()
+    checked = auto()
+
+
+AssertionOperator = Enum(
+    "AssertionOperator",
+    {
+        "equal": "==",
+        "==": "==",
+        "should be": "==",
+        "inequal": "!=",
+        "!=": "!=",
+        "should not be": "!=",
+        "less than": "<",
+        "<": "<",
+        "greater than": ">",
+        ">": ">",
+        "<=": "<=",
+        ">=": ">=",
+        "contains": "*=",
+        "*=": "*=",
+        "starts": "^=",
+        "^=": "^=",
+        "should start with": "^=",
+        "ends": "$=",
+        "should end with": "$=",
+        "$=": "$=",
+        "matches": "$",
+        "validate": "validate",
+        "then": "then",
+        "evaluate": "then",
+    },
+)
