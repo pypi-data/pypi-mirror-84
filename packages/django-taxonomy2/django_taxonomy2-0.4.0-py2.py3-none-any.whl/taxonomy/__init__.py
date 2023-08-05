c68a5daaf0663237d4a1b870fac33335ef7d8193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:21 +0000

    Add package django-taxonomy2/0.4.0/django_taxonomy2-0.4.0-py2.py3-none-any.whl

---
+++ packages/django-taxonomy2/django_taxonomy2-0.4.0-py2.py3-none-any.whl/taxonomy/__init__.py
@@ -0,0 +1,7 @@
+"""Category tree for Django. Replicable and customizable. Includes Admin. Low on features, fast setup."""
+__version__ = '0.4.0'
+
+UNCATEGORIZED = -1
+NO_PARENT = -1
+BIG_DEPTH = 9999999999
+
