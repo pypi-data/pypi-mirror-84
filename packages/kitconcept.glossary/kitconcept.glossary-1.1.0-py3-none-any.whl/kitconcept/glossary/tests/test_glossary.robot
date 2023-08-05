fe715996353c79ebb68244cf04ee28911bc5e2f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:24 +0000

    Add package kitconcept.glossary/1.1.0/kitconcept.glossary-1.1.0-py3-none-any.whl

---
+++ packages/kitconcept.glossary/kitconcept.glossary-1.1.0-py3-none-any.whl/kitconcept/glossary/tests/test_glossary.robot
@@ -0,0 +1,72 @@
+*** Settings ***
+
+Resource  plone/app/robotframework/keywords.robot
+Variables  plone/app/testing/interfaces.py
+Library  Remote  ${PLONE_URL}/RobotRemote
+
+Suite Setup  Open Test Browser
+Suite Teardown  Close all browsers
+
+*** Variables ***
+
+${glossary_title_selector} =  input#form-widgets-IBasic-title
+${term_title_selector} =  input#form-widgets-title
+
+*** Test cases ***
+
+Test CRUD
+    Enable Autologin as  Site Administrator
+    Go to Homepage
+
+    Create Glossary  The Beatles
+    Create Term  The Beatles  John
+    Create Term  The Beatles  Paul
+    Create Term  The Beatles  George
+    Create Term  The Beatles  Ringo
+    Check Terms  The Beatles  John  Paul  George  Ringo
+    Delete Glossary  The Beatles
+
+*** Keywords ***
+
+Click Add Glossary
+    Open Add New Menu
+    Click Link  css=a#glossary
+    Page Should Contain  Add Glossary
+
+Click Add Term
+    Open Add New Menu
+    Click Link  css=a#glossaryterm
+    Page Should Contain  Add GlossaryTerm
+
+Create Glossary
+    [arguments]  ${title}
+
+    Click Add Glossary
+    Input Text  css=${glossary_title_selector}  ${title}
+    Click Button  Save
+    Page Should Contain  Item created
+
+Create Term
+    [arguments]  ${glossary}  ${title}
+
+    Click Link  ${glossary}
+    Click Add Term
+    Input Text  css=${term_title_selector}  ${title}
+    Click Button  Save
+    Page Should Contain  Item created
+
+Check Terms
+    [arguments]  ${glossary}  @{terms}
+
+    Click Link  ${glossary}
+    :FOR  ${term}  IN  @{terms}
+    \   Page Should Contain  ${term}
+
+Delete Glossary
+    [arguments]  ${glossary}
+
+    Click Link  ${glossary}
+    Open Action Menu
+    Click Link  css=a#plone-contentmenu-actions-delete
+    Click Button  Delete
+    Page Should Contain  Plone site
---
+++ packages/kitconcept.glossary/kitconcept.glossary-1.1.0-py3-none-any.whl/kitconcept/glossary/tests/test_setup.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+from kitconcept.glossary.testing import INTEGRATION_TESTING
+from plone.browserlayer.utils import registered_layers
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone import api
+
+import unittest
+
+try:
+    from Products.CMFPlone.factory import _IMREALLYPLONE5
+
+    _IMREALLYPLONE5  # noqa
+except ImportError:
+    PLONE_5 = False
+else:
+    PLONE_5 = True
+
+try:
+    from Products.CMFPlone.utils import get_installer
+except ImportError:
+    get_installer = None
+
+
+class InstallTestCase(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer["request"])
+        else:
+            self.installer = api.portal.get_tool("portal_quickinstaller")
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+        self.installer.installProducts(["kitconcept.glossary"])
+
+    def test_product_installed(self):
+        self.assertTrue(self.installer.isProductInstalled("kitconcept.glossary"))
+
+    def test_addon_layer(self):
+        layers = [layer.getName() for layer in registered_layers()]
+        self.assertIn("IGlossaryLayer", layers)
+
+    def test_add_glossary_permission(self):
+        permission = "kitconcept.glossary: Add Glossary"
+        roles = self.portal.rolesOfPermission(permission)
+        roles = [r["name"] for r in roles if r["selected"]]
+        expected = ["Contributor", "Manager", "Owner", "Site Administrator"]
+        self.assertListEqual(roles, expected)
+
+    def test_add_term_permission(self):
+        permission = "kitconcept.glossary: Add Glossary Term"
+        roles = self.portal.rolesOfPermission(permission)
+        roles = [r["name"] for r in roles if r["selected"]]
+        expected = ["Contributor", "Manager", "Owner", "Site Administrator"]
+        self.assertListEqual(roles, expected)
+
+
+class UninstallTestCase(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer["request"])
+        else:
+            self.installer = api.portal.get_tool("portal_quickinstaller")
+        roles_before = api.user.get_roles(TEST_USER_ID)
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+        self.installer.uninstallProducts(["kitconcept.glossary"])
+        setRoles(self.portal, TEST_USER_ID, roles_before)
+
+    def test_product_uninstalled(self):
+        self.assertFalse(self.installer.isProductInstalled("kitconcept.glossary"))
+
+    # def test_browserlayer_removed(self):
+    #     from kitconcept.glossary.interfaces import IGlossaryLayer
+    #     from plone.browserlayer import utils
+
+    #     self.assertNotIn(IGlossaryLayer, utils.registered_layers())
