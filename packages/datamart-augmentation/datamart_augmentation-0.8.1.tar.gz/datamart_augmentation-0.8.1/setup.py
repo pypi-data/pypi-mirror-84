7c20a22c5288aaf61b0d0a9371f274e6937aa9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:49 +0000

    Add package datamart-augmentation/0.8.1/datamart_augmentation-0.8.1.tar.gz

---
+++ packages/datamart-augmentation/datamart_augmentation-0.8.1.tar.gz/datamart_augmentation-0.8.1/setup.py
@@ -0,0 +1,41 @@
+import io
+import os
+from setuptools import setup
+
+
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+
+req = [
+    'pandas',
+    'numpy',
+    'datamart_materialize==0.8.1',
+    'datamart_profiler==0.8.1',
+]
+with io.open('README.rst', encoding='utf-8') as fp:
+    description = fp.read()
+setup(name='datamart_augmentation',
+      version='0.8.1',
+      packages=['datamart_augmentation'],
+      install_requires=req,
+      description="Augmentation functions for Datamart services",
+      author="Remi Rampin",
+      author_email='remi.rampin@nyu.edu',
+      maintainer="Remi Rampin",
+      maintainer_email='remi.rampin@nyu.edu',
+      url='https://gitlab.com/ViDA-NYU/datamart/datamart',
+      project_urls={
+          'Homepage': 'https://gitlab.com/ViDA-NYU/datamart/datamart',
+          'Source': 'https://gitlab.com/ViDA-NYU/datamart/datamart',
+          'Tracker': 'https://gitlab.com/ViDA-NYU/datamart/datamart/issues',
+      },
+      long_description=description,
+      license='Apache-2.0',
+      keywords=['datamart'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Scientific/Engineering :: Information Analysis'])
