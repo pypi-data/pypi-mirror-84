4ade1eea4cca2b7253f44677cd0f950762ae6f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:28 +0000

    Add package wideboost/0.3.2/wideboost-0.3.2-py3-none-any.whl

---
+++ packages/wideboost/wideboost-0.3.2-py3-none-any.whl/wideboost/objectives/binarylogloss.py
@@ -0,0 +1,53 @@
+import numpy as np 
+from .general_gh import f_gradient_B, f_hessian_B
+
+def binarylogloss_gradient_hessian_FULLHESSIAN(X,B,Y):
+    Y = Y.reshape([Y.shape[0],-1])
+
+    assert len(X.shape) == 2
+    assert len(B.shape) == 2
+    assert len(Y.shape) == 2
+
+    logits = X.dot(B)
+    P = 1 / (1 + np.exp(-logits))
+
+    G = P - Y
+    H = (P - np.square(P)).reshape([Y.shape[0],1,1])
+
+    dX = f_gradient_B(G,B)
+    dX2 = f_hessian_B(H,B)
+
+    eps = 1e-16
+    return dX, np.maximum(dX2,eps)
+
+
+def binarylogloss_gradient_hessian(X,B,Y):
+    # Loss = log(p) when Y == 1
+    # Loss = log(1-p) when Y == 0
+    # p = exp(XB)/(1 + exp(XB))
+    # 1-p = 1/(1 + exp(XB))
+
+    Y = Y.reshape([Y.shape[0],-1])
+
+    assert len(X.shape) == 2
+    assert len(B.shape) == 2
+    assert len(Y.shape) == 2
+
+    logits = X.dot(B)
+    P = 1 / (1 + np.exp(-logits))
+    eps = 1e-16
+
+    dX = (P - Y) * B.transpose()
+    dX2 = np.maximum((P * (1-P) * np.square(B).transpose() ), eps)
+
+    return dX, dX2
+
+
+
+
+
+
+
+
+
+
---
+++ packages/wideboost/wideboost-0.3.2-py3-none-any.whl/wideboost/objectives/tests/test_binarylogloss.py
@@ -0,0 +1,33 @@
+import numpy as np
+from wideboost.objectives import binarylogloss
+
+def test_basic_value():
+    X = np.zeros([1,1])
+    B = np.ones([1,1])
+    Y = np.ones([1,1])
+
+    g,h = binarylogloss.binarylogloss_gradient_hessian(X,B,Y)
+    ### g = P - Y
+    ### h = P * (1-P)
+    P = 0.5
+
+    gmatches = g == P - 1
+    hmatches = h == P * (1-P)
+
+    assert gmatches and hmatches
+
+# eventually deprecated
+def test_old_v_new():
+    np.random.seed(789)
+    X = np.random.random([100,10])
+    B = np.random.random([10,1])
+    Y = np.random.choice([0,1],[100,1])
+
+    g,h = binarylogloss.binarylogloss_gradient_hessian(X,B,Y)
+    go,ho = binarylogloss.binarylogloss_gradient_hessian_FULLHESSIAN(X,B,Y)
+
+    eps = 1e-8
+    gmatches = np.max(np.abs(g - go)) < eps
+    hmatches = np.max(np.abs(h - ho)) < eps
+
+    assert gmatches and hmatches
