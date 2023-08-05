8742f37cb8e6c12bdb2b7bad625426e4cd0c8463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:48 +0000

    Add package xtgeo/2.13.0a4/xtgeo-2.13.0a4-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/xtgeo/xtgeo-2.13.0a4-cp36-cp36m-macosx_10_9_x86_64.whl/xtgeo/cube/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""The XTGeo cube package"""
+from __future__ import division, absolute_import
+from __future__ import print_function
+
+from xtgeo.cube.cube1 import Cube  # noqa
