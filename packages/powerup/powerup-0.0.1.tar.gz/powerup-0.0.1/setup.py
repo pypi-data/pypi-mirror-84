75c984599021147951173357a5975ec3089e250c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:15 +0000

    Add package powerup/0.0.1/powerup-0.0.1.tar.gz

---
+++ packages/powerup/powerup-0.0.1.tar.gz/powerup-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="powerup",
+    version="0.0.1",
+    author="ambientnuance",
+    author_email="ambientnuance@gmail.com",
+    description="(name-squatting placeholder)",
+    packages=setuptools.find_packages(),
+)
