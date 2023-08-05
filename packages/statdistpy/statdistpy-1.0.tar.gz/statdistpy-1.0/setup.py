ad8fb162eb8a384adaa7a822dcb2f2a583b5ee7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:50 +0000

    Add package statdistpy/1.0/statdistpy-1.0.tar.gz

---
+++ packages/statdistpy/statdistpy-1.0.tar.gz/statdistpy-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='statdistpy',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['statdistpy'],
+      author='Luiz Alberto',
+      author_email='gomes.luiz@gmail.com',
+      zip_safe=False)
