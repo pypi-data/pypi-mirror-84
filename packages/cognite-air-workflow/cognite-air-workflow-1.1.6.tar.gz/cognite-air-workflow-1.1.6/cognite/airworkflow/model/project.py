bb270c5c57f9df1e9f09d16ef8a48a9df9cabf18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:08 +0000

    Add package cognite-air-workflow/1.1.6/cognite-air-workflow-1.1.6.tar.gz

---
+++ packages/cognite-air-workflow/cognite-air-workflow-1.1.6.tar.gz/cognite-air-workflow-1.1.6/cognite/airworkflow/model/project.py
@@ -0,0 +1,34 @@
+from enum import Enum
+from typing import Optional
+
+from cognite.airworkflow.util import env
+from cognite.airworkflow.util.projecthelpers import project_name_finder
+
+
+class Project(Enum):
+    NAME = project_name_finder()
+
+    @classmethod
+    def _missing_(cls, value):
+        raise ValueError(f"{value} is not a valid AIR project, must be one of {[p.value for p in Project]}")
+
+
+class ProjectInfo:
+    def __init__(
+        self,
+        project: Project,
+        deployment_key_name: str,
+        client_key_name: str,
+        base_url: str = None,
+    ):
+        self.name: str = project.value
+        self.deployment_key_name: str = deployment_key_name
+        self.client_key_name: str = client_key_name
+        self.base_url: Optional[str] = base_url
+        self.project: Project = project
+
+    def get_deployment_key(self) -> str:
+        return env.get_env_value(self.deployment_key_name)
+
+    def get_client_api_key(self) -> str:
+        return env.get_env_value(self.client_key_name)
