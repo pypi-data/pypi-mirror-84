216e21ae3aeaea47701b75d4d917cf0a780fad52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:47 +0000

    Add package pynessie/0.2.0/pynessie-0.2.0.tar.gz

---
+++ packages/pynessie/pynessie-0.2.0.tar.gz/pynessie-0.2.0/HISTORY.rst
@@ -0,0 +1,15 @@
+=======
+History
+=======
+
+0.1.1 (2020-10-01)
+------------------
+
+* First release on PyPI.
+
+0.2.0 (2020-10-30)
+------------------
+
+* git-like cli interface
+* more complete coverage of REST endpoints
+* better testing
---
+++ packages/pynessie/pynessie-0.2.0.tar.gz/pynessie-0.2.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# flake8: noqa
+"""The setup script."""
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+requirements = [
+    "Click>=7.0", "requests", "simplejson", "confuse", "desert", "marshmallow",
+    "marshmallow_oneofschema", "attrs"
+]
+
+setup_requirements = ["pytest-runner", "pip"]
+
+setup(
+    author="Ryan Murray",
+    author_email="rymurr@dremio.com",
+    python_requires=">=3.5",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="Project Nessie: A Git-like Experience for your Data Lake",
+    entry_points={
+        "console_scripts": [
+            "nessie=pynessie.cli:cli",
+        ],
+    },
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    long_description=readme + "\n" + history,
+    include_package_data=True,
+    keywords="pynessie",
+    name="pynessie",
+    packages=find_packages(include=["pynessie", "pynessie.*"]),
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=[],
+    url="https://github.com/projectnessie/nessie",
+    version="0.2.0",
+    zip_safe=False,
+)
