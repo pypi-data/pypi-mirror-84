72a2265a5ba9efebdf7ff8137e6838bd790bd1c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:25 +0000

    Add package stackprinter/0.2.5/stackprinter-0.2.5.tar.gz

---
+++ packages/stackprinter/stackprinter-0.2.5.tar.gz/stackprinter-0.2.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    python_requires=">=3.4",
+    name="stackprinter",
+    version="0.2.5",
+    author="cknd",
+    author_email="ck-github@mailbox.org",
+    description="Debug-friendly stack traces, with variable values and semantic highlighting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cknd/stackprinter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
