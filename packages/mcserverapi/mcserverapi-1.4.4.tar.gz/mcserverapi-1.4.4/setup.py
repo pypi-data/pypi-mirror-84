37bf104faa5833835851e836dfc461c35cd10d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:52 +0000

    Add package mcserverapi/1.4.4/mcserverapi-1.4.4.tar.gz

---
+++ packages/mcserverapi/mcserverapi-1.4.4.tar.gz/mcserverapi-1.4.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mcserverapi',
+    version='1.4.4',
+    packages=['mcserverapi'],
+    url='https://github.com/TeamNightSky/MCServerAPI',
+    license='GNU License',
+    author='Pixymon',
+    author_email='nlarsen23.student@gmail.com',
+    description='Integratable API for Local Minecraft Servers',
+    install_requires=['requests', 'psutil', 'colorama']
+)
