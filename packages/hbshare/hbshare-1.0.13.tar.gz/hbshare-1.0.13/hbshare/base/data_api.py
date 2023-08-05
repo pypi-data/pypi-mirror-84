3d8faf668c4276e6c18add4e14106ee3e025f8f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:50 +0000

    Add package hbshare/1.0.13/hbshare-1.0.13.tar.gz

---
+++ packages/hbshare/hbshare-1.0.13.tar.gz/hbshare-1.0.13/hbshare/base/data_api.py
@@ -0,0 +1,50 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Pro数据接口 
+Created on 2020/06/15
+@author: meng.lv
+@group : hbshare
+"""
+
+import pandas as pd
+import simplejson as json
+from functools import partial
+import requests
+
+
+class DataApi:
+
+    __token = ''
+    __base_url = 'https://data.howbuy.com'
+
+    def __init__(self, token, timeout=10):
+        """
+        Parameters
+        ----------
+        token: str
+            API接口TOKEN，用于用户认证
+        """
+        self.__token = token
+        self.__timeout = timeout
+
+    def query(self, url, method='get', post_data={}):
+        headers = {
+            "token": self.__token
+        }
+
+        if method == 'get':
+            res = requests.get(url, headers=headers, timeout=self.__timeout)
+        else:
+            res = requests.post(url, json=post_data, headers=headers, timeout=self.__timeout)
+
+        result = json.loads(res.text)
+
+        return result
+
+    def __getattr__(self, name):
+        return partial(self.query, name)
+
+    def __str__(self):
+        return u"%s, %s" % (self.__base_url, self.__token)
---
+++ packages/hbshare/hbshare-1.0.13.tar.gz/hbshare-1.0.13/test/fund_nav_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#coding:utf-8
+
+"""
+@author: Meng.lv
+@contact: meng.lv@howbuy.com
+@software: PyCharm
+@file: fund_nav_test.py
+@time: 2020/6/15 10:50
+"""
+
+import hbshare as hbs
+
+
+def test_get_fund_newest_nav():
+    hbs.set_token("qwertyuisdfghjkxcvbn1000")
+    data = hbs.get_fund_newest_nav_by_code('000004')
+    print(data)
+
+def get_simu_corp_list_by_keyword():
+    hbs.set_token("qwertyuisdfghjkxcvbn1000")
+    #data = hbs.get_simu_corp_list_by_keyword('黑石')
+    data = hbs.get_prod_list_by_corp_code(80535346)
+    print(data)
+
+
+def get_simu_nav_by_code():
+    hbs.set_token("qwertyuisdfghjkxcvbn1000")
+    data, total_count = hbs.get_simu_nav_by_code("P0010", "20190101", "20191231")
+    print(data)
+    print(total_count)
+
+
+if __name__ == "__main__":
+    get_simu_nav_by_code()
