a9a162f03c71221e51af4399dcd1f3b68eb651cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:36 +0000

    Add package pyxtension/1.13.15/pyxtension-1.13.15-py3-none-any.whl

---
+++ packages/pyxtension/pyxtension-1.13.15-py3-none-any.whl/pyxtension/__init__.py
@@ -0,0 +1,25 @@
+__author__ = 'asuiu'
+
+
+class ValidateError(ValueError):
+    def __init__(self, args):
+        ValueError.__init__(self, args)
+
+
+def validate(expr, msg="Invalid argument", exc: Exception = ValidateError):
+    """
+    If the expression val does not evaluate to True, then raise a ValidationError with msg
+    """
+    if not expr:
+        raise exc(msg)
+
+class PydanticValidated:
+    @classmethod
+    def __get_validators__(cls):
+        yield cls._pydantic_validator
+
+    @classmethod
+    def _pydantic_validator(cls, v):
+        if not isinstance(v, cls):
+            raise TypeError(f'{repr(v)} is of type {type(v)} but is expected to be of {cls}')
+        return v
