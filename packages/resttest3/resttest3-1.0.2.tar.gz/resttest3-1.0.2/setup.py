d3426d19cf6fd151b398960c9fabdbd544f0b009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:18 +0000

    Add package resttest3/1.0.2/resttest3-1.0.2.tar.gz

---
+++ packages/resttest3/resttest3-1.0.2.tar.gz/resttest3-1.0.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+with open("requirements.txt") as fp:
+    install_requires = [lib_str.strip() for lib_str in fp.read().split("\n") if not lib_str.startswith("#")]
+
+
+setup(
+    name='resttest3',
+    version="1.0.2",
+    description='Python RESTful API Testing & Micro benchmarking Tool',
+    long_description='Python RESTful API Testing & Microbenchmarking Tool '
+                     '\n Documentation at https://abhijo89-to.github.io/py3resttest/',
+    author="Abhilash Joseph C",
+    author_email='abhilash@softlinkweb.com',
+    url='https://github.com/abhijo89-to/py3resttest',
+    keywords=['rest', 'web', 'http', 'testing', 'api'],
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Utilities'
+    ],
+    packages=find_packages(),
+    python_requires='>=3.5',
+    license='Apache License, Version 2.0',
+    install_requires=install_requires,
+    tests_require=install_requires,
+    test_suite="py3resttest.tests",
+    entry_points={
+        'console_scripts': ['resttest3=py3resttest.runner:main'],
+    }
+
+)
