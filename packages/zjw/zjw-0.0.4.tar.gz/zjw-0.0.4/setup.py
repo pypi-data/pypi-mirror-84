707608e84af971b5c9f8b8b0cbf70391c27cdf3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:19 +0000

    Add package zjw/0.0.4/zjw-0.0.4.tar.gz

---
+++ packages/zjw/zjw-0.0.4.tar.gz/zjw-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fg:
+    long_description = fg.read()
+
+
+setuptools.setup(
+    name="zjw",
+    version = "0.0.4",
+    author = "zjw",
+    author_email="2415528035@qq.com",
+    description="a package for extend",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+
+)
