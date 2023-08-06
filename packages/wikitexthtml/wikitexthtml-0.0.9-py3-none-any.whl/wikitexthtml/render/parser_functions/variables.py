612fec159ff596787cd0983513b0c20337c7d285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:16 +0000

    Add package wikitexthtml/0.0.9/wikitexthtml-0.0.9-py3-none-any.whl

---
+++ packages/wikitexthtml/wikitexthtml-0.0.9-py3-none-any.whl/wikitexthtml/render/parser_functions/variables.py
@@ -0,0 +1,21 @@
+import datetime
+import html
+import wikitextparser
+
+from .exceptions import ParserFunctionWrongArgumentCount
+from ...prototype import WikiTextHtml
+
+
+def variable(instance: WikiTextHtml, parser_function: wikitextparser.ParserFunction):
+    if len(parser_function.arguments) != 0:
+        raise ParserFunctionWrongArgumentCount
+
+    name = parser_function.name.lower()
+    if name == "currentyear":
+        parser_function.string = str(datetime.datetime.now().year)
+    elif name in ("pagename", "fullpagename", "basepagename"):
+        parser_function.string = html.escape(instance.page)
+    elif name == "subpagename":
+        parser_function.string = html.escape(instance.clean_title(instance.page))
+    elif name == "namespace":
+        parser_function.string = ""
