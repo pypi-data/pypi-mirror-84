dd8f46d8813d88a2215a20d9b65c99e56516b443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:27 +0000

    Add package baking-wijesooriya/0.0.7/baking_wijesooriya-0.0.7-py3-none-any.whl

---
+++ packages/baking-wijesooriya/baking_wijesooriya-0.0.7-py3-none-any.whl/baking/naturalize.py
@@ -0,0 +1,8 @@
+from baking import measure as measure
+from baking import mixIngredients as mixIngredients
+from baking import pourBatter as pourBatter
+from baking import setOvenTempTo as setOvenTempTo
+from baking import bakeBatter as bakeBatter
+from baking import assembleCake as assembleCake
+from baking import iceCake as iceCake
+from baking import complete as complete
