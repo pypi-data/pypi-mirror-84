c138068478aa2c450ad654461e17c375f7e80e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:19 +0000

    Add package zzhprobsixth/0.3/zzhprobsixth-0.3.tar.gz

---
+++ packages/zzhprobsixth/zzhprobsixth-0.3.tar.gz/zzhprobsixth-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='zzhprobsixth',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['zzhprobsixth'],
+      zip_safe=False)
