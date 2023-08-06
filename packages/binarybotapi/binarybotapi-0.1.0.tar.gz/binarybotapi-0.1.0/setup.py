55740d7347ddd221e40759449b15040535dd5a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:09 +0000

    Add package binarybotapi/0.1.0/binarybotapi-0.1.0.tar.gz

---
+++ packages/binarybotapi/binarybotapi-0.1.0.tar.gz/binarybotapi-0.1.0/setup.py
@@ -0,0 +1,26 @@
+"""The python wrapper for Binary Option API package setup."""
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+import setuptools
+
+setuptools.setup(
+    name="binarybotapi",
+    version="0.1.0",
+    author="Vishal Bhadade",
+    author_email="vishal.bhadade@gmail.com",
+    description="Binary Option API for IQOption in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vish4mi/binarybotapi",
+    packages=setuptools.find_packages(),
+    install_requires=["pylint","requests","websocket-client"],
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    zip_safe=False
+)
