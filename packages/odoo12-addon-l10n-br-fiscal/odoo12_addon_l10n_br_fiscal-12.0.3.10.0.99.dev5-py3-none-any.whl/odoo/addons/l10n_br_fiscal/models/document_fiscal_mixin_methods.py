294bc06404696763a8f3ec59918ca7408c38514e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:16 +0000

    Add package odoo12-addon-l10n-br-fiscal/12.0.3.10.0.99.dev5/odoo12_addon_l10n_br_fiscal-12.0.3.10.0.99.dev5-py3-none-any.whl

---
+++ packages/odoo12-addon-l10n-br-fiscal/odoo12_addon_l10n_br_fiscal-12.0.3.10.0.99.dev5-py3-none-any.whl/odoo/addons/l10n_br_fiscal/models/document_fiscal_mixin_methods.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2019  Renato Lima - Akretion <renato.lima@akretion.com.br>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import api, models
+
+
+class FiscalDocumentMixinMethods(models.AbstractModel):
+    _name = 'l10n_br_fiscal.document.mixin.methods'
+    _description = 'Document Fiscal Mixin Methods'
+
+    @api.multi
+    def _prepare_br_fiscal_dict(self, default=False):
+        self.ensure_one()
+        fields = self.env["l10n_br_fiscal.document.mixin"]._fields.keys()
+
+        # we now read the record fiscal fields except the m2m tax:
+        vals = self._convert_to_write(self.read(fields)[0])
+
+        # remove id field to avoid conflicts
+        vals.pop('id', None)
+
+        # this will force to create a new fiscal document line:
+        vals['fiscal_document_id'] = False
+
+        if default:  # in case you want to use new rather than write later
+            return {"default_%s" % (k,): vals[k] for k in vals.keys()}
+        return vals
+
+    @api.onchange('fiscal_operation_id')
+    def _onchange_fiscal_operation_id(self):
+        if self.fiscal_operation_id:
+            self.operation_name = self.fiscal_operation_id.name
+            self.comment_ids = self.fiscal_operation_id.comment_ids
