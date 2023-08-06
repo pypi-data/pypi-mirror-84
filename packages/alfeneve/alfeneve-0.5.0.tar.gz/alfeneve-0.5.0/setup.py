73e8b9b19634bbe95dd2f46abfc07cd9fec47cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:21 +0000

    Add package alfeneve/0.5.0/alfeneve-0.5.0.tar.gz

---
+++ packages/alfeneve/alfeneve-0.5.0.tar.gz/alfeneve-0.5.0/setup.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2019 Nick Douma <n.douma@nekoconeko.nl>
+#
+# This file is part of alfeneve .
+#
+# Licensed under the terms of the MIT license, see the
+# LICENSE file in the root of the project.
+
+from setuptools import setup
+
+setup(
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
+)
---
+++ packages/alfeneve/alfeneve-0.5.0.tar.gz/alfeneve-0.5.0/tests/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2019 Nick Douma <n.douma@nekoconeko.nl>
+#
+# This file is part of alfeneve .
+#
+# Licensed under the terms of the MIT license, see the
+# LICENSE file in the root of the project.
