1be49db43e2765388a98980124edafc9cb50dee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:55 +0000

    Add package gamma-facet/1.0.0rc2/gamma-facet-1.0.0rc2.tar.gz

---
+++ packages/gamma-facet/gamma-facet-1.0.0rc2.tar.gz/gamma-facet-1.0.0rc2/src/facet/__init__.py
@@ -0,0 +1,6 @@
+"""
+License: Apache Software License v2.0
+"""
+
+
+__version__ = "1.0.0rc2"
---
+++ packages/gamma-facet/gamma-facet-1.0.0rc2.tar.gz/gamma-facet-1.0.0rc2/src/facet/simulation/__init__.py
@@ -0,0 +1,4 @@
+"""
+Univariate simulations of changes to model outputs based on changes to the inputs.
+"""
+from ._simulation import *
