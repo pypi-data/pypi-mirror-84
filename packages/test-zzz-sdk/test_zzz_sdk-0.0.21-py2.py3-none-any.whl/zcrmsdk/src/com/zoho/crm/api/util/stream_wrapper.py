cd902a4484d54c4e242cba5c883d139a1c831318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:59 +0000

    Add package test-zzz-sdk/0.0.21/test_zzz_sdk-0.0.21-py2.py3-none-any.whl

---
+++ packages/test-zzz-sdk/test_zzz_sdk-0.0.21-py2.py3-none-any.whl/zcrmsdk/src/com/zoho/crm/api/util/stream_wrapper.py
@@ -0,0 +1,49 @@
+import os
+
+
+class StreamWrapper(object):
+
+    """
+    This class handles the file stream and name.
+    """
+
+    def __init__(self, name=None, stream=None, file_path=None):
+
+        """
+        Creates a StreamWrapper class instance with the specified parameters.
+
+        Parameters:
+            name (str) : A string containing the file name.
+            stream (stream) : A stream containing the file stream.
+            file_path (str) : A string containing the absolute file path.
+        """
+
+        if file_path is not None:
+            self.__name = os.path.basename(file_path)
+            self.__stream = open(file_path, 'rb')
+
+        else:
+            self.__name = name
+            self.__stream = stream
+
+    def get_name(self):
+
+        """
+        This is a getter method to get the file name.
+
+        Returns:
+            string : A string representing the file name.
+        """
+
+        return self.__name
+
+    def get_stream(self):
+
+        """
+        This is a getter method to get the file input stream.
+
+        Returns:
+            stream :  A stream representing the file stream.
+        """
+
+        return self.__stream
