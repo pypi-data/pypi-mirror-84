84eca372fd5b028a0ffe9c88e2ac0307a8ce2613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:40 +0000

    Add package poppy-torso/3.0.0/poppy-torso-3.0.0.tar.gz

---
+++ packages/poppy-torso/poppy-torso-3.0.0.tar.gz/poppy-torso-3.0.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import re
+import sys
+
+from setuptools import setup, find_packages
+
+def version():
+    with open('poppy_torso/_version.py') as f:
+        return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read()).group(1)
+
+setup(name='poppy-torso',
+      version=version(),
+      packages=find_packages(),
+
+      install_requires=['pypot >= 4.0.0'],
+
+      include_package_data=True,
+      exclude_package_data={'': ['README', '.gitignore']},
+
+      zip_safe=False,
+
+      author='Poppy Station',
+      author_email='dev@poppy-station.org',
+      description='Poppy Torso Software Library',
+      url='https://poppy-project.org',
+      license='GNU GENERAL PUBLIC LICENSE Version 3',
+
+      )
