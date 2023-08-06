49b20f950bc6d3f3e4fcd46e1f00d70f1bf9470a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:13 +0000

    Add package redbrick-sdk/0.2.7/redbrick_sdk-0.2.7-py3-none-any.whl

---
+++ packages/redbrick-sdk/redbrick_sdk-0.2.7-py3-none-any.whl/redbrick/entity/taxonomy.py
@@ -0,0 +1,10 @@
+from typing import List
+from dataclasses import dataclass
+
+
+@dataclass
+class TaxonomyEntry:
+    """A single entry to the taxonomy object."""
+
+    class_name: str
+    class_id: str
