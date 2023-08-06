6eb9d1161313ca0259f3865efb7bc36edc9690e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:05 +0000

    Add package SimpleGetArgs/0.0.2/SimpleGetArgs-0.0.2-py3-none-any.whl

---
+++ packages/SimpleGetArgs/SimpleGetArgs-0.0.2-py3-none-any.whl/GetArgs/ManagingArgs/Adding_Simple.py
@@ -0,0 +1,14 @@
+class AddSimple:
+    def __init__(self):
+        pass
+
+    def sayhello(self, name):
+        return f"Hello to you, {name}"
+        # return name
+
+    def print_till100(self):
+        lst = []
+        for i in range(1, 101):
+            lst.append(i)
+        return lst
+
