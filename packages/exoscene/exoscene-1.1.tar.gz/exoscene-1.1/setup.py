77e34f13be3cbeb4b960a4602a5a512958e0aa3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:46 +0000

    Add package exoscene/1.1/exoscene-1.1.tar.gz

---
+++ packages/exoscene/exoscene-1.1.tar.gz/exoscene-1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+setup(
+  name='exoscene',
+  version='1.1',
+  description='Library for simulating direct images of exoplanetary systems.',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/nasa/exoscene",
+  author='Neil T. Zimmerman',
+  author_email='neil.t.zimmerman@nasa.gov',
+
+  packages=['exoscene'],
+  package_data={'exoscene': ['./data/bpgs/bpgs_*.fits',
+                             './data/bpgs/bpgs_readme.csv',
+                             './data/cgi_hlc_psf/*.fits']},
+
+  classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: Apache Software License",
+  ],
+
+  python_requires='>=3.6',
+  install_requires=[
+      "astropy >= 4.0",
+      "numpy",
+      "scipy",
+      "scikit-image",
+      "pandas",
+      "ipython",
+      "matplotlib",
+      "notebook"
+  ],
+)
+
