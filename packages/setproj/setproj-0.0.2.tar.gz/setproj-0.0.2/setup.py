d33e39ae6c4bcc3b59e86f8d86c786e1229905af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:13 +0000

    Add package setproj/0.0.2/setproj-0.0.2.tar.gz

---
+++ packages/setproj/setproj-0.0.2.tar.gz/setproj-0.0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+from os import path
+
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
+
+setup(
+    name='setproj',
+    version='0.0.2',
+    packages=find_packages('src'),
+    description='create projects in cli',
+    author='Abhay Shanker Pathak',
+    author_email='abhaysp9955@gmail.com',
+    keywords="project programming language",
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",  # set if not reStructuredText
+    url="https://github.com/coolabhays/project-setup",
+    license="MIT",
+    package_dir={'': 'src'},
+    py_modules=["setproj"],
+    install_requires=[
+        'Click',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities",
+        "Topic :: Terminals",
+        "Operating System :: POSIX"
+    ],
+    entry_points={
+        'console_scripts': [
+            'setproj=src.app:main',
+        ],
+    },
+)
