622474181ff96479bbe837c83aaa824db6d1a8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:39 +0000

    Add package magicdb/0.2.82/magicdb-0.2.82.tar.gz

---
+++ packages/magicdb/magicdb-0.2.82.tar.gz/magicdb-0.2.82/magicdb/utils/async_helpers.py
@@ -0,0 +1,37 @@
+import concurrent.futures
+
+import asyncio
+from functools import wraps
+
+_DEFAULT_POOL = concurrent.futures.ThreadPoolExecutor()
+_DEFAULT_POOL._max_workers = max(_DEFAULT_POOL._max_workers, 20)
+
+
+def threadpool_asyncio(f, executor=None):
+    """Makes a sync function async by returning asyncio futures that can be awaited."""
+
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        future = (executor or _DEFAULT_POOL).submit(f, *args, **kwargs)
+        return asyncio.wrap_future(future)
+
+    return wrap
+
+
+def threadpool(f, executor=None):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        future = (executor or _DEFAULT_POOL).submit(f, *args, **kwargs)
+        return future
+
+    return wrap
+
+
+def promise(f, executor=None):
+    """Makes async function a task -- to be executed concurrently."""
+
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        return asyncio.create_task(f(*args, **kwargs))
+
+    return wrap
