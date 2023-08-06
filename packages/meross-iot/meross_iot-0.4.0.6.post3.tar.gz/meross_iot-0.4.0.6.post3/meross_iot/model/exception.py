57bc4d6406cbd20b7247e147c863a185d41d15c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:35 +0000

    Add package meross-iot/0.4.0.6.post3/meross_iot-0.4.0.6.post3.tar.gz

---
+++ packages/meross-iot/meross_iot-0.4.0.6.post3.tar.gz/meross_iot-0.4.0.6.post3/meross_iot/model/exception.py
@@ -0,0 +1,16 @@
+class UnconnectedError(Exception):
+    pass
+
+
+class CommandTimeoutError(Exception):
+    pass
+
+
+class CommandError(Exception):
+    def __init__(self, error_payload: dict):
+        super().__init__()
+        self.error_payload = error_payload
+
+
+class RateLimitExceeded(Exception):
+    pass
