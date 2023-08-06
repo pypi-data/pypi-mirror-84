c0a25ecaf25cf12b690ff719b02b0c461e263480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:27 +0000

    Add package mkdocs-bootstrap4/0.1.5/mkdocs-bootstrap4-0.1.5.tar.gz

---
+++ packages/mkdocs-bootstrap4/mkdocs-bootstrap4-0.1.5.tar.gz/mkdocs-bootstrap4-0.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.5'
+
+setup(
+    name="mkdocs-bootstrap4",
+    version=VERSION,
+    url='https://github.com/byrnereese/mkdocs-bootstrap4/',
+    license='MIT',
+    description='Bootstrap4 theme for MkDocs',
+    author='Byrne Reese',
+    author_email='byrne@majordojo.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'mkdocs>=1.1',
+        'mkdocs-git-committers-plugin>=0.2.0'
+    ],
+    python_requires='>=2.7.9,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    entry_points={
+        'mkdocs.themes': [
+            'bootstrap4 = mkdocs_bootstrap4',
+        ]
+    },
+    zip_safe=False
+)
