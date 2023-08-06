deb58979fbc04e4099ce055f6bf15958a45cdb90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:03 +0000

    Add package satella/2.14.4/satella-2.14.4-py2.py3-none-any.whl

---
+++ packages/satella/satella-2.14.4-py2.py3-none-any.whl/satella/coding/decorators/__init__.py
@@ -0,0 +1,10 @@
+from .arguments import auto_adapt_to_methods, attach_arguments, for_argument, \
+    execute_before, copy_arguments, replace_argument_if
+from .decorators import wraps, chain_functions, has_keys, short_none, memoize
+from .flow_control import loop_while, queue_get
+from .preconditions import postcondition, precondition
+
+__all__ = ['execute_before', 'postcondition', 'precondition', 'wraps', 'queue_get',
+           'chain_functions', 'has_keys', 'short_none', 'auto_adapt_to_methods',
+           'attach_arguments', 'for_argument', 'loop_while', 'memoize',
+           'copy_arguments', 'replace_argument_if']
