27107d4b37644cfbcb4f8fd3ba1ef0a17dad612b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:21 +0000

    Add package ascat/1.1.2/ascat-1.1.2.tar.gz

---
+++ packages/ascat/ascat-1.1.2.tar.gz/ascat-1.1.2/src/ascat/__init__.py
@@ -0,0 +1,9 @@
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except:
+    __version__ = 'unknown'
+
+from ascat.read_native.cdr import *
+from ascat.h_saf import *
