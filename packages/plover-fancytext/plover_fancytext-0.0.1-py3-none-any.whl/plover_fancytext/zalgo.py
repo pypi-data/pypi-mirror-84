47d8f8a71d2430fbaed294f8a3ba55dd519fb2c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:16 +0000

    Add package plover-fancytext/0.0.1/plover_fancytext-0.0.1-py3-none-any.whl

---
+++ packages/plover-fancytext/plover_fancytext-0.0.1-py3-none-any.whl/plover_fancytext/zalgo.py
@@ -0,0 +1,16 @@
+import random
+
+from .character_helpers import COMBINING_MARKS
+
+
+class Zalgo():
+
+    def add_combining_marks(self, c: str) -> str:
+        for m in random.sample(COMBINING_MARKS, random.randrange(1, 3)):
+            c += m
+        return c
+
+    def __call__(self, str) -> str:
+        if str:
+            return ''.join(self.add_combining_marks(s) for s in str)
+        return None
