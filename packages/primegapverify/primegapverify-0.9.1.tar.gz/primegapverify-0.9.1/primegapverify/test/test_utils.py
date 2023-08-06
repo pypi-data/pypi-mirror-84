b076047abfb87aaf0ced3c322b14153103c61d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:31 +0000

    Add package primegapverify/0.9.1/primegapverify-0.9.1.tar.gz

---
+++ packages/primegapverify/primegapverify-0.9.1.tar.gz/primegapverify-0.9.1/primegapverify/test/test_utils.py
@@ -0,0 +1,88 @@
+# Copyright 2020 Seth Troisi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import parsenumber
+import utils
+
+def brute(s, g, mp):
+    return [n % 2 == 0 or any(n % p == 0 for p in range(3, mp+1, 2))
+        for n in range(s, s + g + 1)]
+
+
+def test_sieve():
+    for s, g, mp in (
+        (1001, 100, 10),
+        (1001, 1000, 10),
+        (1001, 100, 50),
+        (1001, 1000, 50),
+        (5, 1000, 3),
+   ):
+        expect = brute(s, g, mp)
+        result = utils.sieve(s, g, mp)
+        print (len(expect), len(result))
+        print([1 * v for v in expect])
+        print([1 * v for v in result])
+        assert expect == result
+
+
+def test_sieve_primepi():
+    # Easy to generate these with `primesieve <start> <start + gap>
+    for s, g, mp, expected in (
+        (101, 100, 20, 21),
+        (101, 100, 100, 21),
+        (1001, 1000, 3, 334),
+        (1001, 1000, 30, 151),
+        (1001, 1000, 50, 135),
+        (1000001, 10000, 1100, 753),
+    ):
+        assert expected == utils.sieve(s, g, mp).count(False)
+
+
+def test_validate():
+    for s, g in ((101, 2), (103, 4), (113, 14), (360653, 96),
+                 (18361375334787046697, 1550),
+                 (9691983639208775401081992556968666567067, 2982)):
+        assert utils.validate(s, g)
+        if g > 2:
+            assert not utils.validate(s, g - 2)
+        assert not utils.validate(s, g + 2)
+
+
+def test_check_pfgw_available():
+    assert utils.check_pfgw_available()
+
+
+NUM_STATUS = (
+        ("31# +1", True),
+        ("379# +1", True),
+        ("1019# +1", True),
+        ("2657# +1", True),
+        ("503# -617", False),
+        ("503# -631", False),
+        ("503# -659", True)
+)
+
+
+def test_is_prime_large():
+    for str_num, result in NUM_STATUS:
+        num = parsenumber.parse(str_num)
+        assert utils.is_prime_large(num) == result
+        assert utils.is_prime_large(num, str_num) == result
+
+
+def test__is_prime_pfgw():
+    for str_num, result in NUM_STATUS:
+        num = parsenumber.parse(str_num)
+        assert utils._is_prime_pfgw(num) == result
+        assert utils._is_prime_pfgw(str_num) == result
---
+++ packages/primegapverify/primegapverify-0.9.1.tar.gz/primegapverify-0.9.1/primegapverify/verify/sieve_util.cpp
@@ -0,0 +1,107 @@
+// Copyright 2020 Seth Troisi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* sieve_util.cpp
+ * [1] See math in next_prime.c in gmp-lib (My first pass at this logic)
+ *    https://gmplib.org/list-archives/gmp-devel/2020-March/005767.html
+ *    https://gmplib.org/repo/gmp-6.2/file/tip/mpz/nextprime.c
+ */
+
+#include "sieve_util.hpp"
+#include "primes.hpp"
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+
+#include <gmp.h>
+
+namespace sieve_util {
+
+    // Similiar math to mpz/nextprime.c
+    uint64_t calculate_sievelimit(double n_bits, double gap) {
+        // Average gap = bits * ln(2)
+        double adj = gap / (n_bits * log(2));
+
+        double limit = adj * pow(1.0 * n_bits, 2.5) / 124.0;
+        if (limit > MAX_LIMIT) {
+            fprintf(stderr, "LARGE LIMIT=%.2e\n", limit);
+            limit = MAX_LIMIT;
+        }
+        if (limit <= 0) {
+            // impossible(?) but be safe
+            limit = 1;
+        }
+        return limit;
+    }
+
+
+    std::vector<char> sieve(mpz_t &N, uint64_t gap, uint64_t limit, size_t &prime_count) {
+        // Sieve
+        std::vector<char> composite(gap + 1, 0);
+
+        // Handle all evens
+        uint64_t first = mpz_cdiv_ui(N, 2);
+        for (uint32_t d = first; d <= gap; d += 2) {
+            composite[d] = true;
+        }
+
+        prime_count = 1;
+        primes::iterator iter;
+        uint64_t prime = iter.next();
+        assert(prime == 2);  // Skip  2
+        prime = iter.next();
+        assert(prime == 3);
+
+        // small primes can divide multiple numbers
+        for (; prime <= gap && prime <= limit; prime = iter.next()) {
+            prime_count++;
+            uint64_t first = mpz_cdiv_ui(N, prime);
+            if (first % 2 == 1) {
+                first += prime;
+            }
+
+            for (uint32_t d = first; d <= gap; d += 2 * prime) {
+                composite[d] = true;
+            }
+        }
+
+        // TODO make sure limit * limit < N
+
+        /* Think about returning this for debug or something
+        size_t unknowns = std::count(composite.begin(), composite.end(), false);
+        size_t count_c = gap - unknowns;
+        fprintf(stderr, "%ld / %ld = %.2f composite, %ld remaining (primes %ld)\n",
+                gap - unknowns, gap, 100.0 * count_c / gap, unknowns, prime_count);
+        */
+
+        for (; prime <= limit; prime = iter.next()) {
+            prime_count++;
+            uint64_t first = mpz_cdiv_ui(N, prime);
+            if ((first < gap) && ((first & 1) == 0)) {
+                composite[first / 2] = true;
+            }
+        }
+
+        //assert(composite[0] == false);
+        //assert(composite[gap] == false);
+
+        // Possible a vector copy, but fast in the overall scheme of things.
+        return composite;
+    }
+}  // namespace sieve_util
