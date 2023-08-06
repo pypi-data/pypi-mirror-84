468d4a23c3f5311b0df087e0c631b48d9f24e6f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:58 +0000

    Add package duckietown-challenges-runner-daffy/6.0.59/duckietown-challenges-runner-daffy-6.0.59.tar.gz

---
+++ packages/duckietown-challenges-runner-daffy/duckietown-challenges-runner-daffy-6.0.59.tar.gz/duckietown-challenges-runner-daffy-6.0.59/src/duckietown_challenges_runner/logging.py
@@ -0,0 +1,8 @@
+from zuper_commons.logs import ZLogger
+
+elogger = logger = ZLogger("evaluator")
+
+# logging.basicConfig()
+#
+# elogger = logging.getLogger("evaluator")
+# elogger.setLevel(logging.DEBUG)
