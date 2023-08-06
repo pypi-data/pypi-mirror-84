7a4ff6610947a4d46137f5280474420357142573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:28 +0000

    Add package python-xlsxio/0.1.3/python_xlsxio-0.1.3-cp37-cp37m-win_amd64.whl

---
+++ packages/python-xlsxio/python_xlsxio-0.1.3-cp37-cp37m-win_amd64.whl/xlsxio/__init__.py
@@ -0,0 +1,16 @@
+from ._xlsxio import (  # noqa:F401
+    get_xlsxioread_version_string,
+    XlsxioReadFlag,
+    XlsxioReaderSheetList,
+    XlsxioReaderSheet,
+    XlsxioReader,
+)
+
+__version__ = '0.1.3'
+__all__ = (
+    'get_xlsxioread_version_string',
+    'XlsxioReadFlag',
+    'XlsxioReaderSheetList',
+    'XlsxioReaderSheet',
+    'XlsxioReader',
+)
