a99c39313f7530758e2b0297f7ec1b5e4aa7a172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:23 +0000

    Add package reliability/0.5.4/reliability-0.5.4.tar.gz

---
+++ packages/reliability/reliability-0.5.4.tar.gz/reliability-0.5.4/reliability/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# pylint: skip-file
+
+from reliability import Distributions
+from reliability import Fitters
+from reliability import Nonparametric
+from reliability import Other_functions
+from reliability import Reliability_testing
+from reliability import Repairable_systems
+from reliability import Stress_strength
+from reliability import Probability_plotting
+from reliability import Datasets
+from reliability import ALT_fitters
+from reliability import ALT_probability_plotting
+from reliability import PoF
+from reliability import Utils
+
+__version__ = '0.5.4' #Current version
