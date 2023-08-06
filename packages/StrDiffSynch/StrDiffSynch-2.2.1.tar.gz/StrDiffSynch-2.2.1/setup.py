c25181984a6ecbb136b7f63422bf0700769902b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:37 +0000

    Add package StrDiffSynch/2.2.1/StrDiffSynch-2.2.1.tar.gz

---
+++ packages/StrDiffSynch/StrDiffSynch-2.2.1.tar.gz/StrDiffSynch-2.2.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="StrDiffSynch",
+    version="2.2.1",
+    author="Antas",
+    author_email="",
+    description="One of two strings can change into the other when absorbing the difference among them. "
+                "Remote strings could be synchronized through minimum data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/monk-after-90s/StrDiffSynch.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
