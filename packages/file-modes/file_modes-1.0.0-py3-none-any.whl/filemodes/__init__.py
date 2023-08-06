801c1f67db07b7e27bff73896820514f18b252fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:33 +0000

    Add package file-modes/1.0.0/file_modes-1.0.0-py3-none-any.whl

---
+++ packages/file-modes/file_modes-1.0.0-py3-none-any.whl/filemodes/__init__.py
@@ -0,0 +1,14 @@
+### IMPORTS                 ###
+    ## Dependencies             ##
+    ## Dependencies             ##
+### IMPORTS                 ###
+### CONSTANTS               ###
+READ, READB, READPS, READBPS            = 'r', 'rb', 'r+', 'rb+'
+WRITE, WRITEB, WRITEPS, WRITEBPS        = 'w', 'wb', 'w+', 'wb+'
+APPEND, APPENDB, APPENDPS, APPENDBPS    = 'a', 'ab', 'a+', 'ab+'
+### CONSTANTS               ###
+### DOCUMENTATION           ###
+"""
+File containing all valid python file modes
+"""
+### DOCUMENTATION           ###
