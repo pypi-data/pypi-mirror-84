5530971f5f29d8355e6601d215998dba1a826ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:01 +0000

    Add package django-esteid/3.0a1/django-esteid-3.0a1.tar.gz

---
+++ packages/django-esteid/django-esteid-3.0a1.tar.gz/django-esteid-3.0a1/esteid/compat.py
@@ -0,0 +1,79 @@
+"""
+Compatibility utilities that provide signed container info
+in the same format as the DDS service did.
+"""
+import base64
+import hashlib
+
+from pyasice import Container, XmlSignature
+
+from esteid.types import ResponderCertificate, SignedDocInfo
+
+
+def signature_info(xml_signature: XmlSignature):
+    subject_cert = xml_signature.get_certificate()
+    cert_asn1 = subject_cert.asn1
+    personal = cert_asn1.subject.native
+    validity = cert_asn1.native["tbs_certificate"]["validity"]
+    signing_time = xml_signature.get_signing_time()
+
+    return {
+        "id": xml_signature._get_node("ds:Signature").attrib["Id"],
+        "signing_time": signing_time,
+        "status": True,
+        "signature_production_place": None,
+        "signer": {
+            "certificate": {
+                "issuer": xml_signature._get_node("xades:SigningCertificate//ds:X509IssuerName").text,
+                "issuer_serial": xml_signature._get_node("xades:SigningCertificate//ds:X509SerialNumber").text,
+                "policies": [],
+                "subject": personal["common_name"],
+                "valid_from": validity["not_before"].strftime("%Y-%m-%dT%H:%M:%SZ"),
+                "valid_to": validity["not_after"].strftime("%Y-%m-%dT%H:%M:%SZ"),
+            },
+            "full_name": "%s %s" % (personal["given_name"], personal["surname"]),
+            "id_code": personal["serial_number"].split("-")[-1],
+        },
+        "confirmation": {
+            # if ever needed
+            "produced_at": signing_time,
+            "responder_id": "OCSP",
+            # TODO: create an API to extract the data from xml_signature and cover by tests.
+            # Currently it is accessible as a wrapped asn1crypto.x509.Certificate by smth like:
+            # xml_signature.get_responder_certs()[0].native['tbs_certificate']
+            # individual fields: ocsp_cert['issuer'] etc
+            "responder_certificate": ResponderCertificate(
+                issuer="", valid_from="", valid_to="", issuer_serial="", subject=""
+            ),
+        },
+        "signer_role": [{"certified": 0, "role": None}],
+    }
+
+
+def container_info(bdoc_container: Container, full=True):
+    if full:
+        data_files_info = [
+            {
+                "digest_type": "sha256",
+                "digest_value": base64.b64encode(hashlib.sha256(content).digest()).decode(),
+                "filename": name,
+                "id": name,
+                "mime_type": mimetype,
+                "content_type": mimetype,
+                "size": len(content),
+            }
+            for name, content, mimetype in bdoc_container.iter_data_files()
+        ]
+    else:
+        data_files_info = [
+            {
+                "filename": name,
+            }
+            for name in bdoc_container.data_file_names
+        ]
+    return SignedDocInfo.from_dict(
+        {
+            "data_file_info": data_files_info,
+            "signature_info": [signature_info(signature) for signature in bdoc_container.iter_signatures()],
+        }
+    )
