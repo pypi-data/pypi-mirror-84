56ca6975a50afd98d128ede5859689bc2b846541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:26 +0000

    Add package example-pkg-zqj/0.0.2/example-pkg-zqj-0.0.2.tar.gz

---
+++ packages/example-pkg-zqj/example-pkg-zqj-0.0.2.tar.gz/example-pkg-zqj-0.0.2/setup.py
@@ -0,0 +1,35 @@
+#!/GPFS/zhangli_lab_permanent/zhuqingjie/env/py3_tf2/bin/python
+'''
+@Time    : 20/11/05 下午 01:56
+@Author  : zhuqingjie 
+@User    : zhu
+@FileName: setup.py
+@Software: PyCharm
+'''
+import setuptools
+'''
+发布自己的Python库官方教程：
+https://packaging.python.org/tutorials/packaging-projects/
+'''
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-zqj",  # Replace with your own username
+    version="0.0.2",
+    author="zqj",
+    author_email="zhu.qingjie@qq.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
