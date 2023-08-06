12c51ec4a1ddd45373e789c600bc418401f6ec6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:10 +0000

    Add package agro-kit/1.0.0/agro_kit-1.0.0-py2.py3-none-any.whl

---
+++ packages/agro-kit/agro_kit-1.0.0-py2.py3-none-any.whl/agro_kit/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for Agrotech Sensor Kit API."""
+
+from agro_kit.agro_kit import AgroKit, Reading
+
+__author__ = """Zidaan Habib, Fraser Montandon"""
+__email__ = 'hbbzid001@myuct.ac.za'
+
+__version__ = '1.0.0'
