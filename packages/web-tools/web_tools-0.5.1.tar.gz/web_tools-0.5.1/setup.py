9790c9dc965667665fd4a076f3b11a583c39da0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:18 +0000

    Add package web-tools/0.5.1/web_tools-0.5.1.tar.gz

---
+++ packages/web-tools/web_tools-0.5.1.tar.gz/web_tools-0.5.1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['web_tools', 'web_tools.fastapi', 'web_tools.fastapi.request_log']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['SQLAlchemy-JSONField>=0.9.0,<0.10.0',
+ 'fastapi>=0.61.1,<0.62.0',
+ 'gino>=1.0.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'web-tools',
+    'version': '0.5.1',
+    'description': '',
+    'long_description': None,
+    'author': 'rgilfanov',
+    'author_email': 'rgilfanov@fix.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/web-tools/web_tools-0.5.1.tar.gz/web_tools-0.5.1/src/web_tools/fastapi/request_log/middleware.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+import datetime
+from typing import Optional, Type
+import traceback
+
+from fastapi import Request, Response
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+
+from .context import LogContext
+from .model_mixin import SaveLogMixin
+
+
+class LogMiddleware(BaseHTTPMiddleware):
+    request_patched: bool = False
+
+    save_model_class: Optional[Type[SaveLogMixin]]
+
+    @classmethod
+    def patch_request_body(cls):
+        if cls.request_patched:
+            return
+
+        body_original = Request.body
+
+        async def body_patched(request: Request):
+            body_content_key = 'body_content'
+            body_content = request.scope.get(body_content_key)
+            if body_content:
+                return body_content
+            body_content = await body_original(request)
+            request.scope[body_content_key] = body_content
+            return body_content
+
+        Request.body = body_patched
+        cls.request_patched = True
+
+    def __init__(self, save_model_class: Optional[Type[SaveLogMixin]], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.save_model_class = save_model_class
+        self.patch_request_body()
+
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        request_datetime = datetime.datetime.utcnow()
+        exception_traceback = None
+        response = None
+        try:
+            response = await call_next(request)
+            return response
+        except Exception:
+            exception_traceback = traceback.format_exc()
+            raise
+        finally:
+            response_datetime = datetime.datetime.utcnow()
+            log_context = await LogContext.create(request_datetime, request, response_datetime,
+                                                  response=response, exception_traceback=exception_traceback)
+            if self.save_model_class:
+                await self.save_model_class.save_log(log_context, request=request, response=response)
