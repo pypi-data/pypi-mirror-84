e2540d9160c04c2f7c877ec262db9740e0bd5534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:02 +0000

    Add package invenio-app-rdm/0.16.2/invenio_app_rdm-0.16.2-py2.py3-none-any.whl

---
+++ packages/invenio-app-rdm/invenio_app_rdm-0.16.2-py2.py3-none-any.whl/invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/deposits/search/index.js
@@ -0,0 +1,25 @@
+/*
+ * This file is part of Invenio.
+ * Copyright (C) 2020 CERN.
+ *
+ * Invenio is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT License; see LICENSE file for more details.
+ */
+
+import { createSearchAppInit } from "@js/invenio_search_ui";
+import {
+  RDMRecordResultsListItem,
+  RDMRecordResultsGridItem,
+  RDMDepositResults,
+  RDMEmptyResults,
+} from "./components";
+import { RDMRecordSearchBarElement } from "../../search/components";
+
+const initSearchApp = createSearchAppInit({
+  "ResultsList.item": RDMRecordResultsListItem,
+  "ResultsGrid.item": RDMRecordResultsGridItem,
+  "SearchApp.results": RDMDepositResults,
+  "SearchBar.element": RDMRecordSearchBarElement,
+  "SearchApp.searchbar": () => null,
+  "EmptyResults.element": RDMEmptyResults,
+});
---
+++ packages/invenio-app-rdm/invenio_app_rdm-0.16.2-py2.py3-none-any.whl/invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/search/index.js
@@ -0,0 +1,27 @@
+// This file is part of InvenioRDM
+// Copyright (C) 2020 CERN.
+// Copyright (C) 2020 Northwestern University.
+//
+// Invenio App RDM is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import { createSearchAppInit } from "@js/invenio_search_ui";
+import {
+  RDMRecordResultsListItem,
+  RDMRecordResultsGridItem,
+  RDMRecordSearchBarElement,
+  RDMRecordFacetsValues,
+  RDMRecordFacets,
+  RDMBucketAggregationElement,
+  RDMToggleComponent,
+} from "./components";
+
+const initSearchApp = createSearchAppInit({
+  "ResultsList.item": RDMRecordResultsListItem,
+  "ResultsGrid.item": RDMRecordResultsGridItem,
+  "SearchBar.element": RDMRecordSearchBarElement,
+  "BucketAggregationValues.element": RDMRecordFacetsValues,
+  "SearchApp.facets": RDMRecordFacets,
+  "BucketAggregation.element":RDMBucketAggregationElement,
+  'SearchFilters.ToggleComponent':RDMToggleComponent,
+});
