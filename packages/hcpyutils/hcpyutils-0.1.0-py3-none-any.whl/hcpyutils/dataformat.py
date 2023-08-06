3296deab2b6e4411ef10df86e89c98e09ff3a30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:09 +0000

    Add package hcpyutils/0.1.0/hcpyutils-0.1.0-py3-none-any.whl

---
+++ packages/hcpyutils/hcpyutils-0.1.0-py3-none-any.whl/hcpyutils/dataformat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 hcyjs.com, Inc. All Rights Reserved
+
+"""
+    @Time : 2020-11-02 16:03
+    @Author : Yin Jian
+    @Version：V 0.1
+    @File : dataformat.py
+    @desc :
+"""
+import time
+
+print("data_format")
+
+
+def print_date():
+    print(time.time())
