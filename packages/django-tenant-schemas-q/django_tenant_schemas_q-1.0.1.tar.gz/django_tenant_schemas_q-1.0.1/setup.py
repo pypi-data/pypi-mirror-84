5306dd9f456f02a5eeb2bf7a40e795034434b086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:58 +0000

    Add package django-tenant-schemas-q/1.0.1/django_tenant_schemas_q-1.0.1.tar.gz

---
+++ packages/django-tenant-schemas-q/django_tenant_schemas_q-1.0.1.tar.gz/django_tenant_schemas_q-1.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='django_tenant_schemas_q',
+    version='1.0.1',
+    author='Rajesh Yogeshwar',
+    author_email='rajesh.yogeshwar@gmail.com',
+    include_package_data=True,
+    description='An integration package for Django-Q with Django Tenant Schemas',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rajeshyogeshwar/django-tenant-schemas-q',
+    license='MIT',
+    packages=setuptools.find_packages(exclude=["test_project", "test-compose.yml"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "django-tenant-schemas>=1.10.0",
+        "django-q>=1.2.1",
+        "croniter==0.3.36"
+    ]
+)
