524d94377f9eb383424f1e1b26970dde9da73b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:01 +0000

    Add package arithmaticpy/0.1.2/arithmaticpy-0.1.2-py3-none-any.whl

---
+++ packages/arithmaticpy/arithmaticpy-0.1.2-py3-none-any.whl/arithmaticpy/vlad.py
@@ -0,0 +1,14 @@
+def add(arg1: float, arg2: float) -> float:
+    """
+    
+    Simple function to add two numbers (float) together
+    All arguments must be of integers or float.
+    :param arg1: first float value
+    :param arg2: second float value
+    :return: sum of the two arguments
+
+    """
+    
+    # get two numbers and add them
+    total = arg1 + arg2
+    return total
