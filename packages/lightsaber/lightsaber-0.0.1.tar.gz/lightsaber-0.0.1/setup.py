f9b0cd46b4c4c0d54c322b4d2c5a8350a065af6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:43 +0000

    Add package lightsaber/0.0.1/lightsaber-0.0.1.tar.gz

---
+++ packages/lightsaber/lightsaber-0.0.1.tar.gz/lightsaber-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='lightsaber',
+    version='0.0.1',
+    description='training and deploying machine learning models',
+    license='Apache 2.0',
+)
+
+
