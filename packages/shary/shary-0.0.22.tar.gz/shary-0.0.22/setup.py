e1aef4f867f05ff964744574a510f208fbc5342f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:17 +0000

    Add package shary/0.0.22/shary-0.0.22.tar.gz

---
+++ packages/shary/shary-0.0.22.tar.gz/shary-0.0.22/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+  _long_description = f.read()
+
+
+setup(
+  name = 'shary',
+  packages = ['shary', 'shary.services'],
+  entry_points = {
+    "console_scripts": [
+      "shary = shary.cli:cli"
+    ]
+  },
+  version = '0.0.22',
+  license='GNU General Public License Version 3',
+  description = 'API Wrapper around several File Hosting Services',
+  long_description = _long_description,
+  long_description_content_type='text/markdown',
+  author = 'Aaron Levi Can (aaronlyy)',
+  url = 'https://github.com/aaronlyy/shary',
+  download_url = 'https://github.com/aaronlyy/levish/archive/v0.0.22.tar.gz',
+  keywords = ['api', 'wrapper', 'files', 'hosting', 'filehosting'],
+  install_requires=[
+        "requests",
+        "click"
+      ],
+)
