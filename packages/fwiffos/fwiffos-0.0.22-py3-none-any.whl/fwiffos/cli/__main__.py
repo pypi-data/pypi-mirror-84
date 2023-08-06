46efd05f054313a4f5173d0c51144e1e3e11240d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:23 +0000

    Add package fwiffos/0.0.22/fwiffos-0.0.22-py3-none-any.whl

---
+++ packages/fwiffos/fwiffos-0.0.22-py3-none-any.whl/fwiffos/cli/__main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from fwiffos.cli.shell import commands
+import fire
+
+def main():
+    fire.Fire(commands)
---
+++ packages/fwiffos/fwiffos-0.0.22-py3-none-any.whl/fwiffos/cli/sudo.py
@@ -0,0 +1,9 @@
+class LetUs(object):
+    """
+    sudo-like behavior. fwiffo asks to write an audit record on our behalf
+    """
+    def __init__(self):
+        pass
+
+    def letus(self):
+        return "pass"
