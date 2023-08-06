5df1c24b942c01a1e4e768e42fe4b15eb26fceda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:31 +0000

    Add package seedweed/1.0rc4/seedweed-1.0rc4.tar.gz

---
+++ packages/seedweed/seedweed-1.0rc4.tar.gz/seedweed-1.0rc4/seedweed/__init__.py
@@ -0,0 +1,10 @@
+"""Seeded WebAuthn Credentials: reference implementation"""
+
+__version__ = "1.0rc4"
+
+from . import conformance, reference, vectors  # noqa: F401
+from .reference import *  # noqa: F401,F403
+
+
+def load_test_vectors():
+    return vectors.load()
