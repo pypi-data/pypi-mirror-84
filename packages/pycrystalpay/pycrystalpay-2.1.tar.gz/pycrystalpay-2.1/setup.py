c1acec2051ed0ba573fe0cac271cc60cc770bf5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:31 +0000

    Add package pycrystalpay/2.1/pycrystalpay-2.1.tar.gz

---
+++ packages/pycrystalpay/pycrystalpay-2.1.tar.gz/pycrystalpay-2.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pycrystalpay',
+      version='2.1',
+      description='Lib for crystalpy.',
+      packages=['pycrystalpay'],
+      url="https://github.com/seraphineRU/pyCrystalPay/",
+      author_email='seraphineru@mail.ru',
+      zip_safe=False)
