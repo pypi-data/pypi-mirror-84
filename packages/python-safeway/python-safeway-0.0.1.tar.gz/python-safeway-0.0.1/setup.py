997ac4a96f1cae586bc6f40b974f4fcd57a3292d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:58 +0000

    Add package python-safeway/0.0.1/python-safeway-0.0.1.tar.gz

---
+++ packages/python-safeway/python-safeway-0.0.1.tar.gz/python-safeway-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+        name='python-safeway',
+        version='0.0.1',
+        author='3point14thon',
+        author_email='chris.alan.bowers@gmail.com',
+        description='a safeway.com python API',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/3point14thon/python-safeway',
+        packages=setuptools.find_packages(),
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Topic :: Utilities'
+        ],
+        python_requires='>=3.6',
+)
