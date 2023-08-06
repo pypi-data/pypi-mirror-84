9248f74ca1ee7f90884b74b94279959b359296a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:05 +0000

    Add package dnb/0.0.1/dnb-0.0.1.tar.gz

---
+++ packages/dnb/dnb-0.0.1.tar.gz/dnb-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dnb',
+      version='0.0.1',
+      description='D&B Direct API',
+      author='Bülent Aldemir',
+      author_email='buelent@e-evolution.de',
+      license='MIT',
+      packages=['dnb'],
+      zip_safe=False)
