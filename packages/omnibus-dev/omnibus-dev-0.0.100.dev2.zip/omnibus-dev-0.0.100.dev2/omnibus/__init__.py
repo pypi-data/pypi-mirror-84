0e266e91ff87b466ee5e24274e97a29b56fac8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:39 +0000

    Add package omnibus-dev/0.0.100.dev2/omnibus-dev-0.0.100.dev2.zip

---
+++ packages/omnibus-dev/omnibus-dev-0.0.100.dev2.zip/omnibus-dev-0.0.100.dev2/omnibus/__init__.py
@@ -0,0 +1,23 @@
+import sys
+
+
+if not sys.version_info >= (3, 7):
+    raise EnvironmentError
+
+
+import pkgutil
+__path__ = pkgutil.extend_path(__path__, __name__)  # type: ignore
+
+
+def _test_install():
+    try:
+        from . import conftest  # noqa
+    except ImportError:
+        pass
+    else:
+        raise EnvironmentError
+
+    from . import revision
+    rev = revision.get_revision()
+    if not isinstance(rev, str) or len(rev) != 40:
+        raise ValueError(rev)
