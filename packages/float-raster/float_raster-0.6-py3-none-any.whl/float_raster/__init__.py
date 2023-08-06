50f99728748c69247f8c0528736288fdd49aae4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:24 +0000

    Add package float-raster/0.6/float_raster-0.6-py3-none-any.whl

---
+++ packages/float-raster/float_raster-0.6-py3-none-any.whl/float_raster/__init__.py
@@ -0,0 +1,11 @@
+"""
+Module for rasterizing polygons, with float-precision anti-aliasing on
+ a non-uniform rectangular grid.
+
+See the documentation for float_raster.raster(...) for details.
+"""
+
+from .VERSION import __version__
+from .float_raster import *
+
+__author__ = 'Jan Petykiewicz'
