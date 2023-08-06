5bbc3304caa0bf9a71401048c16c9ce3ac51046f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:18 +0000

    Add package mhkit/0.3.1/mhkit-0.3.1.tar.gz

---
+++ packages/mhkit/mhkit-0.3.1.tar.gz/mhkit-0.3.1/mhkit/__init__.py
@@ -0,0 +1,26 @@
+from mhkit import wave
+from mhkit import river
+from mhkit import tidal
+from mhkit import qc
+from mhkit import utils
+from mhkit import power
+from mhkit import loads
+
+# Register datetime converter for a matplotlib plotting methods
+from pandas.plotting import register_matplotlib_converters as _rmc
+_rmc()
+
+# Ignore future warnings
+import warnings as _warn
+_warn.simplefilter(action='ignore', category=FutureWarning)
+
+__version__ = 'v0.3.1'
+
+__copyright__ = """
+Copyright 2019, Alliance for Sustainable Energy, LLC under the terms of 
+Contract DE-AC36-08GO28308, Battelle Memorial Institute under the terms of 
+Contract DE-AC05-76RL01830, and National Technology & Engineering Solutions of 
+Sandia, LLC under the terms of Contract DE-NA0003525. The U.S. Government 
+retains certain rights in this software."""
+
+__license__ = "Revised BSD License"
---
+++ packages/mhkit/mhkit-0.3.1.tar.gz/mhkit-0.3.1/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+from distutils.core import Extension
+import os
+import re
+
+DISTNAME = 'mhkit'
+PACKAGES = find_packages()
+EXTENSIONS = []
+DESCRIPTION = 'Marine and Hydrokinetic Toolkit'
+AUTHOR = 'MHKiT developers'
+MAINTAINER_EMAIL = ''
+LICENSE = 'Revised BSD'
+URL = 'https://github.com/MHKiT-Software/mhkit-python'
+CLASSIFIERS=['Development Status :: 3 - Alpha',
+             'Programming Language :: Python :: 3',
+             'Topic :: Scientific/Engineering',
+             'Intended Audience :: Science/Research',
+             'Operating System :: OS Independent',
+            ]
+DEPENDENCIES = ['pandas>=1.0.0', 
+                'numpy', 
+                'scipy',
+                'matplotlib', 
+                'requests', 
+                'pecos>=0.1.9',
+                'fatpack',
+                'lxml',
+                'scikit-learn']
+
+# use README file as the long description
+file_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(file_dir, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+# get version from __init__.py
+with open(os.path.join(file_dir, 'mhkit', '__init__.py')) as f:
+    version_file = f.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        VERSION = version_match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string.")
+        
+setup(name=DISTNAME,
+      version=VERSION,
+      packages=PACKAGES,
+      ext_modules=EXTENSIONS,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author=AUTHOR,
+      maintainer_email=MAINTAINER_EMAIL,
+      license=LICENSE,
+      url=URL,
+      classifiers=CLASSIFIERS,
+      zip_safe=False,
+      install_requires=DEPENDENCIES,
+      scripts=[],
+      include_package_data=True
+  )
