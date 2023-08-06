60cc9909ed65f4d2edb92f1a4e8ed2ffff653e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:18 +0000

    Add package hydrafloods/0.2.0/hydrafloods-0.2.0-py3-none-any.whl

---
+++ packages/hydrafloods/hydrafloods-0.2.0-py3-none-any.whl/hydrafloods/__init__.py
@@ -0,0 +1,7 @@
+from hydrafloods.datasets import *
+from hydrafloods.geeutils import *
+from hydrafloods.thresholding import *
+from hydrafloods.filtering import *
+# from hydrafloods import *
+
+__version__ = "0.2.0"
