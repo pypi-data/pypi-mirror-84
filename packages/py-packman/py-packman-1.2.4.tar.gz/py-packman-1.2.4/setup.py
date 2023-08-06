8a8cb6c0e9daa2366ede0e3fd0e193b07eeba34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:57 +0000

    Add package py-packman/1.2.4/py-packman-1.2.4.tar.gz

---
+++ packages/py-packman/py-packman-1.2.4.tar.gz/py-packman-1.2.4/setup.py
@@ -0,0 +1,44 @@
+import packman
+from setuptools import setup
+
+PACKAGES=['packman',
+          'packman.molecule',
+          'packman.anm',
+          'packman.apps',
+          'packman.bin',
+          'packman.constants',
+          'packman.utilities'
+          ]
+
+
+SCRIPTS=['packman=packman.bin.PACKMAN:main']
+
+
+setup(name='py-packman',
+      version=packman.__version__,
+      description='A software package for molecular PACKing and Motion ANalysis (PACKMAN)',
+      url='https://github.com/Pranavkhade/PACKMAN',
+      author='Pranav Khade',
+      author_email='pranavk@iastate.edu',
+      license='MIT',
+      packages=PACKAGES,
+      keywords=('protein, dynamics, protein packing, protein domain, protein hinge'),
+      classifiers=[
+              'Intended Audience :: Education',
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: MIT License',
+              'Operating System :: MacOS',
+              'Operating System :: Microsoft :: Windows',
+              'Operating System :: POSIX',
+              'Programming Language :: Python',
+              'Programming Language :: Python :: 2',
+              'Programming Language :: Python :: 3',
+              'Topic :: Scientific/Engineering :: Bio-Informatics',
+              'Topic :: Scientific/Engineering :: Chemistry',
+              'Topic :: Scientific/Engineering :: Mathematics'
+                ],
+      entry_points = {
+              'console_scripts': SCRIPTS,
+                },
+    install_requires=['numpy', 'scipy', 'networkx', 'mlxtend', 'scikit-learn'],
+      )
