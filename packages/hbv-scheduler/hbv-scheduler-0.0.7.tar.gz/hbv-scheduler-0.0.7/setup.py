6c56e421baef30f9dab3f247d2b30338f6792371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:20 +0000

    Add package hbv-scheduler/0.0.7/hbv-scheduler-0.0.7.tar.gz

---
+++ packages/hbv-scheduler/hbv-scheduler-0.0.7.tar.gz/hbv-scheduler-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="hbv-scheduler",  # Replace with your own username
+    version="0.0.7",
+    author="Prashanth Mogali",
+    author_email="m.prashanth54@gmail.com",
+    description="Doctor patient home based visit routing and scheduling algorithm",
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mprashanth54/home-visit-algorithm",
+    package_dir={'': 'hbv'},
+    packages=setuptools.find_packages(where='hbv'),
+    install_requires=['ortools==8.0.8283', 'geopy==2.0.0', 'future==0.18.2'],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
