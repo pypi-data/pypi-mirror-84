843cfb7bca7477e8382ae48f9e2433b1310b5322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:49 +0000

    Add package instaclient/1.9.7/instaclient-1.9.7.tar.gz

---
+++ packages/instaclient/instaclient-1.9.7.tar.gz/instaclient-1.9.7/instaclient/client/urls.py
@@ -0,0 +1,7 @@
+class ClientUrls:
+    LOGIN_URL='https://www.instagram.com/accounts/login/'
+    NAV_USER='https://www.instagram.com/{}/'
+    SEARCH_TAGS='https://www.instagram.com/explore/tags/{}/'
+    FOLLOWERS_URL = 'https://www.instagram.com/{}/followers/'
+    HOME_URL =  'https://instagram.com/'
+    SECURITY_CODE_URL = 'https://www.instagram.com/challenge/'
