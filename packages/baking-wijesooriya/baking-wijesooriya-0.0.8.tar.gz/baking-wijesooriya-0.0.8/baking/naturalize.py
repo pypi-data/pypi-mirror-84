822ff52d7857ed663eaad26fd7a007e9397aa8ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:27 +0000

    Add package baking-wijesooriya/0.0.8/baking-wijesooriya-0.0.8.tar.gz

---
+++ packages/baking-wijesooriya/baking-wijesooriya-0.0.8.tar.gz/baking-wijesooriya-0.0.8/baking/naturalize.py
@@ -0,0 +1,17 @@
+from baking import measure as measure
+from baking import mixIngredients as mixIngredients
+from baking import pourBatter as pourBatter
+from baking import setOvenTempTo as setOvenTempTo
+from baking import bakeBatter as bakeBatter
+from baking import assembleCake as assembleCake
+from baking import iceCake as iceCake
+from baking import complete as complete
+def naturalize():
+    measure = measure
+    mixIngredients = mixIngredients
+    pourBatter = pourBatter
+    setOvenTempTo = setOvenTempTo
+    bakeBatter = bakeBatter
+    assembleCake = assembleCake
+    iceCake = iceCake
+    complete = complete
