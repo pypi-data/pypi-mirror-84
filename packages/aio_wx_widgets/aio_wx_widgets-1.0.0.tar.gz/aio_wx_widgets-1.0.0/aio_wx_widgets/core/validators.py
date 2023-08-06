8d3fc130203f3a17141584c3235bb96e4df33fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:21 +0000

    Add package aio_wx_widgets/1.0.0/aio_wx_widgets-1.0.0.tar.gz

---
+++ packages/aio_wx_widgets/aio_wx_widgets-1.0.0.tar.gz/aio_wx_widgets-1.0.0/aio_wx_widgets/core/validators.py
@@ -0,0 +1,50 @@
+"""Validators"""
+
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+__all__ = [
+    "int_validator",
+    "float_validator",
+    "all_digits_validator",
+    "ValidationError",
+]
+
+
+def int_validator(value, force) -> int:  # noqa
+    """Int validator."""
+    try:
+        converted = int(value)
+    except ValueError:
+        raise ValidationError("Not a valid integer.")
+    if value == str(converted):
+        return converted
+    raise ValidationError("not a valid integer.")
+
+
+def float_validator(value, force) -> float:
+    """Float validator."""
+    try:
+        converted = float(value)
+    except ValueError:
+        raise ValidationError("Not a valid float.")
+    if value == str(converted):
+        return converted
+    if force:
+        return converted
+    return value
+
+
+def all_digits_validator(value, force) -> str:  # noqa
+    """All digits validator.
+
+    Only numbers are allowed.
+    """
+    if value.isdigit():
+        return value
+    raise ValidationError("Only numbers are allowed.")
+
+
+class ValidationError(Exception):
+    """Input validation error"""
