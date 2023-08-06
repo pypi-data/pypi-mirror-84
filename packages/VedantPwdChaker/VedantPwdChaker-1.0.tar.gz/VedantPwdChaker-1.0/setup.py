9bbfefa38302f66bf9c8662c61d37c2c3ff29b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:56 +0000

    Add package VedantPwdChaker/1.0/VedantPwdChaker-1.0.tar.gz

---
+++ packages/VedantPwdChaker/VedantPwdChaker-1.0.tar.gz/VedantPwdChaker-1.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="VedantPwdChaker",
+    version=1.0,
+    long_description="this package is for password cheaking.",
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+
+)
