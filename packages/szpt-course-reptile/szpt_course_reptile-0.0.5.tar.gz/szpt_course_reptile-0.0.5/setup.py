9105403e484d99bfb2b1ae79b1c9f7ed47b08de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:54 +0000

    Add package szpt-course-reptile/0.0.5/szpt_course_reptile-0.0.5.tar.gz

---
+++ packages/szpt-course-reptile/szpt_course_reptile-0.0.5.tar.gz/szpt_course_reptile-0.0.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="szpt_course_reptile",
+    version="0.0.5",
+    author="Cheney",
+    author_email="cheney2001@qq.com",
+    description="use for get szpt course and option course",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "requests==2.24.0",
+        "lxml==4.6.1",
+        "numpy==1.19.4",
+        "selenium==3.141.0",
+    ]
+)
