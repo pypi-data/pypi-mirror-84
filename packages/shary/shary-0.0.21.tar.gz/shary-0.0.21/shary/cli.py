81672bf03d88d13acba8c8f65e95573b1ef0f43a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:16 +0000

    Add package shary/0.0.21/shary-0.0.21.tar.gz

---
+++ packages/shary/shary-0.0.21.tar.gz/shary-0.0.21/shary/cli.py
@@ -0,0 +1,26 @@
+# cli.py
+
+import click # cli
+import os # path and file handling
+
+# import all hosting services
+from .services.anonfiles import AnonfilesApi
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.option("--service", "-s", required=True, type=str, help="Choose a Hosting service")
+@click.option("--file", "-f", required=True, type=str, help="Path to file")
+def upload(service, file):
+    """Uploads a file
+
+    Args:
+        service (str): define the service
+        file (str): path to file
+    """
+    click.echo("work in progress :)")
+
+if __name__ == "__main__":
+    cli()
