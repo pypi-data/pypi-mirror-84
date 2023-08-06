d17e807d6ded27a41bcae0a9ff3c885d7c073f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:31 +0000

    Add package quica/0.1.0/quica-0.1.0.tar.gz

---
+++ packages/quica/quica-0.1.0.tar.gz/quica-0.1.0/quica/quica.py
@@ -0,0 +1,37 @@
+"""Main module."""
+import pandas as pd
+from quica.dataset.dataset import IRRDataset
+from quica.measures.irr import *
+
+class Quica:
+
+    def __init__(self):
+        pass
+
+    def save_to_csv(self, dataset: IRRDataset, csv_path):
+        df = self.get_results(dataset)
+        df.to_csv(csv_path)
+
+    def get_latex(self, dataset: IRRDataset):
+        df = self.get_results(dataset)
+        return df.to_latex()
+
+    def get_results(self, dataset: IRRDataset):
+        measures = [Krippendorff(), FleissK(), ScottsPI()]
+        names = ["krippendorff", "fleiss", "scotts"]
+
+        if dataset.coders == 2:
+            measures.append(CohensK())
+            names.append("cohen")
+
+        results = []
+        for measure in measures:
+            results.append(measure.compute_irr(dataset))
+
+        data = pd.DataFrame({"names": names, "score": results})
+        data.index = data["names"]
+        del data["names"]
+        return data
+
+
+
---
+++ packages/quica/quica-0.1.0.tar.gz/quica-0.1.0/tests/test_quica.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""Tests for `quica` package."""
+
+from quica.measures.irr import *
+from quica.dataset.dataset import IRRDataset
+from quica.quica import Quica
+
+def test_complete_agreement():
+    coder_1 = [0, 1, 0, 1, 0, 1]
+    coder_2 = [0, 1, 0, 1, 0, 1]
+    coder_3 = [0, 1, 0, 1, 0, 0]
+
+    agreeing_coders = [coder_1, coder_2]
+    agreeing_dataset = IRRDataset(agreeing_coders)
+
+    disagreeing_coders = [coder_1, coder_3]
+    disagreeing_dataset = IRRDataset(disagreeing_coders)
+
+    kri = Krippendorff()
+    cohen = CohensK()
+    fliess = FleissK()
+    scotts = ScottsPI()
+
+    assert kri.compute_irr(agreeing_dataset) == 1
+    assert cohen.compute_irr(agreeing_dataset) == 1
+    assert cohen.compute_irr(disagreeing_dataset) < 1
+    assert cohen.compute_irr(disagreeing_dataset) < 1
+    assert fliess.compute_irr(disagreeing_dataset) < 1
+    assert fliess.compute_irr(agreeing_dataset) == 1
+    assert scotts.compute_irr(agreeing_dataset) == 1
+    assert scotts.compute_irr(disagreeing_dataset) < 1
+
+def test_quica_complete():
+    coder_1 = [0, 1, 0, 1, 0, 1]
+    coder_3 = [0, 1, 0, 1, 0, 0]
+    disagreeing_coders = [coder_1, coder_3]
+    disagreeing_dataset = IRRDataset(disagreeing_coders)
+
+    quica = Quica()
+    print(quica.get_results(disagreeing_dataset))
