33d318b6d54b4ad6a3e3dafd3dbf504e7a7d0c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:44 +0000

    Add package pybv/0.4.0/pybv-0.4.0.tar.gz

---
+++ packages/pybv/pybv-0.4.0.tar.gz/pybv-0.4.0/pybv/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""pybv: a lightweight BrainVision I/O tool."""
+
+# Authors: Phillip Alday <phillip.alday@unisa.edu.au>
+#          Chris Holdgraf <choldgraf@berkeley.edu>
+#          Stefan Appelhoff <stefan.appelhoff@mailbox.org>
+#          Tristan Stenner <stenner@med-psych.uni-kiel.de>
+#          Clemens Brunner <clemens.brunner@gmail.com>
+#
+# License: BSD (3-clause)
+
+__version__ = '0.4.0'
+from .io import write_brainvision  # noqa: F401
