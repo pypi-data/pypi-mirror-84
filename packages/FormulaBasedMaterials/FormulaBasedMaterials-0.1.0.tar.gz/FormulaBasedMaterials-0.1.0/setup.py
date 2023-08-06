7c71073c91e3ddd3968358da3ee776c7c02e52df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:06 +0000

    Add package FormulaBasedMaterials/0.1.0/FormulaBasedMaterials-0.1.0.tar.gz

---
+++ packages/FormulaBasedMaterials/FormulaBasedMaterials-0.1.0.tar.gz/FormulaBasedMaterials-0.1.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="FormulaBasedMaterials", # Replace with your own username
+    version="0.1.0",
+    author="Michael, Yu-Chuan, Hsu",
+    author_email="mk60503mk60503@gmail.com",
+    description="A code for generating Formula-Based Materials into Voxel, STL files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MicDonald/FormulaBasedMaterials/archive/v0.1.0.tar.gz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'trimesh',
+        'mpl_toolkits',
+        'time',
+        'warnings',
+        'skimage',
+        'random',
+        'sympy',
+      ],
+    python_requires='>=3.6',
+)
