07791dfe132a3b68af2d4f71d2e1b1438d393a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:01 +0000

    Add package petisco/0.32.19/petisco-0.32.19.tar.gz

---
+++ packages/petisco/petisco-0.32.19.tar.gz/petisco-0.32.19/petisco/event/chaos/domain/interface_event_chaos.py
@@ -0,0 +1,25 @@
+from typing import Dict
+
+from meiga import Result
+from abc import ABCMeta, abstractmethod
+
+
+class IEventChaos:
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def info(self) -> Dict:
+        raise NotImplementedError
+
+    @abstractmethod
+    def nack_simulation(self, *args, **kwargs) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def simulate_failure_on_result(self, result: Result) -> Result:
+        raise NotImplementedError
+
+    @abstractmethod
+    def delay(self):
+        raise NotImplementedError
