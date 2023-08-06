d18a4ef4613341b63e2c6ef831a194429d3874f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:18 +0000

    Add package orca/1.5.4/orca-1.5.4.tar.gz

---
+++ packages/orca/orca-1.5.4.tar.gz/orca-1.5.4/HISTORY.rst
@@ -0,0 +1,53 @@
+v1.5.4
+======
+
+* Support for PyToolz 0.11+
+
+v1.5.3
+======
+
+* Updated requirements to streamline installation.
+
+v1.5.2
+======
+
+* Never released.
+
+v1.5.1
+======
+
+* Update required version of `toolz`.
+
+v1.5.0
+======
+
+* Remove `zbox` from dependencies.
+
+v1.4.0
+======
+
+* Modify `merge_tables` behavior in case of identical column names in multiple
+tables
+* Add `cast` option to `update_col_from_series` to handle type mismatches
+* Updated logic for outputting HDF5 files
+* Add file compression to `run` and `write_tables`
+* Fix base year output for `iter_var` dependencies
+* Add `iter_step` injectable to track current simulation step
+* Change default behavior to output only local tables to `out_run_tables`
+* Use sets instead of lists in `to_frame`
+
+
+v1.3.0
+======
+
+* Add `temporary_tables` context manager
+
+v1.2.0
+======
+
+* Add Orca server and supporting functionality
+
+v1.1.0
+======
+
+* Rename ``clear_sim`` function to ``clear_all``
---
+++ packages/orca/orca-1.5.4.tar.gz/orca-1.5.4/setup.py
@@ -0,0 +1,82 @@
+from __future__ import print_function
+
+import subprocess
+from setuptools import setup, find_packages
+from setuptools.command.sdist import sdist
+
+
+# these make sure the js distribution bundle is created and
+# up-to-date when creating distribution packages.
+cmdclass = {}
+
+
+def build_js_bundle():
+    print('Building JS bundle')
+    subprocess.check_call(['./bin/build_js_bundle.sh'])
+
+
+class sdist_(sdist):
+    def run(self):
+        build_js_bundle()
+        sdist.run(self)
+cmdclass['sdist'] = sdist_
+
+try:
+    from wheel.bdist_wheel import bdist_wheel
+except ImportError:
+    pass
+else:
+    class build_wheel(bdist_wheel):
+        def run(self):
+            build_js_bundle()
+            bdist_wheel.run(self)
+    cmdclass['bdist_wheel'] = build_wheel
+
+# read README as the long description
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='orca',
+    version='1.5.4',
+    description='A pipeline orchestration tool with Pandas support',
+    long_description=long_description,
+    author='UrbanSim Inc.',
+    author_email='info@urbansim.com',
+    license='BSD',
+    url='https://github.com/udst/orca',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: BSD License'
+    ],
+    packages=find_packages(exclude=['*.tests']),
+    package_data={
+        'orca': [
+            'server/static/css/*',
+            'server/static/js/dist/*',
+            'server/templates/*']
+    },
+    # New versions of PyTables ("tables" on pypi) often fail to install correctly, so we
+    # are being conservative here and disallowing them until tested
+    install_requires=[
+        'pandas >= 0.15.0',
+        'tables >=3.1, <3.6; python_version <"3.6"',
+        'tables >=3.1, <3.7; python_version >="3.6"',
+        'toolz >= 0.8.1'
+    ],
+    extras_require={
+        'server': ['flask >= 0.10', 'pygments >= 2.0', 'six >= 1.9.0']
+    },
+    entry_points={
+        'console_scripts': [
+            'orca-server = orca.server.server:main [server]'
+        ]
+    },
+    cmdclass=cmdclass
+)
