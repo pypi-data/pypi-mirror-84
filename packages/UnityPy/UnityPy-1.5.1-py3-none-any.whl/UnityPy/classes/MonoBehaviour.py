db0dcd01991c104fd8d43b5eb5cd6c57d2eddd4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:38 +0000

    Add package UnityPy/1.5.1/UnityPy-1.5.1-py3-none-any.whl

---
+++ packages/UnityPy/UnityPy-1.5.1-py3-none-any.whl/UnityPy/classes/MonoBehaviour.py
@@ -0,0 +1,11 @@
+from .Behaviour import Behaviour
+from .PPtr import PPtr
+
+
+class MonoBehaviour(Behaviour):
+    def __init__(self, reader):
+        super().__init__(reader=reader)
+        self.script = PPtr(reader)
+        self.name = reader.read_aligned_string()
+
+        self.read_type_tree()
