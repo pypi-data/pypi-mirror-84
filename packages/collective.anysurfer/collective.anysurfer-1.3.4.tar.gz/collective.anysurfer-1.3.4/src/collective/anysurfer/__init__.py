6354cd5e10f61d7e8ea21ba195ff80c80e0f4f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:13 +0000

    Add package collective.anysurfer/1.3.4/collective.anysurfer-1.3.4.tar.gz

---
+++ packages/collective.anysurfer/collective.anysurfer-1.3.4.tar.gz/collective.anysurfer-1.3.4/src/collective/anysurfer/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory("collective.anysurfer")
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
