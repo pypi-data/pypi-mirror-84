e1ba049982d51c659d8a91a9dcf9e99fd615fed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:30 +0000

    Add package oarepo-rdm-records/2.1.0/oarepo_rdm_records-2.1.0.tar.gz

---
+++ packages/oarepo-rdm-records/oarepo_rdm_records-2.1.0.tar.gz/oarepo_rdm_records-2.1.0/oarepo_rdm_records/marshmallow/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CESNET Prague.
+#
+# CIS theses repository is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Schemas for marshmallow."""
+
+#from .json import BibliographicRecordSchemaV1, MetadataSchemaV1
+from .json import MetadataSchemaV1
+
+#__all__ = ('BibliographicRecordSchemaV1', 'MetadataSchemaV1')
+__all__ = ('MetadataSchemaV1',)
