eb3fb0345f03c069803cda008476edba2742e2ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:31 +0000

    Add package mkctools/0.6.3/mkctools-0.6.3.tar.gz

---
+++ packages/mkctools/mkctools-0.6.3.tar.gz/mkctools-0.6.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='mkctools',
+    version='0.6.3',
+    description='Machine Learning Libraries',
+    author='Michael Chen',
+    author_email='mc6666@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://github.com/mc6666/mkctools',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',)
