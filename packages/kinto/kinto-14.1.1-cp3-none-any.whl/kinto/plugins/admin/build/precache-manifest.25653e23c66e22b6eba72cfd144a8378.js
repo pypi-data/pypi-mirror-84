ddf7d8cb8a959b28557013ca65ef0dc852cd7fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:11 +0000

    Add package kinto/14.1.1/kinto-14.1.1-cp3-none-any.whl

---
+++ packages/kinto/kinto-14.1.1-cp3-none-any.whl/kinto/plugins/admin/build/precache-manifest.25653e23c66e22b6eba72cfd144a8378.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911b00d8a66eaab3dd054e36cf36bf10",
+    "url": "/v1/admin/index.html"
+  },
+  {
+    "revision": "87bc780c969c2e739ac9",
+    "url": "/v1/admin/static/css/2.439435b7.chunk.css"
+  },
+  {
+    "revision": "87bc780c969c2e739ac9",
+    "url": "/v1/admin/static/js/2.4eb9f794.chunk.js"
+  },
+  {
+    "revision": "b7335f4f5a36b43a5541e3027cbc244e",
+    "url": "/v1/admin/static/js/2.4eb9f794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d6d8a489db0f6d5916",
+    "url": "/v1/admin/static/js/main.2c63efee.chunk.js"
+  },
+  {
+    "revision": "9ef0d6507b831d1cab94",
+    "url": "/v1/admin/static/js/runtime-main.6c5c2596.js"
+  },
+  {
+    "revision": "bf9657ab7f0b4ed0a47f72a95c61dd34",
+    "url": "/v1/admin/static/media/check.bf9657ab.svg"
+  },
+  {
+    "revision": "7f9b096de45a15bbd3a9e0173de0deb4",
+    "url": "/v1/admin/static/media/exclamation-circle-fill.7f9b096d.svg"
+  },
+  {
+    "revision": "e8c7ae6e0daf6dd9aea423d23c370c53",
+    "url": "/v1/admin/static/media/exclamation-triangle-fill.e8c7ae6e.svg"
+  },
+  {
+    "revision": "cb248b18144bc603c7c65f99c8bf82d9",
+    "url": "/v1/admin/static/media/info-circle-fill.cb248b18.svg"
+  }
+]);
