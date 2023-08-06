6bf9772bbc0668f12917a2a2df0546ebfe5fcd7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:33 +0000

    Add package contraptions/3.10.2/contraptions-3.10.2-py2.py3-none-any.whl

---
+++ packages/contraptions/contraptions-3.10.2-py2.py3-none-any.whl/contraptions/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for Contraptions."""
+
+# Package info:
+__author__ = """Ryan Fleck"""
+__email__ = "Ryan.Fleck@protonmail.com"
+__version__ = "3.10.2"
+
+# Export all functions:
+from .math import *
+from .strings import *
