4059da95d654e7f8128d95c46932fa5ac13e8d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:20 +0000

    Add package aio_wx_widgets/1.0.0/aio_wx_widgets-1.0.0-py3-none-any.whl

---
+++ packages/aio_wx_widgets/aio_wx_widgets-1.0.0-py3-none-any.whl/aio_wx_widgets/containers/grid.py
@@ -0,0 +1,46 @@
+"""Grid layout item."""
+
+# pylint: disable=duplicate-code
+
+
+import logging
+
+import wx
+
+from aio_wx_widgets.core.sizers import SizerMixin
+
+_LOGGER = logging.getLogger(__name__)
+
+__all__ = ["Grid", "VERTICAL", "HORIZONTAL"]
+
+VERTICAL = wx.VERTICAL
+HORIZONTAL = wx.HORIZONTAL
+
+
+class Grid(SizerMixin):
+    """A grid component.
+
+    Basically a boxsizer with some defaults.
+    """
+
+    default_sizer_margin = 0
+
+    HOR = HORIZONTAL
+    VER = VERTICAL
+
+    def __init__(self, orientation=HORIZONTAL):
+        """Init."""
+        self.ui_item = self._sizer = wx.BoxSizer(orient=orientation)
+        self._parent = None
+
+    def __call__(self, parent) -> SizerMixin:
+        # setting a specific parent.
+        self._parent = parent
+        return self
+
+    def __enter__(self):
+        """Enter context manager."""
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Exit context manager."""
---
+++ packages/aio_wx_widgets/aio_wx_widgets-1.0.0-py3-none-any.whl/aio_wx_widgets/type_annotations.py
@@ -0,0 +1,26 @@
+"""All type annotations go here."""
+
+from typing import TYPE_CHECKING, Any, TypeVar
+
+if TYPE_CHECKING:
+    from aio_wx_widgets.core.binding import Binding
+
+    from aio_wx_widgets.controller import BaseController
+    from asyncio import AbstractEventLoop
+
+    try:
+        import Protocol  # type: ignore
+    except ImportError:
+        from typing_extensions import Protocol
+
+    assert BaseController
+    assert AbstractEventLoop
+    assert Binding
+
+    class Choice(Protocol):
+        """Typing protocol to be used for static type checking."""
+
+        label: str
+        value: Any
+
+    T_var = TypeVar("T_var")
