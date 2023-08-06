24525889a1742687bfdac234cce66af7a7e03f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:32 +0000

    Add package aribasiccalculator/0.0.1/aribasiccalculator-0.0.1.tar.gz

---
+++ packages/aribasiccalculator/aribasiccalculator-0.0.1.tar.gz/aribasiccalculator-0.0.1/AriBasicCalculator/__init__.py
@@ -0,0 +1,13 @@
+def add_number(num1, num2):
+    return num1 + num2
+
+def subtrack_number(num1, num2):
+    return num1 - num2
+
+def multiply_number(num1, num2):
+    return num1 * num2
+
+def devide_number(num1, num2):
+    return num1 / num2
+
+print(add_numbers(5,6))
