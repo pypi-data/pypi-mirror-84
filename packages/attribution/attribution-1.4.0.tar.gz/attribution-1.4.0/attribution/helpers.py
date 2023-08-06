30b617516e0ca65a8d75ae6b6d0073465adce228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:07 +0000

    Add package attribution/1.4.0/attribution-1.4.0.tar.gz

---
+++ packages/attribution/attribution-1.4.0.tar.gz/attribution-1.4.0/attribution/helpers.py
@@ -0,0 +1,29 @@
+# Copyright 2020 John Reese
+# Licensed under the MIT license
+
+import logging
+import shlex
+import subprocess
+
+LOG = logging.getLogger(__name__)
+
+
+def sh(*cmd: str) -> str:
+    """Run a simple command and return mixed stdout/stderr; raise on non-zero exit"""
+    if len(cmd) == 1:
+        cmd = tuple(shlex.split(cmd[0]))
+    LOG.debug(f"running $ {' '.join(shlex.quote(c) for c in cmd)}")
+    try:
+        p = subprocess.run(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            check=True,
+            encoding="utf-8",
+        )
+        return p.stdout
+    except subprocess.CalledProcessError as e:
+        LOG.debug(
+            f"exit code: {e.returncode}\nstdout:\n{e.stdout}\nstderr:\n{e.stderr}"
+        )
+        raise
