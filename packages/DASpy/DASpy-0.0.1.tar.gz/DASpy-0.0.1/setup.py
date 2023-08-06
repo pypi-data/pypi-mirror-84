73f59d2637ef9e19673fb7f625ef9d12ef3f055f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:55 +0000

    Add package DASpy/0.0.1/DASpy-0.0.1.tar.gz

---
+++ packages/DASpy/DASpy-0.0.1.tar.gz/DASpy-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='DASpy',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='me',
+    author_email='thomas.hudson@earth.ox.ac.uk',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
