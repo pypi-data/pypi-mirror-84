712b2e8bc14014db3ae1835ba9c7b2a742629ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:25 +0000

    Add package contraptions/0.1.0/contraptions-0.1.0-py2.py3-none-any.whl

---
+++ packages/contraptions/contraptions-0.1.0-py2.py3-none-any.whl/contraptions/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Contraptions."""
+
+__author__ = """Ryan Fleck"""
+__email__ = "Ryan.Fleck@protonmail.com"
+__version__ = "0.1.0"
---
+++ packages/contraptions/contraptions-0.1.0-py2.py3-none-any.whl/contraptions/contraptions.py
@@ -0,0 +1,5 @@
+"""Main module."""
+
+
+def add(a: int, b: int) -> int:
+    return a + b
