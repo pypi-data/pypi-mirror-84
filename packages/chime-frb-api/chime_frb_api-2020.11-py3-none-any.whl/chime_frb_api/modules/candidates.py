ea8508f2bc482f85dff4264d865cca4f99b3346a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:44 +0000

    Add package chime-frb-api/2020.11/chime_frb_api-2020.11-py3-none-any.whl

---
+++ packages/chime-frb-api/chime_frb_api-2020.11-py3-none-any.whl/chime_frb_api/modules/candidates.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import logging
+import typing as t
+
+from chime_frb_api.core import API
+
+log = logging.getLogger(__name__)
+
+
+class Candidates:
+    """
+    CHIME/FRB Candidates API
+    """
+
+    def __init__(self, API: API):
+        self.API = API
+
+    def get_all_candidates(self) -> list:
+        """
+        Get all CHIME/FRB Candidates from Candidates Database
+        Parameters
+        ----------
+            None
+        Returns
+        -------
+            list
+        """
+        return self.API.get("/v1/candidates")
---
+++ packages/chime-frb-api/chime_frb_api-2020.11-py3-none-any.whl/chime_frb_api/tests/test_candidates.py
@@ -0,0 +1,11 @@
+import pytest
+from requests.exceptions import HTTPError
+
+from chime_frb_api.backends import frb_master
+
+master = frb_master.FRBMaster(debug=True, base_url="http://localhost:8001")
+
+
+def test_get_all_candidates():
+    candidates = master.candidates.get_all_candidates()
+    assert candidates == []
