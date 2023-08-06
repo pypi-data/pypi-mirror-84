ab02893f8743b0b15778ced0ab04a742563d170e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:19 +0000

    Add package pyrenoweb/0.1.12/pyrenoweb-0.1.12.tar.gz

---
+++ packages/pyrenoweb/pyrenoweb-0.1.12.tar.gz/pyrenoweb-0.1.12/pyrenoweb/const.py
@@ -0,0 +1,12 @@
+BASE_URL = "https://servicesgh.renoweb.dk/v1_13"
+
+DEFAULT_TIMEOUT = 10
+
+TYPE_RESIDUAL = ["Restaffald-Madaffald", "Dagrenovation"]
+TYPE_GLASS = ["Glas"]
+TYPE_METAL_GLASS = ["Metal-Glas"]
+TYPE_METAL = ["Jern"]
+TYPE_PAPER = ["Papir", "Pap"]
+TYPE_PLASTIC = ["Plast"]
+TYPE_STORSKRALD = ["Storskrald"]
+TYPE_HAVEAFFALD = ["Haveaffald"]
