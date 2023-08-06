22f37371ec99fadc57ca964eae90414544c721af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:17 +0000

    Add package rpaframework/6.4.0/rpaframework-6.4.0.tar.gz

---
+++ packages/rpaframework/rpaframework-6.4.0.tar.gz/rpaframework-6.4.0/src/RPA/Desktop/__init__.py
@@ -0,0 +1,31 @@
+import logging
+from robotlibcore import DynamicCore
+
+from RPA.Desktop.keywords import (
+    ApplicationKeywords,
+    ClipboardKeywords,
+    FinderKeywords,
+    KeyboardKeywords,
+    MouseKeywords,
+    ScreenKeywords,
+)
+
+
+class Desktop(DynamicCore):
+    """Cross-platform library for interacting with desktop environments."""
+
+    ROBOT_LIBRARY_SCOPE = "GLOBAL"
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+        # Register keyword libraries to LibCore
+        libraries = [
+            ApplicationKeywords(self),
+            ClipboardKeywords(self),
+            FinderKeywords(self),
+            KeyboardKeywords(self),
+            MouseKeywords(self),
+            ScreenKeywords(self),
+        ]
+        super().__init__(libraries)
