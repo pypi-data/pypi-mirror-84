4f9a547690a0630016b553d9a7a3b6ea08cbb612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:55 +0000

    Add package qball-lang/0.5.1/qball-lang-0.5.1.tar.gz

---
+++ packages/qball-lang/qball-lang-0.5.1.tar.gz/qball-lang-0.5.1/QBall/__init__.py
@@ -0,0 +1,27 @@
+import QBall.lexer
+import QBall.interpreter
+
+
+def evaluate(qballcode: str):
+    try:
+        if qballcode[:4] == "run ":
+            command = open(f"{qballcode[4:]}.qball").read()
+        try:
+            result = lexer.lexer(command).generate_tokens()
+        except NameError:
+            result = lexer.lexer(qballcode).generate_tokens()
+        interpret = interpreter.interpreter(result)
+        pos = interpret.pos
+        interpret.interpret()
+    except Exception as e:
+        try:
+            print(f"\033[91mError at line {pos.line} starting at char {pos.char}: {e}\033[00m")
+        except NameError:
+            print(f"\033[91mLexer error: {e}\033[00m")
+
+
+def global_add(toadd: dict):
+    i = 0
+    while i < len(toadd):
+        QBall.interpreter.global_vars.add(list(toadd.keys())[i], list(toadd.values())[i])
+        i += 1
---
+++ packages/qball-lang/qball-lang-0.5.1.tar.gz/qball-lang-0.5.1/README.md
@@ -0,0 +1,23 @@
+# QBall
+QBall is a high-level interpreted language written in Python. It is object oriented and has an easy to learn and use syntax. The following is an example of outputting "Hello, world!" into the console
+```
+out "Hello, world!";
+```
+It also has any easy function decleration system
+```
+$ This is a single line comment
+((This is a
+multiline comment))
+$ This declares a function outarg that takes an argument a
+_outarg a;
+  out a;
+end
+outarg "Hello, world!";
+```
+# Docs
+Everything you need to know is in documentation.txt
+# Contribute
+Go here to contribute: https://repl.it/@qballlang/QBall
+Changes are commited 8:00 PM EST every day I can
+# Trello board
+The official Trello board: https://trello.com/b/cJM6HsR3/qball
