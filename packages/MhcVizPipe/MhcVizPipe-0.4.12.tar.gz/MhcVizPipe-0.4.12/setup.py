e4efdfec75f7d1c0740aff55531c3fe7f20252e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:15 +0000

    Add package MhcVizPipe/0.4.12/MhcVizPipe-0.4.12.tar.gz

---
+++ packages/MhcVizPipe/MhcVizPipe-0.4.12.tar.gz/MhcVizPipe-0.4.12/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MhcVizPipe',
+    version='0.4.12',
+    packages=find_packages(),
+    url='https://github.com/CaronLab/MhcVizPipe',
+    license='MIT',
+    author='Kevin Kovalchik',
+    author_email='',
+    description='A reporting pipeline for visualization of immunopeptidomics MS data.',
+    python_requires='>=3.7',
+    include_package_data=True,
+    install_requires=[
+        'dash>=1.12.0',
+        'plotly',
+        'dash-bootstrap-components',
+        'pandas',
+        'numpy',
+        'dominate',
+        'upsetplot',
+        'seaborn',
+        'plotly-logo',
+        'waitress'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
