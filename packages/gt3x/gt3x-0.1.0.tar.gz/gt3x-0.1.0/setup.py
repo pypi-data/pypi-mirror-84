8b5866afe72467f98d65a0de14477c4e3ca9b21b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:47 +0000

    Add package gt3x/0.1.0/gt3x-0.1.0.tar.gz

---
+++ packages/gt3x/gt3x-0.1.0.tar.gz/gt3x-0.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gt3x", # Replace with your own username
+    version="0.1.0",
+    author="Shaheen Syed, John Muschelli",
+    author_email="shaheen.syed@uit.no",
+    description="A package to read extract raw acceleration data from .gt3x files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shaheen-syed/gt3x",
+    packages=setuptools.find_packages(exclude = "tests"),  # Required
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",    
+    python_requires='>=3.6',
+)
