f61b31678face44dc10a0e4138e64301937ca11c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:26 +0000

    Add package asn1editor/0.2.3/asn1editor-0.2.3.tar.gz

---
+++ packages/asn1editor/asn1editor-0.2.3.tar.gz/asn1editor-0.2.3/asn1editor/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.2.3"
+# noinspection SpellCheckingInspection
+__author__ = "Florian Fetz"
+
+from . import wxPython
+from .Plugin import Plugin
+from .PluginInterface import PluginInterface
