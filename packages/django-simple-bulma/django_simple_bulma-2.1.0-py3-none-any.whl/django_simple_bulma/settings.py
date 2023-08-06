d7fc1ce694c8608e1b91a2553147ddfe6cd0491a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:32 +0000

    Add package django-simple-bulma/2.1.0/django_simple_bulma-2.1.0-py3-none-any.whl

---
+++ packages/django-simple-bulma/django_simple_bulma-2.1.0-py3-none-any.whl/django_simple_bulma/settings.py
@@ -0,0 +1,8 @@
+"""
+This file ensures the pdoc does not fail.
+
+Without this, pdoc will fail with django.core.exceptions.ImproperlyConfigured:
+Requested settings, but settings are not configured.
+"""
+
+SECRET_KEY = "I'm sure there's a better way to do this..."
