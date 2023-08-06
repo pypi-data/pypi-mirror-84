e92d18c5e7800cd9d367a7efaf5380cf6fe4b392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:21 +0000

    Add package python-on-whales/0.2/python-on-whales-0.2.tar.gz

---
+++ packages/python-on-whales/python-on-whales-0.2.tar.gz/python-on-whales-0.2/python_on_whales/__init__.py
@@ -0,0 +1,3 @@
+raise ImportError("Until 01/05/2021, Python on whales can't be installed with pip. "
+      "If you wish to use the package, follow the steps at "
+      "https://gabrieldemarmiesse.github.io/python-on-whales/#how-to-install ")
---
+++ packages/python-on-whales/python-on-whales-0.2.tar.gz/python-on-whales-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="python-on-whales",
+    version=0.2,
+    install_requires=[],
+    packages=find_packages(),
+)
