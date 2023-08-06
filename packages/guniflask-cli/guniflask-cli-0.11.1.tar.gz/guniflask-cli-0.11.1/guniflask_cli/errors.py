91b7e36823d1f4b156bc7352ca34793bf60be0ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:48 +0000

    Add package guniflask-cli/0.11.1/guniflask-cli-0.11.1.tar.gz

---
+++ packages/guniflask-cli/guniflask-cli-0.11.1.tar.gz/guniflask-cli-0.11.1/guniflask_cli/errors.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+class UsageError(Exception):
+    """
+    Usage error
+    """
+
+    def __init__(self, *args, print_help=False, **kwargs):
+        self.print_help = print_help
+        super().__init__(*args, **kwargs)
+
+
+class AbortedError(Exception):
+    """
+    Aborted error
+    """
+
+
+class TemplateError(Exception):
+    """
+    Template error
+    """
