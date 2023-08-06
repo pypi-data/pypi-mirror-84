0c0bb557aa4505f6836a4cb4f054f7f39b4fad02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:39 +0000

    Add package ptera/0.2.6/ptera-0.2.6-py3-none-any.whl

---
+++ packages/ptera/ptera-0.2.6-py3-none-any.whl/ptera/__init__.py
@@ -0,0 +1,37 @@
+from .core import (
+    BaseOverlay,
+    Overlay,
+    PatternCollection,
+    PteraFunction,
+    interact,
+)
+from .recur import Recurrence
+from .selector import select
+from .selfless import ConflictError, Override, default, override, transform
+from .tags import Tag, TagSet, match_tag, tag
+from .utils import ABSENT
+
+
+class PteraDecorator:
+    def __init__(self, kwargs):
+        self.kwargs = kwargs
+
+    def defaults(self, **defaults):
+        return PteraDecorator({**self.kwargs, "defaults": defaults})
+
+    def __call__(self, fn):
+        if isinstance(fn, PteraFunction):
+            return fn
+        new_fn, state = transform(fn, interact=interact)
+        fn = PteraFunction(new_fn, state)
+        if "defaults" in self.kwargs:
+            fn = fn.new(
+                **{
+                    k: override(v, -0.5)
+                    for k, v in self.kwargs["defaults"].items()
+                }
+            )
+        return fn
+
+
+tooled = PteraDecorator({})
