317ec9f3c804974ff1b791e3dc8dbf674d6c1987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:58 +0000

    Add package musicdl/2.1.9/musicdl-2.1.9.tar.gz

---
+++ packages/musicdl/musicdl-2.1.9.tar.gz/musicdl-2.1.9/musicdl/__init__.py
@@ -0,0 +1,4 @@
+'''version'''
+__version__ = '2.1.9'
+'''author'''
+__author__ = 'Charles'
---
+++ packages/musicdl/musicdl-2.1.9.tar.gz/musicdl-2.1.9/setup.py
@@ -0,0 +1,40 @@
+'''
+Function:
+	setup
+Author:
+	Charles
+微信公众号:
+	Charles的皮卡丘
+GitHub:
+	https://github.com/CharlesPikachu
+'''
+import musicdl
+from setuptools import setup, find_packages
+
+
+'''readme'''
+with open('README.md', 'r', encoding='utf-8') as f:
+	long_description = f.read()
+
+
+'''setup'''
+setup(
+	name='musicdl',
+	version=musicdl.__version__,
+	description='Music Downloader.',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	classifiers=[
+			'License :: OSI Approved :: MIT License',
+			'Programming Language :: Python :: 3',
+			'Intended Audience :: Developers',
+			'Operating System :: OS Independent'],
+	author='Charles',
+	url='https://github.com/CharlesPikachu/Music-Downloader',
+	author_email='charlesblwx@gmail.com',
+	license='MIT',
+	include_package_data=True,
+	install_requires=['requests >= 2.22.0', 'pycryptodome >= 3.8.1', 'click >= 7.0', 'prettytable >= 0.7.2'],
+	zip_safe=True,
+	packages=find_packages()
+)
