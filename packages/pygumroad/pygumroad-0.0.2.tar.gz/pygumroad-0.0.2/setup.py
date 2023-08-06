830afb610b28780aa1e314c34651dd72064927da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:22 +0000

    Add package pygumroad/0.0.2/pygumroad-0.0.2.tar.gz

---
+++ packages/pygumroad/pygumroad-0.0.2.tar.gz/pygumroad-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pygumroad",
+    version="0.0.2",
+    author="Brennon Thomas",
+    author_email="info@opsdisk.com",
+    description="A Python API client for interacting with the Gumroad API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/opsdisk/pygumroad",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests>=2.24.0",
+        "requests-toolbelt>=0.9.1",
+    ],
+    python_requires=">=3.6",
+    license="GNU General Public License v3.0",
+    keywords="python gumroad api client",
+)
