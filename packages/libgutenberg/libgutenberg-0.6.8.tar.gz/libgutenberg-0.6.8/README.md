b42b964ce6d187354c2a261d3cc5ed2380ed5cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:17 +0000

    Add package libgutenberg/0.6.8/libgutenberg-0.6.8.tar.gz

---
+++ packages/libgutenberg/libgutenberg-0.6.8.tar.gz/libgutenberg-0.6.8/README.md
@@ -0,0 +1,10 @@
+# libgutenberg
+Common files used by Project Gutenberg python projects.
+
+# installation
+
+`pipenv install libgutenberg`
+
+or 
+
+`pip install libgutenberg`
---
+++ packages/libgutenberg/libgutenberg-0.6.8.tar.gz/libgutenberg-0.6.8/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+# libgutenberg setup.py
+#
+
+__version__ = '0.6.8'
+
+from setuptools import setup
+
+setup (
+    name         = 'libgutenberg',
+    version      = __version__,
+
+    package_dir = {
+        'libgutenberg': 'libgutenberg',
+    },
+
+    install_requires = [
+        'lxml',
+        'six>=1.4.1',
+    ],
+    extras_require = {
+        'postgres':  ['psycopg2'],
+        'covers': ['cairocffi==0.8.0'],
+    },
+    packages = [
+        'libgutenberg'
+    ],
+
+    # metadata for upload to PyPI
+
+    author = "Marcello Perathoner",
+    maintainer = "Eric Hellman",
+    maintainer_email = "eric@hellman.net",
+    description = "Common files used by Project Gutenberg python projects.",
+    long_description = "Useless as standalone install. Used only as requirement for other packages.",
+    license = "GPL v3",
+    keywords = "project gutenberg",
+    url = "https://github.com/gutenbergtools/libgutenberg/",
+
+    classifiers = [
+        "Intended Audience :: Other Audience",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    platforms = 'OS-independent',
+)
