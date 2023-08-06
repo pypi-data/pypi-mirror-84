9859c40c44c65afceae9ad6787e18ead145c7bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:40 +0000

    Add package user-list-pkg-keskin69/0.1/user_list_pkg_keskin69-0.1.tar.gz

---
+++ packages/user-list-pkg-keskin69/user_list_pkg_keskin69-0.1.tar.gz/user_list_pkg_keskin69-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='user_list_pkg_keskin69',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    url='',
+    license='',
+    author='emuskes',
+    author_email='',
+    description=''
+)
