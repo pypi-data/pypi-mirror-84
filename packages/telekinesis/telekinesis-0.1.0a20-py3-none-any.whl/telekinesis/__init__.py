3bfc93d5ea6b0971dffe3ad8b3ddba783b3bd350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:17 +0000

    Add package telekinesis/0.1.0a20/telekinesis-0.1.0a20-py3-none-any.whl

---
+++ packages/telekinesis/telekinesis-0.1.0a20-py3-none-any.whl/telekinesis/__init__.py
@@ -0,0 +1,21 @@
+from .client import Session, Connection, Channel, Route
+from .broker import Broker
+from .telekinesis import Telekinesis, inject_first_arg, State
+from .helpers import authenticate
+
+from pkg_resources import get_distribution
+
+__version__ = get_distribution(__name__).version
+
+__all__ = [
+    "__version__",
+    "Telekinesis",
+    "Broker",
+    "authenticate",
+    "Session",
+    "Connection",
+    "Channel",
+    "Route",
+    "inject_first_arg",
+    "State",
+]
