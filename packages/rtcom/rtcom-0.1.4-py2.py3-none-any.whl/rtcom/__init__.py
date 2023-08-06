ab861aa6e538fe78c40e4e1dfbd348d118efb38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:02 +0000

    Add package rtcom/0.1.4/rtcom-0.1.4-py2.py3-none-any.whl

---
+++ packages/rtcom/rtcom-0.1.4-py2.py3-none-any.whl/rtcom/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for RealTimeCommunication."""
+
+__author__ = """Raphael Jean"""
+__email__ = 'raphael.jean@rocketmail.com'
+__version__ = '0.1.4'
+
+from .rtcom import RealTimeCommunication, RealTimeCommunicationWriter, RealTimeCommunicationListener
+
