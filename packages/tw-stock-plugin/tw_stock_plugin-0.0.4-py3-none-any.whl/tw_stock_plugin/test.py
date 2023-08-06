79c558efeaa91502b833c5c9b0923aa469c385a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:30 +0000

    Add package tw-stock-plugin/0.0.4/tw_stock_plugin-0.0.4-py3-none-any.whl

---
+++ packages/tw-stock-plugin/tw_stock_plugin-0.0.4-py3-none-any.whl/tw_stock_plugin/test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*
+"""
+      ┏┓       ┏┓
+    ┏━┛┻━━━━━━━┛┻━┓
+    ┃      ☃      ┃
+    ┃  ┳┛     ┗┳  ┃
+    ┃      ┻      ┃
+    ┗━┓         ┏━┛
+      ┗┳        ┗━┓
+       ┃          ┣┓
+       ┃          ┏┛
+       ┗┓┓┏━━━━┳┓┏┛
+        ┃┫┫    ┃┫┫
+        ┗┻┛    ┗┻┛
+    God Bless,Never Bug
+"""
+from datetime import datetime
+from tw_stock_plugin.core.update_stock import UpdateStock
+from tw_stock_plugin.core.stock_trading import StockTrading
+
+# UpdateStock.main()
+
+date_ = datetime(2020, 11, 6).date()
+stock_trading = StockTrading(date_=date_)
+print(stock_trading.get_all().get('6236').change)
