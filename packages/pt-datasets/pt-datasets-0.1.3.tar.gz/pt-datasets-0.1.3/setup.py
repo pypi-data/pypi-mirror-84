0829bc0d293e245998245e799eb93a53ccd9609b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:26 +0000

    Add package pt-datasets/0.1.3/pt-datasets-0.1.3.tar.gz

---
+++ packages/pt-datasets/pt-datasets-0.1.3.tar.gz/pt-datasets-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name="pt-datasets",
+    version="0.1.3",
+    packages=["pt_datasets"],
+    url="https://github.com/AFAgarap/pt-datasets",
+    license="AGPL-3.0 License",
+    author="Abien Fred Agarap",
+    author_email="abienfred.agarap@gmail.com",
+    long_description="PyTorch dataset loader for MNIST, Fashion-MNIST, EMNIST-Balanced, CIFAR10, and SVHN datasets",
+    install_requires=[
+        "numpy==1.19.0",
+        "torchvision==0.7.0",
+        "umap_learn==0.4.6",
+        "torch==1.6.0",
+        "MulticoreTSNE==0.1",
+        "scikit_learn==0.23.2",
+    ],
+)
