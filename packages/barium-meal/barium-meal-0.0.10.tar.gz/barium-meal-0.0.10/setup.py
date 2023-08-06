b9054883b7e485c0c980315681dc76cc5d7c9323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:53 +0000

    Add package barium-meal/0.0.10/barium-meal-0.0.10.tar.gz

---
+++ packages/barium-meal/barium-meal-0.0.10.tar.gz/barium-meal-0.0.10/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="barium-meal",
+    version="0.0.10",
+    author="Peckham Data Centre",
+    author_email="chris@peckhamdata.com",
+    description="Convenience wrapper for OpenTelemetry",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/peckhamdata/barium-meal",
+    packages=['barium_meal'],
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "pytest",
+        "flask",
+        "opentelemetry-exporter-jaeger",
+        "opentelemetry-api",
+        "opentelemetry-instrumentation-requests",
+        "opentelemetry-instrumentation-boto",
+        "opentelemetry-instrumentation-flask"
+    ]
+)
