68f2f5fc31a223d3df9248f3676e17c7d78c6bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:04 +0000

    Add package usf-account-service-client/0.0.3/usf-account-service-client-0.0.3.tar.gz

---
+++ packages/usf-account-service-client/usf-account-service-client-0.0.3.tar.gz/usf-account-service-client-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+def get_version():
+    with open('version') as version_file:
+        return version_file.read()
+
+
+def get_requirements():
+    with open('requirements.txt') as requirements_file:
+        return [dependency.strip() for dependency in requirements_file if dependency.strip()]
+
+
+setup(name='usf-account-service-client',
+      version=get_version(),
+      author="Ukuspeed",
+      author_email="info@ukuspeed.gmail.com",
+      description="Account service client",
+      url="https://github.com/ukuspeed/usf-account-service",
+      include_package_data=True,
+      python_requires='>=3.6',
+      install_requires=get_requirements(),
+      py_modules=['']
+      )
