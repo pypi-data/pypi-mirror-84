5b8469ba49483d3893bf81da672781837b1dc046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:51 +0000

    Add package pw-zoomus/1.0.4/pw_zoomus-1.0.4.tar.gz

---
+++ packages/pw-zoomus/pw_zoomus-1.0.4.tar.gz/pw_zoomus-1.0.4/pw_zoomus/__init__.py
@@ -0,0 +1,12 @@
+"""Python wrapper around the Zoom.us REST API
+   Adapted from Patrick Schmid (prschmid/ zoomus) 
+   Improved by Willy L W S"""
+
+from __future__ import absolute_import, unicode_literals
+
+from pw_zoomus.client import ZoomClient
+from pw_zoomus.util import API_VERSION_1, API_VERSION_2
+
+
+__all__ = ["API_VERSION_1", "API_VERSION_2", "ZoomClient"]
+__version__ = "1.0.4"
