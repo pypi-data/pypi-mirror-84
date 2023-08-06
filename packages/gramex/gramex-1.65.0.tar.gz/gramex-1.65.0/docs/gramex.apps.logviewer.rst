9f37d61b62f3f72a0cbb44980ab20630af299d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:22 +0000

    Add package gramex/1.65.0/gramex-1.65.0.tar.gz

---
+++ packages/gramex/gramex-1.65.0.tar.gz/gramex-1.65.0/docs/gramex.apps.logviewer.rst
@@ -0,0 +1,22 @@
+gramex.apps.logviewer package
+=============================
+
+Submodules
+----------
+
+gramex.apps.logviewer.logviewer module
+--------------------------------------
+
+.. automodule:: gramex.apps.logviewer.logviewer
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
+Module contents
+---------------
+
+.. automodule:: gramex.apps.logviewer
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ packages/gramex/gramex-1.65.0.tar.gz/gramex-1.65.0/gramex/transforms/__init__.py
@@ -0,0 +1,23 @@
+'''Functions to transform data'''
+
+from .transforms import build_transform, build_log_info, condition, flattener, once, handler
+from .badgerfish import badgerfish
+from .template import template, CacheLoader
+from .rmarkdown import rmarkdown
+from .auth import ensure_single_session
+from .twitterstream import TwitterStream
+
+__all__ = [
+    'build_transform',
+    'build_log_info',
+    'badgerfish',
+    'template',
+    'rmarkdown',
+    'ensure_single_session',
+    'condition',
+    'flattener',
+    'once',
+    'CacheLoader',
+    'TwitterStream',
+    'handler'
+]
