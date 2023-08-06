25fa388d4ae156c78242efe8c1e97e43c5c5afaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:28 +0000

    Add package project1-Militello-Bosco-Passarelli/3.0.0/project1_Militello_Bosco_Passarelli-3.0.0-py3-none-any.whl

---
+++ packages/project1-Militello-Bosco-Passarelli/project1_Militello_Bosco_Passarelli-3.0.0-py3-none-any.whl/country_package/countries.py
@@ -0,0 +1,30 @@
+import sqlite3
+import pandas as pd
+import country_package.funzioni_country as fc
+
+conn = sqlite3.connect('TestDB.db')  
+
+c = conn.cursor() # The database will be saved in the location where your 'py' file is saved
+
+c.execute('''CREATE TABLE IF NOT EXISTS COUNTRY
+             ([generated_id] INTEGER PRIMARY KEY,[Country_ID] text, [Country_Name] text, [Population_Milion] float)''')
+
+#per rendere permanente il cambiamento
+conn.commit()
+
+read_country = pd.read_csv ('country.csv')
+read_country.to_sql('COUNTRY', conn, if_exists='replace', index = True) # Replace the values from the csv file into the table 'COUNTRY'
+
+c.execute('''
+SELECT DISTINCT *
+FROM COUNTRY
+          ''')
+
+df = pd.read_sql_query("SELECT * FROM COUNTRY", conn)
+
+print(c.fetchall())
+conn.close()
+
+somma = fc.sumPopulation(df["Population_Milion"])
+name_max = fc.maxPopulation(df)
+name_sort = fc.orderedState(df["Country_Name"])
