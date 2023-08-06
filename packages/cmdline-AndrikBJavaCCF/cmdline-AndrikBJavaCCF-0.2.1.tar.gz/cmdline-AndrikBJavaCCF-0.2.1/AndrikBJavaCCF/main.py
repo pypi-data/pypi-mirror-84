5ce1262943a17b76b0a2f560158d87ae306c2df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:18 +0000

    Add package cmdline-AndrikBJavaCCF/0.2.1/cmdline-AndrikBJavaCCF-0.2.1.tar.gz

---
+++ packages/cmdline-AndrikBJavaCCF/cmdline-AndrikBJavaCCF-0.2.1.tar.gz/cmdline-AndrikBJavaCCF-0.2.1/AndrikBJavaCCF/main.py
@@ -0,0 +1,59 @@
+import sys
+
+from .file import File
+from .formatter import get_files, get_files_rec, fix, validate
+
+__version__ = "0.2.1"
+
+
+def print_help():
+    print(
+        """
+***************** examples *****************
+AndrikBJavaCCF --verify -(p|d|f) /..
+AndrikBJavaCCF -v -(p|d|f) /..
+AndrikBJavaCCF --fix -(p|d|f) /..
+AndrikBJavaCCF -f  -(p|d|f) /..
+AndrikBJavaCCF --help
+AndrikBJavaCCF -h
+where :
+-p - project
+-d - directory
+-f - file
+/.. - path to project, directory or file
+        """
+    )
+
+
+def main():
+    if len(sys.argv) == 1:
+        print("Error! Please, write arguments", '\n')
+        print_help()
+    else:
+        if sys.argv[1] in ('-h', '--help'):
+            print_help()
+        elif len(sys.argv) != 4:
+            print("Error! Please, write arguments\n")
+            print_help()
+        else:
+            mode = sys.argv[1]
+            if len(mode) > 2:
+                mode = mode[1:3]
+
+            p_d_f = sys.argv[2]
+            path = sys.argv[3]
+
+            if p_d_f == '-p':
+                files = get_files_rec(path)
+            elif p_d_f == '-d':
+                files = get_files(path)
+            else:
+                files = [path]
+
+            for i in range(len(files)):
+                files[i] = File(files[i])
+
+            if mode == '-v':
+                validate(files)
+            else:
+                fix(files)
