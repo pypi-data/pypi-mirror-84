b4e8051e41aa581d72bdece8b2c6a5bb5f6e968c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:49 +0000

    Add package HelloSpark/0.0.1/HelloSpark-0.0.1.tar.gz

---
+++ packages/HelloSpark/HelloSpark-0.0.1.tar.gz/HelloSpark-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+long_description = "PyTest Project"
+
+setuptools.setup(
+    name="HelloSpark", # Replace with your own username
+    version="0.0.1",
+    author="Leah Banks",
+    author_email="leah_banks@navyfederal.org",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
