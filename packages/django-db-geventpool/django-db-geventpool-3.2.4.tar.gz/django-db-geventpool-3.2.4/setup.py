de500b0cd9f6f240845159db77abe32bc149b030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:54 +0000

    Add package django-db-geventpool/3.2.4/django-db-geventpool-3.2.4.tar.gz

---
+++ packages/django-db-geventpool/django-db-geventpool-3.2.4.tar.gz/django-db-geventpool-3.2.4/setup.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+import codecs
+
+from setuptools import setup, find_packages
+
+
+def long_description():
+    try:
+        with codecs.open('README.rst', 'r', 'utf-8') as f:
+            return f.read()
+    except:
+        return 'Error loading README.rst'
+
+
+setup(
+    name='django-db-geventpool',
+    version='3.2.4',
+    install_requires=[
+        'django>=1.11',
+        'django<3.1',
+        'psycogreen>=1.0',
+    ],
+    url='https://github.com/jneight/django-db-geventpool',
+    description='Add a DB connection pool using gevent to django',
+    long_description=long_description(),
+    long_description_content_type='text/x-rst',
+    packages=find_packages(),
+    include_package_data=True,
+    license='Apache 2.0',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    author='Javier Cordero Martinez',
+    author_email='j@jcmz.me'
+)
