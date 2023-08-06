374c4bf8fe9b17f8540529752e65b1ee32a98703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:22 +0000

    Add package zope.app.publication/4.5/zope.app.publication-4.5.tar.gz

---
+++ packages/zope.app.publication/zope.app.publication-4.5.tar.gz/zope.app.publication-4.5/setup.py
@@ -0,0 +1,108 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+TEST_REQUIREMENTS = [
+    'zope.annotation',
+    'zope.app.appsetup >= 3.14.0',
+    'zope.app.http >= 4.0',
+    'zope.app.wsgi[testlayer] >= 4.3',
+    'zope.applicationcontrol>=4.0.0a1',
+    'zope.browserpage',
+    'zope.login',
+    'zope.password',
+    'zope.principalregistry',
+    'zope.securitypolicy',
+    'zope.site>=4.0.0a1',
+    'zope.testing',
+    'zope.testrunner',
+    'ZODB >= 5.1',
+]
+
+setup(
+    name='zope.app.publication',
+    version='4.5',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    description='Zope publication',
+    long_description=(
+        read('README.rst') +
+        '\n\n' +
+        read('CHANGES.rst')
+    ),
+    license='ZPL 2.1',
+    keywords="zope publication",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope :: 3',
+    ],
+    url='http://pypi.python.org/pypi/zope.app.publication',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['zope', 'zope.app'],
+    extras_require=dict(test=TEST_REQUIREMENTS),
+    python_requires=', '.join([
+        '>=2.7',
+        '!=3.0.*',
+        '!=3.1.*',
+        '!=3.2.*',
+        '!=3.3.*',
+        '!=3.4.*',
+    ]),
+    install_requires=[
+        'six',
+        'zope.interface',
+        'zope.authentication',
+        'zope.component',
+        'zope.error',
+        'zope.browser>=1.2',
+        'zope.location',
+        'zope.publisher>=4.0.0a2',
+        'zope.traversing>=4.0.0a2',
+        # 'zope.untrustedpython',
+        'zope.i18n>=4.0.0a3',
+        'transaction>=1.1.0',
+        'setuptools',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
