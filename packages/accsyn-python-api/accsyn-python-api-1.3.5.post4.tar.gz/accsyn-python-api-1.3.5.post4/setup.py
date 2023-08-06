c83df6dcc32ba92eadc6045c8e5c94bff2e8516b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:06 +0000

    Add package accsyn-python-api/1.3.5.post4/accsyn-python-api-1.3.5.post4.tar.gz

---
+++ packages/accsyn-python-api/accsyn-python-api-1.3.5.post4.tar.gz/accsyn-python-api-1.3.5.post4/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+	long_description = fh.read()
+
+setuptools.setup(
+
+	 name='accsyn-python-api',  
+
+	 version='1.3.5-4',
+
+	 packages=['accsyn_api'],
+
+	 author="Henrik Norin",
+
+	 author_email="henrik.norin@accsyn.com",
+
+	 description="A Python API for controlling Accsyn fast data delivery software",
+
+	 long_description=long_description,
+
+	 long_description_content_type="text/markdown",
+
+	 url="https://github.com/accsyn/accsyn-python-api.git",
+
+	 classifiers=[
+
+		 "Programming Language :: Python :: 2",
+
+		 "License :: OSI Approved :: MIT License",
+
+		 "Operating System :: OS Independent",
+
+	 ],
+
+ )
