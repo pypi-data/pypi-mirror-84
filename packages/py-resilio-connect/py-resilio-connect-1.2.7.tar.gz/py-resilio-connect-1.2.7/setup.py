28dec1fb74d951c7521b4b08bc67849f4d55ecd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:49 +0000

    Add package py-resilio-connect/1.2.7/py-resilio-connect-1.2.7.tar.gz

---
+++ packages/py-resilio-connect/py-resilio-connect-1.2.7.tar.gz/py-resilio-connect-1.2.7/setup.py
@@ -0,0 +1,11 @@
+import setuptools 
+
+setuptools.setup(name='py-resilio-connect',
+      version='1.2.7',
+      description='Generate simple Resilio Connect Sync Jobs based on a csv file',
+      url='https://github.com/ilanshamir/connect-scripts.git',
+      author='Ilan Shamir',
+      author_email='team@resilio.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
