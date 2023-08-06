7d471d4140d7ddb1d44a6281a9629bdd20e736ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:44 +0000

    Add package minesweeper-client/0.2.1/minesweeper_client-0.2.1-py3-none-any.whl

---
+++ packages/minesweeper-client/minesweeper_client-0.2.1-py3-none-any.whl/minesweeper/game.py
@@ -0,0 +1,37 @@
+from http import HTTPStatus
+
+from .base import HttpClient
+from . import exceptions
+from .constants import EVENT_CLICK_NAIVE
+
+
+class Game(HttpClient):
+    _game_instance = None
+
+    def create(self, **kwargs):
+        resp = self.post("games", json=kwargs)
+        self._game_instance = resp.json()
+
+    def get_one(self, game_id):
+        resp = self.get(f"games/{game_id}")
+        if resp.status_code == HTTPStatus.NOT_FOUND:
+            raise exceptions.GameNotfound(f"Game {game_id} does not exist")
+        self._game_instance = resp.json()
+
+    def events(self, game_id):
+        resp = self.get(f"games/{game_id}/events")
+        if resp.status_code == HTTPStatus.NOT_FOUND:
+            raise exceptions.GameNotfound(f"Game {game_id} does not exist")
+        return resp.json()
+
+    def click(self, row, col):
+        game_id = self._game_instance["id"]
+        data = {
+            "game": game_id,
+            "type": EVENT_CLICK_NAIVE,
+            "row": row,
+            "col": col,
+        }
+
+        resp = self.post(f"games/{game_id}/events", json=data)
+        return resp.json()
