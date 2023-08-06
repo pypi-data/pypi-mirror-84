adcc77a245186202b9b30e219298f8f2110b321d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:10 +0000

    Add package linuxvoice/1.3/linuxvoice-1.3.tar.gz

---
+++ packages/linuxvoice/linuxvoice-1.3.tar.gz/linuxvoice-1.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="linuxvoice", # Replace with your own username
+    version="1.3",
+    author="Jeffrin Jose T",
+    author_email="jeffrin@rajagiritech.edu.in",
+    description="A small package for downloading LINUXVOICE issues",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ahiliation/DOLLECT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
