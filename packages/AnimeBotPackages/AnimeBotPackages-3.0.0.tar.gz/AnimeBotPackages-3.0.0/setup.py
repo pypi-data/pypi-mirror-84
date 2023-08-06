b37db9910b853a92fbf90bf033e176c1e1ca3ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:13 +0000

    Add package AnimeBotPackages/3.0.0/AnimeBotPackages-3.0.0.tar.gz

---
+++ packages/AnimeBotPackages/AnimeBotPackages-3.0.0.tar.gz/AnimeBotPackages-3.0.0/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="AnimeBotPackages",
+    version="3.0.0",
+    author="Pirxcy",
+    description="A Multi Action Discord Bot Based On Anime",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PirxcyFinal/AnimeBot/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'crayons',
+        'dhooks',
+        'discordpy',
+        'discord',
+        'discord.py',
+        'timeago',
+        'jinja2',
+        'jaconv',
+        'requests',
+        'psutil',
+        'pypresence',
+        'BenBotAsync',
+        'uvloop',
+        'sanic',
+        'aiohttp'
+    ],
+)
