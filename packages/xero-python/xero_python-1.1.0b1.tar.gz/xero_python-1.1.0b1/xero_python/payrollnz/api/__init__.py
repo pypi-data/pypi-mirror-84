9e17925d3549861562762cecb27521662624d332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:20 +0000

    Add package xero-python/1.1.0b1/xero_python-1.1.0b1.tar.gz

---
+++ packages/xero-python/xero_python-1.1.0b1.tar.gz/xero_python-1.1.0b1/xero_python/payrollnz/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from xero_python.payrollnz.api.payroll_nz_api import PayrollNZApi
+from xero_python.payrollnz.api.payroll_nz_api import PayrollNzApi
