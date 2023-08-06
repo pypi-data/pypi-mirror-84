c45bc387d8f6175786e8e6e2782b7aff9061d1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:53 +0000

    Add package pyhustler/0.2.0/pyhustler-0.2.0.tar.gz

---
+++ packages/pyhustler/pyhustler-0.2.0.tar.gz/pyhustler-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyhustler']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pyhustler',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'ashdaily',
+    'author_email': 'ashtokyo31@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
