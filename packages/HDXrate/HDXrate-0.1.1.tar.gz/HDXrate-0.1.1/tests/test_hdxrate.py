daffb5cbc551d7f6fe64050bd3753cf5e72a9f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:43 +0000

    Add package HDXrate/0.1.1/HDXrate-0.1.1.tar.gz

---
+++ packages/HDXrate/HDXrate-0.1.1.tar.gz/HDXrate-0.1.1/tests/test_hdxrate.py
@@ -0,0 +1,26 @@
+"""Tests for `hdxrate` package."""
+
+import numpy as np
+from hdxrate.expfact.api import calc_k_int as expfact_k_int
+from hdxrate.psx.api import calc_k_int as psx_k_int
+from hdxrate import k_int_from_sequence
+
+
+class TestHDXrate(object):
+    """Tests for `hdxrate` package."""
+
+    @classmethod
+    def setup_class(cls):
+        cls.sequence = list('MSEQNNTEMTFQIQRIYTK')
+
+    def test_k_int_calculation(self):
+
+        expfact = expfact_k_int(self.sequence, 300, 8.)
+        k_int = k_int_from_sequence(self.sequence, 300, 8., 'expfact')
+        assert np.allclose(k_int, expfact)
+
+        psx = psx_k_int(self.sequence, 300, 8., 'poly')
+        k_int = k_int_from_sequence(self.sequence, 300, 8., 'psx')
+
+        assert np.allclose(k_int, psx)
+
