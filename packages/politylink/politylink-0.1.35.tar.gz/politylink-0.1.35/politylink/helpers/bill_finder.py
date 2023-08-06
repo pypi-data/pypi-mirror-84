ca362237adc619dc6600ecde1db24d294361f62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:50 +0000

    Add package politylink/0.1.35/politylink-0.1.35.tar.gz

---
+++ packages/politylink/politylink-0.1.35.tar.gz/politylink-0.1.35/politylink/helpers/bill_finder.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+from politylink.graphql.client import GraphQLClient
+
+
+class BillFinder:
+    """
+    Cache based Bill finder
+    """
+
+    def __init__(self, bills=None):
+        if bills:
+            self.bills = bills
+        else:
+            client = GraphQLClient()
+            self.bills = client.get_all_bills(['id', 'name', 'bill_number'])
+        self.name2bills = defaultdict(list)
+        self.number2bill = defaultdict(list)
+        for bill in self.bills:
+            if hasattr(bill, 'name'):
+                self.name2bills[bill.name].append(bill)
+            if hasattr(bill, 'bill_number'):
+                self.number2bill[bill.bill_number].append(bill)
+
+    def find(self, text):
+        return self.name2bills[text] + self.number2bill[text]
