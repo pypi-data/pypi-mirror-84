2339236dd054cb182373b53480393edfbba0e64b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:08 +0000

    Add package aio_wx_widgets/1.0.3/aio_wx_widgets-1.0.3-py3-none-any.whl

---
+++ packages/aio_wx_widgets/aio_wx_widgets-1.0.3-py3-none-any.whl/aio_wx_widgets/panels/panel.py
@@ -0,0 +1,32 @@
+"""Panel. Sits in a frame. Holds widgets."""
+from __future__ import annotations
+
+import logging
+
+import wx
+from wx.lib.scrolledpanel import ScrolledPanel
+from aio_wx_widgets import type_annotations as T  # noqa
+from aio_wx_widgets.core.sizers import PanelMixin, SizerMixin
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SimplePanel(PanelMixin, SizerMixin):
+    """A simple panel."""
+
+    def __init__(self, parent, scrollable=False, **kwargs):
+        """Init."""
+
+        if scrollable:
+            self.ui_item = ScrolledPanel(parent)
+            self.ui_item.SetupScrolling()
+        else:
+            self.ui_item = wx.Panel(parent)
+        super().__init__()
+
+        self._sizer = wx.BoxSizer(wx.VERTICAL)
+        self.ui_item.SetSizer(self._sizer)
+
+    @property
+    def controller(self) -> T.BaseController:
+        raise NotImplementedError()
