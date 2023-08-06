01770b34f664456f6271a409fd325217f5fe1f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:27 +0000

    Add package pyworked/0.2.0a2/pyworked-0.2.0a2.tar.gz

---
+++ packages/pyworked/pyworked-0.2.0a2.tar.gz/pyworked-0.2.0a2/pywork/modules/web_server.py
@@ -0,0 +1,22 @@
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+class Web:
+
+    def __init__(self):
+        pass
+
+    def start(self, adr):
+        httpd = HTTPServer(adr, CGIHTTPRequestHandler)
+        httpd.serve_forever()
+
+    def parse(self, file):
+        f = open(file, 'r').read()
+        return(f)
+
+    def heading(self):
+        return("Content-type: text/html")
+
+    def get_style(self, file, htm):
+        css = open(file, 'r').read()
+        ht = open(htm, 'a')
+        ht.write('<style>' + str(css) + '</style>')
