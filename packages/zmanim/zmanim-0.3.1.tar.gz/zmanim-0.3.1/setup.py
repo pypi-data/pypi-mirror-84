e96266e7ad34b7294625b07a4eed973f6b268574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:17 +0000

    Add package zmanim/0.3.1/zmanim-0.3.1.tar.gz

---
+++ packages/zmanim/zmanim-0.3.1.tar.gz/zmanim-0.3.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zmanim",
+    version="0.3.1",
+    author="Pinny Markowitz",
+    author_email="pinny@mwitz.com",
+    description="A Zmanim library for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pinnymz/python-zmanim",
+    packages=setuptools.find_packages(exclude=['test']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['python-dateutil', 'julian', 'memoization'],
+    python_requires='>=3.6'
+)
