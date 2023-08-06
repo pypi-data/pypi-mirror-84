652eaf4a531e461febf16c27973d511eb6c328d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:27 +0000

    Add package luckyhomenumber/0.0.3/luckyhomenumber-0.0.3-py3-none-any.whl

---
+++ packages/luckyhomenumber/luckyhomenumber-0.0.3-py3-none-any.whl/luckyhomenumber/__init__.py
@@ -0,0 +1,4 @@
+from luckyhomenumber.homenumber import Homenumber
+
+print(f"Please try to create your home number object e.g. hm = Homenumber('1488/186')")
+print('Then you can use hm.is_good_address() to check if your home address number is lucky')
