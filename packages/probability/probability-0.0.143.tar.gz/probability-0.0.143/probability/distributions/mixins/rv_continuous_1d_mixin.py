015d9ed04901f478ccc39200e1867c52f93e090d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:04 +0000

    Add package probability/0.0.143/probability-0.0.143.tar.gz

---
+++ packages/probability/probability-0.0.143.tar.gz/probability-0.0.143/probability/distributions/mixins/rv_continuous_1d_mixin.py
@@ -0,0 +1,35 @@
+from matplotlib.axes import Axes
+from scipy.stats import rv_continuous
+from typing import Iterable
+
+from probability.distributions.mixins.plottable_mixin import \
+    ContinuousPlottableMixin
+from probability.distributions.mixins.rv_mixins import RVS1dMixin, \
+    Moment1dMixin, EntropyMixin, Median1dMixin, Mean1dMixin, StD1dMixin, \
+    Var1dMixin, Interval1dMixin, Support1dMixin, PDF1dMixin, \
+    CDFContinuous1dMixin, SFContinuous1dMixin, PPFContinuous1dMixin, \
+    ISFContinuous1dMixin, StatMixin
+
+
+class RVContinuous1dMixin(
+    RVS1dMixin, Moment1dMixin, EntropyMixin, Median1dMixin, Mean1dMixin,
+    StD1dMixin, Var1dMixin, Interval1dMixin, Support1dMixin, PDF1dMixin,
+    CDFContinuous1dMixin, SFContinuous1dMixin, PPFContinuous1dMixin,
+    ISFContinuous1dMixin, ContinuousPlottableMixin, StatMixin, object
+):
+
+    _distribution: rv_continuous
+    _num_samples: int = 1000000
+
+    def plot(self, x: Iterable, kind: str = 'line', color: str = 'C0',
+             ax: Axes = None, **kwargs) -> Axes:
+        """
+        Plot the PDF of the distribution.
+
+        :param x: Range of values of x to plot p(x) over.
+        :param kind: Kind of plot e.g. 'bar', 'line'.
+        :param color: Optional color for the series.
+        :param ax: Optional matplotlib axes to plot on.
+        :param kwargs: Additional arguments for the matplotlib plot function.
+        """
+        return self.pdf().plot(x=x, kind=kind, color=color, ax=ax, **kwargs)
---
+++ packages/probability/probability-0.0.143.tar.gz/probability-0.0.143/probability/distributions/mixins/rv_discrete_1d_mixin.py
@@ -0,0 +1,35 @@
+from matplotlib.axes import Axes
+from scipy.stats import rv_discrete
+from typing import Iterable, Optional
+
+from probability.distributions.mixins.plottable_mixin import \
+    DiscretePlottableMixin
+from probability.distributions.mixins.rv_mixins import RVS1dMixin, \
+    EntropyMixin, Median1dMixin, Mean1dMixin, StD1dMixin, Var1dMixin, \
+    Interval1dMixin, PMF1dMixin, CDFDiscrete1dMixin, SFDiscrete1dMixin, \
+    PPFDiscrete1dMixin, ISFDiscrete1dMixin, StatMixin
+
+
+class RVDiscrete1dMixin(
+    RVS1dMixin, EntropyMixin, Median1dMixin, Mean1dMixin, StD1dMixin,
+    Var1dMixin, Interval1dMixin, PMF1dMixin, CDFDiscrete1dMixin,
+    SFDiscrete1dMixin, PPFDiscrete1dMixin, ISFDiscrete1dMixin,
+    DiscretePlottableMixin, StatMixin, object
+):
+
+    _distribution: rv_discrete
+    _num_samples: int = 1000000
+
+    def plot(self, k: Iterable, kind: str = 'bar', color: str = 'C0',
+             ax: Optional[Axes] = None,
+             **kwargs) -> Axes:
+        """
+        Plot the PMF of the distribution.
+
+        :param k: Range of values of k to plot p(k) over.
+        :param color: Optional color for the series.
+        :param kind: Kind of plot e.g. 'bar', 'line'.
+        :param ax: Optional matplotlib axes to plot on.
+        :param kwargs: Additional arguments for the matplotlib plot function.
+        """
+        return self.pmf().plot(k=k, kind=kind, color=color, ax=ax, **kwargs)
