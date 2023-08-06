6dda0eca96c103a9ee22e54e84d75562449d90c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:16 +0000

    Add package django-flags/5.0.2/django_flags-5.0.2-py2.py3-none-any.whl

---
+++ packages/django-flags/django_flags-5.0.2-py2.py3-none-any.whl/flags/state.py
@@ -0,0 +1,36 @@
+from django.apps import apps
+from django.core.exceptions import AppRegistryNotReady
+
+from flags.sources import get_flags
+
+
+def _flag_state(flag_name, **kwargs):
+    """ This is a private function that performs the actual state checking """
+    flags = get_flags(request=kwargs.get("request"))
+
+    flag = flags.get(flag_name)
+    if flag is not None:
+        return flag.check_state(**kwargs)
+
+    return None
+
+
+def flag_state(flag_name, **kwargs):
+    """ Return the value for the flag by passing kwargs to its conditions """
+    if not apps.ready:
+        raise AppRegistryNotReady(
+            "Feature flag state cannot be checked before the app registry "
+            "is ready."
+        )
+
+    return _flag_state(flag_name, **kwargs)
+
+
+def flag_enabled(flag_name, **kwargs):
+    """ Check if a flag is enabled by passing kwargs to its conditions. """
+    return flag_state(flag_name, **kwargs)
+
+
+def flag_disabled(flag_name, **kwargs):
+    """ Check if a flag is disabled by passing kwargs to its conditions. """
+    return not flag_state(flag_name, **kwargs)
