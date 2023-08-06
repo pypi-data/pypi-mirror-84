c830699ae73846e995c7a2f54896a46bc8c4477e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:10 +0000

    Add package pygame-frame/0.0.6/pygame_frame-0.0.6.tar.gz

---
+++ packages/pygame-frame/pygame_frame-0.0.6.tar.gz/pygame_frame-0.0.6/pyframe/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.6"
+try :
+    import pyframe.constants as constants
+    import pyframe.functions as functions
+    import pyframe.classes as classes
+    import pyframe.exception as exception
+except Exception as e :
+    print("PYFRAME ERROR: Impossible d'importer correctement la librairie")
+    print("(",end="")
+    print(e,end="")
+    print(")")
