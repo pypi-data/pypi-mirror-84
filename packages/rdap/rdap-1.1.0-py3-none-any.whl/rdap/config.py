7e7319a939a731597e1520bd9c26060a214bfce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:51 +0000

    Add package rdap/1.1.0/rdap-1.1.0-py3-none-any.whl

---
+++ packages/rdap/rdap-1.1.0-py3-none-any.whl/rdap/config.py
@@ -0,0 +1,20 @@
+import munge
+
+
+class Config(munge.Config):
+    """
+    command line interface config
+    """
+
+    defaults = {
+        "config": {
+            "rdap": {
+                "bootstrap_url": "https://rdap.org/",
+                "output_format": "yaml",
+                "recurse_roles": ["administrative", "technical"],
+                "lacnic_apikey": None,
+                "timeout": 5,
+            },
+        },
+        "codec": "yaml",
+    }
