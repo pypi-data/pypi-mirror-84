c4deb35ff50fa1f1b69800150bfe7e1f85b080bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:39 +0000

    Add package kg-keycloak/0.7.6/kg_keycloak-0.7.6-py3-none-any.whl

---
+++ packages/kg-keycloak/kg_keycloak-0.7.6-py3-none-any.whl/kg_keycloak/__init__.py
@@ -0,0 +1,13 @@
+from .builder import (
+    KeycloakBuilder
+)
+from .option import (
+    KeycloakOptions
+)
+
+__version__ = "0.7.6"
+
+__all__ = [
+    'KeycloakBuilder',
+    'KeycloakOptions',
+]
