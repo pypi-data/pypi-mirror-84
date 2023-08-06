7a92575e336ca9449b6680ca24712cf7e420c16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:46 +0000

    Add package pyhoo/0.1.1/pyhoo-0.1.1.tar.gz

---
+++ packages/pyhoo/pyhoo-0.1.1.tar.gz/pyhoo-0.1.1/pyhoo/models/iterables.py
@@ -0,0 +1,71 @@
+"""
+    Define a `CustomIterable[T]` generic type, to be used as an `attr.ib()`
+    to parse sequences of any type with:
+    `<ATTR_NAME>: CustomIterable = attr.ib(converter=CustomIterable)`.
+
+    Ex:
+        # Timestamp is defined below
+        timestamp: Timestamp = attr.ib(converter=Timestamp)
+"""
+from __future__ import annotations
+
+import datetime
+from typing import Generic, List, TypeVar
+
+from pyhoo.models.abc import BaseModel
+
+T = TypeVar("T")
+
+
+class CustomIterable(BaseModel, Generic[T]):
+
+    _values: List[T]
+
+    def __init__(self, values: List[T]) -> None:
+        self._values = values
+
+    def __iter__(self) -> "CustomIterator[T]":
+        return CustomIterator(self)
+
+    def __getitem__(self, index: int) -> T:
+        return self._values[index]
+
+    def __len__(self) -> int:
+        return len(self._values)
+
+    def __add__(self, custom_iterable: CustomIterable[T]) -> CustomIterable[T]:
+        return self.__class__(self._values + custom_iterable._values)
+
+
+class CustomIterator(Generic[T]):
+
+    _iterable: CustomIterable[T]
+    _index: int
+
+    def __init__(self, iterable: CustomIterable[T]) -> None:
+        self._iterable = iterable
+        self._index = 0
+
+    def __next__(self) -> T:
+        if self._index < len(self._iterable):
+            result = self._iterable[self._index]
+            self._index += 1
+            return result
+        raise StopIteration
+
+    def __iter__(self) -> CustomIterator[T]:
+        return self
+
+
+class Timestamp(CustomIterable[int]):
+    def to_str(self, format: str = "%Y-%m-%dT%H:%M:%S") -> List[str]:
+        """Convert each timestamp to a datetime object and then format it as specified."""
+        return list(map(lambda date: date.strftime(format), self.to_datetime()))
+
+    def to_datetime(self) -> List[datetime.datetime]:
+        """Convert each timestamp to a datetime object."""
+        return list(map(datetime.datetime.fromtimestamp, self))
+
+
+class Strikes(CustomIterable[float]):
+    pass
