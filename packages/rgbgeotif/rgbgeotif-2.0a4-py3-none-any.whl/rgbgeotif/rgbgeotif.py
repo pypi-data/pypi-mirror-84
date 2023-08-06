5ac084370280e264f083e9e6f7b6b6039e32cb72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:55 +0000

    Add package rgbgeotif/2.0a4/rgbgeotif-2.0a4-py3-none-any.whl

---
+++ packages/rgbgeotif/rgbgeotif-2.0a4-py3-none-any.whl/rgbgeotif/rgbgeotif.py
@@ -0,0 +1,30 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import rasterio
+
+class RGBGeoTif(list):
+    def __init__(self, file, return_img=True, range_scale=None):
+        src = rasterio.open(file)
+        arrays = src.read()
+        if return_img:
+            if range_scale is not None:
+                arrays = np.uint8([255 * ((array - range_scale[0])/(range_scale[1] - range_scale[0]))
+                                   for array in arrays])
+                [self.append(v) for v in arrays]
+            else:
+                for array in arrays:
+                    fig = plt.figure(figsize=array.shape, dpi=1)
+                    plt.set_cmap("Greys")
+                    plt.axis('off')
+                    plt.imshow(array)
+                    fig.tight_layout()
+                    fig.canvas.draw()
+                    w, h = fig.canvas.get_width_height()
+                    image = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8)
+                    image.shape = (w, h, 3)
+                    image = np.roll(image, 3, axis=2)
+                    self.append(image)
+                    plt.close(fig)
+        else:
+            [self.append(v) for v in arrays]
+
