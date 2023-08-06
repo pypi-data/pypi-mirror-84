0c86ac1ea20439ce7f972404fbedcf59653b0aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:23 +0000

    Add package setuptools-rust/0.11.4/setuptools-rust-0.11.4.tar.gz

---
+++ packages/setuptools-rust/setuptools-rust-0.11.4.tar.gz/setuptools-rust-0.11.4/html-py-ever/test/test_selector.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from glob import glob
+
+import html_py_ever
+import pytest
+from bs4 import BeautifulSoup
+
+
+@pytest.mark.parametrize("filename", list(glob("*.html")))
+def test_bench_selector_rust(benchmark, filename):
+    document = html_py_ever.parse_file(filename)
+    benchmark(document.select, "a[href]")
+
+
+@pytest.mark.parametrize("filename", list(glob("*.html")))
+def test_bench_selector_python(benchmark, filename):
+    with open(filename, encoding='utf8') as fp:
+        soup = BeautifulSoup(fp, "html.parser")
+    benchmark(soup.select, "a[href]")
