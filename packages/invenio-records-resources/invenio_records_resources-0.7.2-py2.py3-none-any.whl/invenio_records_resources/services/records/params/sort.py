50c443f5d1a4c2f98952c4d761b02338e6508cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:00 +0000

    Add package invenio-records-resources/0.7.2/invenio_records_resources-0.7.2-py2.py3-none-any.whl

---
+++ packages/invenio-records-resources/invenio_records_resources-0.7.2-py2.py3-none-any.whl/invenio_records_resources/services/records/params/sort.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Invenio-Records-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Sort parameter interpreter API."""
+
+from marshmallow import ValidationError
+
+from .base import ParamInterpreter
+
+
+class SortParam(ParamInterpreter):
+    """Evaluate the 'sort' parameter."""
+
+    def _default_sort(self, params, options):
+        if params.get('q'):
+            return self.config.search_sort_default
+        else:
+            return self.config.search_sort_default_no_query
+
+    def apply(self, identity, search, params):
+        """Evaluate the sort parameter on the search."""
+        options = self.config.search_sort_options
+
+        if 'sort' not in params:
+            params['sort'] = self._default_sort(params, options)
+
+        sort = options.get(params['sort'])
+        if sort is None:
+            raise ValidationError(f"Invalid sort option '{params['sort']}'.")
+
+        return search.sort(*sort['fields'])
