40f74ff71b5168d02aeb85d7ca52b894c90941b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:30 +0000

    Add package revcore-micro/0.5.4/revcore_micro-0.5.4-py3-none-any.whl

---
+++ packages/revcore-micro/revcore_micro-0.5.4-py3-none-any.whl/revcore_micro/flask/utils.py
@@ -0,0 +1,17 @@
+from functools import wraps
+from flask import request
+
+def with_page(model):
+    def decorated(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            key = {model.partition_key: request.args.get(model.partition_key)}
+            if model.sort_key:
+                key[model.sort_key] = request.args.get(model.sort_key)
+
+            kwargs['last_key'] = key
+            return f(*args, **kwargs)
+
+        return wrapped
+
+    return decorated
