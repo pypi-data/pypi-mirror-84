f7f9bcfab48662b0bf9dc14f3758d68461c37733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:50 +0000

    Add package ChadBot2/0.1/ChadBot2-0.1.tar.gz

---
+++ packages/ChadBot2/ChadBot2-0.1.tar.gz/ChadBot2-0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name = "ChadBot2",
+    version = "0.1",
+    author = "Rushi Babaria",
+    url = "https://github.com/captanlevi/FAQ-QnA-matching.git",
+    packages = setuptools.find_packages()
+)
+
