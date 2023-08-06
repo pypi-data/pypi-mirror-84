bec499cb40350aa36b633b2c0db4e77e51103d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:00 +0000

    Add package python-cobol/0.1.3/python_cobol-0.1.3.tar.gz

---
+++ packages/python-cobol/python_cobol-0.1.3.tar.gz/python_cobol-0.1.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='python_cobol',
+    version='0.1.3',
+    url='https://github.com/royopa/python-cobol',
+    license='GPLv3',
+    author='Paulus Schoutsen',
+    author_email='paulus@paulusschoutsen.nl',
+    keywords='cobol mainframe copybook',
+    description=u'Python code to parse and denormalize COBOL Copybooks',
+    packages=['python_cobol'],
+    install_requires=[],
+)
