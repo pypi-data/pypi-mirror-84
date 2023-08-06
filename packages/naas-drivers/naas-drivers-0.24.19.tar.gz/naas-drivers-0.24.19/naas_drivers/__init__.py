9daadf8f12bd593111c05570869a607c75b3898f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:46 +0000

    Add package naas-drivers/0.24.19/naas-drivers-0.24.19.tar.gz

---
+++ packages/naas-drivers/naas-drivers-0.24.19.tar.gz/naas-drivers-0.24.19/naas_drivers/__init__.py
@@ -0,0 +1,71 @@
+from in_drivers import (
+    YahooFinance,
+    Cityfalcon,
+    Geolocator,
+    Newsapi,
+    Prediction,
+    Sentiment,
+    Pdf,
+    Plotly,
+    Html,
+)
+from out_drivers import Bubble, Email, Healthcheck, Ifttt, Integromat, Zappier
+from in_out_drivers import Bobapp, Airtable, Jupyter, Ftp, Git, Gsheet, Mongo, Toucan
+import requests
+import os
+
+__version__ = "0.24.19"
+
+__github_repo = "jupyter-naas/drivers"
+
+if os.environ.get("NAAS_DRIVER_LIGHT_INIT"):
+    exit()
+
+# In drivers
+cityfalcon = Cityfalcon()
+geolocator = Geolocator()
+newsapi = Newsapi()
+prediction = Prediction()
+sentiment = Sentiment()
+yahoofinance = YahooFinance()
+pdf = Pdf()
+plotly = Plotly()
+html = Html()
+
+# Out drivers
+bubble = Bubble()
+email = Email()
+healthcheck = Healthcheck()
+ifttt = Ifttt()
+integromat = Integromat()
+zappier = Zappier()
+
+# InOut drivers
+bobapp = Bobapp()
+airtable = Airtable()
+jupyter = Jupyter()
+ftp = Ftp()
+git = Git()
+gsheet = Gsheet()
+mongo = Mongo()
+toucan = Toucan()
+
+__doc_url = "https://naas.gitbook.io/drivers/"
+
+
+def doc():
+    return __doc_url
+
+
+def version():
+    print(__version__)
+
+
+def get_last_version():
+    url = f"https://api.github.com/repos/{__github_repo}/tags"
+    response = requests.get(url, headers={"Accept": "application/vnd.github.v3+json"})
+    return response.json()[0]["name"]
+
+
+def up_to_date():
+    return get_last_version() == version()
