452b8c8bdfc9586b7e2e0f99f2078d488c28e984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:28 +0000

    Add package project1-Militello-Bosco-Passarelli/3.0.0/project1_Militello_Bosco_Passarelli-3.0.0.tar.gz

---
+++ packages/project1-Militello-Bosco-Passarelli/project1_Militello_Bosco_Passarelli-3.0.0.tar.gz/project1_Militello_Bosco_Passarelli-3.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="project1_Militello_Bosco_Passarelli", 
+    version="3.0.0",
+    author="Patrick Militello, Alessandro Bosco, Raffaello Passarelli",
+    author_email="p.militello@campus.unimib.it, a.bosco10@campus.unimib.it, r.passarelli@campus.unimib.it",
+    description="Applicativo per semplici calcoli sulla popolazione di 10 paesi del mondo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/theunusual/2020_assignment1_countries",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    include_package_data=True
+)
+
