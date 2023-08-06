9fccfbbb3b06b53522338d8037ec69e6c029dbd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:36 +0000

    Add package json-logging/1.2.11/json-logging-1.2.11.tar.gz

---
+++ packages/json-logging/json-logging-1.2.11.tar.gz/json-logging-1.2.11/README.rst
@@ -0,0 +1,8 @@
+json-logging
+============
+
+Python logging library to emit JSON log that can be easily indexed and
+searchable by logging infrastructure such as
+`ELK <https://www.elastic.co/webinars/introduction-elk-stack>`_.
+
+Go here for full documentation: https://github.com/thangbn/json-logging-python
---
+++ packages/json-logging/json-logging-1.2.11.tar.gz/json-logging-1.2.11/setup.py
@@ -0,0 +1,37 @@
+import io
+import sys
+
+from setuptools import setup, find_packages
+
+version_info_major = sys.version_info[0]
+if version_info_major == 3:
+    long_description = open('README.rst', encoding="utf8").read()
+else:
+    io_open = io.open('README.rst', encoding="utf8")
+    long_description = io_open.read()
+
+setup(
+    name="json-logging",
+    version='1.2.11',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*', 'example', 'dist', 'build']),
+    license='Apache License 2.0',
+    description="JSON Python Logging",
+    long_description=long_description,
+    author="Bui Nguyen Thang (Bob)",
+    author_email="bob.bui@outlook.com",
+    keywords=["json", "elastic", "python", "python3", "python2", "logging", "logging-library", "json", "elasticsearch",
+              "elk", "elk-stack", "logstash", "kibana"],
+    platforms='any',
+    url="https://github.com/thangbn/json-logging",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Logging',
+        'Framework :: Flask',
+    ]
+)
