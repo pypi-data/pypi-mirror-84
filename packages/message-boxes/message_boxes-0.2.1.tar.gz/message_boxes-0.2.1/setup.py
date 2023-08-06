7d3689d0424be7f5a3ab702e6e52c313cd6a4519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:20 +0000

    Add package message-boxes/0.2.1/message_boxes-0.2.1.tar.gz

---
+++ packages/message-boxes/message_boxes-0.2.1.tar.gz/message_boxes-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup_args = dict(
+    name='message_boxes',
+    version='0.2.1',
+    description='Create easy windows message boxes with python!',
+    license='MIT',
+    packages=find_packages(),
+    author='Cy4',
+    keywords=['Message', 'Boxes', 'MessageBoxes'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
+    ]
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
