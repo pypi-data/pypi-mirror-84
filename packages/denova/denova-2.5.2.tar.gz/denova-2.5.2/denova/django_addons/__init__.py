62da7c3a608991a9737daf53a986bffa6f83378e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:49 +0000

    Add package denova/2.5.2/denova-2.5.2.tar.gz

---
+++ packages/denova/denova-2.5.2.tar.gz/denova-2.5.2/denova/django_addons/__init__.py
@@ -0,0 +1,9 @@
+'''
+    Extra functions/templates, etc. when you have a django app.
+    django_addons is named to avoid conflict with the django pacakge.
+
+    Copyright 2009-2020 DeNova
+    Last modified: 2020-10-02
+
+    This file is open source, licensed under GPLv3 <http://www.gnu.org/licenses/>.
+'''
