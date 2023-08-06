f4491fcba7459f4db9c89a6bb0aa1da301b52995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:17 +0000

    Add package openpack/2.2.3/openpack-2.2.3.tar.gz

---
+++ packages/openpack/openpack-2.2.3.tar.gz/openpack-2.2.3/test/test_officepack.py
@@ -0,0 +1,19 @@
+import pkg_resources
+
+import py.test
+
+from openpack.officepack import OfficePackage
+
+
+@py.test.fixture
+def officepack_sample(request):
+    return pkg_resources.resource_stream(__name__, 'empty.docx')
+
+
+def test_open(officepack_sample):
+    OfficePackage.from_stream(officepack_sample)
+
+
+def test_start_part(officepack_sample):
+    doc = OfficePackage.from_stream(officepack_sample)
+    assert doc.start_part
