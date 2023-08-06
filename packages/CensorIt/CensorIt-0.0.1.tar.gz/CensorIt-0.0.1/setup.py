e88281f6c819f6f0f867c76908f66f5dce6911ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:45 +0000

    Add package CensorIt/0.0.1/CensorIt-0.0.1.tar.gz

---
+++ packages/CensorIt/CensorIt-0.0.1.tar.gz/CensorIt-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+    name='CensorIt',
+    version='0.0.1',
+    description='A Python Curse Word Censoring Package',
+    url='https://github.com/jack-the-hack/CensorIt',
+    author='Donnie Jack Baldyga',
+    author_email='jackedak@gmail.com',
+    license='MIT License',
+    packages=['CensorIt'],
+    install_requires=[],
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+)
