213e7c4c635ac7729beedc5bc60801d98679ad2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:11 +0000

    Add package exco/0.1.3/exco-0.1.3.tar.gz

---
+++ packages/exco/exco-0.1.3.tar.gz/exco-0.1.3/setup.py
@@ -0,0 +1,47 @@
+from os.path import join, dirname
+
+from setuptools import setup, find_packages
+
+
+def get_version():
+    fname = join(dirname(__file__), "src/exco/__version__.py")
+    with open(fname) as f:
+        ldict = {}
+        code = f.read()
+        exec(code, globals(), ldict)  # version defined here
+        return ldict['version']
+
+
+setup(name='exco',
+      version=get_version(),
+      description='Excel Comment Orm',
+      long_description=open('README.md').read().strip(),
+      author='Piti Ongmongkolkul',
+      author_email='piti118@gmail.com',
+      url='https://github.com/thegangtechnology/exco',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      py_modules=['exco'],
+      install_requires=[
+          'openpyxl',
+          'pyyaml',
+          'stringcase'
+      ],
+      extras_require={
+          'dev': [
+              'ipykernel',
+              'mypy',
+              'autopep8',
+              'pytest',
+              'pytest-cov',
+              'wheel'
+          ],
+          'test': [
+              'pytest',
+              'pytest-cov'
+          ]
+      },
+      license='Private',
+      zip_safe=False,
+      keywords='',
+      classifiers=[''])
