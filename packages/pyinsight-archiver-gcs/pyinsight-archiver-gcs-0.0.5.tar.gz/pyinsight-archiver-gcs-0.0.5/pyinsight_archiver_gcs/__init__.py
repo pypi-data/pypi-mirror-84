dd51730ec29ff956e21b956f7026176354964190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:43 +0000

    Add package pyinsight-archiver-gcs/0.0.5/pyinsight-archiver-gcs-0.0.5.tar.gz

---
+++ packages/pyinsight-archiver-gcs/pyinsight-archiver-gcs-0.0.5.tar.gz/pyinsight-archiver-gcs-0.0.5/pyinsight_archiver_gcs/__init__.py
@@ -0,0 +1,10 @@
+# Module Import
+from pyinsight_archiver_gcs import gcs_archiver
+
+# Object Import
+from pyinsight_archiver_gcs.gcs_archiver import GCSArchiver
+
+# Element Listing
+__all__ = ['GCSArchiver']
+
+__version__ = "0.0.5"
