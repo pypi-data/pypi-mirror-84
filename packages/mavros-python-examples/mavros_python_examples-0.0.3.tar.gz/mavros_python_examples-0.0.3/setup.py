aa3008c21c4f44c0bed5543ae54847d52583b986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:50 +0000

    Add package mavros-python-examples/0.0.3/mavros_python_examples-0.0.3.tar.gz

---
+++ packages/mavros-python-examples/mavros_python_examples-0.0.3.tar.gz/mavros_python_examples-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mavros_python_examples", # Replace with your own username
+    version="0.0.3",
+    author="Masoud Iranmehr",
+    author_email="masoud.iranmehr@gmail.com",
+    description="MAVROS examples",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/masoudir/mavros_python_examples",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
