35872d6d972f86eb134853b180c972d5c9ed2bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:21 +0000

    Add package ChadBot6/0.1/ChadBot6-0.1.tar.gz

---
+++ packages/ChadBot6/ChadBot6-0.1.tar.gz/ChadBot6-0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    name = "ChadBot6",
+    version = "0.1",
+    author = "Rushi Babaria",
+    url = "https://github.com/captanlevi/FAQ-QnA-matching.git",
+    packages = setuptools.find_packages(),
+    install_requires = ["sentence_transformers==0.3.8","pandas", "numpy","regex","nlpaug","spacy", "tqdm"]
+)
+
