57e56b3a6ce1c3758660a8f212fc81aa4ee3ae25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:56 +0000

    Add package rng-wijesooriya/0.0.1/rng-wijesooriya-0.0.1.tar.gz

---
+++ packages/rng-wijesooriya/rng-wijesooriya-0.0.1.tar.gz/rng-wijesooriya-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rng-wijesooriya",
+    version="0.0.1",
+    author="Kiran Wijesooriya",
+    author_email="kiranw1000@gmail.com",
+    description="Random Number Game",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
