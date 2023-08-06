94a46918c7a5ad74821170c013ac7951f6849d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:02 +0000

    Add package torch-metrics/1.1.5/torch_metrics-1.1.5.tar.gz

---
+++ packages/torch-metrics/torch_metrics-1.1.5.tar.gz/torch_metrics-1.1.5/torch_metrics/f1.py
@@ -0,0 +1,12 @@
+import torch
+from torch_metrics.pr import Precision, Recall
+
+class F1Score:
+    def __init__(self):
+        self.prec = Precision()
+        self.recall = Recall()
+        self.epsilon = 1e-10
+    def __call__(self, tensor1, tensor2):
+        precision = self.prec(tensor1, tensor2)
+        recall = self.recall(tensor1, tensor2)
+        return 2*((precision*recall)/(precision+recall+self.epsilon))
