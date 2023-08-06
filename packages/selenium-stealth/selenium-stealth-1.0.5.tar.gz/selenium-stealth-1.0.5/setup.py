83882ac303aacc672557a27ffbc8d470744e1772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:12 +0000

    Add package selenium-stealth/1.0.5/selenium-stealth-1.0.5.tar.gz

---
+++ packages/selenium-stealth/selenium-stealth-1.0.5.tar.gz/selenium-stealth-1.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="selenium-stealth",
+    version="1.0.5",
+    author="Dipraj Patra",
+    author_email="diprajpatra@gmail.com",
+    description="Trying to make python selenium more stealthy.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/diprajpatra/selenium-stealth",
+    packages=setuptools.find_packages(),
+    package_data={"selenium-stealth": ["js/*.js"]},
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    include_package_data=True,
+)
