f2f433662ce7a68b3dbd096f7031d37d630fee09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:56 +0000

    Add package lingua-franca/0.3.1/lingua_franca-0.3.1.tar.gz

---
+++ packages/lingua-franca/lingua_franca-0.3.1.tar.gz/lingua_franca-0.3.1/setup.py
@@ -0,0 +1,54 @@
+import os
+
+from setuptools import setup
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(os.path.dirname(__file__), requirements_file),
+              'r') as f:
+        requirements = f.read().splitlines()
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
+extra_files = package_files('lingua_franca')
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='lingua_franca',
+    version='0.3.1',
+    packages=['test', 'lingua_franca', 'lingua_franca.lang'],
+    url='https://github.com/MycroftAI/lingua-franca',
+    license='Apache2.0',
+    package_data={'': extra_files},
+    include_package_data=True,
+    install_requires=required('requirements.txt'),
+    author='Mycroft AI',
+    author_email='dev@mycroft.ai',
+    description='Mycroft\'s multilingual text parsing and formatting library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
