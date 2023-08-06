899f7564235a4ea0474bd25da6be25353484d882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:59 +0000

    Add package poncho/0.0.1/poncho-0.0.1.tar.gz

---
+++ packages/poncho/poncho-0.0.1.tar.gz/poncho-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='poncho',
+      version='0.0.1',
+      description='Poncho',
+      author='Bülent Aldemir',
+      author_email='buelent@e-evolution.de',
+      license='MIT',
+      packages=['poncho'],
+      zip_safe=False)
