9e9db0b445d5a78a4c9e919eb6975b823127d80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:02 +0000

    Add package chaostoolkit-gremlin-yy/0.3.1/chaostoolkit_gremlin_yy-0.3.1-py3-none-any.whl

---
+++ packages/chaostoolkit-gremlin-yy/chaostoolkit_gremlin_yy-0.3.1-py3-none-any.whl/chaosgremlin/actions.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from functools import wraps
+import json
+import os
+from typing import Any, Dict
+
+from logzero import logger
+import requests
+
+from chaoslib.exceptions import FailedActivity
+from chaoslib.types import Secrets
+
+from chaosgremlin import auth, GREMLIN_BASE_URL
+
+
+__all__ = ["attack"]
+
+
+def attack(command: Dict[str, Any], target: Dict[str, Any],
+           labels: Dict[str, Any] = None, tags: Dict[str, Any] = None,
+           secrets: Secrets = None):
+    """
+    Triggers an attack on the CPU of a host. Please refer to Gremlin's
+    documentation for the meaning of each argument. The `secrets` argument is
+    a mapping which must have the following keys: `email`, `password` and
+    `org_name`.
+
+    The function returns the identifier of the attack or raises
+    :exc:`FailedActivity` if the authentication failed and
+    when the attack could not be started.
+
+    .. seealso:: https://www.gremlin.com/docs/
+    """
+    #session = auth(**secrets)
+
+    url = "{base}/attacks/new?teamId={teamID}".format(base=GREMLIN_BASE_URL,teamID=secrets["team_id"])
+    r = requests.post(
+        url,
+        headers={
+            "Authorization": secrets["api_key"]
+        },
+        json={
+            "command": command,
+            "target": target,
+            "labels": labels,
+            "tags": tags
+        })
+
+    if r.status_code != 201:
+        raise FailedActivity(
+            "Gremlin attack failed: {m}".format(m=r.text))
+
+    result = r.text
+    logger.debug("attack submitted succesfully: {r}".format(r=result))
+
+    return result
