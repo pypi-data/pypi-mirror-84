4e8f7f8891958c9ce520ed0e512bedaf54a63ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:37 +0000

    Add package bffacilities/0.0.16/bffacilities-0.0.16.tar.gz

---
+++ packages/bffacilities/bffacilities-0.0.16.tar.gz/bffacilities-0.0.16/bffacilities/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.0.16"
+__author__ = "BriFuture"
+
+
+from .utils import createLogger, initGetText
+from ._constants import BFF_ROOT_PATH, BFF_OTHER_PATH
+
+
+#Dictionary with console color codes to print text
+terminal_colors = {
+    'HEADER' : "\033[95m",
+    'OKBLUE' : "\033[94m",
+    'RED' : "\033[91m",
+    'OKYELLOW' : "\033[93m",
+    'GREEN' : "\033[92m",
+    'LIGHTBLUE' : "\033[96m",
+    'WARNING' : "\033[93m",
+    'FAIL' : "\033[91m",
+    'ENDC' : "\033[0m",
+    'BOLD' : "\033[1m",
+    'UNDERLINE' : "\033[4m" 
+}
---
+++ packages/bffacilities/bffacilities-0.0.16.tar.gz/bffacilities-0.0.16/bffacilities/_constants.py
@@ -0,0 +1,13 @@
+"""some basic configurations
+"""
+from pathlib import Path
+import os
+import os.path as osp
+
+_BASE_DIR = Path.home() / ".shells" # Basic Path
+_CONFIG_DIR = _BASE_DIR / "configs"  # Default Config Path
+
+_CONFIG_DIR.mkdir(parents=True, exist_ok=True)
+
+BFF_ROOT_PATH = Path(osp.abspath(osp.dirname(__file__)))
+BFF_OTHER_PATH = BFF_ROOT_PATH / "other"
