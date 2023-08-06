e3d21d30ca28dca495830743b6748c2f4afc8751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:43 +0000

    Add package AppZoo/2020.11.5.21.4.21/appzoo-2020.11.5.21.4.21.tar.gz

---
+++ packages/AppZoo/appzoo-2020.11.5.21.4.21.tar.gz/appzoo-2020.11.5.21.4.21/appzoo/utils/logger_utils.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AppZoo.
+# @File         : logger_utils
+# @Time         : 2020/11/5 8:18 下午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
