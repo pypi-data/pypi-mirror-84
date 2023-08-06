e6c07c1da69d1651d7ac148add8934ad5acb4111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:18 +0000

    Add package hwaddress/0.0.1.dev2/hwaddress-0.0.1.dev2.tar.gz

---
+++ packages/hwaddress/hwaddress-0.0.1.dev2.tar.gz/hwaddress-0.0.1.dev2/hwaddress/__init__.py
@@ -0,0 +1,5 @@
+"""Lightweight EUI-48, EUI-64 based hardware (MAC) address library."""
+
+from hwaddress.core import get_address_factory, get_verifier, \
+                           new_hwaddress_class, MAC, MAC_64, GUID, \
+                           EUI_48, EUI_64, WWN, WWNx, IB_LID, IB_GUID, IB_GID
---
+++ packages/hwaddress/hwaddress-0.0.1.dev2.tar.gz/hwaddress-0.0.1.dev2/setup.py
@@ -0,0 +1,24 @@
+"""Setup hwaddress."""
+
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hwaddress",
+    version='0.0.1.dev2',
+    author="Eric Geldmacher",
+    author_email="egeldmacher@wustl.edu",
+    description="Lightweight EUI-48/64 based hardware (MAC) address library.",
+    long_description=long_description,
+    url="https://github.com/ericgeldmacher/hwaddress",
+    license="MIT",
+    packages=['hwaddress'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
