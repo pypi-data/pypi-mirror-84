af1e6b2f319aa67e90020492a0d72660abf23eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:43 +0000

    Add package sphinx-lesson/0.8.3/sphinx_lesson-0.8.3.tar.gz

---
+++ packages/sphinx-lesson/sphinx_lesson-0.8.3.tar.gz/sphinx_lesson-0.8.3/sphinx_lesson/directives.py
@@ -0,0 +1,115 @@
+"""Add some sphinx-docutils directives related to lessons.
+"""
+# pylint: disable=E701
+
+import os
+
+from docutils import nodes
+from docutils.parsers.rst.directives.admonitions \
+  import Admonition as AdmonitionDirective
+from sphinx.util.docutils import SphinxDirective
+from sphinx.util.logging import getLogger
+
+from . import __version__
+LOG = getLogger(__name__)
+
+
+# This includes a heading, to not then have
+class _BaseCRDirective(AdmonitionDirective, SphinxDirective):
+    """A directive to handle CodeRefinery styles
+    """
+    # node_class = challenge
+    required_arguments = 0
+    optional_arguments = 1
+    final_argument_whitespace = True
+    extra_classes = [ ]
+
+    @classmethod
+    def cssname(cls):
+        """Return the CSS class name and Sphinx directive name.
+
+        - Remove 'Directive' from the name of the class
+        - All lowercase
+        - '_' replaced with '-'
+        """
+        return cls.__name__.split('Directive')[0].lower().replace('_', '-')
+
+    def run(self):
+        """Run the normal admonition class, but add in a new features.
+
+        title_text: some old classes had a title which was added at the
+        CSS level. If this is set, then this title will be added by the
+        directive.
+        """
+        name = self.cssname()
+        self.node_class = nodes.admonition
+        # Some jekyll-common nodes have CSS-generated titles, some don't.  The
+        # Admonition class requires a title.  Add one if missing.  The title is
+        # the first argument to the directive.
+        if len(self.arguments) == 0:
+            if hasattr(self, 'title_text'):
+                self.arguments = [self.title_text]
+            else:
+                self.arguments = [name.title()]
+        # Run the upstream directive
+        ret = super().run()
+        # Set CSS classes
+        ret[0].attributes['classes'].append(name)
+        ret[0].attributes['classes'].extend(self.extra_classes)
+        return ret
+
+
+class CalloutDirective(_BaseCRDirective): pass
+class ChallengeDirective(_BaseCRDirective): pass
+class ChecklistDirective(_BaseCRDirective): pass
+class DiscussionDirective(_BaseCRDirective): pass
+class KeypointsDirective(_BaseCRDirective): pass
+class ObjectivesDirective(_BaseCRDirective): pass
+class PrereqDirective(_BaseCRDirective):
+    title_text = "Prerequisites"
+class SolutionDirective(_BaseCRDirective):
+    extra_classes = ['dropdown'] #'toggle-shown' = visible by default
+class TestimonialDirective(_BaseCRDirective): pass
+class OutputDirective(_BaseCRDirective):
+    title_text = 'Output'
+class QuestionsDirective(_BaseCRDirective): pass
+class Instructor_NoteDirective(_BaseCRDirective):
+    title_text = "Instructor note"
+
+# This does work, to add
+# from sphinx.writers.html5 import HTML5Translator
+# def visit_node(self, node):
+#     #import pdb ; pdb.set_trace()
+#     node.attributes['classes'] += [node.__class__.__name__]
+#     self.visit_admonition(node)
+
+
+# Add our custom CSS to the headers.
+def init_static_path(app):
+    static_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                               '_static'))
+    #print('sphinx_lesson static path:', static_path)
+    app.config.html_static_path.append(static_path)
+
+
+def setup(app):
+    "Sphinx extension setup"
+    app.setup_extension('myst_nb')
+    for name, obj in globals().items():
+        #print(name, obj)
+        if (name.endswith('Directive')
+            and issubclass(obj, _BaseCRDirective)
+            and not name.startswith('_')):
+            #print(name, obj.cssname())
+            app.add_directive(obj.cssname(), obj)
+
+    # Add CSS to build
+    # Hint is from https://github.com/choldgraf/sphinx-copybutton/blob/master/sphinx_copybutton/__init__.py  # pylint: ignore=E501
+    app.connect('builder-inited', init_static_path)
+    app.add_css_file("sphinx_lesson.css")
+
+    return {
+        'version': __version__,
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
---
+++ packages/sphinx-lesson/sphinx_lesson-0.8.3.tar.gz/sphinx_lesson-0.8.3/sphinx_lesson/md_transforms.py
@@ -0,0 +1,178 @@
+# pylint: disable=E701
+
+import os
+import re
+import textwrap
+
+from docutils import nodes
+from docutils.parsers.rst.directives.admonitions \
+  import Admonition as AdmonitionDirective
+from sphinx.util.docutils import SphinxDirective
+from sphinx.util.logging import getLogger
+
+from . import __version__
+LOG = getLogger(__name__)
+
+
+
+def transform_code_fences(app, docname, source):
+    """Transform a code fence when read.
+
+    Transform this::
+
+        ```
+        blah
+        ```
+        {: output}
+
+    into this::
+
+        ```{output}
+        blah
+        ```
+
+    """
+    if not app.config.sphinx_lesson_transform_code_fences:
+        return
+    LOG.debug('transform_code_fences: beginning %s', docname)
+    content = source[0]
+    LOG.debug(content)
+    code_fence_re = re.compile(
+        r'^(?P<before>```.*?)(?P<content>\n.*?)(?P<after>^``` ?\n)(?:\{:(?P<class>[^}\n]+)\}$)?',
+        re.DOTALL|re.MULTILINE,
+        )
+    def sub_fence(m):
+        if m.group('class'):
+            LOG.debug("matched: %s", m.group(0))
+            LOG.debug("class: %s", m.group('class'))
+            return m.group('before') + '{%s}'%m.group('class').strip() + m.group('content') + m.group('after')
+        else:
+            return m.group(0)
+
+    newcontent = code_fence_re.sub(sub_fence, content)
+    LOG.debug(newcontent)
+    source[0] = newcontent
+
+def transform_block_quotes(app, docname, source):
+    """
+    Transform this::
+
+        > ## some-heading
+        > text
+        > text
+        {: .block-class}
+
+    into this::
+
+        ```{block-class} some-heading
+        text
+        text
+        ```
+
+    """
+    if not app.config.sphinx_lesson_transform_block_quotes:
+        return
+    LOG.debug('sphinx_lesson: transform_block_quotes: %s', docname)
+    content = source[0]
+    LOG.debug(content)
+
+    block_quote_re = re.compile(
+        r'(?P<heading>> ?#+[^\n]*$\n)?(?P<content>(?:>[^\n]*$\n)+)\{: +(?P<class>[^\}]+)\}',
+        re.DOTALL|re.MULTILINE,
+    )
+
+    def sub_block(m):
+        """Handle each detected block quote"""
+        if m.group('class'):
+            LOG.debug("matched: %s", m.group(0))
+            LOG.debug("class: %s", m.group('class'))
+            # Extract the class, remove leading characters
+            class_ = m.group('class')
+            class_ = re.sub('^[ .]*', '', class_)
+            # heading: tranform explicit heading into directive heading
+            if m.group('heading'):
+                heading = m.group('heading')
+                heading = ' ' + re.sub('^[ >#]*', '', heading)
+            else:
+                heading = ''
+            # content: remove one leading '>' character
+            contentlines = m.group('content').split('\n')
+            contentlines = [ re.sub('^> ?', '', line) for line in contentlines ]
+
+            LOG.debug(contentlines)
+            return ("```{%s}%s\n"%(class_, heading)
+                    + '\n'.join(contentlines)
+                    + "```"
+                    )
+
+        else:
+            return m.group(0)
+
+    newcontent = block_quote_re.sub(sub_block, content)
+    LOG.debug(newcontent)
+    source[0] = newcontent
+
+def transform_html_img(app, docname, source):
+    """
+    Transform this::
+
+        <img src="/path/to/img.png">
+
+    into this::
+
+        ```{figure} /path/to/img.png
+        ```
+
+    Exclude any possible `{{ ... }}` template variables.
+
+    """
+    if not app.config.sphinx_lesson_transform_html_img:
+        return
+    LOG.debug('sphinx_lesson: transform_html_img: %s', docname)
+    content = source[0]
+    LOG.debug(content)
+
+    html_img_re = re.compile(
+        r'<img[^<>]+src="(?:{{[^\{\}\{"<>]+}})?(?P<src>[^<>"]+)"[^<>]*>',
+    )
+
+    def sub_img(m):
+        """Handle each detected block quote"""
+        raw = m.group(0)
+        if re.search(r'style="[^"]*border:', raw):
+            border = textwrap.dedent("""\
+                ---
+                class: with-border
+                ---
+                """)
+        else:
+            border = ""
+        repl = textwrap.dedent("""
+            ```{figure} %(src)s
+            %(border)s```
+            """)%{'src':m.group('src'), 'border': border}
+        LOG.debug(repl)
+        return repl
+
+    newcontent = html_img_re.sub(sub_img, content)
+    LOG.debug(newcontent)
+    source[0] = newcontent
+
+
+
+def setup(app):
+    "Sphinx extension setup"
+    app.setup_extension('myst_nb')
+    # Code frence transformation
+    app.add_config_value('sphinx_lesson_transform_code_fences', True, 'env')
+    app.add_config_value('sphinx_lesson_transform_block_quotes', True, 'env')
+    app.add_config_value('sphinx_lesson_transform_html_img', True, 'env')
+    app.connect('source-read', transform_code_fences)
+    app.connect('source-read', transform_block_quotes)
+    app.connect('source-read', transform_html_img)
+
+    return {
+        'version': __version__,
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
