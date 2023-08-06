c32f7a839a28e321df02cbd3a7b181260b42cb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:30 +0000

    Add package morsetor/0.0.1/morsetor-0.0.1.tar.gz

---
+++ packages/morsetor/morsetor-0.0.1.tar.gz/morsetor-0.0.1/morsetor/__init__.py
@@ -0,0 +1,6 @@
+
+print('\nWelcome to my website ;) ', 'https://vixportfoliowithflask.herokuapp.com/skills')
+
+print('\nONLINE MORSE CODE ENCRYPTER AND DECRYPTER : ', 'https://imvickykumar999.github.io/git-bash/')
+
+print('\nUse help(module name) function to know available functions in it.')
---
+++ packages/morsetor/morsetor-0.0.1.tar.gz/morsetor-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = 'morsetor', # while installing pacakge
+    version = '0.0.1',
+    description = 'Morse : https://github.com/imvickykumar999/git-bash/tree/master',
+    long_description = open('README.md').read(),
+    url = 'https://vixportfoliowithflask.herokuapp.com/skills',
+    author = 'Vicky Kumar',
+    keywords = ['GitHub Uploader','Morse','custom','python package','function and class','3D line',
+    '3D plane', 'angle bw planes or line', 'distance bw point and plane'],
+    license = 'MIT',
+    packages = ['morsetor'], # while importing package
+    install_requires = ['']
+)
