04a62554648c920e1f218c9a98b2f9f00989d1ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:23 +0000

    Add package kora/0.8.28/kora-0.8.28-py3-none-any.whl

---
+++ packages/kora/kora-0.8.28-py3-none-any.whl/kora/gcloud.py
@@ -0,0 +1,9 @@
+"""
+Convenient tools that call gcloud or gsutil
+or work with Google Cloud Platform
+"""
+import os
+
+
+def config(project='kora-id'):
+    os.system(f"gcloud config set project {project}")
