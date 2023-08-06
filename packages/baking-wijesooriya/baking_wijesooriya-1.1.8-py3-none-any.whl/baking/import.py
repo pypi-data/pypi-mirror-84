e012df6d53735ff4bf9842a8b6dab6b0f5762667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:17 +0000

    Add package baking-wijesooriya/1.1.8/baking_wijesooriya-1.1.8-py3-none-any.whl

---
+++ packages/baking-wijesooriya/baking_wijesooriya-1.1.8-py3-none-any.whl/baking/import.py
@@ -0,0 +1,11 @@
+import __init__ as b
+ 
+def import():
+    measure = b.measure
+    mixIngredients = b.mixIngredients
+    pourBatter = b.pourBatter
+    setOvenTempTo = b.setOvenTempTo
+    bakeBatter = b.bakeBatter
+    assembleCake = b.assembleCake
+    iceCake = b.iceCake
+    complete = b.complete
---
+++ packages/baking-wijesooriya/baking_wijesooriya-1.1.8-py3-none-any.whl/baking/naturalize.py
@@ -0,0 +1,18 @@
+import baking as b
+def naturalize():
+    global measure 
+    measure = b.measure
+    global mixIngredients 
+    mixIngredients = b.mixIngredients
+    global pourBatter
+    pourBatter = b.pourBatter
+    global setOvenTempTo 
+    setOvenTempTo = b.setOvenTempTo
+    global bakeBatter
+    bakeBatter = b.bakeBatter
+    global assembleCake
+    assembleCake = b.assembleCake
+    global iceCake
+    iceCake = b.iceCake
+    global complete
+    complete = b.complete
