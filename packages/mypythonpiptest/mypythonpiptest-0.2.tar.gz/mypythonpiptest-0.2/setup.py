7dad5c700ec9e995ae2b910648a91b0ee71959c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:24 +0000

    Add package mypythonpiptest/0.2/mypythonpiptest-0.2.tar.gz

---
+++ packages/mypythonpiptest/mypythonpiptest-0.2.tar.gz/mypythonpiptest-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='mypythonpiptest',
+      version='0.2',
+      description='my first python pip',
+      url='https://github.com/wangzishuo111/mypythonpiptest.git',
+      author='wangzishuo',
+      author_email='wzs_wangzishuo@163.com',
+      packages=['mypythonpiptest'],
+      zip_safe=False)
