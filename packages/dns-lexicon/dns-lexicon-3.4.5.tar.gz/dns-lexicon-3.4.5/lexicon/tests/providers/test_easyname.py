40e4bda4d8dcaab6e23286b38cec11ad16cafcf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:26 +0000

    Add package dns-lexicon/3.4.5/dns-lexicon-3.4.5.tar.gz

---
+++ packages/dns-lexicon/dns-lexicon-3.4.5.tar.gz/dns-lexicon-3.4.5/lexicon/tests/providers/test_easyname.py
@@ -0,0 +1,20 @@
+"""Integration tests for EasyName"""
+from unittest import TestCase
+
+from lexicon.tests.providers.integration_tests import IntegrationTestsV2
+
+
+class EasynameProviderTests(TestCase, IntegrationTestsV2):
+    """TestCase for EasyName"""
+
+    provider_name = "easyname"
+    domain = "nigl.it"
+
+    def _filter_post_data_parameters(self):
+        return ["username", "password"]
+
+    def _filter_headers(self):
+        return ["Cookie"]
+
+    def _test_fallback_fn(self):
+        return lambda x: "placeholder_" + x if x != "priority" else ""
