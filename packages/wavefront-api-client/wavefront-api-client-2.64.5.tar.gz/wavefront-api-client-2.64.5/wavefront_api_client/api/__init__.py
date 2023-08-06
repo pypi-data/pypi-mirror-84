0b8e2be3cb93b86e0be1dae5d214cdedbe18a1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:11 +0000

    Add package wavefront-api-client/2.64.5/wavefront-api-client-2.64.5.tar.gz

---
+++ packages/wavefront-api-client/wavefront-api-client-2.64.5.tar.gz/wavefront-api-client-2.64.5/wavefront_api_client/api/__init__.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from wavefront_api_client.api.account__user_and_service_account_api import AccountUserAndServiceAccountApi
+from wavefront_api_client.api.alert_api import AlertApi
+from wavefront_api_client.api.anomaly_api import AnomalyApi
+from wavefront_api_client.api.api_token_api import ApiTokenApi
+from wavefront_api_client.api.cloud_integration_api import CloudIntegrationApi
+from wavefront_api_client.api.dashboard_api import DashboardApi
+from wavefront_api_client.api.derived_metric_api import DerivedMetricApi
+from wavefront_api_client.api.direct_ingestion_api import DirectIngestionApi
+from wavefront_api_client.api.event_api import EventApi
+from wavefront_api_client.api.external_link_api import ExternalLinkApi
+from wavefront_api_client.api.ingestion_spy_api import IngestionSpyApi
+from wavefront_api_client.api.integration_api import IntegrationApi
+from wavefront_api_client.api.maintenance_window_api import MaintenanceWindowApi
+from wavefront_api_client.api.message_api import MessageApi
+from wavefront_api_client.api.metric_api import MetricApi
+from wavefront_api_client.api.metrics_policy_api import MetricsPolicyApi
+from wavefront_api_client.api.monitored_application_api import MonitoredApplicationApi
+from wavefront_api_client.api.monitored_service_api import MonitoredServiceApi
+from wavefront_api_client.api.notificant_api import NotificantApi
+from wavefront_api_client.api.proxy_api import ProxyApi
+from wavefront_api_client.api.query_api import QueryApi
+from wavefront_api_client.api.role_api import RoleApi
+from wavefront_api_client.api.saved_search_api import SavedSearchApi
+from wavefront_api_client.api.search_api import SearchApi
+from wavefront_api_client.api.source_api import SourceApi
+from wavefront_api_client.api.usage_api import UsageApi
+from wavefront_api_client.api.user_api import UserApi
+from wavefront_api_client.api.user_group_api import UserGroupApi
+from wavefront_api_client.api.webhook_api import WebhookApi
