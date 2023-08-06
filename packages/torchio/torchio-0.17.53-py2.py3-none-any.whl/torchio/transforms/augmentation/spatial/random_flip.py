505002349247eebcc9e93290c044f675915dde85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:30 +0000

    Add package torchio/0.17.53/torchio-0.17.53-py2.py3-none-any.whl

---
+++ packages/torchio/torchio-0.17.53-py2.py3-none-any.whl/torchio/transforms/augmentation/spatial/random_flip.py
@@ -0,0 +1,93 @@
+from typing import Union, Tuple, Optional, List
+import torch
+import numpy as np
+from ....torchio import DATA
+from ....data.subject import Subject
+from ....utils import to_tuple
+from ... import SpatialTransform
+from .. import RandomTransform
+
+
+class RandomFlip(RandomTransform, SpatialTransform):
+    """Reverse the order of elements in an image along the given axes.
+
+    Args:
+        axes: Index or tuple of indices of the spatial dimensions along which
+            the image might be flipped. If they are integers, they must be in
+            ``(0, 1, 2)``. Anatomical labels may also be used, such as
+            ``'Left'``, ``'Right'``, ``'Anterior'``, ``'Posterior'``,
+            ``'Inferior'``, ``'Superior'``, ``'Height'`` and ``'Width'``,
+            ``'AP'`` (antero-posterior), ``'lr'`` (lateral), ``'w'`` (width) or
+            ``'i'`` (inferior). Only the first letter of the string will be
+            used. If the image is 2D, ``'Height'`` and ``'Width'`` may be
+            used.
+        flip_probability: Probability that the image will be flipped. This is
+            computed on a per-axis basis.
+        p: Probability that this transform will be applied.
+        seed: See :py:class:`~torchio.transforms.augmentation.RandomTransform`.
+        keys: See :py:class:`~torchio.transforms.Transform`.
+
+    Example:
+        >>> import torchio as tio
+        >>> fpg = tio.datasets.FPG()
+        >>> flip = tio.RandomFlip(axes=('LR'))  # flip along lateral axis only
+
+    .. tip:: It is handy to specify the axes as anatomical labels when the image
+        orientation is not known.
+    """
+
+    def __init__(
+            self,
+            axes: Union[int, Tuple[int, ...]] = 0,
+            flip_probability: float = 0.5,
+            p: float = 1,
+            seed: Optional[int] = None,
+            keys: Optional[List[str]] = None,
+            ):
+        super().__init__(p=p, seed=seed, keys=keys)
+        self.axes = self.parse_axes(axes)
+        self.flip_probability = self.parse_probability(
+            flip_probability,
+        )
+
+    def apply_transform(self, subject: Subject) -> Subject:
+        axes = self.axes
+        axes_to_flip_hot = self.get_params(self.flip_probability)
+        if any(isinstance(n, str) for n in axes):
+            subject.check_consistent_orientation()
+            image = subject.get_first_image()
+            axes = sorted(3 + image.axis_name_to_index(n) for n in axes)
+        for i in range(3):
+            if i not in axes:
+                axes_to_flip_hot[i] = False
+        random_parameters_dict = {'axes': axes_to_flip_hot}
+        for image in self.get_images(subject):
+            dims = []
+            for dim, flip_this in enumerate(axes_to_flip_hot):
+                if not flip_this:
+                    continue
+                actual_dim = dim + 1  # images are 4D
+                dims.append(actual_dim)
+            if dims:
+                data = image.numpy()
+                data = np.flip(data, axis=dims)
+                data = data.copy()  # remove negative strides
+                data = torch.from_numpy(data)
+                image[DATA] = data
+        subject.add_transform(self, random_parameters_dict)
+        return subject
+
+    @staticmethod
+    def get_params(probability: float) -> List[bool]:
+        return (probability > torch.rand(3)).tolist()
+
+    @staticmethod
+    def parse_axes(axes: Union[int, Tuple[int, ...]]):
+        axes_tuple = to_tuple(axes)
+        for axis in axes_tuple:
+            is_int = isinstance(axis, int)
+            is_string = isinstance(axis, str)
+            if not is_string and not (is_int and axis in (0, 1, 2)):
+                message = f'All axes must be 0, 1 or 2, but found "{axis}"'
+                raise ValueError(message)
+        return axes_tuple
---
+++ packages/torchio/torchio-0.17.53-py2.py3-none-any.whl/torchio/transforms/preprocessing/spatial/resample.py
@@ -0,0 +1,227 @@
+from pathlib import Path
+from numbers import Number
+from typing import Union, Tuple, Optional, List
+
+import torch
+import numpy as np
+import SimpleITK as sitk
+
+from ....data.subject import Subject
+from ....data.image import Image, ScalarImage
+from ....torchio import DATA, AFFINE, TYPE, INTENSITY, TypeTripletFloat
+from ....utils import sitk_to_nib
+from ... import SpatialTransform
+from ... import Interpolation, get_sitk_interpolator
+
+
+TypeSpacing = Union[float, Tuple[float, float, float]]
+TypeTarget = Tuple[
+    Optional[Union[Image, str]],
+    Optional[Tuple[float, float, float]],
+]
+
+
+class Resample(SpatialTransform):
+    """Change voxel spacing by resampling.
+
+    Args:
+        target: Tuple :math:`(s_h, s_w, s_d)`. If only one value
+            :math:`n` is specified, then :math:`s_h = s_w = s_d = n`.
+            If a string or :py:class:`~pathlib.Path` is given,
+            all images will be resampled using the image
+            with that name as reference or found at the path.
+        pre_affine_name: Name of the *image key* (not subject key) storing an
+            affine matrix that will be applied to the image header before
+            resampling. If ``None``, the image is resampled with an identity
+            transform. See usage in the example below.
+        image_interpolation: String that defines the interpolation technique.
+            Supported interpolation techniques for resampling
+            are ``'nearest'``, ``'linear'`` and ``'bspline'``.
+            Using a member of :py:class:`torchio.Interpolation` is still
+            supported for backward compatibility,
+            but will be removed in a future version.
+        p: Probability that this transform will be applied.
+        keys: See :py:class:`~torchio.transforms.Transform`.
+
+    Example:
+        >>> import torchio as tio
+        >>> from torchio import Resample
+        >>> from tio.datasets import Colin27, FPG
+        >>> transform = Resample(1)                     # resample all images to 1mm iso
+        >>> transform = Resample((2, 2, 2))             # resample all images to 2mm iso
+        >>> transform = Resample('t1')                  # resample all images to 't1' image space
+        >>> colin = Colin27()  # this images are in the MNI space
+        >>> fpg = FPG()  # matrices to the MNI space are included here
+        >>> # Resample all images into the MNI space
+        >>> transform = Resample(colin.t1.path, pre_affine_name='affine_matrix')
+        >>> transformed = transform(fpg)  # images in fpg are now in MNI space
+    """
+    def __init__(
+            self,
+            target: Union[TypeSpacing, str, Path],
+            image_interpolation: str = 'linear',
+            pre_affine_name: Optional[str] = None,
+            p: float = 1,
+            keys: Optional[List[str]] = None,
+            ):
+        super().__init__(p=p, keys=keys)
+        self.reference_image, self.target_spacing = self.parse_target(target)
+        self.interpolation = self.parse_interpolation(image_interpolation)
+        self.affine_name = pre_affine_name
+
+    def parse_target(
+            self,
+            target: Union[TypeSpacing, str],
+            ) -> TypeTarget:
+        """
+        If target is an existing path, return a torchio.ScalarImage
+        If it does not exist, return the string
+        If it is not a Path or string, return None
+        """
+        if isinstance(target, (str, Path)):
+            if Path(target).is_file():
+                path = target
+                reference_image = ScalarImage(path)
+            else:
+                reference_image = target
+            target_spacing = None
+        else:
+            reference_image = None
+            target_spacing = self.parse_spacing(target)
+        return reference_image, target_spacing
+
+    @staticmethod
+    def parse_spacing(spacing: TypeSpacing) -> Tuple[float, float, float]:
+        if isinstance(spacing, tuple) and len(spacing) == 3:
+            result = spacing
+        elif isinstance(spacing, Number):
+            result = 3 * (spacing,)
+        else:
+            message = (
+                'Target must be a string, a positive number'
+                f' or a tuple of positive numbers, not {type(spacing)}'
+            )
+            raise ValueError(message)
+        if np.any(np.array(spacing) <= 0):
+            raise ValueError(f'Spacing must be positive, not "{spacing}"')
+        return result
+
+    @staticmethod
+    def check_affine(affine_name: str, image_dict: dict):
+        if not isinstance(affine_name, str):
+            message = (
+                'Affine name argument must be a string,'
+                f' not {type(affine_name)}'
+            )
+            raise TypeError(message)
+        if affine_name in image_dict:
+            matrix = image_dict[affine_name]
+            if not isinstance(matrix, (np.ndarray, torch.Tensor)):
+                message = (
+                    'The affine matrix must be a NumPy array or PyTorch tensor,'
+                    f' not {type(matrix)}'
+                )
+                raise TypeError(message)
+            if matrix.shape != (4, 4):
+                message = (
+                    'The affine matrix shape must be (4, 4),'
+                    f' not {matrix.shape}'
+                )
+                raise ValueError(message)
+
+    @staticmethod
+    def check_affine_key_presence(affine_name: str, subject: Subject):
+        for image_dict in subject.get_images(intensity_only=False):
+            if affine_name in image_dict:
+                return
+        message = (
+            f'An affine name was given ("{affine_name}"), but it was not found'
+            ' in any image in the subject'
+        )
+        raise ValueError(message)
+
+    def apply_transform(self, subject: Subject) -> Subject:
+        use_pre_affine = self.affine_name is not None
+        if use_pre_affine:
+            self.check_affine_key_presence(self.affine_name, subject)
+        for image in self.get_images(subject):
+            # Do not resample the reference image if there is one
+            if image is self.reference_image:
+                continue
+
+            # Choose interpolation
+            if image[TYPE] != INTENSITY:
+                interpolation = Interpolation.NEAREST
+            else:
+                interpolation = self.interpolation
+            interpolator = get_sitk_interpolator(interpolation)
+
+            # Apply given affine matrix if found in image
+            if use_pre_affine and self.affine_name in image:
+                self.check_affine(self.affine_name, image)
+                matrix = image[self.affine_name]
+                if isinstance(matrix, torch.Tensor):
+                    matrix = matrix.numpy()
+                image[AFFINE] = matrix @ image[AFFINE]
+
+            floating_itk = image.as_sitk(force_3d=True)
+
+            # Resample
+            if isinstance(self.reference_image, str):
+                try:
+                    reference_image_sitk = subject[self.reference_image].as_sitk()
+                except KeyError as error:
+                    message = (
+                        f'Reference name "{self.reference_image}"'
+                        ' not found in subject'
+                    )
+                    raise ValueError(message) from error
+            elif isinstance(self.reference_image, ScalarImage):
+                reference_image_sitk = self.reference_image.as_sitk()
+            elif self.reference_image is None:  # target is a spacing
+                reference_image_sitk = self.get_reference_image(
+                    floating_itk,
+                    self.target_spacing,
+                )
+
+            resampler = sitk.ResampleImageFilter()
+            resampler.SetInterpolator(interpolator)
+            resampler.SetReferenceImage(reference_image_sitk)
+            resampled = resampler.Execute(floating_itk)
+
+            array, affine = sitk_to_nib(resampled)
+            image[DATA] = torch.from_numpy(array)
+            image[AFFINE] = affine
+        return subject
+
+    @staticmethod
+    def get_reference_image(
+            image: sitk.Image,
+            spacing: TypeTripletFloat,
+            ) -> sitk.Image:
+        old_spacing = np.array(image.GetSpacing())
+        new_spacing = np.array(spacing)
+        old_size = np.array(image.GetSize())
+        new_size = old_size * old_spacing / new_spacing
+        new_size = np.ceil(new_size).astype(np.uint16)
+        new_size[old_size == 1] = 1  # keep singleton dimensions
+        new_origin_index = 0.5 * (new_spacing / old_spacing - 1)
+        new_origin_lps = image.TransformContinuousIndexToPhysicalPoint(
+            new_origin_index)
+        reference = sitk.Image(*new_size.tolist(), sitk.sitkFloat32)
+        reference.SetDirection(image.GetDirection())
+        reference.SetSpacing(new_spacing.tolist())
+        reference.SetOrigin(new_origin_lps)
+        return reference
+
+    @staticmethod
+    def get_sigma(downsampling_factor, spacing):
+        """Compute optimal standard deviation for Gaussian kernel.
+
+        From Cardoso et al., "Scale factor point spread function matching:
+        beyond aliasing in image resampling", MICCAI 2015
+        """
+        k = downsampling_factor
+        variance = (k ** 2 - 1 ** 2) * (2 * np.sqrt(2 * np.log(2))) ** (-2)
+        sigma = spacing * np.sqrt(variance)
+        return sigma
