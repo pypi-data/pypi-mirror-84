c81db9e295a0b4f1ccb0bad6cfaa6cf3a1a036b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:15 +0000

    Add package mkdocs-with-pdf/0.8.0/mkdocs_with_pdf-0.8.0-py3-none-any.whl

---
+++ packages/mkdocs-with-pdf/mkdocs_with_pdf-0.8.0-py3-none-any.whl/mkdocs_with_pdf/themes/material.py
@@ -0,0 +1,41 @@
+import os
+
+import sass
+from bs4 import BeautifulSoup
+
+
+def get_stylesheet(debug_html: bool) -> str:
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    style = ""
+    for src in ["material.scss", "material-polyfills.css"]:
+        filename = os.path.join(base_path, src)
+        output_style = 'compressed'
+        if debug_html:
+            output_style = 'nested'
+        style += sass.compile(filename=filename, output_style=output_style)
+    return style
+
+
+def get_script_sources() -> list:
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    return list(map(lambda src: os.path.join(base_path, src),
+                    ['material-polyfills.js']))
+
+
+def inject_link(html: str, href: str) -> str:
+    soup = BeautifulSoup(html, 'html.parser')
+
+    footer = soup.select('.md-footer-copyright')
+    if footer and footer[0]:
+        container = footer[0]
+
+        container.append(' ... ')
+        a = soup.new_tag('a', href=href, title='PDF',
+                         download=None, **{'class': 'link--pdf-download'})
+        a.append('download PDF')
+
+        container.append(a)
+
+        return str(soup)
+
+    return html
