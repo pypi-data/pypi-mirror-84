57bc80ca4d84f9bf59927b7613a9290ebbb3dde8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:47 +0000

    Add package kora/0.8.34/kora-0.8.34-py3-none-any.whl

---
+++ packages/kora/kora-0.8.34-py3-none-any.whl/kora/gcloud.py
@@ -0,0 +1,44 @@
+"""
+Convenient tools that call gcloud or gsutil
+or work with Google Cloud Platform
+"""
+import os
+from IPython import get_ipython
+
+sh = get_ipython().system
+
+
+def config(project='kora-id'):
+    sh(f"gcloud config set project {project}")
+
+
+def login():
+    sh('gcloud auth login')
+
+
+def list_projects():
+    """ List all projects associated with this account """
+    sh('gcloud projects list')
+
+
+def list_services(available=False):
+    """ List enabled services. Or all available services.
+    """
+    cmd = "gcloud services list"
+    if available:
+        cmd += " --available"
+    sh(cmd)
+
+
+def enable(service):
+    """ enable service, which can be listed above """
+    if not service.endswith('.googleapis.com'):
+        service += '.googleapis.com'
+    sh(f'gcloud services enable {service}')
+
+
+def upload(fname, target='gs://kora-data'):
+    """ Target can be your own gcs, and can rename the file too.
+    Default target is kora's gcs which is publicly writable (auto-delete everyday)
+    """
+    sh(f"gsutil cp {fname} {target}")
