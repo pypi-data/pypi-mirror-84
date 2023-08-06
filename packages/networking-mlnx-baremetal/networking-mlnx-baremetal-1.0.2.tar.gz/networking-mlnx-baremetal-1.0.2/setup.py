81e34827546dfad6cf8ee38a0e28f53d4b9252f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:17 +0000

    Add package networking-mlnx-baremetal/1.0.2/networking-mlnx-baremetal-1.0.2.tar.gz

---
+++ packages/networking-mlnx-baremetal/networking-mlnx-baremetal-1.0.2.tar.gz/networking-mlnx-baremetal-1.0.2/setup.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from io import open
+from os import path
+
+# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
+import setuptools
+
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    setup_requires=['pbr>=5.4.3'],
+    long_description=long_description,
+    long_description_content_type='text/x-rst; charset=UTF-8',
+    pbr=True)
