ad35bc91474f93553b98c66fde2b3e26b97a97de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:55 +0000

    Add package revcore-micro/0.5.7/revcore_micro-0.5.7.tar.gz

---
+++ packages/revcore-micro/revcore_micro-0.5.7.tar.gz/revcore_micro-0.5.7/revcore_micro/utils.py
@@ -0,0 +1,24 @@
+from decimal import Decimal, ROUND_HALF_UP
+import decimal
+import json
+
+
+def round(f):
+    return Decimal(f'{f}').quantize(0, ROUND_HALF_UP).__int__()
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        try:
+            iterable = iter(o)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+
+        if isinstance(o, decimal.Decimal):
+            value = float(o)
+            if value.is_integer():
+                return int(value)
+            return value
+        return super().default(o)
