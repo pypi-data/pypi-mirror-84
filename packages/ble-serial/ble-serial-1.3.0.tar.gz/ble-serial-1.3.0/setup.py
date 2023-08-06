b7d718dfab871c642952d4b599b1d494f1f2f84f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:21 +0000

    Add package ble-serial/1.3.0/ble-serial-1.3.0.tar.gz

---
+++ packages/ble-serial/ble-serial-1.3.0.tar.gz/ble-serial-1.3.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ble-serial",
+    version="1.3.0",
+    author="Jake",
+    author_email="ble-serial-pypi@ja-ke.tech",
+    description="A package to connect BLE serial adapters",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jakeler/ble-serial",
+    packages=["ble_serial"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'ble-scan=ble_serial.scan:main',
+            'ble-serial=ble_serial.__main__:main',
+        ]
+    },
+)
