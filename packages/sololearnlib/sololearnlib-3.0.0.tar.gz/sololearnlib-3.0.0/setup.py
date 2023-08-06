28bb8a9fc7568f8c76134953807a93e5d69cbb86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:16 +0000

    Add package sololearnlib/3.0.0/sololearnlib-3.0.0.tar.gz

---
+++ packages/sololearnlib/sololearnlib-3.0.0.tar.gz/sololearnlib-3.0.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools 
+
+with open("README.md", "r") as infile:
+  readme_file = infile.read()
+
+setuptools.setup(
+    name="sololearnlib",
+    version="3.0.0",
+    author="OR!ON",
+    author_email="orionconner00@gmail.com", 
+    license="MIT",
+    description="Retrieve public data from 'sololearn.com'.",
+    long_description_content_type="text/markdown",
+    long_description=readme_file,
+    url="https://github.com/Or-i0n/sololearnlib",
+    packages=setuptools.find_packages(),
+    install_requires=[           
+          "bs4",
+      ],
+  classifiers=[
+    "Development Status :: 3 - Alpha",      
+    "Intended Audience :: Developers",      
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+
+  ],
+  python_requires=">=3.5"
+)
