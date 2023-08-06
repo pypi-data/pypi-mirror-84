e2af9b32229e6f20429738733f5ed3e9ae92bce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:50 +0000

    Add package wagtail-bakery/0.5.0/wagtail-bakery-0.5.0.tar.gz

---
+++ packages/wagtail-bakery/wagtail-bakery-0.5.0.tar.gz/wagtail-bakery-0.5.0/CHANGELOG.md
@@ -0,0 +1,52 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [Unreleased](https://github.com/wagtail/wagtail-bakery/compare/0.5.0...HEAD)
+
+## [0.5.0](https://github.com/wagtail/wagtail-bakery/compare/0.4.0...0.5.0)
+
+### Added
+
+- Compatibility with Wagtail 2.11 (#58)
+
+### Removed
+
+- Drop support for Python 3.5
+- Drop support for Django before 2.2
+- Drop support for Wagtail before 2.10
+
+## [0.4.0](https://github.com/wagtail/wagtail-bakery/compare/0.3.0...0.4.0)
+
+### Fixed
+
+- Use model url from inherited model (#42)
+- Allow use of new versions of django-bakery (#44)
+
+### Removed
+
+- Drop support for Wagtail < 2.6, Django < 2.x & Python < 3.5 (#45, #46)
+
+## [0.3.0](https://github.com/wagtail/wagtail-bakery/compare/0.2.0...0.3.0)
+
+### Added
+
+- Compatibility with Wagtail 2.0 (#27, #31, #32, #33)
+
+### Fixed
+
+- UTF8 decoding with Python 3 (#28)
+
+## [0.2.0](https://github.com/wagtail/wagtail-bakery/compare/0.1.0...0.2.0)
+
+### Added
+
+- MIT License
+- Support for generating a static API
+
+## [0.1.0](https://github.com/wagtail/wagtail-bakery/compare/8ad37f6a733699c339bd50a965cc1b0387898205...0.1.0)
+
+Initial Release
---
+++ packages/wagtail-bakery/wagtail-bakery-0.5.0.tar.gz/wagtail-bakery-0.5.0/setup.py
@@ -0,0 +1,48 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+install_requires = [
+    'django-bakery~=0.12.7',
+    'wagtail>=2.10',
+]
+
+test_requires = [
+    'factory-boy<3',
+    'flake8',
+    'isort',
+    'pytest',
+    'pytest-cov',
+    'pytest-django',
+]
+
+setup(
+    name='wagtail-bakery',
+    version='0.5.0',
+    description='A set of helpers for baking your Django Wagtail site out as flat files.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Rob Moorman and Wagtail Core Team',
+    author_email='hello@wagtail.io',
+    install_requires=install_requires,
+    tests_require=test_requires,
+    extras_require={'test': test_requires},
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Wagtail',
+        'Framework :: Wagtail :: 2',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
