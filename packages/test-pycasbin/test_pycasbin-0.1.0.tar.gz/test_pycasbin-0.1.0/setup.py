4567a6a4de78c302c31bb81e6b280a01aa144616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:17 +0000

    Add package test-pycasbin/0.1.0/test_pycasbin-0.1.0.tar.gz

---
+++ packages/test-pycasbin/test_pycasbin-0.1.0.tar.gz/test_pycasbin-0.1.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='test_pycasbin',
+    version='0.1.0',
+    author='yyellowsun',
+    author_email='coder328@163.com',
+    url='https://github.com/yyellowsun/test-pycasbin',
+    description='test-pycasbin',
+    packages=['test_pycasbin'],
+    install_requires=[],
+)
