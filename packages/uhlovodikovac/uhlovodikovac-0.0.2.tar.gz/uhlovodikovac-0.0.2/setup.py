32cdb4b7a9bf0367e884ebf3bab9b7c1defd21ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:41 +0000

    Add package uhlovodikovac/0.0.2/uhlovodikovac-0.0.2.tar.gz

---
+++ packages/uhlovodikovac/uhlovodikovac-0.0.2.tar.gz/uhlovodikovac-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="uhlovodikovac", # Replace with your own username
+    version="0.0.2",
+    author="Bertik23",
+    author_email="bertikxxiii@gmail.com",
+    description="Package for Hydrocarbon to Image conversion",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bertik23/uhlovodikovac",
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "Pillow ~= 8.0.0",
+        "numpy ~= 1.17.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    package_dir={"":"src"},
+    package_data={'uhlovodikovac': ['hydrocarbons.jsonc']}
+)
