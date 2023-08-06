a342c42f3e274cc3112fbef1f19316e3f848dfd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:39 +0000

    Add package masc/0.3.1/masc-0.3.1.tar.gz

---
+++ packages/masc/masc-0.3.1.tar.gz/masc-0.3.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='masc',
+    version='0.3.1',
+    author='Santiago Faci',
+    author_email='santi@arkabytes.com',
+    description='A malware web scanner',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/sfaci/masc',
+    packages=['masc'],
+    keywords='malware scanner security',
+    package_data={
+        'masc': [
+            'dicts/*',
+            'signatures/checksum/*',
+            'signatures/rules/*',
+            'masc.conf',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'masc=masc.main:main',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+    install_requires=[
+        'watchdog',
+        'yara-python',
+        'python-magic',
+        'termcolor',
+        'pyclamd',
+        'progress',
+    ],
+)
