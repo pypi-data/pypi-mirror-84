2943b9e05f9b56cada63f99658a71e518d7bb465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:44 +0000

    Add package prettyqt/0.132.1/PrettyQt-0.132.1.tar.gz

---
+++ packages/prettyqt/PrettyQt-0.132.1.tar.gz/PrettyQt-0.132.1/prettyqt/network/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""Network module.
+
+Contains QtNetWork-based classes
+"""
+
+from .networkcookie import NetworkCookie
+from .networkcookiejar import NetworkCookieJar
+from .networkrequest import NetworkRequest
+from .networkproxy import NetworkProxy
+from .hostaddress import HostAddress
+from .networkaddressentry import NetworkAddressEntry
+from .networkinterface import NetworkInterface
+from .networkdatagram import NetworkDatagram
+from .abstractsocket import AbstractSocket
+from .tcpsocket import TcpSocket
+from .udpsocket import UdpSocket
+from .httppart import HttpPart
+from .httpmultipart import HttpMultiPart
+from .networkaccessmanager import NetworkAccessManager
+
+__all__ = [
+    "NetworkProxy",
+    "AbstractSocket",
+    "HostAddress",
+    "NetworkDatagram",
+    "NetworkAddressEntry",
+    "NetworkInterface",
+    "TcpSocket",
+    "UdpSocket",
+    "HttpPart",
+    "HttpMultiPart",
+    "NetworkCookie",
+    "NetworkCookieJar",
+    "NetworkRequest",
+    "NetworkAccessManager",
+]
