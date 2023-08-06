0699ae3329be9c6cdacab0f10c1584b96a6dcbec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:24 +0000

    Add package standards/0.0.1/standards-0.0.1.tar.gz

---
+++ packages/standards/standards-0.0.1.tar.gz/standards-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='standards',
+      version='0.0.1',
+      description='Standards',
+      author='Bülent Aldemir',
+      author_email='buelent@e-evolution.de',
+      license='MIT',
+      packages=['standards'],
+      zip_safe=False)
