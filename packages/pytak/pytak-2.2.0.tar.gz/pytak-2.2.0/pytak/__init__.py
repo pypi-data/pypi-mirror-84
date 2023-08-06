7a427be3afe4983fbee6c0ed983119fc5b7da51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:26 +0000

    Add package pytak/2.2.0/pytak-2.2.0.tar.gz

---
+++ packages/pytak/pytak-2.2.0.tar.gz/pytak-2.2.0/pytak/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Python Team Awareness Kit (PyTAK) Module.
+
+"""
+Python Team Awareness Kit (PyTAK) Module.
+~~~~
+
+
+:author: Greg Albrecht W2GMD <oss@undef.net>
+:copyright: Copyright 2020 Orion Labs, Inc.
+:license: Apache License, Version 2.0
+:source: <https://github.com/ampledata/pytak>
+
+"""
+
+from .constants import (LOG_LEVEL, LOG_FORMAT, DEFAULT_COT_PORT,  # NOQA
+                        DEFAULT_BACKOFF, DEFAULT_SLEEP)
+
+from .functions import split_host  # NOQA
+
+from .classes import (NetworkClient, CoTWorker, AsyncNetworkClient,  # NOQA
+                      AsyncCoTWorker, FTSClient, EventWorker)
+
+__author__ = 'Greg Albrecht W2GMD <oss@undef.net>'
+__copyright__ = 'Copyright 2020 Orion Labs, Inc.'
+__license__ = 'Apache License, Version 2.0'
