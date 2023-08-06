018b37ae24d3119b9d5e559c104945c930d0dfdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:52 +0000

    Add package coffea/0.6.46/coffea-0.6.46.tar.gz

---
+++ packages/coffea/coffea-0.6.46.tar.gz/coffea-0.6.46/coffea/processor/test_items/__init__.py
@@ -0,0 +1,5 @@
+from .NanoTestProcessor import NanoTestProcessor
+from .NanoEvents0Processor import NanoEvents0Processor
+from .NanoEventsProcessor import NanoEventsProcessor
+
+__all__ = ["NanoTestProcessor", "NanoEvents0Processor", "NanoEventsProcessor"]
