7cac1c69aabef14d84b81540d0e525b5c8f2c342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:05 +0000

    Add package alerta-server/7.5.7/alerta_server-7.5.7-py2.py3-none-any.whl

---
+++ packages/alerta-server/alerta_server-7.5.7-py2.py3-none-any.whl/alerta/webhooks/grafana.py
@@ -0,0 +1,92 @@
+import json
+from typing import Any, Dict
+
+from werkzeug.datastructures import ImmutableMultiDict
+
+from alerta.app import alarm_model, qb
+from alerta.exceptions import ApiError, RejectException
+from alerta.models.alert import Alert
+from alerta.utils.api import process_alert
+
+from . import WebhookBase
+
+JSON = Dict[str, Any]
+
+
+def parse_grafana(alert: JSON, match: Dict[str, Any], args: ImmutableMultiDict) -> Alert:
+    alerting_severity = args.get('severity', 'major')
+
+    if alert['state'] == 'alerting':
+        severity = alerting_severity
+    elif alert['state'] == 'ok':
+        severity = alarm_model.DEFAULT_NORMAL_SEVERITY
+    else:
+        severity = 'indeterminate'
+
+    environment = args.get('environment', 'Production')  # TODO: verify at create?
+    event_type = args.get('event_type', 'performanceAlert')
+    group = args.get('group', 'Performance')
+    origin = args.get('origin', 'Grafana')
+    service = args.get('service', 'Grafana')
+    timeout = args.get('timeout', type=int)
+
+    attributes = match.get('tags', None) or dict()
+    attributes = {k.replace('.', '_'): v for (k, v) in attributes.items()}
+
+    attributes['ruleId'] = str(alert['ruleId'])
+    if 'ruleUrl' in alert:
+        attributes['ruleUrl'] = '<a href="%s" target="_blank">Rule</a>' % alert['ruleUrl']
+    if 'imageUrl' in alert:
+        attributes['imageUrl'] = '<a href="%s" target="_blank">Image</a>' % alert['imageUrl']
+
+    return Alert(
+        resource=match['metric'],
+        event=alert['ruleName'],
+        environment=environment,
+        severity=severity,
+        service=[service],
+        group=group,
+        value='%s' % match['value'],
+        text=alert.get('message', None) or alert.get('title', alert['state']),
+        tags=list(),
+        attributes=attributes,
+        origin=origin,
+        event_type=event_type,
+        timeout=timeout,
+        raw_data=json.dumps(alert)
+    )
+
+
+class GrafanaWebhook(WebhookBase):
+    """
+    Grafana Alert alert notification webhook
+    See http://docs.grafana.org/alerting/notifications/#webhook
+    """
+
+    def incoming(self, path, query_string, payload):
+
+        if payload and payload['state'] == 'alerting':
+            return [parse_grafana(payload, match, query_string) for match in payload.get('evalMatches', [])]
+
+        elif payload and payload['state'] == 'ok' and payload.get('ruleId'):
+            try:
+                query = qb.from_dict({'attributes.ruleId': str(payload['ruleId'])})
+                existingAlerts = Alert.find_all(query)
+            except Exception as e:
+                raise ApiError(str(e), 500)
+
+            alerts = []
+            for updateAlert in existingAlerts:
+                updateAlert.severity = alarm_model.DEFAULT_NORMAL_SEVERITY
+                updateAlert.status = 'closed'
+
+                try:
+                    alert = process_alert(updateAlert)
+                except RejectException as e:
+                    raise ApiError(str(e), 403)
+                except Exception as e:
+                    raise ApiError(str(e), 500)
+                alerts.append(alert)
+            return alerts
+        else:
+            raise ApiError('no alerts in Grafana notification payload', 400)
---
+++ packages/alerta-server/alerta_server-7.5.7-py2.py3-none-any.whl/alerta/webhooks/newrelic.py
@@ -0,0 +1,56 @@
+from typing import Any, Dict
+
+from alerta.models.alert import Alert
+
+from . import WebhookBase
+
+JSON = Dict[str, Any]
+
+
+class NewRelicWebhook(WebhookBase):
+    """
+    New Relic webhook notification channel
+    See https://docs.newrelic.com/docs/alerts/new-relic-alerts/managing-notification-channels/notification-channels-control-where-send-alerts
+    """
+
+    def incoming(self, path, query_string, payload):
+
+        if 'version' not in payload:
+            raise ValueError('New Relic Legacy Alerting is not supported')
+
+        status = payload['current_state'].lower()
+        if status == 'open':
+            severity = payload['severity'].lower()
+        elif status == 'acknowledged':
+            severity = payload['severity'].lower()
+            status = 'ack'
+        elif status == 'closed':
+            severity = 'ok'
+        elif payload['severity'].lower() == 'info':
+            severity = 'informational'
+            status = 'open'
+        else:
+            severity = payload['severity'].lower()
+            status = 'open'
+
+        attributes = dict()
+        if 'incident_url' in payload:
+            attributes['moreInfo'] = '<a href="%s" target="_blank">Incident URL</a>' % payload['incident_url']
+        if 'runbook_url' in payload:
+            attributes['runBook'] = '<a href="%s" target="_blank">Runbook URL</a>' % payload['runbook_url']
+
+        return Alert(
+            resource=payload['targets'][0]['name'],
+            event=payload['condition_name'],
+            environment='Production',
+            severity=severity,
+            status=status,
+            service=[payload['account_name']],
+            group=payload['targets'][0]['type'],
+            text=payload['details'],
+            tags=['{}:{}'.format(key, value) for (key, value) in payload['targets'][0]['labels'].items()],
+            attributes=attributes,
+            origin='New Relic/v%s' % payload['version'],
+            event_type=payload['event_type'].lower(),
+            raw_data=payload
+        )
