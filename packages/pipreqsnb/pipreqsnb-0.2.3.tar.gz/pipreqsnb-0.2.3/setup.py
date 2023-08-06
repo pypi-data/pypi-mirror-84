1df228795c25b68acebebcb7926eed3934297b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:46 +0000

    Add package pipreqsnb/0.2.3/pipreqsnb-0.2.3.tar.gz

---
+++ packages/pipreqsnb/pipreqsnb-0.2.3.tar.gz/pipreqsnb-0.2.3/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setuptools.setup(
+    name='pipreqsnb',
+    version='0.2.3',
+    description='Pipreqs with jupyter notebook support',
+    url='https://github.com/ivanlen/pipreqsnb',
+    author='Ivan Lengyel',
+    author_email='ivalengy@gmail.com',
+    entry_points={'console_scripts': ['pipreqsnb=pipreqsnb.pipreqsnb:main']},
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    license='MIT License',
+    packages=setuptools.find_packages(),
+    install_requires=['pipreqs'],
+    zip_safe=False)
