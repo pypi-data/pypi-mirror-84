6c46a04c7bcee7acdeff1cccb81b2fe421f9fa93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:37 +0000

    Add package psycopgbinary/0.0.1/psycopgbinary-0.0.1-py2.py3-none-any.whl

---
+++ packages/psycopgbinary/psycopgbinary-0.0.1-py2.py3-none-any.whl/psycopgbinary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This is almost empty file.
+This package is just the placeholder for "psycopg2-binary" package, but with
+name usable in import statement. E.g.:
+import psycopg2binary
+"""
+
+from psycopg2 import *
