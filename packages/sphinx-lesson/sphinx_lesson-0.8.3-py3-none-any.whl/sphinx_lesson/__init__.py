9cf63d4933cbcef37b609dd7a9742e205b0d8c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:43 +0000

    Add package sphinx-lesson/0.8.3/sphinx_lesson-0.8.3-py3-none-any.whl

---
+++ packages/sphinx-lesson/sphinx_lesson-0.8.3-py3-none-any.whl/sphinx_lesson/__init__.py
@@ -0,0 +1,17 @@
+from ._version import __version__
+
+def setup(app):
+    "Sphinx extension setup"
+    app.setup_extension('myst_nb')
+    app.setup_extension('sphinx_copybutton')
+    app.setup_extension('sphinx_minipres')
+    app.setup_extension('sphinx_tabs.tabs')
+    app.setup_extension('sphinx_togglebutton')
+    app.setup_extension(__name__+'.directives')
+    app.setup_extension(__name__+'.md_transforms')
+
+    return {
+        'version': __version__,
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
