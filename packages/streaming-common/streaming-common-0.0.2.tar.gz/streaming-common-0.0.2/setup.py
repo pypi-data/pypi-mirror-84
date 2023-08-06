0895b787ea34cd4c606c03e69ad07ee2ff10a290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:20 +0000

    Add package streaming-common/0.0.2/streaming-common-0.0.2.tar.gz

---
+++ packages/streaming-common/streaming-common-0.0.2.tar.gz/streaming-common-0.0.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="streaming-common",
+    version="0.0.2",
+    description="Model structure that the data gets converted to before serialization into Kinesis stream.",
+    url="https://github.com/seniorplanning/streaming-common",
+    download_url="https://github.com/seniorplanning/streaming-common/archive/v_02.tar.gz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
