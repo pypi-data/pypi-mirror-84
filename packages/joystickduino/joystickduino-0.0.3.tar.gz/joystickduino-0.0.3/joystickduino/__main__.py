88435e96e6528b813d22df9c4be8a4e8ed64d07d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:06 +0000

    Add package joystickduino/0.0.3/joystickduino-0.0.3.tar.gz

---
+++ packages/joystickduino/joystickduino-0.0.3.tar.gz/joystickduino-0.0.3/joystickduino/__main__.py
@@ -0,0 +1,40 @@
+
+import sys
+if sys.argv[0].endswith("__main__.py"):
+    sys.argv[0] = "python -m joystickduino"
+
+import pyautogui, sys
+import time
+import serial
+import os
+
+os.system('clear')
+print('____________________________\n')
+print('Using Joystick to control mouse | 1.0.1 ')
+print('Created by Shaurya Pratap Singh')
+i = input('Default port?(y/N) ')
+port = None
+if i == 'y':
+    port = '/dev/cu.usbmodem14201'
+else:
+    p = input('Port: ')
+    port = p
+print('connecting..............')
+time.sleep(3)
+print('connected.')
+print('ctrl+c to exit;')
+print('For gui type j on terminal')
+print('_____________________________')
+pyautogui.FAILSAFE=False
+ArduinoSerial=serial.Serial(port,9600) 
+
+while 1:
+    data=str(ArduinoSerial.readline().decode('ascii'))   
+    (x,y,z)=data.split(":")           
+    (X,Y)=pyautogui.position()        
+    x=int(x)                          
+    y=int(y)
+    pyautogui.moveTo(X+x,Y-y)    
+
+    if '1' in z:                        
+        pyautogui.click(button="right")  
---
+++ packages/joystickduino/joystickduino-0.0.3.tar.gz/joystickduino-0.0.3/setup.py
@@ -0,0 +1,20 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="joystickduino",
+    version="0.0.3",
+    author='Someone',
+    author_email="shaurya.p.singh21@gmail.com",
+    description="may the force be with you",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pyserial',
+        'pyautogui'
+    ]
+)
