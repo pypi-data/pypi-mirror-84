7e4e9191b59442c09c0c7e568862d2367fad5a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:55 +0000

    Add package ecs-connect/2.0.0/ecs_connect-2.0.0-py2.py3-none-any.whl

---
+++ packages/ecs-connect/ecs_connect-2.0.0-py2.py3-none-any.whl/ecs_connect/cli.py
@@ -0,0 +1,78 @@
+""" Wrapper script for awscli which connects to containers running in
+    ecs seamlessly """
+import click
+import logging
+from ecs_connect.version import __version__
+from ecs_connect.config import ECSConfig
+from ecs_connect.ecs import ECSHandler
+from ecs_connect.ssm import SSMHandler
+
+
+@click.command()
+@click.option('--profile', help="Name of the profile to use in ~/.ecs-connect. \
+If none is provided, then the default profile will be used.\n")
+@click.option('--awsprofile', help="Name of the aws profile to use form ecs-connect. \
+If none is provided, then the default aws profile will be used.\n")
+@click.option('--cluster', help="Name of the ECS cluster. \
+If provided, then parameter from profile will be overridden.\n")
+@click.option('--service', help="Name of the service. \
+If provided, then parameter from profile will be overridden.\n")
+@click.option('--task', help="Name of the task. \
+If provided, then parameter from profile will be overridden.\n")
+@click.option('--bastion', help="Instance Id of the bastion node, \
+required for task running in FARGATE. \
+If provided, then parameter from profile will be overridden.\n")
+@click.option('--cmd', help="Initilization command to run. \
+If provided, then parameter from profile will be overridden.\n")
+@click.option('-a', '--all', is_flag=True,
+              help='Displays all running containers\n')
+@click.option('-V', '--version', is_flag=True,
+              help='Displays version number\n')
+@click.option('-v', '--verbose', is_flag=True, help='Enables verbose mode')
+@click.option('-d', '--debug', is_flag=True, help='Enables debug mode')
+def main(profile, awsprofile, cluster, service, task, bastion, cmd, all, version, verbose, debug):
+    if version:
+        print(__version__)
+        exit(0)
+
+    # Set up logging
+    logger = logging.getLogger('ecs-connect')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.WARN)
+    formatter = logging.Formatter('%(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    if verbose:
+        handler.setLevel(logging.INFO)
+    if debug:
+        handler.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+
+    ecs_config = ECSConfig(logger)
+    if not profile:
+        profile = "default"
+    if not cluster:
+        cluster = ecs_config.get_cluster(profile)
+    if not awsprofile:
+        awsprofile = ecs_config.get_awsprofile(profile)
+    if not task:
+        task = ecs_config.get_task(profile)
+    if task is None:
+        if not service:
+            service = ecs_config.get_service(profile)
+    else:
+        service = None
+    if not bastion:
+        bastion = ecs_config.get_bastion(profile)
+    if not cmd:
+        cmd = ecs_config.get_cmd(profile)
+
+    ecs = ECSHandler(awsprofile, cluster, service, task, bastion, logger)
+    instance_id, bastion_enabled = ecs.get_ec2_instance_id()
+
+    ssm = SSMHandler(awsprofile, instance_id, service, bastion_enabled, bastion, logger)
+    ssm.run(cmd, all)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/ecs-connect/ecs_connect-2.0.0-py2.py3-none-any.whl/ecs_connect/ssm.py
@@ -0,0 +1,83 @@
+"""Main module."""
+
+import boto3
+import subprocess
+import time
+
+
+class SSMHandler():
+    """ SSM handler  """
+    def __init__(self, awsprofile, ec2_id, service, bastion_enabled, bastion_id, logger):
+        self.awsprofile = awsprofile
+        self.ec2_id = ec2_id
+        self.service = service
+        self.bastion_enabled = bastion_enabled
+        self.bastion_id = bastion_id
+        self.logger = logger
+
+        self.ssm_client = boto3.session.Session(profile_name=self.awsprofile).client('ssm')
+
+    def get_conatiners(self, all):
+        if all:
+            exec_cmd = f'sudo docker ps'
+        else:
+            exec_cmd = f'sudo docker ps -f name={self.service}'
+        response = self.ssm_client.send_command(
+                    InstanceIds=[self.ec2_id],
+                    DocumentName="AWS-RunShellScript",
+                    Parameters={'commands': [exec_cmd]}, )
+        command_id = response['Command']['CommandId']
+
+        while True:
+            time.sleep(1)
+            output = self.ssm_client.get_command_invocation(
+                  CommandId=command_id,
+                  InstanceId=self.ec2_id,
+                )
+            if output['Status'] != "InProgress":
+                break
+
+        running_container = output['StandardOutputContent'].count('\n') - 1
+        if running_container <= 0:
+            self.logger.error("No container running with name %s.\
+                \nUse --all/-a to display all running container.",
+                              self.service)
+            exit(1)
+
+        if running_container == 1:
+            lines = output['StandardOutputContent'].split('\n')
+            conatiner_details = lines[1].split()
+            return conatiner_details[0]
+
+        print(output['StandardOutputContent'])
+        container_id = input('Please enter conatiner id: ')
+        return container_id
+
+    def start_session(self, container_id, exec_cmd):
+        command = f'aws --profile {self.awsprofile} ssm start-session --target {self.ec2_id} \
+        --document-name AWS-StartInteractiveCommand \
+        --parameters command="sudo docker exec -it {container_id} {exec_cmd}"'
+        self.logger.info("Running: %s", command)
+
+        subprocess.call(command, shell=True)
+
+    def start_bastion_session(self, exec_cmd):
+        command = f'aws --profile {self.awsprofile} ssm start-session --target {self.bastion_id} \
+        --document-name AWS-StartInteractiveCommand \
+        --parameters command="ssh -i /home/ssm-user/bastion root@{self.ec2_id}"'
+        self.logger.info("Running: %s", command)
+
+        subprocess.call(command, shell=True)
+
+    def run_directly(self, exec_cmd, all):
+        container_id = self.get_conatiners(all)
+        self.start_session(container_id, exec_cmd)
+
+    def run_through_bastion(self, exec_cmd, all):
+        self.start_bastion_session(exec_cmd)
+
+    def run(self, exec_cmd, all):
+        if self.bastion_enabled:
+            self.run_through_bastion(exec_cmd, all)
+        else:
+            self.run_directly(exec_cmd, all)
