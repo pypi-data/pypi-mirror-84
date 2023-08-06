7488f10a0b64a8e5b973e9864566981e6e15770b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:26 +0000

    Add package origo-sdk-python/0.3.1/origo-sdk-python-0.3.1.tar.gz

---
+++ packages/origo-sdk-python/origo-sdk-python-0.3.1.tar.gz/origo-sdk-python-0.3.1/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="origo-sdk-python",
+    version="0.3.1",
+    author="Oslo Origo",
+    author_email="dataplattform@oslo.kommune.no",
+    description="SDK for origo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/oslokommune/origo-sdk-python",
+    package_data={"origo": ["py.typed"]},
+    packages=setuptools.find_packages(".", exclude=["tests*"]),
+    install_requires=[
+        "requests",
+        "urllib3",
+        "PrettyTable",
+        "python-keycloak",
+        "PyJWT",
+        "jsonschema",
+    ],
+    data_files=[
+        (
+            "schema",
+            [
+                "origo/pipelines/resources/schemas/pipelines.json",
+                "origo/pipelines/resources/schemas/pipeline-instances.json",
+                "origo/pipelines/resources/schemas/pipeline-inputs.json",
+                "origo/pipelines/resources/schemas/schemas.json",
+            ],
+        )
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    zip_safe=False,
+)
