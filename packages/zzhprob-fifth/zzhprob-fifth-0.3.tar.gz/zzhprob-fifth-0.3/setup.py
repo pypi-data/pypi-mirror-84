1f8cfbbf7989f7bb51010b227a285727c11d13db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:16 +0000

    Add package zzhprob-fifth/0.3/zzhprob-fifth-0.3.tar.gz

---
+++ packages/zzhprob-fifth/zzhprob-fifth-0.3.tar.gz/zzhprob-fifth-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='zzhprob-fifth',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['zzhprob-fifth'],
+      zip_safe=False)
