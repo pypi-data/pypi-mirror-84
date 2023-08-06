1697d7d012ebb64bdb3529d35278eebe9c8be38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:17 +0000

    Add package kg-ingressnginx/0.7.4/kg_ingressnginx-0.7.4-py3-none-any.whl

---
+++ packages/kg-ingressnginx/kg_ingressnginx-0.7.4-py3-none-any.whl/kg_ingressnginx/__init__.py
@@ -0,0 +1,13 @@
+from .builder import (
+    IngressNGINXBuilder
+)
+from .option import (
+    IngressNGINXOptions
+)
+
+__version__ = "0.7.4"
+
+__all__ = [
+    'IngressNGINXBuilder',
+    'IngressNGINXOptions',
+]
