bc82526951f9cb8771befa558fc22fe2c2ebfb11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:01 +0000

    Add package elements/0.1.1/elements-0.1.1.tar.gz

---
+++ packages/elements/elements-0.1.1.tar.gz/elements-0.1.1/README.md
@@ -0,0 +1,11 @@
+# 🔥 Elements
+
+Work in progress.
+
+```
+python3 setup.py develop --user
+```
+
+```
+python3 -m pytest -v -rs --cov=elements tests
+```
---
+++ packages/elements/elements-0.1.1.tar.gz/elements-0.1.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name='elements',
+    version='0.1.1',
+    description='Building blocks for productive machine learning research.',
+    url='http://github.com/danijar/elements',
+    install_requires=[],
+    packages=['elements'],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Scientific/Engineering',
+    ],
+)
