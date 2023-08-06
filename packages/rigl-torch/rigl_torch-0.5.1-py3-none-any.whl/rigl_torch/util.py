76249facb6c3542341b1477473b8495842b79ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:40 +0000

    Add package rigl-torch/0.5.1/rigl_torch-0.5.1-py3-none-any.whl

---
+++ packages/rigl-torch/rigl_torch-0.5.1-py3-none-any.whl/rigl_torch/util.py
@@ -0,0 +1,52 @@
+import torch
+import torchvision
+
+
+def get_weighted_layers(model, i=0, layers=None, linear_layers_mask=None):
+    if layers is None:
+        layers = []
+    if linear_layers_mask is None:
+        linear_layers_mask = []
+
+    items = model._modules.items()
+    if i == 0:
+        items = [(None, model)]
+
+    for layer_name, p in items:
+        if isinstance(p, torch.nn.Linear):
+            layers.append([p])
+            linear_layers_mask.append(1)
+        elif hasattr(p, 'weight'):
+            layers.append([p])
+            linear_layers_mask.append(0)
+        elif isinstance(p, torch.nn.BatchNorm2d):
+            layers[-1].append(p)
+        elif isinstance(p, torch.nn.ReLU):
+            layers[-1].append(p)
+        elif isinstance(p, torch.nn.MaxPool2d) or isinstance(p, torch.nn.AdaptiveAvgPool2d):
+            layers[-1].append(p)
+        elif layer_name == 'downsample':
+            layers.append(p)
+            linear_layers_mask.append(0)
+        elif isinstance(p, torchvision.models.resnet.Bottleneck) or isinstance(p, torchvision.models.resnet.BasicBlock):
+            _, linear_layers_mask, i = get_weighted_layers(p, i=i + 1, layers=layers, linear_layers_mask=linear_layers_mask)
+        else:
+            _, linear_layers_mask, i = get_weighted_layers(p, i=i + 1, layers=layers, linear_layers_mask=linear_layers_mask)
+
+    return layers, linear_layers_mask, i 
+
+
+
+def get_W(model, return_linear_layers_mask=False):
+    layers, linear_layers_mask, _ = get_weighted_layers(model)
+
+    W = []
+    for layer in layers:
+        idx = 0 if hasattr(layer[0], 'weight') else 1
+        W.append(layer[idx].weight)
+
+    assert len(W) == len(linear_layers_mask)
+
+    if return_linear_layers_mask:
+        return W, linear_layers_mask
+    return W
