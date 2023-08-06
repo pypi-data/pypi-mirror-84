7d9e9f0416d8226d9a2e535de40dd80ce9d05140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:33 +0000

    Add package frictionless/3.23.6/frictionless-3.23.6-py2.py3-none-any.whl

---
+++ packages/frictionless/frictionless-3.23.6-py2.py3-none-any.whl/frictionless/describe/package.py
@@ -0,0 +1,30 @@
+from ..package import Package
+
+
+def describe_package(source, *, hashing=None, basepath=None, expand=False):
+    """Describe the given source as a package
+
+    API      | Usage
+    -------- | --------
+    Public   | `from frictionless import describe_package`
+
+    Parameters:
+        source (any): data source
+        hashing? (str): a hashing algorithm for resources
+        basepath? (str): package basepath
+        expand? (bool): if `True` it will expand the metadata
+
+    Returns:
+        Package: data package
+
+    """
+
+    # Infer package
+    package = Package(hashing=hashing, basepath=basepath)
+    package.infer(source)
+
+    # Expand package
+    if expand:
+        package.expand()
+
+    return package
