fda4a76e7d1e7bc24f64adcd61625b58b160bbd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:48 +0000

    Add package jinfo/0.1/jinfo-0.1-py3-none-any.whl

---
+++ packages/jinfo/jinfo-0.1-py3-none-any.whl/jinfo/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+from .sequence import DNASeq, RNASeq, AASeq
+from .utils import one_hot_dna, random_dna, DNASeq_from_NCBI
+from .tables import DNA_VOCAB, RNA_VOCAB, AA_VOCAB, CODON_TABLE
