5b73632cd762f7c45e8d3cd3b606a408a1698b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:00 +0000

    Add package drf-spectacular/0.11.0/drf-spectacular-0.11.0.tar.gz

---
+++ packages/drf-spectacular/drf-spectacular-0.11.0.tar.gz/drf-spectacular-0.11.0/tests/test_plumbing.py
@@ -0,0 +1,71 @@
+from django.conf.urls import include
+from django.db import models
+from django.urls import re_path
+from rest_framework import serializers
+
+from drf_spectacular.openapi import AutoSchema
+from drf_spectacular.plumbing import (
+    detype_pattern, follow_field_source, force_instance, is_field, is_serializer,
+)
+
+
+def test_is_serializer():
+    assert not is_serializer(serializers.SlugField)
+    assert not is_serializer(serializers.SlugField())
+
+    assert not is_serializer(models.CharField)
+    assert not is_serializer(models.CharField())
+
+    assert is_serializer(serializers.Serializer)
+    assert is_serializer(serializers.Serializer())
+
+
+def test_is_field():
+    assert is_field(serializers.SlugField)
+    assert is_field(serializers.SlugField())
+
+    assert not is_field(models.CharField)
+    assert not is_field(models.CharField())
+
+    assert not is_field(serializers.Serializer)
+    assert not is_field(serializers.Serializer())
+
+
+def test_force_instance():
+    assert isinstance(force_instance(serializers.CharField), serializers.CharField)
+    assert force_instance(5) == 5
+    assert force_instance(dict) == dict
+
+
+def test_follow_field_source_forward_reverse(no_warnings):
+    class FFS1(models.Model):
+        field_bool = models.BooleanField()
+
+    class FFS2(models.Model):
+        ffs1 = models.ForeignKey(FFS1, on_delete=models.PROTECT)
+
+    class FFS3(models.Model):
+        ffs2 = models.ForeignKey(FFS2, on_delete=models.PROTECT)
+        field_float = models.FloatField()
+
+    forward_field = follow_field_source(FFS3, ['ffs2', 'ffs1', 'field_bool'])
+    reverse_field = follow_field_source(FFS1, ['ffs2', 'ffs3', 'field_float'])
+    forward_model = follow_field_source(FFS3, ['ffs2', 'ffs1'])
+    reverse_model = follow_field_source(FFS1, ['ffs2', 'ffs3'])
+
+    assert isinstance(forward_field, models.BooleanField)
+    assert isinstance(reverse_field, models.FloatField)
+    assert isinstance(forward_model, models.ForeignKey)
+    assert isinstance(reverse_model, models.AutoField)
+
+    auto_schema = AutoSchema()
+    assert auto_schema._map_model_field(forward_field, None)['type'] == 'boolean'
+    assert auto_schema._map_model_field(reverse_field, None)['type'] == 'number'
+    assert auto_schema._map_model_field(forward_model, None)['type'] == 'integer'
+    assert auto_schema._map_model_field(reverse_model, None)['type'] == 'integer'
+
+
+def test_detype_patterns_with_module_includes(no_warnings):
+    detype_pattern(
+        pattern=re_path(r'^', include('tests.test_fields'))
+    )
