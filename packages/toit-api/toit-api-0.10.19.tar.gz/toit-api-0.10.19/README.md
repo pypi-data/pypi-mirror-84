c27b9f0076f2237562033ad908359f0bd75454d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:54 +0000

    Add package toit-api/0.10.19/toit-api-0.10.19.tar.gz

---
+++ packages/toit-api/toit-api-0.10.19.tar.gz/toit-api-0.10.19/README.md
@@ -0,0 +1,11 @@
+# Toitware python API [![pypi version](https://img.shields.io/pypi/v/toit-api?color=success)](https://pypi.org/project/toit-api/)
+
+Generated protobuf and grpc stubs for node and typescript.
+
+## Package
+
+Available on pypi: https://pypi.org/project/toit-api/
+
+## Example
+
+See https://github.com/toitware/demos/tree/master/api/python
---
+++ packages/toit-api/toit-api-0.10.19.tar.gz/toit-api-0.10.19/setup.py
@@ -0,0 +1,25 @@
+__version__="0.10.19"
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="toit-api",
+    version=__version__,
+    author="Toitware",
+    author_email="developers@toitware.com",
+    description="Toitware",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/toitware/api",
+    package_dir={'': 'src'},
+    packages=setuptools.find_namespace_packages(where='./src', include='toit.*'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=('protobuf','grpcio'),
+    python_requires='>=3.6',
+)
