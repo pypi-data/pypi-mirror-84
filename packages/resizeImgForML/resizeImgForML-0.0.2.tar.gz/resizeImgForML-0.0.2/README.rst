8d5eb8ef96772ad19a30e1a3db402ceef1b333d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:34 +0000

    Add package resizeImgForML/0.0.2/resizeImgForML-0.0.2.tar.gz

---
+++ packages/resizeImgForML/resizeImgForML-0.0.2.tar.gz/resizeImgForML-0.0.2/README.rst
@@ -0,0 +1,35 @@
+Requirements
+------------
+* Python 3.x
+
+Features
+--------
+* nothing
+
+History
+-------
+
+* 0.0.1 (2020-10-27)
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+* first release
+
+Usage
+------------
+please look at Github README
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Plan to upgrade
+---------------------
+
+* open image through one unipue path(it is useful for WebApp like Django) 
+* open one directory and all of image at the directory 
+* resize & save images for MNIST
+
+and more, if i can
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+* first release
---
+++ packages/resizeImgForML/resizeImgForML-0.0.2.tar.gz/resizeImgForML-0.0.2/setup.py
@@ -0,0 +1,70 @@
+from setuptools import setup, find_packages
+import os
+
+name = 'resizeImgForML'
+version = '0.0.2'
+
+def read_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+short_description = '`resizeImgForML` is a package for resize images for machie learning.'
+
+long_description = """\
+Requirements
+------------
+* Python 3.x
+
+Features
+--------
+* nothing
+
+History
+-------
+
+* 0.0.1 (2020-10-27)
+
+0.0.2 (2020-11-02)
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+* first release
+
+Usage
+------------
+please look at Github README
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Plan to upgrade
+---------------------
+
+* open image through one unipue path(it is useful for WebApp like Django) 
+* open one directory and all of image at the directory 
+* resize & save images for MNIST
+
+and more, if i can
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+* first release
+"""
+
+setup(
+    name=name,
+    version=version,
+    url='https://github.com/k-mawa/resizeImgForML',
+    description=short_description,
+    long_description=long_description,
+    keywords=['machine', 'machine learning', 'resize', 'Pillow', 'PIL', 'openCV'],
+    packages=find_packages(),
+    install_requires=read_from_file('requirements.txt'),
+    author='Kosuke Mawatari',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+
+)
