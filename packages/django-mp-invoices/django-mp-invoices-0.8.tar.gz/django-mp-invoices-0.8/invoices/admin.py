656e467940b1c9cb3765680d533603f7de7fafe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:49 +0000

    Add package django-mp-invoices/0.8/django-mp-invoices-0.8.tar.gz

---
+++ packages/django-mp-invoices/django-mp-invoices-0.8.tar.gz/django-mp-invoices-0.8/invoices/admin.py
@@ -0,0 +1,38 @@
+
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from cap.decorators import template_list_item, short_description
+
+from invoices.models import Arrival, Sale
+
+
+@admin.register(Arrival, Sale)
+class InvoiceAdmin(admin.ModelAdmin):
+
+    list_display = [
+        'get_name', 'type', 'customer', 'get_total_qty', 'get_total',
+        'get_item_actions'
+    ]
+    list_display_links = ['type']
+    list_filter = ['type', 'created']
+    search_fields = ['id']
+
+    def has_add_permission(self, request):
+        return False
+
+    @template_list_item('invoices/name_cell.html', _('Created'))
+    def get_name(self, obj):
+        return {'object': obj}
+
+    @short_description(_('Total'))
+    def get_total(self, obj):
+        return obj.total
+
+    @short_description(_('Total qty'))
+    def get_total_qty(self, obj):
+        return obj.total_qty
+
+    @template_list_item('invoices/list_item_actions.html', _('Actions'))
+    def get_item_actions(self, obj):
+        return {'object': obj}
