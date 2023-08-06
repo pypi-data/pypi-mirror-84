9ecb3f5534579169559bdbb63c4452dbd8e8305d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:23 +0000

    Add package console-conjugator/0.0.7/console-conjugator-0.0.7.tar.gz

---
+++ packages/console-conjugator/console-conjugator-0.0.7.tar.gz/console-conjugator-0.0.7/Deutschconjugation/cli_scripts.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infinitive", help="The unconjugated verb")
+
+    parser.add_argument(
+        "pronoun", help="The pronoun that the infinitve is being conjugated for"
+    )
+
+    parser.add_argument(
+        "tense",
+        help="The tense of the conjugated phrase in quotes if \
+                    there are multiple words",
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def to_lower_case():
+    args = get_args()
+    return args.infinitive.lower(), args.pronoun.lower(), args.tense.lower()
+
+
+def main():
+    args = get_args()
+    from Deutschconjugation import conjugator
+
+    infinitive, pronoun, tense = (
+        conjugator.format(args.infinitive),
+        format(args.pronoun),
+        format(args.tense),
+    )
+    conj_out = f"\033[34m{conjugator.conjugate(infinitive, pronoun, tense)}\033[0m"
+
+    print(f"\033[32m{pronoun}\033[0m {conj_out}")
+
+
+if __name__ == "__main__":
+    main()
