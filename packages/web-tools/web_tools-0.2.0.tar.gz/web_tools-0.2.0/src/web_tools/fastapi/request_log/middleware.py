0a66d2d32682b17a55de0033e7c909032514642d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:48 +0000

    Add package web-tools/0.2.0/web_tools-0.2.0.tar.gz

---
+++ packages/web-tools/web_tools-0.2.0.tar.gz/web_tools-0.2.0/src/web_tools/fastapi/request_log/middleware.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import datetime
+from typing import Optional, Type
+import traceback
+
+from fastapi import Request, Response
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+
+from .context import LogContext
+from .model_mixin import SaveLogMixin
+
+
+class LogMiddleware(BaseHTTPMiddleware):
+    save_model_class: Optional[Type[SaveLogMixin]]
+
+    def __init__(self, save_model_class: Optional[Type[SaveLogMixin]], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.save_model_class = save_model_class
+
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        request_datetime = datetime.datetime.utcnow()
+        exception_traceback = None
+        response = None
+        try:
+            response = await call_next(request)
+            return response
+        except Exception:
+            exception_traceback = traceback.format_exc()
+            raise
+        finally:
+            response_datetime = datetime.datetime.utcnow()
+            log_context = await LogContext.create(request_datetime, request, response_datetime,
+                                                  response=response, exception_traceback=exception_traceback)
+            if self.save_model_class:
+                await self.save_model_class.save_log(log_context, request=request, response=response)
