10f06119a8fb009662dfce416ce33264cd18c810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:17 +0000

    Add package telekinesis/0.1.0a20/telekinesis-0.1.0a20.tar.gz

---
+++ packages/telekinesis/telekinesis-0.1.0a20.tar.gz/telekinesis-0.1.0a20/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="telekinesis",
+    version="0.1.0-alpha.20",
+    author="Telekinesis Cloud",
+    author_email="support@telekinesis.cloud",
+    description="A websockets based RPC to easily develop, deploy and call services online.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/telekinesis-cloud/telekinesis",
+    packages=setuptools.find_packages(),
+    install_requires=["websockets", "cryptography", "makefun", "aiohttp", "bson", "ujson"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
