c66010c57c518ed30cc194d500b09be225a3a559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:34 +0000

    Add package kloppy/1.3.0/kloppy-1.3.0.tar.gz

---
+++ packages/kloppy/kloppy-1.3.0.tar.gz/kloppy-1.3.0/kloppy/infra/serializers/__init__.py
@@ -0,0 +1,13 @@
+from .tracking import (
+    TrackingDataSerializer,
+    TRACABSerializer,
+    MetricaTrackingSerializer,
+    EPTSSerializer,
+)
+from .event import (
+    StatsBombSerializer,
+    OptaSerializer,
+    MetricaEventsJsonSerializer,
+)
+
+# NOT YET: from .event import EventDataSerializer, MetricaEventSerializer
---
+++ packages/kloppy/kloppy-1.3.0.tar.gz/kloppy-1.3.0/kloppy/infra/serializers/event/__init__.py
@@ -0,0 +1,6 @@
+from .base import EventDataSerializer
+
+# from .metrica import MetricaEventSerializer
+from .statsbomb import StatsBombSerializer
+from .opta import OptaSerializer
+from .metrica import MetricaEventsJsonSerializer
