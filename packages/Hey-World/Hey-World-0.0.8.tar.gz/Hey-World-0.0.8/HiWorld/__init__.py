39430299b7afd3d55a56c29f3478d4b6d4ea819b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:10 +0000

    Add package Hey-World/0.0.8/Hey-World-0.0.8.tar.gz

---
+++ packages/Hey-World/Hey-World-0.0.8.tar.gz/Hey-World-0.0.8/HiWorld/__init__.py
@@ -0,0 +1,14 @@
+# from . import hi_world
+#
+#
+# __all__ = [
+#     'HiWorld.py'
+# ]
+#
+# from .APIHelper import (
+#     remove_emoji,
+# )
+
+from .HiWorld import (
+    hey
+)
---
+++ packages/Hey-World/Hey-World-0.0.8.tar.gz/Hey-World-0.0.8/README.md
@@ -0,0 +1,9 @@
+# hello-world
+A sample repo for creating a python pip package
+
+
+* Code Snippet  
+from HiWorld import HiWorld  
+HiWorld.hey()  
+👋 Hi there, Rahul here 🤗   
+
