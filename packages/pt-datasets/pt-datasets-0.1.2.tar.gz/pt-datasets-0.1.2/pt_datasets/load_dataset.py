c09ed47f5c6960c39b928acf4d13a321d9ce0dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:03 +0000

    Add package pt-datasets/0.1.2/pt-datasets-0.1.2.tar.gz

---
+++ packages/pt-datasets/pt-datasets-0.1.2.tar.gz/pt-datasets-0.1.2/pt_datasets/load_dataset.py
@@ -0,0 +1,101 @@
+# PyTorch Datasets utility repository
+# Copyright (C) 2020  Abien Fred Agarap
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Function for loading datasets"""
+from typing import Tuple
+
+import torch
+import torchvision
+
+__author__ = "Abien Fred Agarap"
+
+
+def load_dataset(
+    name: str = "mnist", data_folder: str = "~/torch_datasets"
+) -> Tuple[object, object]:
+    """
+    Returns a tuple of torchvision dataset objects.
+
+    Parameters
+    ----------
+    name: str
+        The name of the dataset to load. Current choices:
+            1. mnist (MNIST)
+            2. fashion_mnist (FashionMNIST)
+            3. emnist (EMNIST/Balanced)
+            4. cifar10 (CIFAR10)
+            5. svhn (SVHN)
+    data_folder: str
+        The path to the folder for the datasets.
+
+    Returns
+    -------
+    Tuple[object, object]
+        A tuple consisting of the training dataset and the test dataset.
+    """
+    supported_datasets = ["mnist", "fashion_mnist", "emnist", "cifar10", "svhn"]
+
+    name = name.lower()
+
+    assert (
+        name in supported_datasets
+    ), f"[ERROR] Dataset {name} is not supported. Supported datasets: mnist, fashion_mnist, emnist, cifar10, svhn."
+
+    transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+
+    if name == "mnist":
+        train_dataset = torchvision.datasets.MNIST(
+            root=data_folder, train=True, download=True, transform=transform
+        )
+        test_dataset = torchvision.datasets.MNIST(
+            root=data_folder, train=False, download=True, transform=transform
+        )
+    elif name == "fashion_mnist":
+        train_dataset = torchvision.datasets.FashionMNIST(
+            root=data_folder, train=True, download=True, transform=transform
+        )
+        test_dataset = torchvision.datasets.FashionMNIST(
+            root=data_folder, train=False, download=True, transform=transform
+        )
+    elif name == "emnist":
+        train_dataset = torchvision.datasets.EMNIST(
+            root=data_folder,
+            train=True,
+            split="balanced",
+            download=True,
+            transform=transform,
+        )
+        test_dataset = torchvision.datasets.EMNIST(
+            root=data_folder,
+            train=False,
+            split="balanced",
+            download=True,
+            transform=transform,
+        )
+    elif name == "cifar10":
+        train_dataset = torchvision.datasets.CIFAR10(
+            root=data_folder, train=True, download=True, transform=transform
+        )
+        test_dataset = torchvision.datasets.CIFAR10(
+            root=data_folder, train=False, download=True, transform=transform
+        )
+    elif name == "svhn":
+        train_dataset = torchvision.datasets.SVHN(
+            root=data_folder, split="train", download=True, transform=transform
+        )
+        test_dataset = torchvision.datasets.SVHN(
+            root=data_folder, split="test", download=True, transform=transform
+        )
+    return (train_dataset, test_dataset)
