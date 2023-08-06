94a4e257f6099461dc81030d3a0d91dd4b9a6a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:53 +0000

    Add package panda3d-viewer/0.4.0/panda3d_viewer-0.4.0.tar.gz

---
+++ packages/panda3d-viewer/panda3d_viewer-0.4.0.tar.gz/panda3d_viewer-0.4.0/panda3d_viewer/__init__.py
@@ -0,0 +1,9 @@
+"""This package contains Viewer, a simpe and efficient cross-platform Panda3D based 3D viewer."""
+
+from .viewer import Viewer
+from .viewer_config import ViewerConfig
+from .viewer_errors import ViewerClosedError, ViewerError
+
+__all__ = ('Viewer', 'ViewerConfig', 'ViewerError', 'ViewerClosedError')
+
+__version__ = '0.4.0'
