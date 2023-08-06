1eb960b7cfc1b791f1eac02e8c2248b5c73dae40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:34 +0000

    Add package pyILT/0.0.10/pyILT-0.0.10.tar.gz

---
+++ packages/pyILT/pyILT-0.0.10.tar.gz/pyILT-0.0.10/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: POSIX :: Linux',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='pyILT',
+  version='0.0.10',
+  description='Extraction of data from ILThermo',
+  long_description=open('/home/joao_ines/Documents/SLE/pyILThermo/README.txt').read() + '\n\n' + open('/home/joao_ines/Documents/SLE/pyILThermo/CHANGELOG.txt').read(),
+  url='',  
+  author='Joao Ines - FCT-UNL',
+  author_email='j.ines@fct.unl.pt',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='ILThermo_DB', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
