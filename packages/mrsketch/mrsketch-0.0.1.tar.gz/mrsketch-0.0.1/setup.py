50f640805c6cfea1bd7790dca6e9d9ef69b1b7ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:52 +0000

    Add package mrsketch/0.0.1/mrsketch-0.0.1.tar.gz

---
+++ packages/mrsketch/mrsketch-0.0.1.tar.gz/mrsketch-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='mrsketch',
+    version='0.0.1',
+    description='Simple Multi-Dimentional Multi-resolution graph for building sketches',
+    url='https://github.com/Simonl07/mdmr-graph',
+    author='Simon Lu',
+    author_email='mlu18@dons.usfca.edu',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3',
+    ]
+)
