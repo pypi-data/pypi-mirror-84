ff46ef1746a5d5fcaf4194f896999b1e806176b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:36 +0000

    Add package tinytag/1.5.0/tinytag-1.5.0.tar.gz

---
+++ packages/tinytag/tinytag-1.5.0.tar.gz/tinytag-1.5.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from os.path import join
+from setuptools import setup, find_packages
+
+
+def get_version():
+    with open(join("tinytag", "__init__.py")) as f:
+        version_line = next(line for line in f if line.startswith("__version__ ="))
+        return version_line.split("=")[1].strip().strip("\"'")
+
+
+setup(
+        name="tinytag",
+        version=get_version(),
+        packages=find_packages(),
+        )
