71526e4664dbd10bfe5af8da066714a7e3dd91bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:57 +0000

    Add package eve-panel/0.3.4/eve_panel-0.3.4.tar.gz

---
+++ packages/eve-panel/eve_panel-0.3.4.tar.gz/eve_panel-0.3.4/eve_panel/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""Eve-panel.
+
+A marriage between Eve and Panel.
+
+Example:
+    
+Todo:
+
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+"""
+
+__author__ = """Yossi Mosbacher"""
+__email__ = 'joe.mosbacher@gmail.com'
+__version__ = '0.3.4'
+
+import panel as pn
+
+from .auth import EveAuthBase
+from .domain import EveDomain
+from .eve_client import EveClient
+from .item import EveItem
+from .resource import EveResource
+from .utils import from_app_config
+from .menu import Menu, css
+from .web_client import EveWebClient
+
+
+def notebook():
+    """ Load notebook support. """
+    return pn.extension('ace')
+
+def extension():
+   pn.extension('ace')
+   pn.config.raw_css.append(css)
+   
