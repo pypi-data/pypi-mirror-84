d02ddb7e230a426da153202522dfe205765e10ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:12 +0000

    Add package fink/0.0.1/fink-0.0.1.tar.gz

---
+++ packages/fink/fink-0.0.1.tar.gz/fink-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='fink',
+      version='0.0.1',
+      description='Fink UI',
+      author='Bülent Aldemir',
+      author_email='buelent@e-evolution.de',
+      license='MIT',
+      packages=['fink'],
+      zip_safe=False)
