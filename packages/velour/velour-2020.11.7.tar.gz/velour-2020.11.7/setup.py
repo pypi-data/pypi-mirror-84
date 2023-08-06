ba89c121a4bd46d237a23080fbe90e2486eff962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:29 +0000

    Add package velour/2020.11.7/velour-2020.11.7.tar.gz

---
+++ packages/velour/velour-2020.11.7.tar.gz/velour-2020.11.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="velour", 
+    version="2020.11.07",
+    author="Raphaël Tinarrage",
+    author_email="raphael.tinarrage@gmail.com",
+    description="Topological inference from point clouds with persistent homology",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/raphaeltinarrage/velour",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
