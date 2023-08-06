2887830ecca54ea2d9530537875cfe5edd59d37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:22 +0000

    Add package pilton/1.1.0/pilton-1.1.0.tar.gz

---
+++ packages/pilton/pilton-1.1.0.tar.gz/pilton-1.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+        name='pilton',
+        version='1.1.0',
+        py_modules= ['pilton'],
+        package_data     = {
+            "": [
+                "*.txt",
+                "*.md",
+                "*.rst",
+                "*.py"
+                ]
+            },
+        license='Creative Commons Attribution-Noncommercial-Share Alike license',
+        long_description=open('README.md').read(),
+        install_requires=[
+            'python-shell-colors'
+        ]
+        )
