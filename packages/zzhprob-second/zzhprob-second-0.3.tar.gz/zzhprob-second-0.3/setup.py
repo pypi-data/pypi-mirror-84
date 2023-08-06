717daf887e3b3797fb79a7f5e1634f3194320e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:55 +0000

    Add package zzhprob-second/0.3/zzhprob-second-0.3.tar.gz

---
+++ packages/zzhprob-second/zzhprob-second-0.3.tar.gz/zzhprob-second-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='zzhprob-second',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
