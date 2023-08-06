a3dc268ad66276e36b758cc424ed5df15fbf43b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:33 +0000

    Add package cmdline-AndrikBJavaCCF/0.1.1/cmdline_AndrikBJavaCCF-0.1.1-py3.8.egg

---
+++ packages/cmdline-AndrikBJavaCCF/cmdline_AndrikBJavaCCF-0.1.1-py3.8.egg/AndrikBJavaCCF/file.py
@@ -0,0 +1,19 @@
+from .lexer import tokenize_file
+
+
+class File:
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.tokens = tokenize_file(filename)
+
+    def __repr__(self):
+        return self.filename
+
+    def __str__(self):
+        return str(self.filename)
+
+    def print_file(self):
+        file = open(self.filename, mode="w", encoding='utf-8')
+        for token in self.tokens:
+            file.write(token.second_value)
