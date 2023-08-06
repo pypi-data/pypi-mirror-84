53392a699455cbdc77887820f052716569fec590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:59 +0000

    Add package beefrost/0.0.1/beefrost-0.0.1.tar.gz

---
+++ packages/beefrost/beefrost-0.0.1.tar.gz/beefrost-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="beefrost",
+    version="0.0.1",
+    author="Myriam Cournet",
+    author_email="myriam.cournet@cnes.fr",
+    description="A small example package",
+    url="https://github.com/CNES/beefrost",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
