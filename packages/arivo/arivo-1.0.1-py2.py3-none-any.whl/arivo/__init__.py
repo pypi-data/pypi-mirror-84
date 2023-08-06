cf4dac55260549cdb4e1d90c3dc7a159cba149df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:40 +0000

    Add package arivo/1.0.1/arivo-1.0.1-py2.py3-none-any.whl

---
+++ packages/arivo/arivo-1.0.1-py2.py3-none-any.whl/arivo/__init__.py
@@ -0,0 +1,16 @@
+api_key = ""
+api_url = ""
+webhook_secret = ""
+
+
+def use_staging():
+    global api_url
+    api_url = "https://api.digimon.arivo.fun"
+
+
+def use_production():
+    global api_url
+    api_url = "https://api.parken.arivo.app"
+
+
+use_production()
