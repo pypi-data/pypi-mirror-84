a267d0ed0bf1595af292dc7519a754303b65e214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:05 +0000

    Add package NotSoFastQC/1.0/NotSoFastQC-1.0-py3.8.egg

---
+++ packages/NotSoFastQC/NotSoFastQC-1.0-py3.8.egg/NotSoFastQC/modules.py
@@ -0,0 +1,15 @@
+# Dictionary of available modules. Can be added to anytime and code will still work.
+# Would require graph generation methods in FastQCManager if graph output was needed.
+module_dict = {"BS": "Basic Statistics",
+               1:  "Per base sequence quality",
+               2:  "Per tile sequence quality",
+               3:  "Per sequence quality scores",
+               4:  "Per base sequence content",
+               5:  "Per sequence GC content",
+               6:  "Per base N content",
+               7:  "Sequence Length Distribution",
+               8:  "Sequence Duplication Levels",
+               9:  "Overrepresented sequences",
+               10: "Adapter Content",
+               11: "Kmer Content",
+               }
