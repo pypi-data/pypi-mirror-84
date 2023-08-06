28867b59756ae66af242ec9b5d18ae4370f9ee83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:06 +0000

    Add package mitosheet/0.1.38/mitosheet-0.1.38-py2.py3-none-any.whl

---
+++ packages/mitosheet/mitosheet-0.1.38-py2.py3-none-any.whl/mitosheet/utils.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Mito.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+Contains helpful utility functions
+"""
+import os
+import json
+import pandas as pd
+import numpy as np
+from string import ascii_letters, digits
+
+MITO_FOLDER = os.path.expanduser("~/.mito")
+
+
+def empty_column_python_code():
+    """
+    Helper functions for creating an empty entry
+    for column_python_code - which can then be filled 
+    in later (or left empty for an unedited column)
+    """
+    return {
+        'column_name_change': None,
+        'column_type_change': None,
+        'column_value_changes': {},
+        'column_formula_changes': ''
+    }
+
+
+def is_valid_header(column_header):
+    """
+    A header is valid if:
+    1. It is a string
+    2. It only contains alphanumber characters, or _
+    3. It has at least one non-numeric character.
+
+    Valid examples: A, ABC, 012B, 213_bac, 123_123
+    Invalid examples: 123, 123!!!, ABC!, 123-123
+
+    This is a result of not being able to distingush column headers from constants
+    otherwise, and would not be necessary if we had a column identifier!
+    """
+
+    return isinstance(column_header, str) and ( # Condition (1)
+        set(column_header).issubset(set(ascii_letters).union(set(digits)).union(set(['_']))) and # Condition (2)
+        not column_header.isdigit() # Condition (3)
+    )
+
+
+def get_invalid_headers(df: pd.DataFrame):
+    """
+    Given a dataframe, returns a list of all the invalid headers this list has. 
+    """
+    return [
+        header for header in df.columns.tolist()
+        if not is_valid_header(header)
+    ]
+
+
+def dfs_to_json(dfs):
+    return json.dumps([df_to_json_dumpsable(df) for df in dfs])
+
+
+def df_to_json_dumpsable(df):
+    """
+    Returns a dataframe represented in a way that can be turned into a 
+    JSON object with json.dumps
+    """
+    json_obj = json.loads(df.to_json(orient="split"))
+    # Then, we go through and find all the null values (which are infinities),
+    # and set them to undefined.
+    for d in json_obj['data']:
+        for idx, e in enumerate(d):
+            if e is None:
+                d[idx] = 'undefined'
+
+    return json_obj
+
+
+def get_column_filter_type(series):
+    """
+    Given a series, take's the series dtype and catergorizes the correct
+    column_filter_type so that the filter modal can display only
+    valid filtering options
+    
+    TODO: unify this with the other Mito types!
+    """
+    #TODO: if a column has strings and int, it will get classified as a string, we should have more nuance
+    if series.dtype == np.float64 or series.dtype == np.int64 or series.dtype == np.datetime64:
+        return 'number'
+    if series.dtype == np.object:
+        return 'string'
+    
+    # TODO: what do we do if it's none? for now just return string, for no reason.
+    return 'string'
+    
