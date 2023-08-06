728121c7ab712969cad86bdd87a1f812a019e7bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:41 +0000

    Add package sqldiff/0.0.2/sqldiff-0.0.2.tar.gz

---
+++ packages/sqldiff/sqldiff-0.0.2.tar.gz/sqldiff-0.0.2/sqldiff/__init__.py
@@ -0,0 +1,7 @@
+from sqldiff.meta.column_description import ColumnDescription
+from sqldiff.meta.postgres_meta import get_meta
+from sqldiff.meta.teradata_meta import get_meta
+
+from sqldiff.comp.compare import compare, ColumnsComparisonResult, KeyColumnComparisonResult
+
+from sqldiff.relation.relation_manager import get_relations
