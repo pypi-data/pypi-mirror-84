7a400b5e5c27d69cf85db95d55c8e2a134dcb37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:51 +0000

    Add package racetime-bot/1.8.0/racetime_bot-1.8.0-py3-none-any.whl

---
+++ packages/racetime-bot/racetime_bot-1.8.0-py3-none-any.whl/racetime_bot/bot.py
@@ -0,0 +1,254 @@
+import asyncio
+import json
+from functools import partial
+
+import aiohttp
+import requests
+import websockets
+
+from .handler import RaceHandler
+
+
+class Bot:
+    """
+    The racetime.gg bot class.
+
+    This bot uses event loops to connect to multiple race rooms at once. Each
+    room is assigned a handler object, which you can determine with the
+    `get_handler_class`/`get_handler_kwargs` methods.
+
+    When implementing your own bot, you will need to specify what handler you
+    wish to use, as the default `RaceHandler` will not actually do anything
+    other than connect to the room and log the messages.
+    """
+    racetime_host = 'racetime.gg'
+    racetime_secure = True
+    scan_races_every = 30
+    reauthorize_every = 36000
+
+    continue_on = [
+        # Exception types that will not cause the bot to shut down.
+        websockets.ConnectionClosed,
+        websockets.ConnectionClosedOK,
+        websockets.ConnectionClosedError,
+    ]
+
+    def __init__(self, category_slug, client_id, client_secret, logger,
+                 ssl_context=None):
+        """
+        Bot constructor.
+        """
+        self.logger = logger
+        self.category_slug = category_slug
+        self.ssl_context = ssl_context
+
+        self.loop = asyncio.get_event_loop()
+        self.last_scan = None
+        self.handlers = {}
+        self.races = {}
+        self.state = {}
+
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.access_token, self.reauthorize_every = self.authorize()
+
+    def get_handler_class(self):
+        """
+        Returns the handler class for races. Each race the bot finds will have
+        its own handler object of this class.
+        """
+        return RaceHandler
+
+    def get_handler_kwargs(self, ws_conn, state):
+        """
+        Returns a dict of keyword arguments to be passed into the handler class
+        when instantiated.
+
+        Override this method if you need to pass additional kwargs to your race
+        handler.
+        """
+        return {
+            'conn': ws_conn,
+            'logger': self.logger,
+            'state': state,
+            'command_prefix': '!',
+        }
+
+    def should_handle(self, race_data):
+        """
+        Determine if a race we've found should be handled by this bot or not.
+
+        Returns True if the race should have a handler created for it, False
+        otherwise.
+        """
+        status = race_data.get('status', {}).get('value')
+        return status not in self.get_handler_class().stop_at
+
+    def authorize(self):
+        """
+        Get an OAuth2 token from the authentication server.
+        """
+        resp = requests.post(self.http_uri('/o/token'), {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'grant_type': 'client_credentials',
+        })
+        resp.raise_for_status()
+        data = json.loads(resp.content)
+        if not data.get('access_token'):
+            raise Exception('Unable to retrieve access token.')
+        return data.get('access_token'), data.get('expires_in', 36000)
+
+    def create_handler(self, race_data):
+        """
+        Create a new WebSocket connection and set up a handler object to manage
+        it.
+        """
+        connect_kwargs = {
+            'extra_headers': {
+                'Authorization': 'Bearer ' + self.access_token,
+            },
+        }
+        if self.ssl_context is not None and self.racetime_secure:
+            connect_kwargs['ssl'] = self.ssl_context
+        ws_conn = websockets.connect(
+            self.ws_uri(race_data.get('websocket_bot_url')),
+            **connect_kwargs,
+        )
+
+        race_name = race_data.get('name')
+        if race_name not in self.state:
+            self.state[race_name] = {}
+
+        cls = self.get_handler_class()
+        kwargs = self.get_handler_kwargs(ws_conn, self.state[race_name])
+
+        handler = cls(**kwargs)
+        handler.data = race_data
+
+        self.logger.info(
+            'Created handler for %(race)s'
+            % {'race': race_data.get('name')}
+        )
+
+        return handler
+
+    async def reauthorize(self):
+        """
+        Reauthorize with the token endpoint, to generate a new access token
+        before the current one expires.
+
+        This method runs in a constant loop, creating a new access token when
+        needed.
+        """
+        while True:
+            # Divide the reauthorization interval by 2 to avoid token expiration
+            delay = self.reauthorize_every / 2
+            await asyncio.sleep(delay)
+            self.logger.info('Get new access token')
+            self.access_token, self.reauthorize_every = self.authorize()
+
+    async def refresh_races(self):
+        """
+        Retrieve current race information from the category detail API
+        endpoint, retrieving the current race list. Creates a handler and task
+        for any race that should be handled but currently isn't.
+
+        This method runs in a constant loop, checking for new races every few
+        seconds.
+        """
+        def done(task_name, *args):
+            del self.handlers[task_name]
+
+        while True:
+            self.logger.info('Refresh races')
+            try:
+                async with aiohttp.request(
+                    method='get',
+                    url=self.http_uri(f'/{self.category_slug}/data'),
+                    raise_for_status=True,
+                ) as resp:
+                    data = json.loads(await resp.read())
+            except Exception:
+                self.logger.error('Fatal error when attempting to retrieve race data.', exc_info=True)
+                await asyncio.sleep(self.scan_races_every)
+                continue
+            self.races = {}
+            for race in data.get('current_races', []):
+                self.races[race.get('name')] = race
+
+            for name, summary_data in self.races.items():
+                if name not in self.handlers:
+                    try:
+                        async with aiohttp.request(
+                            method='get',
+                            url=self.http_uri(summary_data.get('data_url')),
+                            raise_for_status=True,
+                        ) as resp:
+                            race_data = json.loads(await resp.read())
+                    except Exception:
+                        self.logger.error('Fatal error when attempting to retrieve summary data.', exc_info=True)
+                        await asyncio.sleep(self.scan_races_every)
+                        continue
+                    if self.should_handle(race_data):
+                        handler = self.create_handler(race_data)
+                        self.handlers[name] = self.loop.create_task(handler.handle())
+                        self.handlers[name].add_done_callback(partial(done, name))
+                    else:
+                        if name in self.state:
+                            del self.state[name]
+                        self.logger.info(
+                            'Ignoring %(race)s by configuration.'
+                            % {'race': race_data.get('name')}
+                        )
+
+            await asyncio.sleep(self.scan_races_every)
+
+    def handle_exception(self, loop, context):
+        """
+        Handle exceptions that occur during the event loop.
+        """
+        self.logger.error(context)
+        exception = context.get('exception')
+        if exception:
+            self.logger.exception(context)
+
+        if not exception or exception.__class__ not in self.continue_on:
+            loop.stop()
+
+    def run(self):
+        """
+        Run the bot. Creates an event loop then iterates over it forever.
+        """
+        self.loop.create_task(self.reauthorize())
+        self.loop.create_task(self.refresh_races())
+        self.loop.set_exception_handler(self.handle_exception)
+        self.loop.run_forever()
+
+    def http_uri(self, url):
+        """
+        Generate a HTTP/HTTPS URI from the given URL path fragment.
+        """
+        return self.uri(
+            proto='https' if self.racetime_secure else 'http',
+            url=url,
+        )
+
+    def ws_uri(self, url):
+        """
+        Generate a WS/WSS URI from the given URL path fragment.
+        """
+        return self.uri(
+            proto='wss' if self.racetime_secure else 'ws',
+            url=url,
+        )
+
+    def uri(self, proto, url):
+        """
+        Generate a URI from the given protocol and URL path fragment.
+        """
+        return '%(proto)s://%(host)s%(url)s' % {
+            'proto': proto,
+            'host': self.racetime_host,
+            'url': url,
+        }
---
+++ packages/racetime-bot/racetime_bot-1.8.0-py3-none-any.whl/racetime_bot/handler.py
@@ -0,0 +1,342 @@
+import json
+import uuid
+
+
+class RaceHandler:
+    """
+    Standard race handler.
+
+    You should use this class as a basis for creating your own handler that
+    can consume incoming messages, react to race data changes, and send stuff
+    back to the race room.
+    """
+    # This is used by `should_stop` to determine when the handler should quit.
+    stop_at = ['cancelled', 'finished']
+
+    def __init__(self, logger, conn, state, command_prefix='!'):
+        """
+        Base handler constructor.
+
+        Sets up the following attributes:
+        * conn - WebSocket connection, used internally.
+        * data - Race data dict, as retrieved from race detail API endpoint.
+        * logger - The logger instance bot was instantiated with.
+        * state - A dict of stateful data for this race
+        * ws - The open WebSocket, used internally.
+
+        About data vs state - data is the race information retrieved from the
+        server and can be read by your handler, but should not be written to.
+        The state on the other hand can be used by your handler to preserve
+        information about the race. It is preserved even if the handler is
+        recreated (e.g. due to disconnect). Use it for any information you
+        want.
+        """
+        self.conn = conn
+        self.data = {}
+        self.logger = logger
+        self.state = state
+        self.command_prefix = command_prefix
+        self.ws = None
+
+    def should_stop(self):
+        """
+        Determine if the handler should be terminated. This is checked after
+        every receieved message.
+
+        By default, checks if the race state matches one of the values in
+        `stop_at`.
+        """
+        return self.data.get('status', {}).get('value') in self.stop_at
+
+    async def begin(self):
+        """
+        Bot actions to perform when first connecting to a race room.
+
+        Override this method to add an intro for when your bot first appears.
+        """
+        pass
+
+    async def consume(self, data):
+        """
+        Standard message consumer. This is called for every message we receive
+        from the site.
+
+        This implementation will attempt to find an appropriate method to call
+        to handle the incoming data, based on its type. For example if we have
+        a "race.data" type message, it will call `self.race_data(data)`.
+        """
+        msg_type = data.get('type')
+
+        self.logger.info('[%(race)s] Received %(msg_type)s' % {
+            'race': self.data.get('name'),
+            'msg_type': msg_type,
+        })
+
+        method = msg_type.replace('.', '_')
+        if msg_type and hasattr(self, method):
+            await getattr(self, method)(data)
+        else:
+            self.logger.info(f'No handler for {msg_type}, ignoring.')
+
+    async def end(self):
+        """
+        Bot actions to perform just before disconnecting from a race room.
+
+        Override this method to add an outro for when your bot leaves.
+        """
+        pass
+
+    async def error(self, data):
+        """
+        Consume an incoming "error" type message.
+
+        By default, just raises the message as an exception.
+        """
+        raise Exception(data.get('errors'))
+
+    async def chat_message(self, data):
+        """
+        Consume an incoming "chat.message" type message.
+
+        This method assumes a standard bot operation. It checks the first word
+        in the message, and if it looks like an exclaimation command like
+        "!seed", then it will call the relevant method, i.e.
+        `self.ex_seed(args, message)` (where `args` is the remainder of the
+        message split up by words, and message is the original message blob).
+        """
+        message = data.get('message', {})
+
+        if message.get('is_bot') or message.get('is_system'):
+            self.logger.info('Ignoring bot/system message.')
+            return
+
+        words = message.get('message', '').lower().split(' ')
+        if words and words[0].startswith(self.command_prefix.lower()):
+            method = 'ex_' + words[0][len(self.command_prefix):]
+            args = words[1:]
+            if hasattr(self, method):
+                self.logger.info('[%(race)s] Calling handler for %(word)s' % {
+                    'race': self.data.get('name'),
+                    'word': words[0],
+                })
+                try:
+                    await getattr(self, method)(args, message)
+                except Exception as e:
+                    self.logger.error('Command raised exception.', exc_info=True)
+
+
+    async def race_data(self, data):
+        """
+        Consume an incoming "race.data" message.
+
+        By default just updates the `data` attribute on the object. If you
+        want to react to race changes, you can override this method to add
+        further functionality.
+        """
+        self.data = data.get('race')
+
+    async def send_message(self, message):
+        """
+        Send a chat message to the race room.
+
+        `message` should be the message string you want to send.
+        """
+        await self.ws.send(json.dumps({
+            'action': 'message',
+            'data': {
+                'message': message,
+                'guid': str(uuid.uuid4()),
+            }
+        }))
+        self.logger.info('[%(race)s] Message: "%(message)s"' % {
+            'race': self.data.get('name'),
+            'message': message,
+        })
+
+    async def set_raceinfo(self, info, overwrite=False, prefix=True):
+        """
+        Set the `info` field on the race room's data.
+
+        `info` should be the information you wish to set. By default, this
+        method will prefix your information with the existing info, if needed.
+        You can change this to suffix with `prefix=False`, or disable this
+        behaviour entirely with `overwrite=True`.
+        """
+        if self.data.get('info') and not overwrite:
+            if prefix:
+                info = info + ' | ' + self.data.get('info')
+            else:
+                info = self.data.get('info') + ' | ' + info
+
+        await self.ws.send(json.dumps({
+            'action': 'setinfo',
+            'data': {'info': info}
+        }))
+        self.logger.info('[%(race)s] Set info: "%(info)s"' % {
+            'race': self.data.get('name'),
+            'info': info,
+        })
+
+    async def set_open(self):
+        """
+        Set the room in an open state.
+        """
+        await self.ws.send(json.dumps({
+            'action': 'make_open'
+        }))
+        self.logger.info('[%(race)s] Make open' % {
+            'race': self.data.get('name')
+        })
+
+    async def set_invitational(self):
+        """
+        Set the room in an invite-only state.
+        """
+        await self.ws.send(json.dumps({
+            'action': 'make_invitational'
+        }))
+        self.logger.info('[%(race)s] Make invitational' % {
+            'race': self.data.get('name')
+        })
+
+    async def force_start(self):
+        """
+        Forces a start of the race.
+        """
+        await self.ws.send(json.dumps({
+            'action': 'begin'
+        }))
+        self.logger.info('[%(race)s] Forced start' % {
+            'race': self.data.get('name')
+        })
+
+    async def cancel_race(self):
+        """
+        Forcibly cancels a race.
+        """
+        await self.ws.send(json.dumps({
+            'action': 'cancel'
+        }))
+        self.logger.info('[%(race)s] cancelled' % {
+            'race': self.data.get('name')
+        })
+
+    async def invite_user(self, user):
+        """
+        Invites a user to the race.
+
+        `user` should be the hashid of the user.
+        """
+        await self.ws.send(json.dumps({
+            'action': 'invite',
+            'data': {
+                'user': user
+            }
+        }))
+        self.logger.info('[%(race)s] invited %(user)s' % {
+            'race': self.data.get('name'),
+            'user': user
+        })
+
+    async def accept_request(self, user):
+        """
+        Accepts a request to join the race room.
+
+        `user` should be the hashid of the user.
+        """
+        await self.ws.send(json.dumps({
+            'action': 'accept_request',
+            'data': {
+                'user': user
+            }
+        }))
+        self.logger.info('[%(race)s] accept join request %(user)s' % {
+            'race': self.data.get('name'),
+            'user': user
+        })
+
+    async def force_unready(self, user):
+        """
+        Forcibly unreadies an entrant.
+
+        `user` should be the hashid of the user.
+        """
+        await self.ws.send(json.dumps({
+            'action': 'force_unready',
+            'data': {
+                'user': user
+            }
+        }))
+        self.logger.info('[%(race)s] force unready %(user)s' % {
+            'race': self.data.get('name'),
+            'user': user
+        })
+
+    async def remove_entrant(self, user):
+        """
+        Forcibly removes an entrant from the race.
+
+        `user` should be the hashid of the user.
+        """
+        await self.ws.send(json.dumps({
+            'action': 'remove_entrant',
+            'data': {
+                'user': user
+            }
+        }))
+        self.logger.info('[%(race)s] removed entrant %(user)s' % {
+            'race': self.data.get('name'),
+            'user': user
+        })
+
+    async def add_monitor(self, user):
+        """
+        Adds a user as a race monitor.
+
+        `user` should be the hashid of the user.
+        """
+        await self.ws.send(json.dumps({
+            'action': 'add_monitor',
+            'data': {
+                'user': user
+            }
+        }))
+        self.logger.info('[%(race)s] added race monitor %(user)s' % {
+            'race': self.data.get('name'),
+            'user': user
+        })
+
+    async def remove_monitor(self, user):
+        """
+        Removes a user as a race monitor.
+
+        `user` should be the hashid of the user.
+        """
+        await self.ws.send(json.dumps({
+            'action': 'remove_monitor',
+            'data': {
+                'user': user
+            }
+        }))
+        self.logger.info('[%(race)s] added race monitor %(user)s' % {
+            'race': self.data.get('name'),
+            'user': user
+        })
+
+    async def handle(self):
+        """
+        Low-level handler for the race room. This will loop over the websocket,
+        processing any messages that come in.
+        """
+        self.logger.info('[%(race)s] Handler started' % {
+            'race': self.data.get('name'),
+        })
+        async with self.conn as ws:
+            self.ws = ws
+            await self.begin()
+            async for message in self.ws:
+                data = json.loads(message)
+                await self.consume(data)
+                if self.should_stop():
+                    await self.end()
+                    break
