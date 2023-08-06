9cb555a30d4bc961129e466fe63c36ace7d3720f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:48 +0000

    Add package pagerank-basic/0.2/pagerank_basic-0.2.tar.gz

---
+++ packages/pagerank-basic/pagerank_basic-0.2.tar.gz/pagerank_basic-0.2/pagerank_basic/graph.py
@@ -0,0 +1,95 @@
+import random
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class Graph:
+	
+	def __init__(self, num_of_nodes = 0):
+		"""
+		Intitailze the no. of nodes in web link, 
+					out_degree, adjacency list of each node,
+		"""
+		self.num_of_nodes = num_of_nodes
+		self.num_of_nodes = 0
+		self.adj_list = []
+		self._out_degree = []
+		self.in_link_nodes = []
+		self.edges = []
+
+		for i in range(0, num_of_nodes, 1):
+			self.adj_list.append([])
+			self._out_degree.append(0)
+			self.in_link_nodes.append(set())
+
+	
+	def add_node(self):
+		"""
+		Increase no. of nodes in web link by one
+		"""
+		self.num_of_nodes += 1
+		self.adj_list.append([])
+		self.in_link_nodes.append([])
+
+	def add_edge(self, u, v):
+		"""
+		add_edge(u, v) adds a directed edge from node u to node v
+		"""
+		self.adj_list[u].append(v)
+		self._out_degree[u] += 1
+		self.in_link_nodes[v].add(u)
+		self.edges.append([u, v])
+
+	def visualize(self):
+		"""
+		Produces a visualization of graph
+		"""
+		G = nx.DiGraph(directed=True)
+		G.add_edges_from(self.edges)
+
+		options = {
+		    'node_color': 'blue',
+		    'node_size': 500,
+		    'width': 2,
+		    'arrowstyle': '-|>',
+		    'arrowsize': 12,
+		}
+
+		nx.draw_networkx(G, **options)
+
+	@property
+	def out_degree(self):
+		"""
+		Getter for _out_degree[]
+		"""
+		return self._out_degree
+
+	@out_degree.setter
+	def out_degree(self, x):
+		raise ("can't set attribute")
+
+
+def generate_graph(nodes, edges):
+
+	"""
+	Returns an unweighted, directed graph with no self-loops or multiple edges
+	"""
+	g = Graph(nodes)
+	lt = min(edges, nodes * (nodes - 1))
+	cnt = 0
+	edge_set = set()
+
+	while(cnt != lt):
+		u = random.randint(0, nodes - 1)
+		v = random.randint(0, nodes - 1)
+
+		if(u != v and (not (u, v) in edge_set)):
+			cnt += 1
+			edge_set.add((u, v))
+			g.add_edge(u, v)
+
+	g.visualize()
+	plt.show()
+
+	return g
+
+	
---
+++ packages/pagerank-basic/pagerank_basic-0.2.tar.gz/pagerank_basic-0.2/pagerank_basic/pagerank.py
@@ -0,0 +1,55 @@
+def calc_page_rank(prev_rank, in_link_nodes, out_degree, d=0.85, total_nodes=10, total_edges=30):
+	new_rank = [(1.0 - d) / total_nodes] * total_nodes
+	rank_sum = 0.0
+
+	# Calculate new rank for each node
+	for node in range(0, total_nodes, 1):
+
+		# iterate all incoming_node such that there's an edge incoming_node -> node
+		for incoming_node in in_link_nodes[node]:
+			# PR(x) = (1 - d)/N + d * (for each edge y -> x, PR(y)/out(y))
+			new_rank[node] += d * (prev_rank[incoming_node] / out_degree[incoming_node])
+
+		rank_sum += new_rank[node]
+
+
+	# print("rank_sum = ",rank_sum)
+	return new_rank, rank_sum
+
+
+
+def get_all_ranks(init_rank, in_link_nodes, out_degree, d=0.85, total_nodes=10, total_edges=30, iterations=3):
+	"""
+	Args:
+	init_rank -> array of initial ranks
+	in_link_nodes -> list of no. of incoming links for all nodes
+	out_degree -> list of no. of out degree for all nodes
+	total_nodes -> total no. of nodes
+	total_edges -> total no. of edges
+	d -> damping factor
+	total_iterations
+
+	Returns:
+	list of ranks(size: iterations+1)
+	list of rank_sums(size: iterations+1)
+	"""
+	ranks = [0]*(iterations + 1)
+	rank_sums = [0]*(iterations + 1)
+	ranks[0] = init_rank
+	for i in range(1, iterations + 1, 1):
+		ranks[i], rank_sums[i] = calc_page_rank(ranks[i - 1], in_link_nodes, out_degree, d, total_nodes, total_edges)
+	
+	return ranks, rank_sums
+
+
+def get_whole_ranks(ranks):
+	"""
+	Args: array of ranks from a single iteration
+	Returns: array of whole no. ranks
+	"""
+	whole_ranks = {}
+	sorted_ranks = sorted(ranks)
+	for i in range(len(ranks)):
+		whole_ranks[i+1] = sorted_ranks.index(ranks[i])+1
+
+	return whole_ranks
