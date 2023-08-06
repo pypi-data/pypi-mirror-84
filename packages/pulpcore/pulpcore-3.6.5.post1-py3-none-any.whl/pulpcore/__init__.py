d3e7d1c8dd7ae5fc1e46bb0355a82b2949891caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:08 +0000

    Add package pulpcore/3.6.5.post1/pulpcore-3.6.5.post1-py3-none-any.whl

---
+++ packages/pulpcore/pulpcore-3.6.5.post1-py3-none-any.whl/pulpcore/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "3.6.5.post1"
+
+
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
---
+++ packages/pulpcore/pulpcore-3.6.5.post1-py3-none-any.whl/pulpcore/plugin/serializers/__init__.py
@@ -0,0 +1,35 @@
+# Import Serializers in platform that are potentially useful to plugin writers
+from pulpcore.app.serializers import (  # noqa
+    ArtifactSerializer,
+    AsyncOperationResponseSerializer,
+    BaseDistributionSerializer,
+    ContentChecksumSerializer,
+    ContentGuardSerializer,
+    DetailRelatedField,
+    ExporterSerializer,
+    ExportSerializer,
+    FilesystemExporterSerializer,
+    IdentityField,
+    ImporterSerializer,
+    ImportSerializer,
+    ModelSerializer,
+    MultipleArtifactContentSerializer,
+    NestedRelatedField,
+    NoArtifactContentSerializer,
+    PublicationDistributionSerializer,
+    PublicationExportSerializer,
+    PublicationSerializer,
+    RelatedField,
+    RemoteSerializer,
+    RepositorySerializer,
+    RepositorySyncURLSerializer,
+    RepositoryVersionDistributionSerializer,
+    SingleArtifactContentSerializer,
+    SingleContentArtifactField,
+    validate_unknown_fields,
+)
+
+from .content import (  # noqa
+    NoArtifactContentUploadSerializer,
+    SingleArtifactContentUploadSerializer,
+)
