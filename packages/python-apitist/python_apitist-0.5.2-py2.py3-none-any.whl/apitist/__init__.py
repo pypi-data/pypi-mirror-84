68fcc94df639f32fa05a988b2fe6ee67c9ee17f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:47 +0000

    Add package python-apitist/0.5.2/python_apitist-0.5.2-py2.py3-none-any.whl

---
+++ packages/python-apitist/python_apitist-0.5.2-py2.py3-none-any.whl/apitist/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = "python-apitist"
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound as e:
+    __version__ = "unknown"
+    print(e)
+finally:
+    del get_distribution, DistributionNotFound
