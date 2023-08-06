2629a9b039d5278ba9f453ba38814d8459e71358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:02 +0000

    Add package distributed-worker/1.3.2/distributed-worker-1.3.2.tar.gz

---
+++ packages/distributed-worker/distributed-worker-1.3.2.tar.gz/distributed-worker-1.3.2/setup.py
@@ -0,0 +1,27 @@
+import io
+import setuptools
+
+
+setuptools.setup(
+    name='distributed-worker',
+    version='1.3.2',
+    description=(
+        'A Python wrapper around multiprocessing for easy cross-machine computation '
+    ),
+    author='Casper',
+    author_email='casper@devdroplets.com',
+    url='https://github.com/i404788/distributed-worker',
+    license='BSD 2-Clause "Simplified" License',
+    long_description=io.open('README.rst', encoding='utf-8').read(),
+    packages=['distributed_worker'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
