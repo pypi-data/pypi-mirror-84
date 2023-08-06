ec5dcc2d982956bd8a6f19a8c664342ab7f0e018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:39 +0000

    Add package tamcolors/1.2.0/tamcolors-1.2.0-cp38-cp38-win_amd64.whl

---
+++ packages/tamcolors/tamcolors-1.2.0-cp38-cp38-win_amd64.whl/tamcolors/tam/__init__.py
@@ -0,0 +1,14 @@
+from . import tam_loop
+
+
+"""
+tam draws text with color to the terminal
+tam can get key input from the terminal
+tam supports
+* Darwin
+* Linux
+* Windows
+"""
+
+__all__ = ("tam_loop",
+           )
