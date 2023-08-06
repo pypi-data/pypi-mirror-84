f7af37d7f47af682533a1a8be1d9a20b6c8d7b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:53 +0000

    Add package pi-display-webthing/0.0.14/pi_display_webthing-0.0.14-py3-none-any.whl

---
+++ packages/pi-display-webthing/pi_display_webthing-0.0.14-py3-none-any.whl/pi_display_webthing/display_webthing.py
@@ -0,0 +1,158 @@
+from webthing import (SingleThing, Property, Thing, Value, WebThingServer)
+from pi_display_webthing.display import Display
+from pi_display_webthing.lcd import Lcd
+import tornado.ioloop
+import logging
+
+
+class DisplayWebThing(Thing):
+    # regarding capabilities refer https://iot.mozilla.org/schemas
+    # there is also another schema registry http://iotschema.org/docs/full.html not used by webthing
+
+    def __init__(self, name: str, description: str, lcd: Lcd):
+        Thing.__init__(
+            self,
+            'urn:dev:ops:lcddisplay-1',
+            (name + ' Display Controller').strip(),
+            ['Display'],
+            description
+        )
+
+        self.display = Display(lcd, self.__update_text)
+
+        self.display_text = Value("")
+        self.add_property(
+            Property(self,
+                     'displayed_text',
+                     self.display_text,
+                     metadata={
+                         'title': 'Displayed text',
+                         'type': 'string',
+                         'description': 'Displayed text',
+                         'readOnly': True,
+                     }))
+
+        self.num_rows = Value(lcd.num_lines)
+        self.add_property(
+            Property(self,
+                     'num_rows',
+                     self.num_rows,
+                     metadata={
+                         'title': 'Number of rows',
+                         'type': 'Integer',
+                         'description': 'Number of rows',
+                         'readOnly': True,
+                     }))
+
+        self.num_chars = Value(lcd.num_chars_per_line)
+        self.add_property(
+            Property(self,
+                     'num_chars',
+                     self.num_chars,
+                     metadata={
+                         'title': 'Number characters per row',
+                         'type': 'Integer',
+                         'description': 'Number characters per row',
+                         'readOnly': True,
+                     }))
+
+        self.upper_layer_text = Value("", self.display.panel(Display.LAYER_UPPER).update_text)
+        self.add_property(
+            Property(self,
+                     'upper_layer_text',
+                     self.upper_layer_text,
+                     metadata={
+                         'title': 'upper layer text',
+                         'type': 'string',
+                         'description': 'The text of the upper layer',
+                         'readOnly': False,
+                     }))
+
+        self.upper_layer_text_ttl = Value(-1, self.display.panel(Display.LAYER_UPPER).update_ttl)
+        self.add_property(
+            Property(self,
+                     'upper_layer_text_ttl',
+                     self.upper_layer_text_ttl,
+                     metadata={
+                         'title': 'upper layer text (time-to-live)',
+                         'type': 'integer',
+                         'description': 'The time-to-live of the upper layer. Value -1 deactivates ttl',
+                         'readOnly': False,
+                     }))
+
+        self.middle_layer_text = Value("", self.display.panel(Display.LAYER_MIDDLE).update_text)
+        self.add_property(
+            Property(self,
+                     'middle_layer_text',
+                     self.middle_layer_text,
+                     metadata={
+                         'title': 'middle layer text',
+                         'type': 'string',
+                         'description': 'The text of the middle layer',
+                         'readOnly': False,
+                     }))
+
+        self.middle_layer_text_ttl = Value(-1, self.display.panel(Display.LAYER_MIDDLE).update_ttl)
+        self.add_property(
+            Property(self,
+                     'middle_layer_text_ttl',
+                     self.middle_layer_text_ttl,
+                     metadata={
+                         'title': 'middle layer text (time-to-live)',
+                         'type': 'integer',
+                         'description': 'The time-to-live of the middle layer. Value -1 deactivates ttl',
+                         'readOnly': False,
+                     }))
+
+        self.lower_layer_text = Value("", self.display.panel(Display.LAYER_LOWER).update_text)
+        self.add_property(
+            Property(self,
+                     'lower_layer_text',
+                     self.lower_layer_text,
+                     metadata={
+                         'title': 'lower layer text',
+                         'type': 'string',
+                         'description': 'The text of the lower layer',
+                         'readOnly': False,
+                     }))
+
+        self.lower_layer_text_ttl = Value(-1, self.display.panel(Display.LAYER_LOWER).update_ttl)
+        self.add_property(
+            Property(self,
+                     'lower_layer_text_ttl',
+                     self.lower_layer_text_ttl,
+                     metadata={
+                         'title': 'lower layer text (time-to-live)',
+                         'type': 'integer',
+                         'description': 'The time-to-live of the lower layer. Value -1 deactivates ttl',
+                         'readOnly': False,
+                     }))
+
+        self.ioloop = tornado.ioloop.IOLoop.current()
+
+
+    def __update_text(self):
+        self.ioloop.add_callback(self.__update_text_props)
+
+    def __update_text_props(self):
+        self.display_text.notify_of_external_update(self.display.text)
+        self.upper_layer_text.notify_of_external_update(self.display.panel(Display.LAYER_UPPER).display_text)
+        self.upper_layer_text_ttl.notify_of_external_update(self.display.panel(Display.LAYER_UPPER).ttl)
+        self.middle_layer_text.notify_of_external_update(self.display.panel(Display.LAYER_MIDDLE).display_text)
+        self.middle_layer_text_ttl.notify_of_external_update(self.display.panel(Display.LAYER_MIDDLE).ttl)
+        self.lower_layer_text.notify_of_external_update(self.display.panel(Display.LAYER_LOWER).display_text)
+        self.lower_layer_text_ttl.notify_of_external_update(self.display.panel(Display.LAYER_LOWER).ttl)
+
+
+def run_server(hostname: str, port: int, name:str, i2c_expander: str, i2c_address: int, num_lines: int, num_chars_per_line: int, description: str):
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+    lcd = Lcd.createI2C(i2c_expander, i2c_address)
+    display_webthing = DisplayWebThing(name, description, lcd)
+    server = WebThingServer(SingleThing(display_webthing), hostname=hostname, port=port)
+    try:
+        logging.info('starting the server')
+        server.start()
+    except KeyboardInterrupt:
+        logging.info('stopping the server')
+        server.stop()
+        logging.info('done')
