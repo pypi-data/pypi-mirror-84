30dec4aafa54bde578271c964397985675318083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:47 +0000

    Add package amulet-nbt/1.0.0b1/amulet_nbt-1.0.0b1.tar.gz

---
+++ packages/amulet-nbt/amulet_nbt-1.0.0b1.tar.gz/amulet_nbt-1.0.0b1/setup_nbt.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='amulet_nbt',
+    version='1.0.0b1',
+    description='',
+    long_description='',
+    author='James Clare',
+    author_email='amuleteditor@gmail.com',
+    packages=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
