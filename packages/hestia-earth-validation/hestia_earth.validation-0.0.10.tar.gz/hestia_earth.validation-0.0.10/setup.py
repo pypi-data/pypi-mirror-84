b9b45cee17c1f4cbdf09b5ecdabca07cf37d16f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:46 +0000

    Add package hestia-earth-validation/0.0.10/hestia_earth.validation-0.0.10.tar.gz

---
+++ packages/hestia-earth-validation/hestia_earth.validation-0.0.10.tar.gz/hestia_earth.validation-0.0.10/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='hestia_earth.validation',
+    version='0.0.10',
+    description='Hestia Data Validation library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Guillaume Royer',
+    author_email='guillaumeroyer.mail@gmail.com',
+    license='GPL-3.0-or-later',
+    url='https://gitlab.com/hestia-earth/hestia-data-validation',
+    keywords=['hestia', 'data', 'validation'],
+    packages=find_packages(exclude=("tests", "scripts")),
+    python_requires='>=3',
+    classifiers=[],
+    install_requires=[
+        'hestia_earth.schema>=0.1.8',
+        'requests',
+        'python-dateutil',
+        'area',
+    ]
+)
