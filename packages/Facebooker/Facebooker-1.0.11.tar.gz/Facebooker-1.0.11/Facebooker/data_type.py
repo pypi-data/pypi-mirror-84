e11750b5401d059394047b158b80c18f240d49c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:40 +0000

    Add package Facebooker/1.0.11/Facebooker-1.0.11.tar.gz

---
+++ packages/Facebooker/Facebooker-1.0.11.tar.gz/Facebooker-1.0.11/Facebooker/data_type.py
@@ -0,0 +1,37 @@
+class PostInfo:
+    def __init__(self,
+                 post_id:str,
+                 author:str,
+                 content:str,
+                 time:str
+                 ):
+        self.id = post_id
+        self.author = author
+        self.content = content
+        self.time = time
+
+class CommentInfo:
+    def __init__(self,
+                 comment_id:str,
+                 author:str,
+                 content:str,
+                 time:str,
+                 url:str
+                 ):
+        self.id = comment_id
+        self.author = author
+        self.content = content
+        self.time = time
+        self.url = url
+
+class ReplyInfo:
+    def __init__(self,
+                 reply_id:str,
+                 author:str,
+                 content:str,
+                 time:str,
+                 ):
+        self.id = reply_id
+        self.author = author
+        self.content = content
+        self.time = time
