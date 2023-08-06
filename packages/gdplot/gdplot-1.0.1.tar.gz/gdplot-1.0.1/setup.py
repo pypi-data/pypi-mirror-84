bcdfb7338855f727fd861c8d3eac6051db2e6b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:56 +0000

    Add package gdplot/1.0.1/gdplot-1.0.1.tar.gz

---
+++ packages/gdplot/gdplot-1.0.1.tar.gz/gdplot-1.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="gdplot",
+    version="1.0.1",
+    author="Suman Kumar",
+    author_email="sumankumar0091@gmail.com",
+    description="Plot Chart from your Google Sheets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sumankumar0091/gdplot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords = ['Chart', 'gsheet', 'plot'],
+
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'gsheets'
+    ]
+)
