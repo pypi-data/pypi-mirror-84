c0e2a8a977251566fd81277cb28f3d2608bc4405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:27 +0000

    Add package tommy2-helloworld-library/1.0/tommy2-helloworld-library-1.0.tar.gz

---
+++ packages/tommy2-helloworld-library/tommy2-helloworld-library-1.0.tar.gz/tommy2-helloworld-library-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tommy2-helloworld-library",
+    version='1.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/up1/demo-helloworld-library',
+    author='tommy',
+    author_email='thanita.k96@gmail.com',
+)
