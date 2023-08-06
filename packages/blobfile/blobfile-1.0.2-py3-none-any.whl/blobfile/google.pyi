3b6b6874628a98e400db767021193e49e0714057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:49 +0000

    Add package blobfile/1.0.2/blobfile-1.0.2-py3-none-any.whl

---
+++ packages/blobfile/blobfile-1.0.2-py3-none-any.whl/blobfile/google.pyi
@@ -0,0 +1,8 @@
+# This file was generated automatically by filter-stubs.py
+
+from typing import Any
+from typing import List
+from typing import Mapping
+from typing import Optional
+from typing import Tuple
+from blobfile.common import Request
