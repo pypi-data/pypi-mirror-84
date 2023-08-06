5eb0c5c7dc58361051029bcae3b7567f62ce0364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:14 +0000

    Add package suresh_shopify/1.0.0/suresh_shopify-1.0.0.tar.gz

---
+++ packages/suresh_shopify/suresh_shopify-1.0.0.tar.gz/suresh_shopify-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'suresh_shopify',
+    packages = ['suresh_shopify'],
+    version = '1.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'It is to download data as json file from shopify',
+    author = 'Suresh Arunachalam T',
+    author_email = 'tsuresh97@gmail.com',
+    url = 'https://github.com/tsuresh97/suresh_shopify/',
+    download_url = 'https://github.com/tsuresh97/suresh_shopify/archive/1.0.0.tar.gz',
+    keywords = ['shopify'],
+    classifiers = [],
+)
