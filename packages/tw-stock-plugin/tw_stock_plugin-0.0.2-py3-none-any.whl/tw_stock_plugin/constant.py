eb735b0918c6f70352bf4a49d36193c7dc4dc9b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:39 +0000

    Add package tw-stock-plugin/0.0.2/tw_stock_plugin-0.0.2-py3-none-any.whl

---
+++ packages/tw-stock-plugin/tw_stock_plugin-0.0.2-py3-none-any.whl/tw_stock_plugin/constant.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*
+"""
+      ┏┓       ┏┓
+    ┏━┛┻━━━━━━━┛┻━┓
+    ┃      ☃      ┃
+    ┃  ┳┛     ┗┳  ┃
+    ┃      ┻      ┃
+    ┗━┓         ┏━┛
+      ┗┳        ┗━┓
+       ┃          ┣┓
+       ┃          ┏┛
+       ┗┓┓┏━━━━┳┓┏┛
+        ┃┫┫    ┃┫┫
+        ┗┻┛    ┗┻┛
+    God Bless,Never Bug
+"""
+
+
+class Domain:
+    TAIWAN_STOCK_EXCHANGE_CORPORATION = 'https://www.twse.com.tw'
+    TAIPEI_EXCHANGE = 'https://www.tpex.org.tw'
+    TAIWAN_STOCK_EXCHANGE_CORPORATION_ISIN = 'https://isin.twse.com.tw'
---
+++ packages/tw-stock-plugin/tw_stock_plugin-0.0.2-py3-none-any.whl/tw_stock_plugin/test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*
+"""
+      ┏┓       ┏┓
+    ┏━┛┻━━━━━━━┛┻━┓
+    ┃      ☃      ┃
+    ┃  ┳┛     ┗┳  ┃
+    ┃      ┻      ┃
+    ┗━┓         ┏━┛
+      ┗┳        ┗━┓
+       ┃          ┣┓
+       ┃          ┏┛
+       ┗┓┓┏━━━━┳┓┏┛
+        ┃┫┫    ┃┫┫
+        ┗┻┛    ┗┻┛
+    God Bless,Never Bug
+"""
+from tw_stock_plugin.core.update_stock import UpdateStock
+from tw_stock_plugin.core.stock_info import StockInfo
+
+UpdateStock.main()
+
+stock_info = StockInfo()
+print(stock_info.get('2330').type)
