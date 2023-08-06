1fc539b373c3543e920f393d30a95b17885783d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:51 +0000

    Add package scikit-hep/1.2.1/scikit-hep-1.2.1.tar.gz

---
+++ packages/scikit-hep/scikit-hep-1.2.1.tar.gz/scikit-hep-1.2.1/skhep/__init__.py
@@ -0,0 +1,30 @@
+# Licensed under a 3-clause BSD style license, see LICENSE.
+"""
+A community-driven and oriented Python software project for High Energy Physics.
+"""
+
+from __future__ import absolute_import
+
+from .utils._show_versions import show_versions
+
+
+__all__ = ['banner',
+           'project_url',
+           'project_url_GitHub',
+           'project_url_PyPI',
+           'show_versions'
+           ]
+
+
+# -----------------------------------------------------------------------------
+# Project and package info
+# -----------------------------------------------------------------------------
+__version__ = '1.2.1'
+
+project_url = 'http://scikit-hep.org'
+project_url_GitHub = 'https://github.com/scikit-hep/scikit-hep'
+project_url_PyPI = 'https://pypi.python.org/pypi/scikit-hep'
+
+__doc__ = __doc__.format(__version__)
+
+banner = __doc__
---
+++ packages/scikit-hep/scikit-hep-1.2.1.tar.gz/scikit-hep-1.2.1/skhep/dataset/__init__.py
@@ -0,0 +1,11 @@
+# Licensed under a 3-clause BSD style license, see LICENSE.
+"""
+Subpackage for the description of datasets.
+"""
+
+#-----------------------------------------------------------------------------
+# Import statements
+#-----------------------------------------------------------------------------
+from __future__ import absolute_import
+
+from .numpydataset import NumpyDataset
