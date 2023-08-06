2d7da2dc5943f16fedaa2fcd5309aef157e7ac8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:55 +0000

    Add package qball-lang/0.5.3/qball-lang-0.5.3.tar.gz

---
+++ packages/qball-lang/qball-lang-0.5.3.tar.gz/qball-lang-0.5.3/QBall/token.py
@@ -0,0 +1,38 @@
+from enum import Enum
+
+class TokenTypes(Enum):
+  eof = -1
+  builtin = 0
+  plus = 1
+  minus = 2
+  multiply = 3
+  divide = 4
+  integer = 5
+  floating = 6
+  string = 7
+  underscore = 8
+  dquote = 9
+  squote = 10
+  semi = 11
+  equal = 12
+  lbrack = 13
+  rbrack = 14
+  and_ = 15
+  or_ = 16
+  xor = 17
+  greater = 18
+  less = 19
+  greatere = 20
+  lesse = 21
+  lparen = 22
+  rparen = 23
+  iequal = 24
+  nequal = 25
+
+class Result:
+  def __init__(self, type, value=None):
+    self.type = type
+    self.value = value
+
+  def __repr__(self):
+    return f"{self.type}:{self.value}"
