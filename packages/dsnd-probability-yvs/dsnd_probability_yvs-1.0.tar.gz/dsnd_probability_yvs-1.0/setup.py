bba18869667593b8e2bdc0dceb8044b195cde590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:11 +0000

    Add package dsnd-probability-yvs/1.0/dsnd_probability_yvs-1.0.tar.gz

---
+++ packages/dsnd-probability-yvs/dsnd_probability_yvs-1.0.tar.gz/dsnd_probability_yvs-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_yvs',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_yvs'],
+      author = 'Sandeep Yerra',
+      author_email = 'sandeep.yvs@gmail.com',
+      zip_safe=False)
