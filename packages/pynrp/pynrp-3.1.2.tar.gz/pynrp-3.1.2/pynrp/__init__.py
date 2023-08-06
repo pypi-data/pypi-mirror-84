5716e9457d8a2e2858027301f4a17ecbc9c3bfe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:04 +0000

    Add package pynrp/3.1.2/pynrp-3.1.2.tar.gz

---
+++ packages/pynrp/pynrp-3.1.2.tar.gz/pynrp-3.1.2/pynrp/__init__.py
@@ -0,0 +1,6 @@
+"""
+This package contains the Virtual Coach implementation and interfaces to the Neurorobotics
+Platform backend services.
+"""
+
+from pynrp.version import VERSION as __version__  # pylint: disable=W0611
