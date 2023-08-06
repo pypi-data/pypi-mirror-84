dbfd3b071a53fc6cb3b4f1d60fe8d06633b84ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:52 +0000

    Add package PyChromeDevTools/0.2/PyChromeDevTools-0.2.tar.gz

---
+++ packages/PyChromeDevTools/PyChromeDevTools-0.2.tar.gz/PyChromeDevTools-0.2/setup.py
@@ -0,0 +1,20 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="PyChromeDevTools",
+    description="PyChromeDevTools : Interact with Google Chrome using the Chrome DevTools Protocol.",
+    license="Apache License 2.0",
+    version="0.2",
+    author="Martino Trevisan",
+    author_email="martino.trevisan@polito.it",
+    maintainer="Martino Trevisan",
+    maintainer_email="martino.trevisan@polito.it",
+    url="https://github.com/marty90/PyChromeDevTools",
+    download_url = 'https://github.com/marty90/PyChromeDevTools/tarball/1.0.1',
+    packages=['PyChromeDevTools'],
+    install_requires=['requests', 'websocket-client']
+)
