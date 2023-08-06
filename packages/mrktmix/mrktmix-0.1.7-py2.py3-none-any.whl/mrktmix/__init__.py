a45ddc655e527ecff972d709551af9d771e282af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:30 +0000

    Add package mrktmix/0.1.7/mrktmix-0.1.7-py2.py3-none-any.whl

---
+++ packages/mrktmix/mrktmix-0.1.7-py2.py3-none-any.whl/mrktmix/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.1.7'
+from mrktmix.data_create import create_mdldata
+from mrktmix.data_create import spread_notna
+from mrktmix.optimization import optimize_spend
+from mrktmix.transformation import aggregate_data
+from mrktmix.transformation import apply_apl
+from mrktmix.transformation import apply_coef
+from mrktmix.transformation import assess_error
+from mrktmix.transformation import collapse_date
+from mrktmix.transformation import create_base
+from mrktmix.transformation import segregate_data
