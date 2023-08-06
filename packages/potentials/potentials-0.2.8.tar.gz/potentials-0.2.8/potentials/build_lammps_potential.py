90950a890265a83076ebd6aa88765d850f393f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:44 +0000

    Add package potentials/0.2.8/potentials-0.2.8.tar.gz

---
+++ packages/potentials/potentials-0.2.8.tar.gz/potentials-0.2.8/potentials/build_lammps_potential.py
@@ -0,0 +1,38 @@
+from .build import (EamBuilder, EimBuilder, KimBuilder, LibParamBuilder,
+               PairBuilder, ParamFileBuilder)
+
+def build_lammps_potential(pair_style, **kwargs):
+    """
+    Wrapper function for the PotentialLAMMPSBuilder subclasses.
+    
+    Parameters
+    ----------
+    pair_style : str
+        The LAMMPS pair_style setting associated with the potential.
+    **kwargs : dict
+        Any keyword parameters supported by PotentialLAMMPSBuilder and the
+        subclass that matches the pair_style.
+    """
+    
+    subclasses = [PairBuilder, ParamFileBuilder, LibParamBuilder, 
+                  EamBuilder, EimBuilder, KimBuilder]
+    # Strip acceleration tags
+    gputags = ['gpu', 'intel', 'kk', 'omp', 'opt']
+    terms = pair_style.split('/')
+    if terms[-1] in gputags:
+        base_pair_style = '/'.join(terms[:-1])
+    else:
+        base_pair_style = pair_style
+
+    # Identify builder to use
+    match = False
+    for subclass in subclasses:
+        if base_pair_style in subclass().supported_pair_styles:
+            match = True
+            break
+    
+    if match:
+        return subclass(pair_style=pair_style, **kwargs)
+    else:
+        raise ValueError(f'pair_style {pair_style} not in current list of supported styles')
+
---
+++ packages/potentials/potentials-0.2.8.tar.gz/potentials-0.2.8/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+def getreadme():
+    with open('README.rst') as readme_file:
+        return readme_file.read()
+   
+setup(name = 'potentials',
+      version = '0.2.8',
+      description = 'API database tools for accessing the NIST Interatomic Potentials Repository: explore and download interatomic potentials and computed properties.',
+      long_description = getreadme(),
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Scientific/Engineering :: Physics'
+      ],
+      keywords = [
+        'atom', 
+        'atomic', 
+        'atomistic', 
+        'molecular dynamics', 
+        'interatomic potential',
+        'force field'
+      ], 
+      url = 'https://github.com/usnistgov/potentials',
+      author = 'Lucas Hale',
+      author_email = 'lucas.hale@nist.gov',
+      packages = find_packages(),
+      install_requires = [
+        'xmltodict',
+        'DataModelDict',
+        'unidecode',
+        'numpy', 
+        'matplotlib',
+        'pandas',
+        'requests',
+        'habanero',
+        'bibtexparser',
+        'cdcs==0.1.4',
+        'ipywidgets',
+      ],
+      package_data={'': ['*']},
+      zip_safe = False)
