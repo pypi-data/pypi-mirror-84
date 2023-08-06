fec5814413a1574436ba39b7c3ec00b13e4598a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:44 +0000

    Add package sc3/1.0.0a0/sc3-1.0.0a0-py3.6.egg

---
+++ packages/sc3/sc3-1.0.0a0-py3.6.egg/sc3/synth/all.py
@@ -0,0 +1,11 @@
+
+from . buffer import *
+from . bus import *
+from . env import *
+from . node import *
+from . server import *
+from . synthdef import *
+from . synthdesc import *
+from . spec import *
+# from . ugen import *  # Done by ugens.
+from . ugens import *
