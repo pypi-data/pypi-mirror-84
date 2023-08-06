0af73f61a044fb38bff76f3fc7dfeb8b746a0ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:56 +0000

    Add package geodist/0.2/geodist-0.2.tar.gz

---
+++ packages/geodist/geodist-0.2.tar.gz/geodist-0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="geodist",
+    version="0.2",
+    author="Dor Hay",
+    author_email="dorhay@gmail.com",
+    description="GeoDist Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dorhay/geodist",
+    packages=setuptools.find_packages(),
+    install_requires=["pyproj", "shapely"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+    ],
+    python_requires='>=3.7',
+)
