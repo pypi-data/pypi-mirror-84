11295850c97efe1f5cc3911d2856752ea277c285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:45 +0000

    Add package wormstools/1.3.7/wormstools-1.3.7.tar.gz

---
+++ packages/wormstools/wormstools-1.3.7.tar.gz/wormstools-1.3.7/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import setuptools
+from distutils.core import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setup(name="wormstools",
+      version='1.3.7',
+      description="Wrapper for WoRMS Rest API",
+      long_description = readme,
+      long_description_content_type='text/markdown',
+      author='Ulises Rosas',
+      author_email='ulisesfrosasp@gmail.com',
+      url='https://github.com/Ulises-Rosas/WoRMStools',
+      packages = ['wormstools'],
+      package_dir = {'wormstools': 'src'},
+      entry_points={
+        'console_scripts': [
+            'worms = wormstools.worms:main'
+            ]
+      },
+      classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License'
+            ]
+
+      )
