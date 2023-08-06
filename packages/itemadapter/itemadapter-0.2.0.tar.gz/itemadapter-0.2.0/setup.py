d037ee4c72afe0e8278d91ee0dac12d145e66201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:04 +0000

    Add package itemadapter/0.2.0/itemadapter-0.2.0.tar.gz

---
+++ packages/itemadapter/itemadapter-0.2.0.tar.gz/itemadapter-0.2.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="itemadapter",
+    version="0.2.0",
+    license="BSD",
+    description="Common interface for data container classes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Eugenio Lacuesta",
+    author_email="eugenio.lacuesta@gmail.com",
+    url="https://github.com/scrapy/itemadapter",
+    packages=["itemadapter"],
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Framework :: Scrapy",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
