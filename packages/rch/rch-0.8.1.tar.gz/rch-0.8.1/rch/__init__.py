0e5334ee2517db615c9f5cef0c70ce63a309a6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:11 +0000

    Add package rch/0.8.1/rch-0.8.1.tar.gz

---
+++ packages/rch/rch-0.8.1.tar.gz/rch-0.8.1/rch/__init__.py
@@ -0,0 +1,6 @@
+import rch.eng
+import rch.sudoku
+import rch.web
+
+__version__ = '0.8.1'
+__author__ = 'Riley Hales'
---
+++ packages/rch/rch-0.8.1.tar.gz/rch-0.8.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+with open('requirements.txt', 'r') as req:
+    install_requires = req.read().splitlines()
+
+setup(
+    name='rch',
+    packages=['rch'],
+    version='0.8.1',
+    install_requires=install_requires,
+    python_requires='>3',
+    description='A package containing the personal python utilities of Riley Hales',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Riley Hales',
+    author_email='rchales@byu.edu',
+    url='https://rileyhales.com',
+    project_urls=dict(Documentation='https://pyrch.readthedocs.io',
+                      Source='https://github.com/rileyhales/pyrch',
+                      License='https://choosealicense.com/licenses/bsd-3-clause-clear'),
+    license='BSD 3-Clause Clear License',
+    classifiers=('Programming Language :: Python :: 3',
+                 'Topic :: Scientific/Engineering',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: BSD License',
+                 'Natural Language :: English',),
+)
