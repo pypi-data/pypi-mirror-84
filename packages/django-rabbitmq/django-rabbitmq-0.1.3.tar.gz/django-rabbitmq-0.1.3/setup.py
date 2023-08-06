080007cca94ba0da37aa274583f221487dc3c476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:49 +0000

    Add package django-rabbitmq/0.1.3/django-rabbitmq-0.1.3.tar.gz

---
+++ packages/django-rabbitmq/django-rabbitmq-0.1.3.tar.gz/django-rabbitmq-0.1.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-rabbitmq",
+    version="0.1.3",
+    author="Yuhui Wang",
+    author_email="wangyuhuiever@163.com",
+    description="Start a RabbitMQ consumer after django server start.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wangyuhuiever/django-rabbitmq",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
