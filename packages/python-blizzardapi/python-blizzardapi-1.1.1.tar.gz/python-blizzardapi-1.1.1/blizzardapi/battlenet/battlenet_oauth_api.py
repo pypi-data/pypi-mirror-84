185bc78ecbc2e7eac378ef423f5c9f6c411bef41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:42 +0000

    Add package python-blizzardapi/1.1.1/python-blizzardapi-1.1.1.tar.gz

---
+++ packages/python-blizzardapi/python-blizzardapi-1.1.1.tar.gz/python-blizzardapi-1.1.1/blizzardapi/battlenet/battlenet_oauth_api.py
@@ -0,0 +1,21 @@
+from ..api import Api
+
+
+class BattlenetOauthApi(Api):
+    """All OAuth API methods"""
+
+    def __init__(self, client_id, client_secret):
+        super().__init__(client_id, client_secret)
+
+    # User Authentication
+
+    def get_user_info(self, region, access_token):
+        """
+        User Authentication - Returns basic information about the user
+        associated with the current bearer token.
+        """
+        resource = "/oauth/userinfo"
+        query_params = {
+            "access_token": access_token,
+        }
+        return super().get_oauth_resource(resource, region, query_params)
---
+++ packages/python-blizzardapi/python-blizzardapi-1.1.1.tar.gz/python-blizzardapi-1.1.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+__version__ = "1.1.1"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requirements = [
+    "requests==2.24.0",
+]
+
+setuptools.setup(
+    name="python-blizzardapi",
+    version=__version__,
+    author="Trevor Phillips",
+    author_email="trevorcoreyphillips@gmail.com",
+    description="python-blizzardapi is a client library for Blizzard's APIs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/trevorphillips/python-blizzardapi",
+    packages=setuptools.find_packages(),
+    install_requires=install_requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
