af0113d8b7b11a73bc968caa9fd1ad2266a54eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:35 +0000

    Add package unfor19-appy/0.0.1rc1/unfor19-appy-0.0.1rc1.tar.gz

---
+++ packages/unfor19-appy/unfor19-appy-0.0.1rc1.tar.gz/unfor19-appy-0.0.1rc1/setup.py
@@ -0,0 +1,16 @@
+from os import getcwd, path
+from setuptools import setup, sic
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+with open("version", "r") as fh:
+    version = fh.read()
+
+with open(path.join(getcwd(), 'src', 'appy', '__init__.py'), "w") as fh:
+    fh.write(f"__version__ = '{version}'\n")
+
+setup(
+    version=sic(version),  # treat version as is
+    download_url=f'https://github.com/unfor19/python-project/archive/{version}.tar.gz',  # noqa: E501
+)
