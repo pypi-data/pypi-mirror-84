46c5eadd829f5b46b2ce5c63619a80e14d29b5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:43 +0000

    Add package qsymm/1.3.0/qsymm-1.3.0-py3-none-any.whl

---
+++ packages/qsymm/qsymm-1.3.0-py3-none-any.whl/qsymm/tests/test_util.py
@@ -0,0 +1,52 @@
+import pytest
+import warnings
+import sympy
+import itertools
+import numpy as np
+import scipy.linalg as la
+
+from pytest import raises
+from ..linalg import matrix_basis, sparse_basis
+from ..groups import PointGroupElement
+
+
+def test_sparse_basis():
+    reals = [True, False]
+    for _, real in itertools.product(range(3), reals):
+        dim = np.random.randint(5, 10)
+        H = np.random.rand(dim, dim)
+        U = la.expm(1j*(H+H.T.conj()))
+        # Sparsify a full rank matrix
+        sparse_U = sparse_basis(U, num_digits=4, reals=real)
+        assert sparse_U.shape == U.shape
+        # Make one row linearly dependent on the others
+        U[-1, :] = sum([np.random.rand()*row for row in U[:-1, :]])
+        with pytest.warns(UserWarning):
+            sparse_U = sparse_basis(U, num_digits=4, reals=real)
+
+        assert sparse_U.shape[0] == dim-1
+
+
+def test_spatial_types():
+    S1 = PointGroupElement(sympy.eye(2), False, False,
+                           np.eye(3))
+    S2 = PointGroupElement(sympy.Matrix([[0, 1], [1, 0]]), True, False,
+                           np.eye(3))
+    S3 = PointGroupElement(np.eye(2), False, False,
+                           1j * np.eye(3))
+    C6s = PointGroupElement(sympy.ImmutableMatrix(
+                                [[sympy.Rational(1, 2), sympy.sqrt(3)/2],
+                                 [-sympy.sqrt(3)/2,       sympy.Rational(1, 2)]]
+                                                 ))
+    C6f = PointGroupElement(np.array(
+                                    [[1/2, np.sqrt(3)/2],
+                                     [-np.sqrt(3)/2, 1/2]]
+                                                     ))
+
+    assert S2**2 == S1
+    assert not S1 == S2
+    assert S1 == S3
+    assert C6s == C6f
+    # Mixing sympy with other types raises an error
+    with raises(ValueError):
+        S = C6s * C6f
