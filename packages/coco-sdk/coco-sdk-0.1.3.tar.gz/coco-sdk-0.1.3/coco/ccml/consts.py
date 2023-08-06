b5c6a0c4ca89d4f3dc3f8af53951589c4f3b4c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:27 +0000

    Add package coco-sdk/0.1.3/coco-sdk-0.1.3.tar.gz

---
+++ packages/coco-sdk/coco-sdk-0.1.3.tar.gz/coco-sdk-0.1.3/coco/ccml/consts.py
@@ -0,0 +1,23 @@
+CCML_DICTIONARY = {
+    "amazon": {
+        "phoneme": None,
+        "voice": None,
+        "language": "lang",
+        "part-of-speech": "w",
+        "domain": "amazon:domain",
+        "effect": "amazon:effect",
+        "emotion": "amazon:emotion",
+    },
+    "google": {"mark": None, "media": None, "parallel": "par", "sequential": "seq",},
+    "common": {
+        "speak": None,
+        "break": None,
+        "say-as": None,
+        "audio": None,
+        "p": None,
+        "s": None,
+        "prosody": None,
+        "emphasis": None,
+        "sub": None,
+    },
+}
