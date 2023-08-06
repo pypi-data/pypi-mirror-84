1ba74170293639977f26b74f3d900ce69b7c7e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:29 +0000

    Add package order.py/0.0.1/order.py-0.0.1.tar.gz

---
+++ packages/order.py/order.py-0.0.1.tar.gz/order.py-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'order.py',
+  packages = ['order'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'Official Order Python API wrapper',
+  author = 'portasynthinca3',
+  author_email = 'portasynthinca3@gmail.com',
+  url = 'https://github.com/ordermsg/order.py',
+  download_url = 'https://github.com/ordermsg/order.py/archive/0.0.1.tar.gz',
+  keywords = ['api', 'api-client'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
