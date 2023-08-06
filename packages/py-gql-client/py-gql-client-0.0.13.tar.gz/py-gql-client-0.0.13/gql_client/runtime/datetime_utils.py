7a0dbef693ac003ff6d04d143a043e39f0110d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:28 +0000

    Add package py-gql-client/0.0.13/py-gql-client-0.0.13.tar.gz

---
+++ packages/py-gql-client/py-gql-client-0.0.13.tar.gz/py-gql-client-0.0.13/gql_client/runtime/datetime_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from typing import Optional
+
+from dataclasses import field
+from datetime import datetime, timedelta, tzinfo
+
+from marshmallow import fields as marshmallow_fields
+
+
+class simple_utc(tzinfo):
+    def tzname(self, dt: Optional[datetime]) -> Optional[str]:
+        return "UTC"
+
+    def utcoffset(self, dt: Optional[datetime]) -> Optional[timedelta]:
+        return timedelta(0)
+
+
+def isoformat(time: datetime) -> str:
+    return datetime.isoformat(time.replace(tzinfo=simple_utc()))
+
+
+DATETIME_FIELD = field(
+    metadata={
+        "dataclasses_json": {
+            "encoder": isoformat,
+            "decoder": datetime.fromisoformat,
+            "mm_field": marshmallow_fields.DateTime(format="iso"),
+        }
+    }
+)
