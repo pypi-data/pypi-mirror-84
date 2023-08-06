7572cf11e9468dfa1b925545625808a440836d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:44 +0000

    Add package wafec.wrapt.custom/1.0.17/wafec.wrapt.custom-1.0.17.tar.gz

---
+++ packages/wafec.wrapt.custom/wafec.wrapt.custom-1.0.17.tar.gz/wafec.wrapt.custom-1.0.17/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="wafec.wrapt.custom",
+    version="1.0.17",
+    author="Wallace",
+    author_email="wallacefcardoso@gmail.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=[
+        'wrapt>=1.12.1',
+        'Flask>=1.1.2',
+        'requests>=2.0.0',
+        'psutil>=5.7.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            'comm_cmd = wafec_wrapt_custom.comm_command_line:main'
+        ]
+    }
+)
