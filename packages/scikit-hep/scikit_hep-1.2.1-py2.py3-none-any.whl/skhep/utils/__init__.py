5ea76c45f94177169eb1d1b2c007fd24f95f2f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:51 +0000

    Add package scikit-hep/1.2.1/scikit_hep-1.2.1-py2.py3-none-any.whl

---
+++ packages/scikit-hep/scikit_hep-1.2.1-py2.py3-none-any.whl/skhep/utils/__init__.py
@@ -0,0 +1,7 @@
+# Licensed under a 3-clause BSD style license, see LICENSE.
+"""
+Module for miscellaneous and general utilities.
+"""
+
+from .exceptions import *
+from .provenance import ObjectOrigin, FileOrigin, Transformation, Formatting
