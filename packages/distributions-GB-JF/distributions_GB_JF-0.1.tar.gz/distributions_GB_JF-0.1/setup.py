25815d2f71902a164a3f1274a0e8f7912ae7128c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:34 +0000

    Add package distributions-GB-JF/0.1/distributions_GB_JF-0.1.tar.gz

---
+++ packages/distributions-GB-JF/distributions_GB_JF-0.1.tar.gz/distributions_GB_JF-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_GB_JF',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_GB_JF'],
+      author= 'Junmei Fan',
+      author_email= 'biofjm@gmail.com',
+      zip_safe=False)
