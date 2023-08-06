02251f547588ffaa9c755b7c0aceeaa811a659b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:35 +0000

    Add package bing-translation-for-python/1.2/bing_translation_for_python-1.2-py3-none-any.whl

---
+++ packages/bing-translation-for-python/bing_translation_for_python-1.2-py3-none-any.whl/bing_translation_for_python/public/errors.py
@@ -0,0 +1,14 @@
+class TargetLanguageNotSupported(Exception):
+    pass
+
+
+class EmptyTextError(Exception):
+    pass
+
+
+class FileError(Exception):
+    pass
+
+
+class EqualTextLanguage(Exception):
+    pass
