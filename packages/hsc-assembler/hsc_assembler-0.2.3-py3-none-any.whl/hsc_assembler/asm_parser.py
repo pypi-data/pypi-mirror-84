eaebb73e0940380799478c70dfc83493b940983b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:47 +0000

    Add package hsc-assembler/0.2.3/hsc_assembler-0.2.3-py3-none-any.whl

---
+++ packages/hsc-assembler/hsc_assembler-0.2.3-py3-none-any.whl/hsc_assembler/asm_parser.py
@@ -0,0 +1,212 @@
+# TODO: Improve error messages
+# Replace this with a parsing library (e.g pyparsing)?
+from __future__ import annotations
+
+import typing
+
+from hsc_instructions.arg_types import Label, PointerDeref, Register, Shift, ShiftType
+from hsc_instructions.errors import ParseError, format_error
+from hsc_instructions.instruction import Instruction
+from hsc_instructions.instruction_info import Instruction as InstructionEnum
+from hsc_instructions.sized_numbers import (
+    PositiveSizedNumber,
+    Uint5,
+    Uint16,
+    Uint24,
+    UintOverflowError,
+)
+
+from hsc_assembler.token_enums import Syntax, TokenType
+
+if typing.TYPE_CHECKING:
+    from .parseable import Parseable  # noqa
+    from .token_enums import Token
+
+    TPA = typing.TypeVar("TPA", bound=Parseable)
+
+
+__all__ = ["InstructionsNT", "parse"]
+
+
+class InstructionsNT(typing.NamedTuple):
+    instructions: typing.Sequence[Instruction]
+    symbol_table: typing.Dict[str, int]
+
+
+def comma_split(tokens: typing.Iterable[Token]) -> typing.Iterator[typing.List[Token]]:
+    current_tokens: typing.List[Token] = []
+    for token in tokens:
+        if token.type is TokenType.SYNTAX and token.value is Syntax[","]:
+            yield current_tokens
+            current_tokens = []
+        else:
+            current_tokens.append(token)
+    if current_tokens:
+        yield current_tokens
+
+
+def empty_token_check(
+    tokens: typing.Sequence[Token], line: int
+) -> typing.Sequence[Token]:
+    if not tokens:
+        raise ParseError("Empty operand", line)
+    return tokens
+
+
+def extact_token(one_token: typing.Sequence[Token], line: int) -> Token:
+    empty_token_check(one_token, line)
+    if len(one_token) > 1:
+        raise ParseError(
+            f'Extra text "{"".join(token.text for token in one_token[1:])}"',
+            line,
+        )
+    return one_token[0]
+
+
+def token_type_check(
+    token: Token,
+    token_types: typing.Union[TokenType, typing.Iterable[TokenType]],
+    line: int,
+) -> Token:
+    if isinstance(token_types, TokenType):
+        token_types = (token_types,)
+    if token.type not in token_types:
+        type_names = " or ".join(token_type.name.lower() for token_type in token_types)
+        message = f'Token "{token.text}" instead of {type_names}'
+        raise ParseError(message, line)
+    return token
+
+
+TN = typing.TypeVar("TN", bound=PositiveSizedNumber)
+
+
+def extract_token_num(token: Token, num_type: typing.Type[TN], line: int) -> TN:
+    token_type_check(token, (TokenType.UINT, TokenType.STRING), line)
+    try:
+        return num_type(token.value)
+    except UintOverflowError:
+        assert isinstance(token.value, int)
+        message = (
+            f"Number or string {token.value} is too large. "  # type: ignore
+            f"The max size for this operand is {num_type.MAX}. "
+            f"The orignal text was {token.text}."
+        )
+        raise ParseError(message, line) from None
+
+
+@Register.make_parse_function
+def parse_register(
+    cls: typing.Type[Register], tokens: typing.Sequence[Token], line: int
+) -> Register:
+    token = token_type_check(extact_token(tokens, line), TokenType.REGISTER, line)
+    assert isinstance(token.value, cls)
+    return token.value
+
+
+@PointerDeref.make_parse_function
+def parse_pointer_deref(
+    cls: typing.Type[PointerDeref],
+    tokens: typing.Sequence[Token],
+    line: int,
+) -> PointerDeref:
+    empty_token_check(tokens, line)
+    if tokens[0].value != Syntax["["] or tokens[-1].value != Syntax["]"]:
+        str_tokens = "".join(token.text for token in tokens)
+        raise ParseError(f"Invalid pointer dereference {str_tokens}", line)
+    register = Register.parse(tokens[1], line)
+    if len(tokens) > 3:
+        if tokens[2].value != Syntax["+"]:
+            raise ParseError(f'Expected "+" sign, instead got {tokens[2].text}', line)
+        increment = cls.increment_parse(tokens[3:-1], line)
+    else:
+        increment = Uint16(0)
+    return cls(register, increment)
+
+
+@Uint16.make_parse_function
+@Uint24.make_parse_function
+def parse_number(
+    cls: typing.Type[PositiveSizedNumber], tokens: typing.Sequence[Token], line: int
+) -> PositiveSizedNumber:
+    return extract_token_num(extact_token(tokens, line), cls, line)
+
+
+@Shift.make_parse_function
+def parse_shift(
+    cls: typing.Type[Shift], tokens: typing.Sequence[Token], line: int
+) -> Shift:
+    empty_token_check(tokens, line)
+    register = Register.parse(tokens[0], line)
+    if len(tokens) == 1:
+        shift_type = ShiftType["<<"]
+        amount = Uint5(0)
+    elif tokens[1].value not in {Syntax[">>"], Syntax["<<"]}:
+        raise ParseError(f"Invalid operator {tokens[1].text}", line)
+    else:
+        assert isinstance(tokens[1].value, Syntax)
+        shift_type = ShiftType[tokens[1].value.name]
+        amount = extract_token_num(extact_token(tokens[2:], line), Uint5, line)
+    return cls(shift_type, register, amount)
+
+
+@Label.make_parse_function
+def parse_label(
+    cls: typing.Type[Label], tokens: typing.Sequence[Token], line: int
+) -> Label:
+    return Label(
+        token_type_check(extact_token(tokens, line), TokenType.LABEL, line).value
+    )
+
+
+def parse_args(
+    parser_types: typing.Iterable[typing.Iterable[typing.Type[TPA]]],
+    args: typing.Iterable[typing.Sequence[Token]],
+) -> typing.Iterator[TPA]:
+    errors = []
+    for arg_types, arg in zip(parser_types, args):
+        for arg_type in arg_types:
+            try:
+                parsed_arg = arg_type.parse(arg, arg[0].line)  # type: ignore
+                break
+            except ParseError as exc:
+                error = exc.args[0]
+        else:
+            errors.append(error)
+        if not errors:
+            yield parsed_arg
+    if errors:
+        raise ParseError.collect_errors(errors)
+
+
+def parse(tokens: typing.Iterator[typing.Sequence[Token]]) -> InstructionsNT:
+    instructions = []
+    symbol_table = {}
+    ip = 0
+    errors = []
+    for line in tokens:
+        line_num = line[0].line
+        if line[-1].value is Syntax[":"]:
+            assert line[-1].type is TokenType.SYNTAX
+            assert len(line) == 2
+            assert line[0].type is TokenType.LABEL
+            assert isinstance(line[0].value, str)
+            symbol_table[line[0].value] = ip
+        else:
+            ip += 4
+            assert line[0].type is TokenType.INSTRUCTION
+            instruction = line[0].value
+            assert isinstance(instruction, InstructionEnum)
+            raw_args = list(comma_split(line[1:]))
+            if len(raw_args) != len(instruction.value.types):
+                message = (
+                    f"Opcode {instruction.name} takes {len(instruction.value)} "
+                    f"arguments, but was given {len(raw_args)} arguments."
+                )
+                errors.append(format_error(message, line_num))
+
+            args = parse_args(instruction.value.types, raw_args)
+
+            instructions.append(Instruction(instruction, list(args)))
+    if errors:
+        raise ParseError.collect_errors(errors)
+    return InstructionsNT(instructions, symbol_table)
---
+++ packages/hsc-assembler/hsc_assembler-0.2.3-py3-none-any.whl/hsc_assembler/scanner.py
@@ -0,0 +1,174 @@
+from __future__ import annotations
+
+import dataclasses
+import shlex
+import typing
+
+from hsc_instructions.arg_types import Register
+from hsc_instructions.errors import ScanError, exception_chain
+from hsc_instructions.instruction_info import Instruction
+from hsc_instructions.sized_numbers import MAX_NUM
+
+from hsc_assembler.token_enums import Syntax, TokenType
+
+
+# This is defined here, instead of in token_enums.py because of its scanner methods
+# Maybe the visitor pattern would help here for code organization?
+@dataclasses.dataclass
+class Token:
+    type: TokenType
+    # These are the types Token.value would be depending on the Token.type
+    # INSTRUCTION: Instruction (the specific instruction)
+    # REGISTER: Register (the specific register)
+    # SYNTAX: Syntax (the specific syntax)
+    # LABEL: str (Name of the label)
+    # STRING: int
+    # INT: int
+    value: typing.Union[str, bytes, int, Register, Instruction, Syntax]
+    line: int
+    text: str
+
+    @classmethod
+    def scan_instruction(cls, token_string: str, line: int) -> Token:
+        try:
+            instruction = Instruction[token_string.upper()]
+        except KeyError:
+            raise ScanError(f"Invalid instruction {token_string}", line) from None
+        return Token(TokenType.INSTRUCTION, instruction, line, token_string)
+
+    @classmethod
+    def scan_register(cls, token_string: str, line: int) -> Token:
+        try:
+            register = Register[token_string.lower()]
+        except KeyError:
+            raise ScanError(f"Invalid register {token_string}", line) from None
+        return cls(TokenType.REGISTER, register, line, token_string)
+
+    @classmethod
+    def scan_string(cls, token_string: str, line: int) -> Token:
+        """
+        Scans a string to a string (or character) token.
+        If the token isn't valid, returns None.
+        """
+        if token_string[0] in {"'", '"'} and token_string[-1] == token_string[0]:
+            value = int.from_bytes(
+                token_string[1:-1].encode()
+                + (b"\0" if token_string[0] == '"' else b""),
+                "big",
+            )
+            return cls(TokenType.STRING, value, line, token_string)
+        raise ScanError(f"Invalid string {token_string}", line)
+
+    @classmethod
+    def scan_syntax(cls, token_string: str, line: int) -> Token:
+        """ Scans a string to a syntactical token. """
+        try:
+            syntax = Syntax[token_string]
+        except KeyError:
+            raise ScanError(f"Invalid syntax {token_string}", line)
+        return cls(TokenType.SYNTAX, syntax, line, token_string)
+
+    @classmethod
+    def scan_integer(cls, token_string: str, line: int) -> Token:
+        try:
+            value = int(token_string, 0)
+        except ValueError:
+            raise ScanError(f"Invalid integer {token_string}", line) from None
+        if value < 0:
+            raise ScanError(f"Negative integer {value} is not supported", line)
+        if value > MAX_NUM:
+            message = (
+                f"Integer {value} is too big, the largest supported number is {MAX_NUM}"
+            )
+            raise ScanError(message, line)
+        return cls(TokenType.UINT, value, line, token_string)
+
+    @classmethod
+    def scan_label(cls, token_string: str, line: int) -> Token:
+        """
+        Scans a label.
+
+        Any string is a valid label unless it has a character in the `Syntax` enum.
+        """
+        if any(syntax in token_string for syntax in Syntax.__members__):
+            raise ScanError(
+                f"Invalid label {token_string}. "
+                "Does this have a syntax character in it? They're one of these:\n"
+                f"{list(Syntax.__members__)}",
+                line,
+            )
+        return cls(TokenType.LABEL, token_string, line, token_string)
+
+    @classmethod
+    def scan_token(cls, token_string: str, line: int) -> Token:
+        scanners = (
+            cls.scan_instruction,
+            cls.scan_register,
+            cls.scan_string,
+            cls.scan_syntax,
+            cls.scan_integer,
+            cls.scan_label,
+        )
+        return exception_chain(
+            scanners,
+            ScanError(f'Invalid token "{token_string}"', line),
+            token_string,
+            line,
+        )
+
+
+def counted_lowered_no_comment_lines(
+    text: str,
+) -> typing.Iterator[typing.Tuple[int, str]]:
+    """
+    Yields the line number and the lower cased line without comments.
+
+    If the line is empty, only has whitespace, or only has comments the line
+    counts towards the line count but isn't yielded. The line count starts at 1.
+    """
+    for line_num, line in enumerate(text.lower().splitlines(), 1):
+        line = line.split(";", 1)[0].strip()
+        if not line.isspace() and line:
+            yield line_num, line
+
+
+def scan_label_def(str_token: str, line: int) -> typing.Tuple[Token, Token]:
+    if str_token[-1] == ":":
+        return (
+            Token.scan_label(str_token[:-1], line),
+            Token(TokenType.SYNTAX, Syntax[":"], line, str_token),
+        )
+    raise ScanError(f"Invalid label definition {str_token}", line)
+
+
+def scan(text: str) -> typing.Iterator[typing.List[Token]]:
+    # This keeps going after it errors to find all the errors in the program
+    errors: typing.List[str] = []
+    for line_num, line in counted_lowered_no_comment_lines(text):
+        try:
+            if line[-1] == ":":
+                line_tokens = list(scan_label_def(line, line_num))
+            else:
+                # Using shlex seems to work, but there may be issues
+                # TODO: Shlex doesn't fully support unicode characters.
+                tokens = shlex.shlex(line, punctuation_chars="><", posix=False)
+                try:
+                    instruction = Token.scan_instruction(tokens.get_token(), line_num)
+                except ScanError as exc:
+                    errors.append(exc.args[0])
+                    # This is to prevent a NameError.
+                    # If instruction is None, errors isn't empty so it isn't yielded
+                    # The "type: ignore" is to quiet the type checker
+                    instruction = None  # type: ignore
+
+                line_tokens = [
+                    instruction,
+                    *(Token.scan_token(token, line_num) for token in tokens),
+                ]
+            if not errors:
+                yield line_tokens
+        except ScanError as exc:
+            errors.append(exc.args[0])
+
+    if errors:
+        raise ScanError.collect_errors(errors)
