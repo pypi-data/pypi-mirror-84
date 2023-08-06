c4242bd3b687e91d4d078dbcc5abe6b5aee8cf8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:37 +0000

    Add package pdpipe/0.0.53/pdpipe-0.0.53-py3-none-any.whl

---
+++ packages/pdpipe/pdpipe-0.0.53-py3-none-any.whl/pdpipe/exceptions.py
@@ -0,0 +1,19 @@
+"""Custom exceptions for pdpipe."""
+
+
+class FailedPreconditionError(Exception):
+    """An exception raised when a pipeline stage is applied to a dataframe for
+    which the stage precondition does not hold.
+    """
+
+
+class PipelineApplicationError(Exception):
+    """An exception raised when an exception is thrown during the application
+    of a pipeline or a pipeline stage to a dataframe.
+    """
+
+
+class UnfittedPipelineStageError(Exception):
+    """An exception raised when a (non-fit) transform is attempted with an
+    unfitted pipeline stage.
+    """
