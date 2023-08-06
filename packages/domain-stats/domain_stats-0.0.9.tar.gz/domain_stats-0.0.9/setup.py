7f6e0b07bb197352ffb81fd3b42ebe9d53a9f6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:09 +0000

    Add package domain-stats/0.0.9/domain_stats-0.0.9.tar.gz

---
+++ packages/domain-stats/domain_stats-0.0.9.tar.gz/domain_stats-0.0.9/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="domain_stats", 
+    version="0.0.9",
+    author="MarkBaggett",
+    author_email="lo127001@gmail.com",
+    description="Malicious Domain Detection base on domain creation and first contact",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/markbaggett/domain_stats",
+    packages=setuptools.find_packages(),
+    install_requires = ['requests','pyyaml', 'rdap', 'python-dateutil>2.7','publicsuffixlist','flask','gunicorn', 'diskcache'],
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points = {
+        'console_scripts': ['domain-stats=domain_stats.launch:main',
+                            'domain-stats-settings=domain_stats.settings:main'],
+    },
+    package_data={'domain_stats': ['data/*.*']}
+)
