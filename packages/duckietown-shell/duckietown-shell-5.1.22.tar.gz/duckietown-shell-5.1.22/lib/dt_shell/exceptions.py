46bbc842df0331173fcc4c8e62a83a25bdbf1b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:05 +0000

    Add package duckietown-shell/5.1.22/duckietown-shell-5.1.22.tar.gz

---
+++ packages/duckietown-shell/duckietown-shell-5.1.22.tar.gz/duckietown-shell-5.1.22/lib/dt_shell/exceptions.py
@@ -0,0 +1,48 @@
+__all__ = [
+    "InvalidEnvironment",
+    "UserError",
+    "CommandsLoadingException",
+    "ConfigNotPresent",
+    "ConfigInvalid",
+    "CouldNotGetVersion",
+    "NoCacheAvailable",
+    "URLException",
+]
+
+
+class InvalidEnvironment(Exception):
+    pass
+
+
+class UserError(Exception):
+    """ an error that will be briefly printed"""
+
+    pass
+
+
+class InvalidConfig(Exception):
+    pass
+
+
+class CommandsLoadingException(Exception):
+    pass
+
+
+class ConfigInvalid(Exception):
+    pass
+
+
+class ConfigNotPresent(Exception):
+    pass
+
+
+class CouldNotGetVersion(Exception):
+    pass
+
+
+class URLException(Exception):
+    pass
+
+
+class NoCacheAvailable(Exception):
+    pass
