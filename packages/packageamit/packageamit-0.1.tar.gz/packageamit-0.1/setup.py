0714c6685aaa655e188d6b68f88d70283a734633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:32 +0000

    Add package packageamit/0.1/packageamit-0.1.tar.gz

---
+++ packages/packageamit/packageamit-0.1.tar.gz/packageamit-0.1/setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup
+setup(name="packageamit",
+      version="0.1",
+      description="This is code with amit package",
+      long_description="This is a very very long description",
+      author="Harry",
+      packages=['packageamit'],
+      install_requires=[])
