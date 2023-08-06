123ee8728104e85ecbccec4fb9cb6ff6559c40f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:24 +0000

    Add package pyupdog/1.0.5/pyupdog-1.0.5.tar.gz

---
+++ packages/pyupdog/pyupdog-1.0.5.tar.gz/pyupdog-1.0.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+setuptools.setup(
+    name='pyupdog',
+    version='1.0.5',
+    author='Joseph Halstead',
+    author_email='josephhalstead89@gmail.com',
+    description='Python program for calculating UPD',
+    long_description='test',
+    long_description_content_type='text/markdown',
+    url='https://github.com/josephhalstead/pyvariantfilter',
+    packages=setuptools.find_packages(),
+    scripts= ['UPDog.py'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+   'pysam>=0.15.2',
+   'pandas>=0.23.4',
+   'seaborn>=0.10.1',
+   'scipy>=1.4.1',
+   'pyvariantfilter>=1.0.4',
+   'numpy>=1.18.1'
+],
+)
