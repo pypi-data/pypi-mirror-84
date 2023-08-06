a930f8b394e1db73c4a8cf37e84010c9cbb3ec21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:36 +0000

    Add package styler-identity/0.5.3/styler_identity-0.5.3-py2.py3-none-any.whl

---
+++ packages/styler-identity/styler_identity-0.5.3-py2.py3-none-any.whl/styler_identity/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Styler Identity."""
+
+__author__ = """Bruno Toshio Sugano"""
+__email__ = 'bruno.sugano@styler.link'
+__version__ = '0.5.3'
+
+
+from styler_identity.identity import Identity
