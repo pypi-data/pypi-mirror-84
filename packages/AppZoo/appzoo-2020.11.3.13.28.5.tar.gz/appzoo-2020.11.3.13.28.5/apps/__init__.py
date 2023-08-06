7bf0e28dee0e68dd8be24d4115bacbbeda74609f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:15 +0000

    Add package AppZoo/2020.11.3.13.28.5/appzoo-2020.11.3.13.28.5.tar.gz

---
+++ packages/AppZoo/appzoo-2020.11.3.13.28.5.tar.gz/appzoo-2020.11.3.13.28.5/apps/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : StreamlitApp.
+# @File         : __init__.py
+# @Time         : 2020/11/3 11:38 上午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
