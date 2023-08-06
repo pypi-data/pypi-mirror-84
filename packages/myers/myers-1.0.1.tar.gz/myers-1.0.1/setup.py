bea284095e153f689f18d281db79204c235f83ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:06 +0000

    Add package myers/1.0.1/myers-1.0.1.tar.gz

---
+++ packages/myers/myers-1.0.1.tar.gz/myers-1.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import myers
+
+_classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Utilities',
+]
+
+if __name__ == '__main__':
+    setup(
+        name='myers',
+        version=myers.__version__,
+        author='Tom Ritchford',
+        author_email='tom@swirly.com',
+        url='https://github.com/rec/myers',
+        py_modules=['myers'],
+        description='Compute the Myers diff of two lists',
+        long_description=open('README.rst').read(),
+        license='MIT',
+        classifiers=_classifiers,
+        keywords=['testing', 'modules'],
+        scripts=['myers.py'],
+    )
