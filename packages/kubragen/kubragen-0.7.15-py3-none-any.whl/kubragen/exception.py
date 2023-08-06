62d2d28419ab3f8383bdc225c391fb7ac7c0ef9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:28 +0000

    Add package kubragen/0.7.15/kubragen-0.7.15-py3-none-any.whl

---
+++ packages/kubragen/kubragen-0.7.15-py3-none-any.whl/kubragen/exception.py
@@ -0,0 +1,38 @@
+class KGException(Exception):
+    pass
+
+
+class InvalidOperationError(KGException):
+    pass
+
+
+class InvalidParamError(KGException):
+    pass
+
+
+class NotFoundError(KGException):
+    pass
+
+
+class NotSupportedError(KGException):
+    pass
+
+
+class InvalidNameError(KGException):
+    pass
+
+
+class OptionError(KGException):
+    pass
+
+
+class TypeError(KGException):
+    pass
+
+
+class MergeError(KGException):
+    pass
+
+
+class InvalidJsonPatchError(KGException):
+    pass
