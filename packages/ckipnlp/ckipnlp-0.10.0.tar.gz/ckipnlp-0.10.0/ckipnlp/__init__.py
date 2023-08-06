df0d043141aa6e7dbaa5c246c5b882139bbe43b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:43 +0000

    Add package ckipnlp/0.10.0/ckipnlp-0.10.0.tar.gz

---
+++ packages/ckipnlp/ckipnlp-0.10.0.tar.gz/ckipnlp-0.10.0/ckipnlp/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+The Official CKIP CoreNLP Toolkit.
+"""
+
+__author_name__ = 'Mu Yang'
+__author_email__ = 'emfomy@gmail.com'
+__copyright__ = '2018-2020 CKIP Lab'
+
+__title__ = 'CKIPNLP'
+__version__ = '0.10.0'
+__description__ = 'CKIP CoreNLP'
+__license__ = 'CC BY-NC-SA 4.0'
+
+__url__ = 'https://ckipnlp.readthedocs.io'
+__download_url__ = __url__+'/tarball/'+__version__
---
+++ packages/ckipnlp/ckipnlp-0.10.0.tar.gz/ckipnlp-0.10.0/ckipnlp/pipeline/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+This module implements CKIPNLP pipelines.
+"""
+
+__author__ = 'Mu Yang <http://muyang.pro>'
+__copyright__ = '2018-2020 CKIP Lab'
+__license__ = 'CC BY-NC-SA 4.0'
+
+from .kernel import (
+    CkipPipeline,
+    CkipDocument,
+)
+
+from .coref import (
+    CkipCorefPipeline,
+    CkipCorefDocument,
+)
