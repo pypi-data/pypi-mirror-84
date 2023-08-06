caee3dc0e113171e5e6f64466421a26272050d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:19 +0000

    Add package researcher/0.0.10/researcher-0.0.10.tar.gz

---
+++ packages/researcher/researcher-0.0.10.tar.gz/researcher-0.0.10/researcher/experiment.py
@@ -0,0 +1,30 @@
+import datetime
+
+from researcher.globals import DATE_FORMAT, RESULTS_NAME
+from researcher.results import Results
+
+class Experiment():
+    def __init__(self, data):
+        if data[RESULTS_NAME]:
+            self.results = Results(data[RESULTS_NAME])
+        else:
+            self.results = None
+        
+        self.data = data
+        self.timestamp = datetime.datetime.strptime(self.data["timestamp"], DATE_FORMAT)
+    
+    def is_trial(self):
+        return "trial" in self.data and self.data["trial"]
+
+    def get_hash(self):
+        return self.data["hash"]
+
+    def identifier(self):
+        title =  self.data["title"] if "title" in self.data else ""
+
+        id = "{}_{}".format(title, self.data["hash"][:8])
+
+        if self.is_trial():
+            id = "trial_" + id
+        
+        return id
