9481942073d5e5ddb0aba38bcd34dde6a9c988d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:41 +0000

    Add package pyshipupdate/0.0.13/pyshipupdate-0.0.13-py3-none-any.whl

---
+++ packages/pyshipupdate/pyshipupdate-0.0.13-py3-none-any.whl/pyshipupdate/aws.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+from pathlib import Path
+from tempfile import mkdtemp
+import shutil
+import os
+from semver import VersionInfo
+from typing import List
+import zipfile
+
+from balsa import get_logger
+from awsimple import S3Access
+from awsimple.s3 import BucketNotFound
+from typeguard import typechecked
+
+from pyshipupdate import Updater, __application_name__, version_from_clip_zip, CLIP_EXT, create_bucket_name
+
+log = get_logger(__application_name__)
+
+
+@dataclass
+class UpdaterAwsS3(Updater, S3Access):
+    """
+    pyship updater via AWS S3
+    """
+
+    def __init__(self, target_app_name: str, target_app_author: str):
+        Updater.__init__(self, target_app_name, target_app_author)
+        S3Access.__init__(self, create_bucket_name(target_app_name, target_app_author))
+
+    @typechecked
+    def get_available_versions(self) -> List[VersionInfo]:
+        available_versions = []
+        try:
+            bucket_dir = self.dir()
+        except BucketNotFound as e:
+            log.info(f"Bucket not found,{self.bucket_name=},{e}")  # may want to make this a warning
+            bucket_dir = {}
+        for s3_key in bucket_dir:
+            version = version_from_clip_zip(self.target_app_name, s3_key)
+            if version is not None:
+                available_versions.append(version)
+        available_versions.sort()
+        return available_versions
+
+    @typechecked
+    def install_clip(self, version: VersionInfo, destination_dir: Path) -> bool:
+        clip_name = f"{self.target_app_name}_{str(version)}"
+        s3_key = f"{clip_name}.{CLIP_EXT}"
+        download_path = Path(destination_dir, s3_key)
+        extract_path = Path(destination_dir, clip_name)
+        if self.object_exists(s3_key):
+            self.download_cached(s3_key, download_path)
+            log.info(f"extracting {download_path} ({download_path.absolute()}) to {extract_path} ({extract_path.absolute()})")
+            with zipfile.ZipFile(download_path, 'r') as zip_ref:
+                zip_ref.extractall(extract_path)
+            log.info(f"removing {download_path} ({download_path.absolute()})")
+            os.remove(download_path)
+            install_success = True
+        else:
+            log.warning(f"{self.bucket_name}/{s3_key} not found")
+            install_success = False
+        return install_success
---
+++ packages/pyshipupdate/pyshipupdate-0.0.13-py3-none-any.whl/pyshipupdate/updater.py
@@ -0,0 +1,86 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from enum import Enum
+from typing import List
+
+from semver import VersionInfo
+from appdirs import user_data_dir
+from typeguard import typechecked
+from balsa import get_logger
+
+from pyshipupdate import __application_name__, create_bucket_name
+
+log = get_logger(__application_name__)
+
+
+class PreReleaseTypes(Enum):
+    test = "test"
+    dev = "dev"
+    alpha = "alpha"
+    beta = "beta"
+
+
+class Updater(ABC):
+    """
+    pyship updater
+    Updates a pyship app to its latest released version.  Instantiated and called by a running pyship app.
+    """
+
+    def __init__(self, target_app_name: str, target_app_author: str, allowed_pre_release: List[PreReleaseTypes] = None):
+        self.target_app_name = target_app_name
+        self.target_app_author = target_app_author
+        self.allowed_pre_release = allowed_pre_release  # test, dev, beta, etc.
+
+    @abstractmethod
+    def get_available_versions(self) -> List[VersionInfo]:
+        """
+        get available versions
+        """
+        ...
+
+    @abstractmethod
+    def install_clip(self, version: VersionInfo, destination_dir: Path) -> bool:
+        """
+        put a clip into a destination dir
+        :param version: version of clip to get
+        :param destination_dir: dir to put the clip
+        :return: True if were able to get the clip, False otherwise
+        """
+        ...
+
+    @typechecked(always=True)
+    def get_greatest_version(self) -> (VersionInfo, None):
+        """
+        determine the greatest version and return it
+        :return: the greatest version, or None if no versions available
+        """
+        available_versions = self.get_available_versions()
+        log.debug(f"{available_versions=}")
+        if len(available_versions) == 0:
+            greatest_version = None
+        else:
+            greatest_version = sorted(available_versions)[-1]
+        log.info(f"{greatest_version=}")
+        return greatest_version
+
+    @typechecked(always=True)
+    def update(self, current_version: (str, VersionInfo), app_dir: Path = None) -> bool:
+        """
+        update this (the target) application (clip dir)
+        :param current_version: current version of the running app (both string and VersionInfo allowed)
+        :param app_dir: destination directory or None to use the user data area
+        """
+
+        if app_dir is None:
+            app_dir = Path(user_data_dir(self.target_app_name, self.target_app_author))
+
+        did_update = False
+        if isinstance(current_version, str):
+            current_version = VersionInfo.parse(current_version)
+        log.info(f"{current_version=}")
+        greatest_version = self.get_greatest_version()
+        log.info(f"{greatest_version=}")
+        if greatest_version is not None and greatest_version > current_version:
+            app_dir.mkdir(parents=True, exist_ok=True)
+            did_update = self.install_clip(greatest_version, app_dir)
+        return did_update
