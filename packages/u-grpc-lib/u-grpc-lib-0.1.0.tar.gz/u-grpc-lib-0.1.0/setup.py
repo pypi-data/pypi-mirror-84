8cd5050c72a06bf476e168c53fd33930a6f3f8e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:55 +0000

    Add package u-grpc-lib/0.1.0/u-grpc-lib-0.1.0.tar.gz

---
+++ packages/u-grpc-lib/u-grpc-lib-0.1.0.tar.gz/u-grpc-lib-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['u_grpc_lib']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['grpcio-tools>=1.33.2,<2.0.0', 'grpcio>=1.33.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'u-grpc-lib',
+    'version': '0.1.0',
+    'description': 'Generate protobuf and grpc stub python files to a package.',
+    'long_description': None,
+    'author': 'duyixian',
+    'author_email': 'duyixian@meideng.net',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
