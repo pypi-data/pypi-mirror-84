3bcfd9f51d8c7d05ff746645d1bd600c005a4394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:11 +0000

    Add package phablytics/2.2.1/phablytics-2.2.1-py2.py3-none-any.whl

---
+++ packages/phablytics/phablytics-2.2.1-py2.py3-none-any.whl/phablytics/web/constants.py
@@ -0,0 +1,20 @@
+SITE_NAME = 'Phablytics'
+
+NAV_LINKS = [
+    {
+        'name': 'Home',
+        'path': '/',
+    },
+    {
+        'name': 'Metrics',
+        'path': '/metrics',
+    },
+    {
+        'name': 'Reports',
+        'path': '/reports',
+    },
+    {
+        'name': 'Help',
+        'path': '/help',
+    },
+]
