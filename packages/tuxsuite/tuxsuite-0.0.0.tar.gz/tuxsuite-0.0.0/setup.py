cf0ebd62df67cceca091fde7b2a7a19d58167265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:11 +0000

    Add package tuxsuite/0.0.0/tuxsuite-0.0.0.tar.gz

---
+++ packages/tuxsuite/tuxsuite-0.0.0.tar.gz/tuxsuite-0.0.0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+setuptools.setup(
+    name="tuxsuite",
+    version="0.0.0",
+    author="Linaro Limited",
+    description="Linux kernel development service - client",
+    long_description="Linaro kernel development service - client",
+    long_description_content_type="text/markdown",
+    url="https://tuxsuite.com/",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Testing",
+        "Topic :: System :: Operating System Kernels :: Linux",
+    ],
+    python_requires=">=3.6",
+)
