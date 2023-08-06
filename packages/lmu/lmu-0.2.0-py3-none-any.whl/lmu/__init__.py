f9612ecacb17fbedd59f7cf06bf3e906988ae8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:52 +0000

    Add package lmu/0.2.0/lmu-0.2.0-py3-none-any.whl

---
+++ packages/lmu/lmu-0.2.0-py3-none-any.whl/lmu/__init__.py
@@ -0,0 +1,12 @@
+"""LMU provides a package for deep learning with Legendre Memory Units."""
+
+from .layers import (
+    LMUCell,
+    LMUFFT,
+    LMU,
+)
+
+from .version import version as __version__
+
+__copyright__ = "2019-2020, Applied Brain Research"
+__license__ = "Free for non-commercial use; see LICENSE.rst"
