e9c8c1c425539e4fd2711328321c47f87c051d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:47 +0000

    Add package python-apitist/0.5.2/python-apitist-0.5.2.tar.gz

---
+++ packages/python-apitist/python-apitist-0.5.2.tar.gz/python-apitist-0.5.2/src/apitist/hooks.py
@@ -0,0 +1,97 @@
+import types
+import warnings
+from typing import Type
+
+import attr
+from requests import PreparedRequest, Request, Response
+
+from apitist.utils import is_attrs_class
+
+from .constructor import converter
+from .logging import Logging
+from .requests import PreparedRequestHook, RequestHook, ResponseHook
+
+warnings.warn("Please use apitist (https://pypi.org/project/apitist) package now", DeprecationWarning)
+
+
+class RequestDebugLoggingHook(RequestHook):
+    formatter = "Request {req.method} {req.url} {req.data}"
+
+    def run(self, request: Request) -> Request:
+        Logging.logger.debug(self.formatter.format(req=request))
+        return request
+
+
+class RequestInfoLoggingHook(RequestHook):
+    formatter = "Request {req.method} {req.url} {req.data}"
+
+    def run(self, request: Request) -> Request:
+        Logging.logger.info(self.formatter.format(req=request))
+        return request
+
+
+class PrepRequestDebugLoggingHook(PreparedRequestHook):
+    formatter = "Request {req.method} {req.url} {req.body}"
+
+    def run(self, request: PreparedRequest) -> PreparedRequest:
+        Logging.logger.debug(self.formatter.format(req=request))
+        return request
+
+
+class PrepRequestInfoLoggingHook(PreparedRequestHook):
+    formatter = "Request {req.method} {req.url} {req.body}"
+
+    def run(self, request: PreparedRequest) -> PreparedRequest:
+
+        Logging.logger.info(self.formatter.format(req=request))
+        return request
+
+
+class ResponseDebugLoggingHook(ResponseHook):
+    formatter = (
+        "Response {res.status_code} {res.request.method} "
+        "{res.url} {res.content}"
+    )
+
+    def run(self, response: Response) -> Response:
+        Logging.logger.debug(self.formatter.format(res=response))
+        return response
+
+
+class ResponseInfoLoggingHook(ResponseHook):
+    formatter = (
+        "Response {res.status_code} {res.request.method} "
+        "{res.url} {res.content}"
+    )
+
+    def run(self, response: Response) -> Response:
+        Logging.logger.info(self.formatter.format(res=response))
+        return response
+
+
+class RequestConverterHook(RequestHook):
+    def run(self, request: Request) -> Request:
+        if is_attrs_class(request.data):
+            request.json = converter.unstructure(request.data)
+            request.data = None
+        return request
+
+
+class ResponseConverterHook(ResponseHook):
+    def run(self, response: Response) -> Response:
+        def func(self, t: Type) -> Response:
+            try:
+                self.data = converter.structure(self.json(), t)
+            except TypeError as e:
+                fields = attr.fields_dict(t)
+                raise TypeError(
+                    f"Got miss-matched parameters in dicts. "
+                    f"Info about first level:"
+                    f"\n\tExpect: {sorted(fields)}"
+                    f"\n\tActual: {sorted(dict(self.json()).keys())}"
+                    f"\n\nOriginal exception: {e}"
+                )
+            return self
+
+        response.structure = types.MethodType(func, response)
+        return response
---
+++ packages/python-apitist/python-apitist-0.5.2.tar.gz/python-apitist-0.5.2/tests/test_random.py
@@ -0,0 +1,263 @@
+import logging
+import typing
+
+import pytest
+
+import attr
+
+from apitist.constructor import converter
+
+
+class TypeStr(str):
+    pass
+
+
+@attr.s
+class Child:
+    value1: int = attr.ib()
+    value2: typing.List[str] = attr.ib()
+    value3: typing.Tuple[str] = attr.ib(default=tuple())
+    value4: str = attr.ib(default=None)
+
+
+@attr.s
+class Data:
+    val1: str = attr.ib()
+    val2: Child = attr.ib()
+    val3: str = attr.ib(default=None)
+
+
+class TestRandomer:
+    def test_add_type(self, randomer):
+        def func():
+            return "test"
+
+        randomer.add_type(str, func)
+        assert randomer.available_hooks == {str: func}
+
+    def test_add_type_exists(self, randomer, capture):
+        def func():
+            return "test"
+
+        randomer.add_type(str, func)
+        randomer.add_type(str, func)
+        assert capture.records[0].levelno == logging.WARN
+
+    def test_add_types(self, randomer):
+        def func():
+            return "test"
+
+        randomer.add_types({str: func})
+        assert randomer.available_hooks == {str: func}
+
+    def test_get_hook_exists(self, random):
+        assert random.get_hook(str)
+
+    def test_get_hook_not_exists(self, random):
+        with pytest.raises(TypeError):
+            random.get_hook(list)
+
+    def test_run_hook_exists(self, random):
+        assert random.run_hook(str)
+
+    def test_run_hook_not_exists(self, random):
+        with pytest.raises(TypeError):
+            random.run_hook(list)
+
+    def test_run_hook_one_subclass(self, randomer):
+        def func():
+            return "test"
+
+        def func2():
+            return "test2"
+
+        randomer.add_type(str, func)
+        randomer.add_type(TypeStr, func2)
+        assert randomer.run_hook(str) == "test"
+        assert randomer.run_hook(TypeStr) == "test2"
+
+    @pytest.mark.parametrize(
+        "key,value",
+        [
+            ("str", "str"),
+            ("int", 10),
+            ("list", ["some", "data"]),
+            ("dict", {"key": 100, "val": {10, 12, 13}}),
+        ],
+    )
+    def test_run_hook_kwargs_pass(self, randomer, key, value):
+        def func(**kwargs):
+            if kwargs.get(key):
+                return kwargs.get(key)
+            return "test"
+
+        randomer.add_type(str, func)
+        assert randomer.run_hook(str) == "test"
+        assert randomer.run_hook(str, **{key: value}) == value
+
+    @pytest.mark.parametrize(
+        "type_,object_",
+        [
+            (Child, Child(50, ["test"], ("test",), "test")),
+            (
+                Data,
+                Data("test", Child(50, ["test"], ("test",), "test"), "test"),
+            ),
+        ],
+    )
+    def test_random_object(self, random, type_, object_):
+        assert random.object(type_) == object_
+
+    def test_random_object_missing_type(self, random):
+        assert random.object(dict) is None
+
+    @pytest.mark.parametrize(
+        "type_,object_",
+        [
+            (Child, Child(50, ["test"], tuple(), None)),
+            (Data, Data("test", Child(50, ["test"], tuple(), None), None)),
+        ],
+    )
+    def test_random_object_required_only(self, random, type_, object_):
+        assert random.object(type_, required_only=True) == object_
+
+    @pytest.mark.parametrize(
+        "type_,ignore,object_",
+        [
+            (Child, ["value1"], Child(None, ["test"], ("test",), "test")),
+            (
+                Data,
+                ["value1", "val1"],
+                Data(None, Child(None, ["test"], ("test",), "test"), "test"),
+            ),
+            (Data, ["value1", "val2"], Data("test", None, "test")),
+        ],
+    )
+    def test_random_object_ignore(self, random, type_, ignore, object_):
+        assert random.object(type_, ignore=ignore) == object_
+
+    @pytest.mark.parametrize(
+        "type_,ignore,object_",
+        [
+            (Child, ["value1"], Child(50, None, tuple(), None)),
+            (Data, ["value1", "val1"], Data("test", None, None)),
+            (
+                Data,
+                ["value1", "val2"],
+                Data(None, Child(50, None, tuple(), None), None),
+            ),
+        ],
+    )
+    def test_random_object_ignore_inverse(
+        self, random, type_, ignore, object_
+    ):
+        assert random.object(type_, ignore=ignore, inverse=True) == object_
+
+    @pytest.mark.parametrize(
+        "type_,ignore,object_",
+        [
+            (Child, ["value1"], Child(50, None, tuple(), None)),
+            (Data, ["value1", "val1"], Data("test", None, None)),
+            (
+                Data,
+                ["value1", "val2"],
+                Data(None, Child(50, None, tuple(), None), None),
+            ),
+        ],
+    )
+    def test_random_object_only(self, random, type_, ignore, object_):
+        assert random.object(type_, only=ignore) == object_
+
+    def test_random_object_only_ignore(self, random):
+        with pytest.raises(ValueError):
+            random.object(Data, only=["value1"], ignore=["value2"])
+
+    @pytest.mark.parametrize(
+        "type_,object_,params",
+        [
+            (Child, Child(560, ["test"], ("test",), "test"), {"value1": 560}),
+            (
+                Data,
+                Data("rest", Child(560, ["test"], ("test",), "test"), "test"),
+                {"value1": 560, "val1": "rest"},
+            ),
+        ],
+    )
+    def test_random_object_set_params(self, random, type_, object_, params):
+        assert random.object(type_, **params) == object_
+
+    @pytest.mark.parametrize(
+        "type_,use,exp_data",
+        [
+            (Child, ["value1"], {"value1": 50}),
+            (Child, ["value2"], {"value2": ["test"]}),
+            (Child, ["value3"], {"value3": ("test",)}),
+            (Child, ["value4"], {"value4": "test"}),
+            (Data, ["val1"], {"val1": "test"}),
+            (
+                Data,
+                ["val1", "value2"],
+                {"val1": "test", "val2": {"value2": ["test"]}},
+            ),
+        ],
+    )
+    def test_random_partial(self, random, type_, use, exp_data):
+        data = random.partial(type_, use=use)
+        assert converter.unstructure(data) == exp_data
+
+    def test_random_partial_missing_type(self, random):
+        assert random.partial(dict) is None
+
+    @pytest.mark.parametrize(
+        "type_,use,params,exp_data",
+        [
+            (
+                Child,
+                ["value1"],
+                {"value2": "rest"},
+                {"value1": 50, "value2": "rest"},
+            ),
+            (
+                Data,
+                ["val1"],
+                {"value2": "rest"},
+                {"val1": "test", "val2": {"value2": "rest"}},
+            ),
+        ],
+    )
+    def test_random_partial_set_param(
+        self, random, type_, use, params, exp_data
+    ):
+        data = random.partial(type_, use=use, **params)
+        assert converter.unstructure(data) == exp_data
+
+    @pytest.mark.parametrize(
+        "use,exp_data",
+        [
+            (["value4"], {"value4": "test"}),
+            (
+                ["value3", "value6"],
+                {"value5": {"value3": {"data": 120}}, "value6": {"data": 120}},
+            ),
+        ],
+    )
+    def test_random_partial_attrs_custom_hook(self, random, use, exp_data):
+        @attr.s
+        class Child2:
+            value1: int = attr.ib()
+
+        @attr.s
+        class Child1:
+            value2: int = attr.ib()
+            value3: Child2 = attr.ib()
+
+        @attr.s
+        class Parent:
+            value4: str = attr.ib()
+            value5: Child1 = attr.ib()
+            value6: Child2 = attr.ib()
+
+        random.add_type(Child2, lambda: {"data": 120})
+
+        data = random.partial(Parent, use=use)
+        assert converter.unstructure(data) == exp_data
