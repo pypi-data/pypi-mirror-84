b048ee0b4ebc36e4cae26b68bf73fc1be1f48332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:09 +0000

    Add package kg-kubestatemetrics/0.7.3/kg_kubestatemetrics-0.7.3.tar.gz

---
+++ packages/kg-kubestatemetrics/kg_kubestatemetrics-0.7.3.tar.gz/kg_kubestatemetrics-0.7.3/kg_kubestatemetrics/__init__.py
@@ -0,0 +1,13 @@
+from .builder import (
+    KubeStateMetricsBuilder
+)
+from .option import (
+    KubeStateMetricsOptions
+)
+
+__version__ = "0.7.3"
+
+__all__ = [
+    'KubeStateMetricsBuilder',
+    'KubeStateMetricsOptions',
+]
