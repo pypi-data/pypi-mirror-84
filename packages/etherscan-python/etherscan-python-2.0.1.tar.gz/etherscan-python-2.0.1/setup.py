dfa74eb5232404db8d74b4ac434d01f0efaf8e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:44 +0000

    Add package etherscan-python/2.0.1/etherscan-python-2.0.1.tar.gz

---
+++ packages/etherscan-python/etherscan-python-2.0.1.tar.gz/etherscan-python-2.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="etherscan-python",
+    version="2.0.1",
+    description="A minimal, yet complete, python API for etherscan.io.",
+    url="https://github.com/pcko1/etherscan-python",
+    author="Panagiotis-Christos Kotsias",
+    author_email="kotsias.pan@gmail.com",
+    license="MIT",
+    packages=[
+        "etherscan",
+        "etherscan.enums",
+        "etherscan.modules",
+        "etherscan.utils",
+    ],
+    install_requires=["requests", "coverage"],
+    include_package_data=True,
+    zip_safe=False,
+)
