9a3587efd153c7c76a5f3f2befff2b22a946a18a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:09 +0000

    Add package dnaStreaming/2.0.1/dnaStreaming-2.0.1.tar.gz

---
+++ packages/dnaStreaming/dnaStreaming-2.0.1.tar.gz/dnaStreaming-2.0.1/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='dnaStreaming',
+    version='2.0.1',
+    description='Dow Jones DNA Streaming Project',
+    author='Chris Flesche',
+    author_email='chris.flesche@dowjones.com',
+    url='https://github.com/dowjones/dj-dna-streams-python',
+    download_url='https://github.com/dowjones/dj-dna-streams-python/archive/release-2.0.0.tar.gz',
+    keywords=['DOWJONES', 'FACTIVA', 'STREAMS'],
+    packages=find_packages(exclude='tests'),
+
+    # metadata for upload to PyPI
+    license="MIT",
+
+    include_package_data=True,
+
+    install_requires=[
+        'googleapis-common-protos>=1.6.0',
+        'google-auth>=1.7.0',
+        'google-cloud-pubsub==1.7.0',
+        'google-cloud-core==1.3.0',
+        'google-api-core==1.22.4',
+        'mock>=3.0.5',
+        'oauth2client>=4.1.3',
+        'requests>=2.22.0'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
