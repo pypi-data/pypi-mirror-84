4bce05fbf20bb9ec0d2c10e8b56d177dd93402a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:18 +0000

    Add package core-exporters-app/1.12.0/core_exporters_app-1.12.0.tar.gz

---
+++ packages/core-exporters-app/core_exporters_app-1.12.0.tar.gz/core_exporters_app-1.12.0/core_exporters_app/views/admin/forms.py
@@ -0,0 +1,48 @@
+""" Forms admin exporter
+"""
+from django import forms
+
+from core_exporters_app.components.exporter.models import Exporter
+from core_main_app.components.template import api as template_api
+from django_mongoengine.forms import DocumentForm
+
+
+class EditExporterForm(DocumentForm):
+    name = forms.CharField(
+        label="Name",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Type the new name"}
+        ),
+    )
+
+    class Meta(object):
+        document = Exporter
+        fields = ["name"]
+
+
+class AssociatedTemplatesForm(forms.Form):
+    """Associated Template form"""
+
+    id = forms.CharField(widget=forms.HiddenInput(), required=False)
+    templates_manager = forms.MultipleChoiceField(
+        label="", widget=forms.SelectMultiple(), required=False
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(AssociatedTemplatesForm, self).__init__(*args, **kwargs)
+        self.fields["templates_manager"].choices = _get_templates_versions()
+
+
+def _get_templates_versions():
+    """Get templates versions.
+
+    Returns:
+        List of templates versions.
+
+    """
+    templates = []
+    # display all template, global and from users
+    template_list = template_api.get_all()
+    for template in template_list:
+        templates.append((template.id, template.display_name))
+    return templates
