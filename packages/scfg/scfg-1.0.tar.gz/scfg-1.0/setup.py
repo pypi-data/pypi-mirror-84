ad1593218821b2adfa0da23d2af8f25a2b78bd2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:24 +0000

    Add package scfg/1.0/scfg-1.0.tar.gz

---
+++ packages/scfg/scfg-1.0.tar.gz/scfg-1.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="scfg",
+    version="1.0",
+    packages=setuptools.find_packages()
+)
---
+++ packages/scfg/scfg-1.0.tar.gz/scfg-1.0/symbolic-control-flow-graphs/test_module.py
@@ -0,0 +1,13 @@
+"""
+Testing file for SCFG construction.
+"""
+
+class Test(object):
+  def method(self, a):
+    a = 10
+    for i in range(a):
+      if i < 5:
+        f()
+      else:
+        g()
+    c = 15
