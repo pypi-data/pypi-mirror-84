b343c405ae788710ce31e566eecf6bffd374a5aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:08 +0000

    Add package ptype/0.2.2/ptype-0.2.2.tar.gz

---
+++ packages/ptype/ptype-0.2.2.tar.gz/ptype-0.2.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+   long_description = fh.read()
+
+setuptools.setup(
+   name='ptype',
+   version='0.2.2',
+   description='Probabilistic type inference',
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   author='Taha Ceritli, Christopher K. I. Williams, James Geddes, Roly Perera',
+   author_email='t.y.ceritli@sms.ed.ac.uk, ckiw@inf.ed.ac.uk, jgeddes@turing.ac.uk, rperera@turing.ac.uk',
+   url='https://github.com/alan-turing-institute/ptype-dmkd',
+   packages=setuptools.find_packages(),
+   classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+   ],
+   install_requires=['numpy', 'scipy', 'scikit-learn', 'matplotlib', 'pandas', 'greenery', 'clevercsv']
+)
