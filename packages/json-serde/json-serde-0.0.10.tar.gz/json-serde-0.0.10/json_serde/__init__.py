e3b8b12f4ef0542fefb7af087246da9325bc0513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:41 +0000

    Add package json-serde/0.0.10/json-serde-0.0.10.tar.gz

---
+++ packages/json-serde/json-serde-0.0.10.tar.gz/json-serde-0.0.10/json_serde/__init__.py
@@ -0,0 +1,37 @@
+"""
+JSON de/serialization library.
+"""
+
+from .serde import (
+    JsonSerde,
+    String,
+    Integer,
+    Float,
+    Boolean,
+    Nested,
+    List,
+    IsoDateTime,
+    Uuid,
+    IsoDate,
+    SerdeError,
+)
+from ._utils import Absent
+
+__version__ = "0.0.10"
+__author__ = "heartsucker"
+
+
+__all__ = [
+    "JsonSerde",
+    "String",
+    "Integer",
+    "Float",
+    "Boolean",
+    "Nested",
+    "List",
+    "IsoDateTime",
+    "Uuid",
+    "IsoDate",
+    "SerdeError",
+    "Absent",
+]
