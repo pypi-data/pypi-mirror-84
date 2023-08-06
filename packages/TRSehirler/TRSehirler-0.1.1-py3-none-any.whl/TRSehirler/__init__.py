d0b2cede172165a1a571c14bb6972579796b5341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:52 +0000

    Add package TRSehirler/0.1.1/TRSehirler-0.1.1-py3-none-any.whl

---
+++ packages/TRSehirler/TRSehirler-0.1.1-py3-none-any.whl/TRSehirler/__init__.py
@@ -0,0 +1,20 @@
+# Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+YAZAR       = 'keyiflerolsun'
+YAZAR_POSTA = 'keyiflerolsun@gmail.com'
+
+PAKET       = 'TRSehirler'
+
+VERSIYON    = '0.1.1'
+
+REPO        = 'https://github.com/keyiflerolsun/TRSehirler'
+ACIKLAMA    = 'Türkiye Cumhuriyeti Devleti Şehirlerini; İl, Plaka, Telefon veya İlçe bilgisinden bulun..'
+ANAHTAR_KLM = ['TRSehirler', 'KekikAkademi', 'keyiflerolsun']
+
+GEREKSINIM  = [
+    "tinydb==4.2.0",
+    "tabulate==0.8.7",
+    "attrdict==2.0.1"
+]
+
+from TRSehirler.Sehirler import Sehir
