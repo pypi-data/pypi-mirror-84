d9a71ad8eba00f51f51d8db4bb5725f3eea4a616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:10 +0000

    Add package romius/0.1/romius-0.1.tar.gz

---
+++ packages/romius/romius-0.1.tar.gz/romius-0.1/romius/__init__.py
@@ -0,0 +1,1 @@
+from .romius import hi
---
+++ packages/romius/romius-0.1.tar.gz/romius-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='romius',
+      version='0.1',
+      description='Romius library',
+      packages=['romius'],
+      author_email='roman.kuksenko1@gmail.com',
+      zip_safe=False)
