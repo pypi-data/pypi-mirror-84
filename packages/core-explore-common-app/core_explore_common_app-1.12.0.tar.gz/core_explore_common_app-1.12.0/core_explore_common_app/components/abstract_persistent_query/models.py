ccd8ddebd37ff1c52395d4bdf20df848ce2feafc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:17 +0000

    Add package core-explore-common-app/1.12.0/core_explore_common_app-1.12.0.tar.gz

---
+++ packages/core-explore-common-app/core_explore_common_app-1.12.0.tar.gz/core_explore_common_app-1.12.0/core_explore_common_app/components/abstract_persistent_query/models.py
@@ -0,0 +1,11 @@
+""" Abstract Persistent Query model
+"""
+from core_explore_common_app.components.abstract_query.models import AbstractQuery
+
+
+class AbstractPersistentQuery(AbstractQuery):
+    """Abstract Persistent Query"""
+
+    meta = {
+        "abstract": True,
+    }
