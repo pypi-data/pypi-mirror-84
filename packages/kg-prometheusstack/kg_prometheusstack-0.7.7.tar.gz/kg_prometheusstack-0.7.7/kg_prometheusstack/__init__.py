8f1953cf120ffaca15f4673b960ac3eb0a93ec5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:09 +0000

    Add package kg-prometheusstack/0.7.7/kg_prometheusstack-0.7.7.tar.gz

---
+++ packages/kg-prometheusstack/kg_prometheusstack-0.7.7.tar.gz/kg_prometheusstack-0.7.7/kg_prometheusstack/__init__.py
@@ -0,0 +1,13 @@
+from .builder import (
+    PrometheusStackBuilder
+)
+from .option import (
+    PrometheusStackOptions
+)
+
+__version__ = "0.7.7"
+
+__all__ = [
+    'PrometheusStackBuilder',
+    'PrometheusStackOptions',
+]
