79d855eda571dee5e1a45e777cff782d8b2a187c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:54 +0000

    Add package deephyper/0.1.13/deephyper-0.1.13.tar.gz

---
+++ packages/deephyper/deephyper-0.1.13.tar.gz/deephyper-0.1.13/deephyper/search/nas/__init__.py
@@ -0,0 +1,31 @@
+from deephyper.core.parser import add_arguments_from_signature
+from deephyper.search import Search
+
+__all__ = [
+    "ambs",
+    "random",
+    "ppo",
+    "regevo",
+    "rl",
+    "baselines",
+    "env",
+    "model",
+    "optimizer",
+]
+
+
+class NeuralArchitectureSearch(Search):
+    def __init__(
+        self,
+        problem,
+        run="deephyper.search.nas.model.run.alpha.run",
+        evaluator="ray",
+        **kwargs
+    ):
+
+        super().__init__(problem, run=run, evaluator=evaluator, **kwargs)
+
+    @staticmethod
+    def _extend_parser(parser):
+        add_arguments_from_signature(parser, NeuralArchitectureSearch)
+        return parser
