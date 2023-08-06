be64dd68ef6204b9a31daea1c433b65549db1f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:18 +0000

    Add package wagtailmedia/0.7.0/wagtailmedia-0.7.0-py2.py3-none-any.whl

---
+++ packages/wagtailmedia/wagtailmedia-0.7.0-py2.py3-none-any.whl/wagtailmedia/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class WagtailMediaAppConfig(AppConfig):
+    name = 'wagtailmedia'
+    label = 'wagtailmedia'
+    verbose_name = "Wagtail media"
+
+    def ready(self):
+        from wagtailmedia.signal_handlers import register_signal_handlers
+        register_signal_handlers()
