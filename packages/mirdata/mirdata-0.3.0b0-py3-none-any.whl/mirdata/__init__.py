e990b1d27ac745758791ba4f3193a9e87f2c5e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:07 +0000

    Add package mirdata/0.3.0b0/mirdata-0.3.0b0-py3-none-any.whl

---
+++ packages/mirdata/mirdata-0.3.0b0-py3-none-any.whl/mirdata/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+
+from .version import version as __version__
+
+DATASETS = [
+    "beatles",
+    "beatport_key",
+    "dali",
+    "giantsteps_key",
+    "giantsteps_tempo",
+    "groove_midi",
+    "gtzan_genre",
+    "guitarset",
+    "ikala",
+    "maestro",
+    "medley_solos_db",
+    "medleydb_melody",
+    "medleydb_pitch",
+    "mridangam_stroke",
+    "orchset",
+    "rwc_classical",
+    "rwc_jazz",
+    "rwc_popular",
+    "salami",
+    "tinysol",
+]
+
+from .core import Dataset
