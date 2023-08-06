a0bb06792136936e0115949ec52ccdfcec2b182b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:03 +0000

    Add package mmh3cffi/0.2.0/mmh3cffi-0.2.0.tar.gz

---
+++ packages/mmh3cffi/mmh3cffi-0.2.0.tar.gz/mmh3cffi-0.2.0/build_mmh3cffi.py
@@ -0,0 +1,18 @@
+"""FFI build script."""
+import os
+from cffi import FFI
+
+
+with open(os.path.join("csrc", "mmh3.h"), 'r') as f: HEADER = f.read()
+
+FFI_BUILDER = FFI()
+FFI_BUILDER.cdef(HEADER)
+FFI_BUILDER.set_source(
+    'mmh3cffi._cimpl',
+    '#include "mmh3.h"',
+    sources=[os.path.join('csrc', 'mmh3.c')],
+    include_dirs=['csrc']
+)
+
+if __name__ == '__main__':
+    FFI_BUILDER.compile()
