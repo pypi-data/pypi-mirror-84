c1589fea77931b980bebb1efaa513222a3198d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:50 +0000

    Add package mod9-asr/0.0.2/mod9-asr-0.0.2.tar.gz

---
+++ packages/mod9-asr/mod9-asr-0.0.2.tar.gz/mod9-asr-0.0.2/mod9/reformat/__init__.py
@@ -0,0 +1,9 @@
+"""
+The internals that power these wrappers.
+
+For example, ``mod9.reformat.google`` converts input and output
+between Mod9 and Google forms and ``mod9.reformat.utils`` handles
+communicating with the Mod9 ASR Engine TCP Server.
+
+Most users will not need to change this code.
+"""
