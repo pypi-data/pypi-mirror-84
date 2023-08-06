3a2a6cbe459e2e1a0e110b9586739f0f8b25109e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:27 +0000

    Add package jgpackage/0.0.2/jgpackage-0.0.2-py3-none-any.whl

---
+++ packages/jgpackage/jgpackage-0.0.2-py3-none-any.whl/samplePackage/__init__.py
@@ -0,0 +1,9 @@
+"""This is a sample module."""
+
+from __future__ import annotations
+
+__all__ = ["format_text"]
+__version__ = "0.0.2"
+__author__ = "Juliana Guamá"
+
+from .python_code import format_text
