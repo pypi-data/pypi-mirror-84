000a38be5121a1571f9d081508666ef2bfc57c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:06 +0000

    Add package infoselect/1.0.0/infoselect-1.0.0.tar.gz

---
+++ packages/infoselect/infoselect-1.0.0.tar.gz/infoselect-1.0.0/setup.py
@@ -0,0 +1,23 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="infoselect",
+    version="1.0.0",
+    author="Felipe Maia Polo & Felipe Leno da Silva",
+    author_email="felipemaiapolo@gmail.com, f.leno@usp.br",
+    description="Mutual Information Based Feature Selection in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/felipemaiapolo/infoselect',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['scipy','numpy','pandas','sklearn','matplotlib'],
+) 
