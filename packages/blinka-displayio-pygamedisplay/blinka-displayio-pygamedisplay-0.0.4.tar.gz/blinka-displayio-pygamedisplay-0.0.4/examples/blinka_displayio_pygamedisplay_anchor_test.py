e24fa7f81716e0def2708ee964365328f54fdff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:38 +0000

    Add package blinka-displayio-pygamedisplay/0.0.4/blinka-displayio-pygamedisplay-0.0.4.tar.gz

---
+++ packages/blinka-displayio-pygamedisplay/blinka-displayio-pygamedisplay-0.0.4.tar.gz/blinka-displayio-pygamedisplay-0.0.4/examples/blinka_displayio_pygamedisplay_anchor_test.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: 2020 Tim C, written for Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
+"""
+Testing anchor point of adafruit_display_text label.
+"""
+import terminalio
+import displayio
+from adafruit_display_text import label
+from blinka_displayio_pygamedisplay import PyGameDisplay
+
+display = PyGameDisplay(width=1770, height=920)
+
+text_area = label.Label(terminalio.FONT, text="Hello world", scale=3)
+
+text_area.anchor_point = (0.5, 0.5)
+text_area.anchored_position = (display.width // 2, display.height // 2)
+print(text_area.bounding_box)
+print(f"{text_area.x}, {text_area.y}")
+main_group = displayio.Group()
+main_group.append(text_area)
+display.show(main_group)
+
+# text_area.y = 37
+while display.running:
+    pass
---
+++ packages/blinka-displayio-pygamedisplay/blinka-displayio-pygamedisplay-0.0.4.tar.gz/blinka-displayio-pygamedisplay-0.0.4/examples/blinka_displayio_pygamedisplay_imageload_bmp_test.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2020 Tim C, written for Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
+"""
+Use adafruit_imageload to show a bitmap on the screen
+"""
+import displayio
+import adafruit_imageload
+from blinka_displayio_pygamedisplay import PyGameDisplay
+
+display = PyGameDisplay(icon="blinka.png", width=800, height=600)
+
+bitmap, palette = adafruit_imageload.load(
+    "robot_friend.bmp", bitmap=displayio.Bitmap, palette=displayio.Palette
+)
+
+# Create a TileGrid to hold the bitmap
+tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
+
+# Create a Group to hold the TileGrid
+img_group = displayio.Group()
+
+# Add the TileGrid to the Group
+img_group.append(tile_grid)
+
+# Add the Group to the Display
+display.show(img_group)
+
+# Loop forever so you can enjoy your image
+while display.running:
+    pass
