7678afbb4666fcd384f5f3e25f121d0f13c22feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:25 +0000

    Add package ecommerce-extensions/0.1.0/ecommerce_extensions-0.1.0-py3-none-any.whl

---
+++ packages/ecommerce-extensions/ecommerce_extensions-0.1.0-py3-none-any.whl/ecommerce_extensions/settings/common.py
@@ -0,0 +1,41 @@
+"""
+Common Django settings for ecommerce_extensions project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.11/ref/settings/
+"""
+
+from __future__ import unicode_literals
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'secret-key'
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.sites',
+    'ecommerce_extensions.tenant.apps.TenantConfig',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.11/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_TZ = True
