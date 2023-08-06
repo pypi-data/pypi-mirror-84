f3cf6386b64ff21cf6fc562f9b95fc9244e010c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:43 +0000

    Add package azure-servicebus/7.0.0b8/azure_servicebus-7.0.0b8-py2.py3-none-any.whl

---
+++ packages/azure-servicebus/azure_servicebus-7.0.0b8-py2.py3-none-any.whl/azure/servicebus/aio/_base_handler_async.py
@@ -0,0 +1,275 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+import logging
+import asyncio
+import uuid
+import time
+from typing import TYPE_CHECKING, Any, Callable, Optional, Dict
+
+import uamqp
+from uamqp import compat
+from uamqp.message import MessageProperties
+
+from azure.core.credentials import AccessToken
+
+from .._base_handler import _generate_sas_token, BaseHandler as BaseHandlerSync
+from .._common._configuration import Configuration
+from .._common.utils import create_properties
+from .._common.constants import (
+    TOKEN_TYPE_SASTOKEN,
+    MGMT_REQUEST_OP_TYPE_ENTITY_MGMT,
+    ASSOCIATEDLINKPROPERTYNAME,
+    CONTAINER_PREFIX, MANAGEMENT_PATH_SUFFIX)
+from ..exceptions import (
+    ServiceBusError,
+    OperationTimeoutError,
+    _create_servicebus_exception
+)
+
+if TYPE_CHECKING:
+    from azure.core.credentials import TokenCredential
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class ServiceBusSASTokenCredential(object):
+    """The shared access token credential used for authentication.
+    :param str token: The shared access token string
+    :param int expiry: The epoch timestamp
+    """
+    def __init__(self, token: str, expiry: int) -> None:
+        """
+        :param str token: The shared access token string
+        :param int expiry: The epoch timestamp
+        """
+        self.token = token
+        self.expiry = expiry
+        self.token_type = b"servicebus.windows.net:sastoken"
+
+    async def get_token(self, *scopes: str, **kwargs: Any) -> AccessToken:  # pylint:disable=unused-argument
+        """
+        This method is automatically called when token is about to expire.
+        """
+        return AccessToken(self.token, self.expiry)
+
+
+class ServiceBusSharedKeyCredential(object):
+    """The shared access key credential used for authentication.
+
+    :param str policy: The name of the shared access policy.
+    :param str key: The shared access key.
+    """
+
+    def __init__(self, policy: str, key: str) -> None:
+        self.policy = policy
+        self.key = key
+        self.token_type = TOKEN_TYPE_SASTOKEN
+
+    async def get_token(self, *scopes: str, **kwargs: Any) -> AccessToken:  # pylint:disable=unused-argument
+        if not scopes:
+            raise ValueError("No token scope provided.")
+        return _generate_sas_token(scopes[0], self.policy, self.key)
+
+
+class BaseHandler:
+    def __init__(
+        self,
+        fully_qualified_namespace: str,
+        entity_name: str,
+        credential: "TokenCredential",
+        **kwargs: Any
+    ) -> None:
+        self.fully_qualified_namespace = fully_qualified_namespace
+        self._entity_name = entity_name
+
+        subscription_name = kwargs.get("subscription_name")
+        self._mgmt_target = self._entity_name + (("/Subscriptions/" + subscription_name) if subscription_name else '')
+        self._mgmt_target = "{}{}".format(self._mgmt_target, MANAGEMENT_PATH_SUFFIX)
+        self._credential = credential
+        self._container_id = CONTAINER_PREFIX + str(uuid.uuid4())[:8]
+        self._config = Configuration(**kwargs)
+        self._running = False
+        self._handler = None  # type: uamqp.AMQPClientAsync
+        self._auth_uri = None
+        self._properties = create_properties(self._config.user_agent)
+
+    @classmethod
+    def _convert_connection_string_to_kwargs(cls, conn_str, **kwargs):
+        # pylint:disable=protected-access
+        return BaseHandlerSync._convert_connection_string_to_kwargs(conn_str, **kwargs)
+
+    @classmethod
+    def _create_credential_from_connection_string_parameters(cls, token, token_expiry, policy, key):
+        if token and token_expiry:
+            return ServiceBusSASTokenCredential(token, token_expiry)
+        return ServiceBusSharedKeyCredential(policy, key)
+
+    async def __aenter__(self):
+        await self._open_with_retry()
+        return self
+
+    async def __aexit__(self, *args):
+        await self.close()
+
+    async def _handle_exception(self, exception, **kwargs):
+        error, error_need_close_handler, error_need_raise = \
+            _create_servicebus_exception(_LOGGER, exception, self, **kwargs)
+        if error_need_close_handler:
+            await self._close_handler()
+        if error_need_raise:
+            raise error
+
+        return error
+
+    async def _do_retryable_operation(self, operation, timeout=None, **kwargs):
+        # type: (Callable, Optional[float], Any) -> Any
+        # pylint: disable=protected-access
+        require_last_exception = kwargs.pop("require_last_exception", False)
+        operation_requires_timeout = kwargs.pop("operation_requires_timeout", False)
+        retried_times = 0
+        max_retries = self._config.retry_total
+
+        abs_timeout_time = (time.time() + timeout) if (operation_requires_timeout and timeout) else None
+
+        while retried_times <= max_retries:
+            try:
+                if operation_requires_timeout and abs_timeout_time:
+                    remaining_timeout = abs_timeout_time - time.time()
+                    kwargs["timeout"] = remaining_timeout
+                return await operation(**kwargs)
+            except StopAsyncIteration:
+                raise
+            except Exception as exception:  # pylint: disable=broad-except
+                last_exception = await self._handle_exception(exception, **kwargs)
+                if require_last_exception:
+                    kwargs["last_exception"] = last_exception
+                retried_times += 1
+                if retried_times > max_retries:
+                    _LOGGER.info(
+                        "%r operation has exhausted retry. Last exception: %r.",
+                        self._container_id,
+                        last_exception,
+                    )
+                    raise last_exception
+                await self._backoff(
+                    retried_times=retried_times,
+                    last_exception=last_exception,
+                    abs_timeout_time=abs_timeout_time
+                )
+
+    async def _backoff(
+            self,
+            retried_times,
+            last_exception,
+            abs_timeout_time=None,
+            entity_name=None
+    ):
+        entity_name = entity_name or self._container_id
+        backoff = self._config.retry_backoff_factor * 2 ** retried_times
+        if backoff <= self._config.retry_backoff_max and (
+                abs_timeout_time is None or (backoff + time.time()) <= abs_timeout_time
+        ):
+            await asyncio.sleep(backoff)
+            _LOGGER.info(
+                "%r has an exception (%r). Retrying...",
+                entity_name,
+                last_exception,
+            )
+        else:
+            _LOGGER.info(
+                "%r operation has timed out. Last exception before timeout is (%r)",
+                entity_name,
+                last_exception,
+            )
+            raise last_exception
+
+    async def _mgmt_request_response(
+        self,
+        mgmt_operation,
+        message,
+        callback,
+        keep_alive_associated_link=True,
+        timeout=None,
+        **kwargs
+    ):
+        # type: (bytes, uamqp.Message, Callable, bool, Optional[float], Any) -> uamqp.Message
+        """
+        Execute an amqp management operation.
+
+        :param bytes mgmt_operation: The type of operation to be performed. This value will
+         be service-specific, but common values include READ, CREATE and UPDATE.
+         This value will be added as an application property on the message.
+        :param message: The message to send in the management request.
+        :paramtype message: ~uamqp.message.Message
+        :param callback: The callback which is used to parse the returning message.
+        :paramtype callback: Callable[int, ~uamqp.message.Message, str]
+        :param keep_alive_associated_link: A boolean flag for keeping associated amqp sender/receiver link alive when
+         executing operation on mgmt links.
+        :param timeout: timeout in seconds for executing the mgmt operation.
+        :rtype: None
+        """
+        await self._open()
+
+        application_properties = {}
+        # Some mgmt calls do not support an associated link name (such as list_sessions).  Most do, so on by default.
+        if keep_alive_associated_link:
+            try:
+                application_properties = {ASSOCIATEDLINKPROPERTYNAME:self._handler.message_handler.name}
+            except AttributeError:
+                pass
+
+        mgmt_msg = uamqp.Message(
+            body=message,
+            properties=MessageProperties(
+                reply_to=self._mgmt_target,
+                encoding=self._config.encoding,
+                **kwargs),
+            application_properties=application_properties)
+        try:
+            return await self._handler.mgmt_request_async(
+                mgmt_msg,
+                mgmt_operation,
+                op_type=MGMT_REQUEST_OP_TYPE_ENTITY_MGMT,
+                node=self._mgmt_target.encode(self._config.encoding),
+                timeout=timeout * 1000 if timeout else None,
+                callback=callback)
+        except Exception as exp:  # pylint: disable=broad-except
+            if isinstance(exp, compat.TimeoutException):
+                raise OperationTimeoutError("Management operation timed out.", error=exp)
+            raise
+
+    async def _mgmt_request_response_with_retry(self, mgmt_operation, message, callback, timeout=None, **kwargs):
+        # type: (bytes, Dict[str, Any], Callable, Optional[float], Any) -> Any
+        return await self._do_retryable_operation(
+            self._mgmt_request_response,
+            mgmt_operation=mgmt_operation,
+            message=message,
+            callback=callback,
+            timeout=timeout,
+            operation_requires_timeout=True,
+            **kwargs
+        )
+
+    async def _open(self):  # pylint: disable=no-self-use
+        raise ValueError("Subclass should override the method.")
+
+    async def _open_with_retry(self):
+        return await self._do_retryable_operation(self._open)
+
+    async def _close_handler(self):
+        if self._handler:
+            await self._handler.close_async()
+            self._handler = None
+        self._running = False
+
+    async def close(self) -> None:
+        """Close down the handler connection.
+
+        If the handler has already closed, this operation will do nothing. An optional exception can be passed in to
+        indicate that the handler was shutdown due to error.
+
+        :rtype: None
+        """
+        await self._close_handler()
---
+++ packages/azure-servicebus/azure_servicebus-7.0.0b8-py2.py3-none-any.whl/azure/servicebus/exceptions.py
@@ -0,0 +1,334 @@
+# ------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+
+from typing import Optional
+
+from uamqp import errors, constants
+from azure.core.exceptions import AzureError
+
+from ._common.constants import SESSION_LOCK_LOST, SESSION_LOCK_TIMEOUT
+
+
+_NO_RETRY_ERRORS = (
+    constants.ErrorCodes.DecodeError,
+    constants.ErrorCodes.LinkMessageSizeExceeded,
+    constants.ErrorCodes.NotFound,
+    constants.ErrorCodes.NotImplemented,
+    constants.ErrorCodes.LinkRedirect,
+    constants.ErrorCodes.NotAllowed,
+    constants.ErrorCodes.UnauthorizedAccess,
+    constants.ErrorCodes.LinkStolen,
+    constants.ErrorCodes.ResourceLimitExceeded,
+    constants.ErrorCodes.ConnectionRedirect,
+    constants.ErrorCodes.PreconditionFailed,
+    constants.ErrorCodes.InvalidField,
+    constants.ErrorCodes.ResourceDeleted,
+    constants.ErrorCodes.IllegalState,
+    constants.ErrorCodes.FrameSizeTooSmall,
+    constants.ErrorCodes.ConnectionFramingError,
+    constants.ErrorCodes.SessionUnattachedHandle,
+    constants.ErrorCodes.SessionHandleInUse,
+    constants.ErrorCodes.SessionErrantLink,
+    constants.ErrorCodes.SessionWindowViolation,
+    b"com.microsoft:argument-out-of-range",
+    b"com.microsoft:entity-disabled",
+    b"com.microsoft:auth-failed",
+    b"com.microsoft:precondition-failed",
+    b"com.microsoft:argument-error")
+
+
+_AMQP_SESSION_ERROR_CONDITIONS = (
+    SESSION_LOCK_LOST,
+    SESSION_LOCK_TIMEOUT
+)
+
+
+_AMQP_CONNECTION_ERRORS = (
+    errors.LinkDetach,
+    errors.ConnectionClose,
+    errors.MessageHandlerError,
+    errors.AMQPConnectionError
+)
+
+
+_AMQP_MESSAGE_ERRORS = (
+    errors.MessageAlreadySettled,
+    errors.MessageContentTooLarge,
+    errors.MessageException
+)
+
+
+def _error_handler(error):
+    """Handle connection and service errors.
+
+    Called internally when an event has failed to send so we
+    can parse the error to determine whether we should attempt
+    to retry sending the event again.
+    Returns the action to take according to error type.
+
+    :param error: The error received in the send attempt.
+    :type error: Exception
+    :rtype: ~uamqp.errors.ErrorAction
+    """
+    if error.condition == b'com.microsoft:server-busy':
+        return errors.ErrorAction(retry=True, backoff=4)
+    if error.condition == b'com.microsoft:timeout':
+        return errors.ErrorAction(retry=True, backoff=2)
+    if error.condition == b'com.microsoft:operation-cancelled':
+        return errors.ErrorAction(retry=True)
+    if error.condition == b"com.microsoft:container-close":
+        return errors.ErrorAction(retry=True, backoff=4)
+    if error.condition in _NO_RETRY_ERRORS:
+        return errors.ErrorAction(retry=False)
+    return errors.ErrorAction(retry=True)
+
+
+def _handle_amqp_connection_error(logger, exception, handler):
+    # Handle all exception inherited from uamqp.errors.AMQPConnectionError
+    error_need_close_handler = True
+    error_need_raise = False
+    error = None
+    if isinstance(exception, errors.LinkDetach) and exception.condition in _AMQP_SESSION_ERROR_CONDITIONS:
+        # In session lock lost or no active session case, we don't retry, close the handler and raise the error
+        error_need_raise = True
+        if exception.condition == SESSION_LOCK_LOST:
+            try:
+                session_id = handler._session_id  # pylint: disable=protected-access
+            except AttributeError:
+                session_id = None
+            error = SessionLockExpired("Connection detached - lock on Session {} lost.".format(session_id))
+        elif exception.condition == SESSION_LOCK_TIMEOUT:
+            error = NoActiveSession("Queue has no active session to receive from.")
+    elif isinstance(exception, (errors.LinkDetach, errors.ConnectionClose)):
+        # In other link detach and connection case, should retry
+        logger.info("Handler detached due to exception: (%r).", exception)
+        if exception.condition == constants.ErrorCodes.UnauthorizedAccess:
+            error = ServiceBusAuthorizationError(str(exception), error=exception)
+        elif exception.condition == constants.ErrorCodes.NotAllowed and 'requires sessions' in str(exception):
+            message = str(exception) + '\n\nsession_id must be set when getting a receiver for sessionful entity.'
+            error = ServiceBusConnectionError(message, error=exception)
+        else:
+            error = ServiceBusConnectionError(str(exception), error=exception)
+    elif isinstance(exception, errors.MessageHandlerError):
+        logger.info("Handler error: (%r).", exception)
+        error = ServiceBusConnectionError(str(exception), error=exception)
+    else:
+        # handling general uamqp.errors.AMQPConnectionError
+        logger.info("Failed to open handler: (%r).", exception)
+        message = "Failed to open handler: {}.".format(exception)
+        error = ServiceBusConnectionError(message, error=exception)
+        error_need_raise, error_need_close_handler = True, False
+
+    return error, error_need_close_handler, error_need_raise
+
+
+def _handle_amqp_message_error(logger, exception, **kwargs):
+    # Handle amqp message related errors
+    error_need_close_handler = True
+    error_need_raise = False
+    error = None
+    if isinstance(exception, errors.MessageAlreadySettled):
+        # This one doesn't need retry, should raise the error
+        logger.info("Message already settled (%r)", exception)
+        error = MessageAlreadySettled(kwargs.get("settle_operation", "Unknown operation"))
+        error_need_close_handler = False
+        error_need_raise = True
+    elif isinstance(exception, errors.MessageContentTooLarge) or \
+            (isinstance(exception, errors.MessageException) and
+             exception.condition == constants.ErrorCodes.LinkMessageSizeExceeded):
+        # This one doesn't need retry, should raise the error
+        logger.info("Message content is too large (%r).", exception)
+        error = MessageContentTooLarge("Message content is too large.", error=exception)
+        error_need_close_handler = False
+        error_need_raise = True
+    else:
+        # handling general uamqp.errors.MessageException
+        logger.info("Message send failed (%r)", exception)
+        if exception.condition == constants.ErrorCodes.ClientError and 'timed out' in str(exception):
+            error = OperationTimeoutError("Send operation timed out", error=exception)
+        else:
+            error = MessageSendFailed(error=exception)
+        error_need_raise = False
+
+    return error, error_need_close_handler, error_need_raise
+
+
+def _create_servicebus_exception(logger, exception, handler, **kwargs):  # pylint: disable=too-many-statements
+    # transform amqp exceptions into servicebus exceptions
+    error_need_close_handler = True
+    error_need_raise = False
+    if isinstance(exception, _AMQP_CONNECTION_ERRORS):
+        error, error_need_close_handler, error_need_raise = \
+            _handle_amqp_connection_error(logger, exception, handler)
+    elif isinstance(exception, _AMQP_MESSAGE_ERRORS):
+        error, error_need_close_handler, error_need_raise = \
+            _handle_amqp_message_error(logger, exception, **kwargs)
+    elif isinstance(exception, errors.AuthenticationException):
+        logger.info("Authentication failed due to exception: (%r).", exception)
+        error = ServiceBusAuthenticationError(str(exception), error=exception)
+    else:
+        logger.info("Unexpected error occurred (%r). Shutting down.", exception)
+        if kwargs.get("settle_operation"):
+            error = MessageSettleFailed(kwargs.get("settle_operation"), error=exception)
+        elif not isinstance(exception, ServiceBusError):
+            error = ServiceBusError("Handler failed: {}.".format(exception), error=exception)
+        else:
+            error = exception
+
+    try:
+        err_condition = exception.condition
+        if err_condition in _NO_RETRY_ERRORS:
+            error_need_raise = True
+    except AttributeError:
+        pass
+
+    return error, error_need_close_handler, error_need_raise
+
+
+class _ServiceBusErrorPolicy(errors.ErrorPolicy):
+
+    def __init__(self, max_retries=3, is_session=False):
+        self._is_session = is_session
+        super(_ServiceBusErrorPolicy, self).__init__(max_retries=max_retries, on_error=_error_handler)
+
+    def on_unrecognized_error(self, error):
+        if self._is_session:
+            return errors.ErrorAction(retry=False)
+        return super(_ServiceBusErrorPolicy, self).on_unrecognized_error(error)
+
+    def on_link_error(self, error):
+        if self._is_session:
+            return errors.ErrorAction(retry=False)
+        return super(_ServiceBusErrorPolicy, self).on_link_error(error)
+
+    def on_connection_error(self, error):
+        if self._is_session:
+            return errors.ErrorAction(retry=False)
+        return super(_ServiceBusErrorPolicy, self).on_connection_error(error)
+
+
+class ServiceBusError(AzureError):
+    """Base exception for all Service Bus errors which can be used for default error handling.
+
+    :param str message: The message object stringified as 'message' attribute
+    :keyword error: The original exception if any
+    :paramtype error: Exception
+    :ivar exc_type: The exc_type from sys.exc_info()
+    :ivar exc_value: The exc_value from sys.exc_info()
+    :ivar exc_traceback: The exc_traceback from sys.exc_info()
+    :ivar exc_msg: A string formatting of message parameter, exc_type and exc_value
+    :ivar str message: A stringified version of the message parameter
+    """
+
+
+class ServiceBusConnectionError(ServiceBusError):
+    """An error occurred in the connection."""
+
+
+class ServiceBusAuthorizationError(ServiceBusError):
+    """An error occurred when authorizing the connection."""
+
+
+class ServiceBusAuthenticationError(ServiceBusError):
+    """An error occurred when authenticate the connection."""
+
+
+class NoActiveSession(ServiceBusError):
+    """No active Sessions are available to receive from."""
+
+
+class OperationTimeoutError(ServiceBusError):
+    """Operation timed out."""
+
+
+class ServiceBusMessageError(ServiceBusError):
+    """An error occurred when an operation on a message failed because the message is in an incorrect state."""
+
+
+class MessageContentTooLarge(ServiceBusMessageError, ValueError):
+    """Message content is larger than the service bus frame size."""
+
+
+class MessageAlreadySettled(ServiceBusMessageError):
+    """Failed to settle the message.
+
+    An attempt was made to complete an operation on a message that has already
+    been settled (completed, abandoned, dead-lettered or deferred).
+    This error will also be raised if an attempt is made to settle a message
+    received via ReceiveAndDelete mode.
+
+    :param str action: The settlement operation, there are four types of settlement,
+     `complete/abandon/defer/dead_letter`.
+
+    """
+
+    def __init__(self, action):
+        # type: (str) -> None
+        message = "Unable to {} message as it has already been settled".format(action)
+        super(MessageAlreadySettled, self).__init__(message)
+
+
+class MessageSettleFailed(ServiceBusMessageError):
+    """Attempt to settle a message failed.
+
+    :param str action: The settlement operation, there are four types of settlement,
+     `complete/abandon/defer/dead_letter`.
+    :param error: The original exception if any.
+    :type error: Exception
+
+    """
+    def __init__(self, action, error):
+        # type: (str, Exception) -> None
+        message = "Failed to {} message. Error: {}".format(action, error)
+        super(MessageSettleFailed, self).__init__(message, error=error)
+
+
+class MessageSendFailed(ServiceBusMessageError):
+    """A message failed to send to the Service Bus entity."""
+
+    def __init__(self, error):
+        # type: (Exception) -> None
+        message = "Message failed to send. Error: {}".format(error)
+        self.condition = None
+        self.description = None
+        if hasattr(error, 'condition'):
+            self.condition = error.condition  # type: ignore
+            self.description = error.description  # type: ignore
+        super(MessageSendFailed, self).__init__(message, error=error)
+
+
+class MessageLockExpired(ServiceBusMessageError):
+    """The lock on the message has expired and it has been released back to the queue.
+
+    It will need to be received again in order to settle it.
+
+    """
+
+    def __init__(self, message=None, error=None):
+        # type: (Optional[str], Optional[Exception]) -> None
+        message = message or "Message lock expired"
+        super(MessageLockExpired, self).__init__(message, error=error)
+
+
+class SessionLockExpired(ServiceBusError):
+    """The lock on the session has expired.
+
+    All unsettled messages that have been received can no longer be settled.
+
+    """
+
+    def __init__(self, message=None, error=None):
+        # type: (Optional[str], Optional[Exception]) -> None
+        message = message or "Session lock expired"
+        super(SessionLockExpired, self).__init__(message, error=error)
+
+
+class AutoLockRenewFailed(ServiceBusError):
+    """An attempt to renew a lock on a message or session in the background has failed."""
+
+
+class AutoLockRenewTimeout(ServiceBusError):
+    """The time allocated to renew the message or session lock has elapsed."""
