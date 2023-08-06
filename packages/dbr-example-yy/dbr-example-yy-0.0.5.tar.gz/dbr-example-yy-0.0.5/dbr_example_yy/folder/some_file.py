8e5f0c02777a5b66307577b04516653daae0c6c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:31 +0000

    Add package dbr-example-yy/0.0.5/dbr-example-yy-0.0.5.tar.gz

---
+++ packages/dbr-example-yy/dbr-example-yy-0.0.5.tar.gz/dbr-example-yy-0.0.5/dbr_example_yy/folder/some_file.py
@@ -0,0 +1,9 @@
+def test(**kwargs):
+    spark.conf.set(
+      'fs.azure.account.key.{}.blob.core.windows.net'.format(kwargs['storate_account_name']),
+      dbutils.secrets.get(scope=kwargs['storage_scope'], key=kwargs['storage_key']))
+
+    df = spark.read.parquet('wasbs://{}@{}.blob.core.windows.net/2020-03-03/'.format(kwargs['src_blob_container'], kwargs['storate_account_name']))
+    # df = spark.read.parquet('wasbs://samplecontainer@{}.blob.core.windows.net/2020-03-03/'.format(kwargs['storate_account_name']))
+    df.write.mode('overwrite').parquet('wasbs://{}@{}.blob.core.windows.net/2020-03-03/'.format(kwargs['dst_blob_container'], kwargs['storate_account_name']))
+    display(df)
---
+++ packages/dbr-example-yy/dbr-example-yy-0.0.5.tar.gz/dbr-example-yy-0.0.5/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="dbr-example-yy",
+    version="0.0.5",
+    author="",
+    author_email="",
+    description="",
+    long_description='',
+    long_description_content_type="text/markdown",
+    url="https://github.com/user/dbr-example",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
