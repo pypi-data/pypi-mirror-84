edb7dc7a7e00e2815ad5791f2312571a70afe1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:08 +0000

    Add package rwa-wdm/0.2.1/rwa_wdm-0.2.1.tar.gz

---
+++ packages/rwa-wdm/rwa_wdm-0.2.1.tar.gz/rwa_wdm-0.2.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+description='''Routing and wavelength assignment simulator over WDM networks
+
+More information on https://rwa-wdm.readthedocs.io/en/latest/index.html
+'''
+
+setuptools.setup(
+    name='rwa_wdm',
+    version='0.2.1',
+    license='MIT',
+    author='Cassio Batista',
+    author_email='cassiotb@ufpa.br',
+    description='Routing and wavelength assignment simulator over WDM networks',
+    long_description=description.strip(),
+    url='https://github.com/cassiobatista/rwa-wdm-sim',
+    packages=setuptools.find_packages(),
+    install_requires=['argcomplete', 'networkx', 'numpy', 'matplotlib'],
+    python_requires='>=3.7'
+)
