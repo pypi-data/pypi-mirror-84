52661d94a528e5da1b2f6faa4cb6a8c639f9b350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:17 +0000

    Add package gocardless-pro/1.22.0/gocardless_pro-1.22.0.tar.gz

---
+++ packages/gocardless-pro/gocardless_pro-1.22.0.tar.gz/gocardless_pro-1.22.0/setup.py
@@ -0,0 +1,19 @@
+import codecs
+from setuptools import setup, find_packages
+
+with codecs.open('README.rst', 'r', 'utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name = 'gocardless_pro',
+    version = '1.22.0',
+    packages = find_packages(exclude=['tests']),
+    install_requires = ['requests>=2.6', 'six'],
+    author = 'GoCardless',
+    author_email = 'engineering@gocardless.com',
+    description = 'A client library for the GoCardless Pro API.',
+    long_description = long_description,
+    license = 'MIT',
+    keywords = 'gocardless directdebit payments sepa bacs',
+    url = 'https://github.com/gocardless/gocardless-pro-python',
+)
