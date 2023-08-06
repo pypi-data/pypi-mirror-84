42c54b95564ead2834b763cb69155f1d8adceaba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:54 +0000

    Add package pycaged/0.7.4/pycaged-0.7.4.tar.gz

---
+++ packages/pycaged/pycaged-0.7.4.tar.gz/pycaged-0.7.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+classifiers = ['Operating System :: Microsoft :: Windows :: Windows 10',
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3.8"]
+   
+
+#Calling setup
+setup(
+    name = 'pycaged',
+    version = '0.7.4',
+    description = 'fetches CAGED microdata / busca microdados do CAGED',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url = '',
+    author = 'Heitor Caixeta',
+    author_email = 'heitor.ca.mesquita@gmail.com',
+    license = 'MIT',
+    classifiers = classifiers,
+    keywords = ['caged', 'ibge', 'emprego'],
+    packages = find_packages(),
+    include_packages_data = True,
+    install_requires = ['py7zr','pandas','wget'],
+    zip_safe = False)
+
+
+
