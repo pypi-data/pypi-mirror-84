baa1c5016d70b8f593cb21ede751676edb72070b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:21 +0000

    Add package rcm-hello-py/0.0.3/rcm_hello_py-0.0.3-py3-none-any.whl

---
+++ packages/rcm-hello-py/rcm_hello_py-0.0.3-py3-none-any.whl/src/hello.py
@@ -0,0 +1,5 @@
+#  Copyright (c) 2020 Pim Hazebroek
+#  This program is made available under the terms of the MIT License.
+#  For full details check the LICENSE file at the root of the project.
+
+print('Hello Py')
