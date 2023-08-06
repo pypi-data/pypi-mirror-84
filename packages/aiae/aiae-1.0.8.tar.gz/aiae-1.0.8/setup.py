3c162633f04daeefcd2b5fb60bd656655cc1a0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:04 +0000

    Add package aiae/1.0.8/aiae-1.0.8.tar.gz

---
+++ packages/aiae/aiae-1.0.8.tar.gz/aiae-1.0.8/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aiae']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aihelper>=0.2.11,<0.3.0',
+ 'matplotlib>=3.2.2,<4.0.0',
+ 'oyaml>=0.9,<0.10',
+ 'pandas>=1.0.4,<2.0.0',
+ 'scipy>=1.5.3,<2.0.0',
+ 'seaborn>=0.11.0,<0.12.0',
+ 'xlsxwriter>=1.2.9,<2.0.0']
+
+setup_kwargs = {
+    'name': 'aiae',
+    'version': '1.0.8',
+    'description': '',
+    'long_description': None,
+    'author': 'mjaquier',
+    'author_email': 'mjaquier@me.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
