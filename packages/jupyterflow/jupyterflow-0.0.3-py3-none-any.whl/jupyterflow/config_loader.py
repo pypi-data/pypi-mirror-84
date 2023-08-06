66a1ae521cc092c50a6f226c3eaf36c571f4589c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:18 +0000

    Add package jupyterflow/0.0.3/jupyterflow-0.0.3-py3-none-any.whl

---
+++ packages/jupyterflow/jupyterflow-0.0.3-py3-none-any.whl/jupyterflow/config_loader.py
@@ -0,0 +1,33 @@
+import yaml
+import os
+
+# import pkgutil
+
+
+def load_config():
+    # load user config
+    return _load_user_config()
+
+
+def load_config():
+    home = os.environ['HOME']
+    CONFIG_FILE = os.path.join(home, '.jupyterflow.yaml')
+    
+    if os.path.isfile(CONFIG_FILE):
+        with open(CONFIG_FILE) as f:
+            conf = yaml.safe_load(f)
+            if conf is None:
+                return {}
+            return conf
+    return {}
+
+
+
+# def _load_default_config():
+#     data = pkgutil.get_data(__name__, "templates/default_config.yaml")
+#     return yaml.safe_load(data)
+
+
+# def _merge_config(config, default):
+#     # default.update(config)
+#     return config
