f77dc813fd698fb4442d9326b8f593ecffdee649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:41 +0000

    Add package trusuggest/1.0.5/trusuggest-1.0.5.tar.gz

---
+++ packages/trusuggest/trusuggest-1.0.5.tar.gz/trusuggest-1.0.5/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="trusuggest",
+    version="1.0.5",
+    author="Rocking Rohit",
+    url="https://github.com/rocking-rohit/trusuggest-python",
+    packages=setuptools.find_packages()
+)
