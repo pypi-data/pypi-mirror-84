bafbc009d2faaa3a163438795ba501c3b60d796a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:51 +0000

    Add package iapp/2020.11.5.17.5.51/iapp-2020.11.5.17.5.51-py3-none-any.whl

---
+++ packages/iapp/iapp-2020.11.5.17.5.51-py3-none-any.whl/iapp/app_run.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : tql-App.
+# @File         : app_run
+# @Time         : 2020/11/5 4:48 下午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  :
+"""
+'console_scripts': [
+    'app-run=iapp.app_run.run:run'
+]
+"""
+import os
+import fire
+from pathlib import Path
+from iapp.utils import get_module_path
+
+
+class AppRun(object):
+    """doc"""
+
+    def __init__(self, **kwargs):
+        """
+
+        :param arg:
+        """
+        self.apps_path = Path(get_module_path('../apps', __file__))
+
+    def apps_list(self):
+        return list(self.apps_path.glob('*app.py'))
+
+    def app_run(self, app_name):
+        app_file = list(self.apps_path.glob('*app_name*'))
+        return os.system(f"python {app_file}")
+
+
+def cli():
+    fire.Fire(AppRun)
+
+
+if __name__ == '__main__':
+    print(AppRun().apps_list())
