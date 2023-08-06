c44800cd9a9b84bab08f1e961d478d253da8f9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:11 +0000

    Add package regipy/1.7.1/regipy-1.7.1-py3-none-any.whl

---
+++ packages/regipy/regipy-1.7.1-py3-none-any.whl/regipy/__init__.py
@@ -0,0 +1,4 @@
+from .registry import *
+
+__title__ = 'regipy'
+__version__ = '1.7.1'
---
+++ packages/regipy/regipy-1.7.1-py3-none-any.whl/regipy/plugins/utils.py
@@ -0,0 +1,36 @@
+import jsonlines
+
+import attr
+from tqdm import tqdm
+
+from regipy.plugins.plugin import PLUGINS
+
+
+def dump_hive_to_json(registry_hive, output_path, name_key_entry, verbose=False):
+    with jsonlines.open(output_path, mode='w') as writer:
+        for entry in tqdm(registry_hive.recurse_subkeys(name_key_entry, as_json=True), disable=not verbose):
+            writer.write(attr.asdict(entry))
+
+
+def run_relevant_plugins(registry_hive, as_json=False, plugins=None):
+    """
+    Execute the relevant plugins on the hive
+    :param registry_hive: a RegistryHive object
+    :param as_json: Whether to return result as json
+    :param plugins: List of plugin to execute (names according to the NAME field in each plugin)
+    :return: The result, as dict
+    """
+    plugin_results = {}
+    for plugin_class in PLUGINS:
+        plugin = plugin_class(registry_hive, as_json=as_json)
+
+        # If the list of plugins is defined, but the plugin is not in the list skip it.
+        if plugins and not plugin.NAME in plugins:
+            continue
+
+        if plugin.can_run():
+            plugin.run()
+            plugin_results[plugin.NAME] = plugin.entries
+    return plugin_results
+
+
