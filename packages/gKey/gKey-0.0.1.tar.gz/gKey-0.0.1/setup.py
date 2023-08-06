656492df8d5a223fb88030f83c729010732ea149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:02 +0000

    Add package gKey/0.0.1/gKey-0.0.1.tar.gz

---
+++ packages/gKey/gKey-0.0.1.tar.gz/gKey-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gKey", # Replace with your own username
+    version="0.0.1",
+    author="gdavid7",
+    description="Python library used to encrypt and decrypt strings in the simplest possible way, while also being incredibly secure.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gdavid7/gkey",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
