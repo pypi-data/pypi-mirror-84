6cc699cb2396d24532ddc874e45ba93c7d453b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:24 +0000

    Add package baking-wijesooriya/0.0.3/baking_wijesooriya-0.0.3-py3-none-any.whl

---
+++ packages/baking-wijesooriya/baking_wijesooriya-0.0.3-py3-none-any.whl/baking/import.py
@@ -0,0 +1,12 @@
+import __init__ as b
+ 
+measure = b.measure
+mixIngredients = b.mixIngredients
+pourBatter = b.pourBatter
+setOvenTempTo = b.setOvenTempTo
+bakeBatter = b.bakeBatter
+assembleCake = b.assembleCake
+iceCake = b.iceCake
+complete = b.complete
+
+measure("flour")
