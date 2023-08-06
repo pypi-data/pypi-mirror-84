e0c96feaf0a27af075292fc7c2beb8317e25b3c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:47 +0000

    Add package dbr-example-yy/0.0.2/dbr-example-yy-0.0.2.tar.gz

---
+++ packages/dbr-example-yy/dbr-example-yy-0.0.2.tar.gz/dbr-example-yy-0.0.2/dbr_example_yy/package_main.py
@@ -0,0 +1,11 @@
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('storate_account_name', nargs='+')
+parser.add_argument('storage_scope', nargs='+')
+parser.add_argument('storage_key', nargs='+')
+parser.add_argument('src_blob_container', nargs='+')
+parser.add_argument('dst_blob_container', nargs='+')
+args = parser.parse_args()
+from folder import some_file
+some_file.test(args)
