e04395cf91761856896532d35b1f624b36ad81f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:55 +0000

    Add package aiocells/0.3.2/aiocells-0.3.2.tar.gz

---
+++ packages/aiocells/aiocells-0.3.2.tar.gz/aiocells-0.3.2/src/aiocells/demo_17.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import asyncio
+import functools
+import logging
+
+import aiocells
+
+
+logger = logging.getLogger()
+
+
+async def async_main():
+
+    clock = aiocells.ModClock()
+    graph = aiocells.DependencyGraph()
+
+    # Here, we simplify the previous demo by using a single variable to
+    # store the time and a single printer to announce the modifications
+    # when they happen. Because we are using 'compute_flow', the graph
+    # is computed when any of the timers go off.
+    time = aiocells.ModVariable(clock)
+    printer = aiocells.ModPrinter(clock, time, "time changed to {value}")
+    graph.add_precedence(time, printer)
+
+    # Set the time after 1 second
+    timer_1 = aiocells.source(functools.partial(aiocells.timer, 1, time))
+    graph.add_precedence(timer_1, time)
+
+    # Set the time after 3 seconds
+    timer_3 = aiocells.source(functools.partial(aiocells.timer, 3, time))
+    graph.add_precedence(timer_3, time)
+
+    while await aiocells.compute_flow(graph):
+        pass
+
+
+def main():
+    asyncio.run(async_main())
