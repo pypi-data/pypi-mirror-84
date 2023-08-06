573cd6f882e4d58dec5df117b1273a0e180d1a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:52 +0000

    Add package pyModis/2.0.8/pyModis-2.0.8-py3.5.egg

---
+++ packages/pyModis/pyModis-2.0.8-py3.5.egg/pymodis/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+
+from . import downmodis
+from . import parsemodis
+from . import convertmodis
+from . import optparse_required
+try:
+    from . import qualitymodis
+    from . import convertmodis_gdal
+except ImportError:
+    print("qualitymodis and convertmodis_gdal modules not enabled, "
+          "maybe Python GDAL is missing")
+    pass
+from . import productmodis
+try:
+    from . import optparse_gui
+except:
+    print("WxPython missing, no GUI enabled")
+__version__ = '2.0.8'
