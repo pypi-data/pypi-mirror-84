8c70ba71383eb4a3e1933b3d9c5862f87aebd93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:30 +0000

    Add package django-admin-urls/0.1.0/django-admin-urls-0.1.0.tar.gz

---
+++ packages/django-admin-urls/django-admin-urls-0.1.0.tar.gz/django-admin-urls-0.1.0/dj_admin_urls/urls.py
@@ -0,0 +1,35 @@
+from typing import Type
+
+from django.db.models.base import Model
+from django.urls import reverse
+
+from .utils import obj_model_name_with_app_name
+
+__all__ = (
+    'admin_add_url',
+    'admin_change_url',
+    'admin_delete_url',
+    'admin_index_ur',
+)
+
+
+def admin_add_url(model: Type[Model]):
+    app_label, model_name = obj_model_name_with_app_name(model)
+
+    return reverse(f"admin:{app_label}_{model_name}_add")
+
+
+def admin_change_url(obj: Type[Model]):
+    app_label, model_name = obj_model_name_with_app_name(obj)
+
+    return reverse(f"admin:{app_label}_{model_name}_change", args=(obj.pk,))
+
+
+def admin_delete_url(obj: Type[Model]):
+    app_label, model_name = obj_model_name_with_app_name(obj)
+
+    return reverse(f"admin:{app_label}_{model_name}_delete", args=(obj.pk,))
+
+
+def admin_index_url():
+    return reverse("admin:index")
---
+++ packages/django-admin-urls/django-admin-urls-0.1.0.tar.gz/django-admin-urls-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dj_admin_urls']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Django>=3.0.0']
+
+setup_kwargs = {
+    'name': 'django-admin-urls',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Alex Uzjakov',
+    'author_email': 'cyberbudy@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
