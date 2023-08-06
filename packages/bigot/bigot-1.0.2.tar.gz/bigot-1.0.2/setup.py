2e3da1a543a8993a514303024144508f45e0d80e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:24 +0000

    Add package bigot/1.0.2/bigot-1.0.2.tar.gz

---
+++ packages/bigot/bigot-1.0.2.tar.gz/bigot-1.0.2/setup.py
@@ -0,0 +1,25 @@
+
+"""Setup."""
+from setuptools import setup, find_packages
+
+with open("README.MD", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="bigot",
+    version="1.0.2",
+    packages=find_packages(),
+    install_requires=[
+        "memory-profiler",
+        "pandas",
+        "plotly",
+        "pandas",
+    ],
+    author="Arnaud de Saint Meloir",
+    author_email="arnaud.desaintmeloir@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arnaudsm/bigot",
+    description="Benchmarking library with Space and Time Complexity estimation",  # noqa
+    include_package_data=True
+)
