f2fb86be8cb5327687b30558b19c547c35c15755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:30 +0000

    Add package datetoken/0.5.2/datetoken-0.5.2-py2.py3-none-any.whl

---
+++ packages/datetoken/datetoken-0.5.2-py2.py3-none-any.whl/datetoken/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.5.2'
+
+DEFAULT_TOKEN = 'now'
---
+++ packages/datetoken/datetoken-0.5.2-py2.py3-none-any.whl/datetoken/utils.py
@@ -0,0 +1,20 @@
+from .evaluator import Datetoken
+
+
+def token_to_date(token=None, **kwargs):
+    """ Shortcut to rapidly get a datetime object from string tokens
+    :return: datetime.datetime
+    """
+    return Datetoken(token=token, **kwargs).to_date()
+
+
+def token_to_utc_date(token=None, **kwargs):
+    """
+    Quickly evaluate a token and coerce it back to UTC
+    :param token: string payload
+    :param kwargs:
+        - at: datetime.datetime
+        - tz: string or pytz.timezone
+    :return:
+    """
+    return Datetoken(token=token, **kwargs).to_utc_date()
