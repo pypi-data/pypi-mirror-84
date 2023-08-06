d8c836355253fc291fe4f1d30fce803ea41caa31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:16 +0000

    Add package pycurd/0.1.7/pycurd-0.1.7-py3-none-any.whl

---
+++ packages/pycurd/pycurd-0.1.7-py3-none-any.whl/pycurd/error.py
@@ -0,0 +1,27 @@
+
+class PyCrudException(Exception):
+    pass
+
+
+class DBException(PyCrudException):
+    pass
+
+
+class PermissionException(PyCrudException):
+    pass
+
+
+class InvalidQueryConditionColumn(PyCrudException):
+    pass
+
+
+class InvalidQueryConditionValue(PyCrudException):
+    pass
+
+
+class UnknownQueryOperator(PyCrudException):
+    pass
+
+
+class UnsupportedQueryOperator(PyCrudException):
+    pass
