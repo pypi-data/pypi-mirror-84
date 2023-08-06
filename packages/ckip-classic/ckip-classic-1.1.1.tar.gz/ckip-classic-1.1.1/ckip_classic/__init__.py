e91a05992b620000be379aaa720f7db42788ccd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:15 +0000

    Add package ckip-classic/1.1.1/ckip-classic-1.1.1.tar.gz

---
+++ packages/ckip-classic/ckip-classic-1.1.1.tar.gz/ckip-classic-1.1.1/ckip_classic/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+__author_name__ = 'Mu Yang'
+__author_email__ = 'emfomy@gmail.com'
+__copyright__ = '2018-2020 CKIP Lab'
+
+__title__ = 'CkipClassic'
+__version__ = '1.1.1'
+__description__ = 'CKIP Classic NLP Tools'
+__license__ = 'CC BY-NC-SA 4.0'
+
+__url__ = 'https://ckip-classic.readthedocs.io'
+__download_url__ = __url__+'/tarball/'+__version__
