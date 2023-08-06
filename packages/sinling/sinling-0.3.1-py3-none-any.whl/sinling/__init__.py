9a0e45134a0d4fbc04748dde394173da2b97fccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:46 +0000

    Add package sinling/0.3.1/sinling-0.3.1-py3-none-any.whl

---
+++ packages/sinling/sinling-0.3.1-py3-none-any.whl/sinling/__init__.py
@@ -0,0 +1,1 @@
+from sinling.sinhala import *
---
+++ packages/sinling/sinling-0.3.1-py3-none-any.whl/sinling/core/__init__.py
@@ -0,0 +1,4 @@
+from sinling.core.joiner import *
+from sinling.core.stemmer import *
+from sinling.core.tokenizer import *
+from sinling.core.tagger import *
