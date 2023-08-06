f34b29c56e1bd64aed95a7175cbbf46329334f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:44 +0000

    Add package Pranay-Udacity-distributions/0.1/Pranay_Udacity_distributions-0.1.tar.gz

---
+++ packages/Pranay-Udacity-distributions/Pranay_Udacity_distributions-0.1.tar.gz/Pranay_Udacity_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Pranay_Udacity_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Pranay_Udacity_distributions'],
+      author="Pranay Barkataki",
+      author_email="pranay.barkataki@gmail.com",
+      zip_safe=False)
