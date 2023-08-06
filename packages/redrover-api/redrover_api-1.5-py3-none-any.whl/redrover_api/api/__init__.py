a7f81e069fa950ef5fa1187b799b3727f89904ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:00 +0000

    Add package redrover-api/1.5/redrover_api-1.5-py3-none-any.whl

---
+++ packages/redrover-api/redrover_api-1.5-py3-none-any.whl/redrover_api/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from redrover_api.api.connection_api import ConnectionApi
+from redrover_api.api.organization_api import OrganizationApi
+from redrover_api.api.reference_data_api import ReferenceDataApi
+from redrover_api.api.sso_api import SsoApi
+from redrover_api.api.user_api import UserApi
+from redrover_api.api.vacancy_api import VacancyApi
