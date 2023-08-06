9000cf579797a66e3a1a0a63ed75942f34688d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:04 +0000

    Add package resizeImgForML/0.0.7/resizeImgForML-0.0.7.tar.gz

---
+++ packages/resizeImgForML/resizeImgForML-0.0.7.tar.gz/resizeImgForML-0.0.7/resizeImgForML/__init__.py
@@ -0,0 +1,7 @@
+from .resizeImgForML import *
+
+__copyright__    = 'Copyright (C) 2020 Kosuke Mawatari'
+__version__      = '0.0.7'
+__license__      = 'MIT'
+__author__       = 'Kosuke Mawatari'
+__url__          = 'https://github.com/k-mawa/resizeImgForML'
