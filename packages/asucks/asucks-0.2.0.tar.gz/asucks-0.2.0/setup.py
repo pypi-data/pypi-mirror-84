ce5c0282adc9fb1854ef958d338077c5721ce18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:50 +0000

    Add package asucks/0.2.0/asucks-0.2.0.tar.gz

---
+++ packages/asucks/asucks-0.2.0.tar.gz/asucks-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['asucks']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp==3.7.2', 'click==7.1.2']
+
+setup_kwargs = {
+    'name': 'asucks',
+    'version': '0.2.0',
+    'description': 'SOCKS5 async python server',
+    'long_description': None,
+    'author': 'Gabriel Tincu',
+    'author_email': 'gabi@aiven.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
