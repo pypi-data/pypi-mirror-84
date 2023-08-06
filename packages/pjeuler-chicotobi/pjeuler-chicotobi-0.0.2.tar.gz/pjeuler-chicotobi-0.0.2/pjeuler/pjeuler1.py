f76a8f718b8ffa9b1a9320cec9152a3909f355bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:09 +0000

    Add package pjeuler-chicotobi/0.0.2/pjeuler-chicotobi-0.0.2.tar.gz

---
+++ packages/pjeuler-chicotobi/pjeuler-chicotobi-0.0.2.tar.gz/pjeuler-chicotobi-0.0.2/pjeuler/pjeuler1.py
@@ -0,0 +1,13 @@
+from tools import *
+
+def pjeuler1():
+  val = 0
+  for i in range(1000):
+    if i%3 == 0 or i%5 == 0:
+      val += i
+  return val
+
+def pjeuler2():
+  arr = np.array(fib(40))
+  arr = arr[arr%2==0]
+  return sum(arr[arr<4e6])
