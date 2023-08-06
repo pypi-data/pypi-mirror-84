6714d9270b6ba424aca10d32fe2cc76ef573ee18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:49 +0000

    Add package nitrogen/2.0.dev0/nitrogen-2.0.dev0.tar.gz

---
+++ packages/nitrogen/nitrogen-2.0.dev0.tar.gz/nitrogen-2.0.dev0/README.md
@@ -0,0 +1,7 @@
+# nitrogen
+
+NITROGEN is a Python package for nuclear motion calculations and ab initio rovibronic spectroscopy of small molecules.
+
+It is available from PyPI via `pip install nitrogen`. The source repository is [here](https://github.com/bchangala/nitrogen).
+
+NITROGEN ver. 2.x is a complete re-write in Python, currently in initial development and planning. The original C code (ver. 1.x) is available [here](https://www.colorado.edu/nitrogen).
---
+++ packages/nitrogen/nitrogen-2.0.dev0.tar.gz/nitrogen-2.0.dev0/nitrogen/__init__.py
@@ -0,0 +1,25 @@
+"""
+NITROGEN
+========
+
+A Python package for quantum nuclear motion
+calculations and rovibronic spectroscopy.
+
+"""
+
+# Import sub-packages and modules into namespace
+from . import autodiff
+from . import linalg 
+from . import dvr 
+from . import dfun 
+from . import coordsys 
+from . import ham
+from . import constants
+
+__version__ = '2.0.dev0'
+
+
+#################################################################
+# Define some top-level constants
+pi = 3.14159265358979323846264338327950288419716939937510   # pi
+deg = pi / 180.0 # 1 degree in radians
