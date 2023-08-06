8a3c94ca79218580a250c1409eb9526544b7bd23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:55 +0000

    Add package api-for-iot-module/1.0.1/api_for_iot_module-1.0.1.tar.gz

---
+++ packages/api-for-iot-module/api_for_iot_module-1.0.1.tar.gz/api_for_iot_module-1.0.1/docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use API for IoT Module in a project::
+
+    import api_for_iot_module
---
+++ packages/api-for-iot-module/api_for_iot_module-1.0.1.tar.gz/api_for_iot_module-1.0.1/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=7.0', ]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Cheslyn Williams",
+    author_email='cheslynwilliams318@gmail.com',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="API for IoT Module contains functions to be used for implementing a API for a camera module ",
+    entry_points={
+        'console_scripts': [
+            'api_for_iot_module=api_for_iot_module.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='api_for_iot_module',
+    name='api_for_iot_module',
+    packages=find_packages(),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/ChezzyBoi/api_for_iot_module',
+    version='1.0.1',
+    zip_safe=False,
+)
