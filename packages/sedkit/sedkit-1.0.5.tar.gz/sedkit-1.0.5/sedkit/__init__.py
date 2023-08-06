9f510bf9cdf25653f4bd16e87a511f494af61594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:42 +0000

    Add package sedkit/1.0.5/sedkit-1.0.5.tar.gz

---
+++ packages/sedkit/sedkit-1.0.5.tar.gz/sedkit-1.0.5/sedkit/__init__.py
@@ -0,0 +1,4 @@
+from .catalog import Catalog
+from .sed import SED, VegaSED
+from .spectrum import Spectrum, FileSpectrum, Vega, Blackbody
+from .modelgrid import ModelGrid, BTSettl, SpexPrismLibrary
