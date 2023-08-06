50f8a1e41d5627dd54cc06e878b8ebb15098191a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:48 +0000

    Add package SQLAlchemy-Api-Handler/0.5.4/SQLAlchemy_Api_Handler-0.5.4-py2.py3-none-any.whl

---
+++ packages/SQLAlchemy-Api-Handler/SQLAlchemy_Api_Handler-0.5.4-py2.py3-none-any.whl/sqlalchemy_api_handler/api_handler.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+from sqlalchemy import BigInteger,\
+                       Column
+
+from sqlalchemy_api_handler.bases.activator import Activator
+from sqlalchemy_api_handler.utils.humanize import humanize
+
+
+class ApiHandler(Activator):
+
+    id = Column(BigInteger,
+                primary_key=True,
+                autoincrement=True)
+
+    def dump(self):
+        pprint(vars(self))
+
+    def __repr__(self):
+        self_id = "unsaved"\
+               if self.id is None\
+               else str(self.id) + "/" + humanize(self.id)
+        return '<%s #%s>' % (self.__class__.__name__, self_id)
