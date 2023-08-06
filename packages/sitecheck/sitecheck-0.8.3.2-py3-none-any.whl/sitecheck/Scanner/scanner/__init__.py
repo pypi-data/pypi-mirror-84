985f08929eebfcfc23d06c5115ee3eee84f6df4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:24 +0000

    Add package sitecheck/0.8.3.2/sitecheck-0.8.3.2-py3-none-any.whl

---
+++ packages/sitecheck/sitecheck-0.8.3.2-py3-none-any.whl/sitecheck/Scanner/scanner/__init__.py
@@ -0,0 +1,14 @@
+"""
+    Geo-Instruments
+    Sitecheck Scanner
+"""
+# __name__ = "scanner"
+# __author__ = "Dan Edens"
+# __url__= "https://github.com/DanEdens/Sitecheck_Scrapper"
+# __status__  = "production"
+
+import logging
+import os
+
+logger = logging.getLogger('root')
+projectstore = os.environ['ROOT_DIR'] + '\\projects.ini'
