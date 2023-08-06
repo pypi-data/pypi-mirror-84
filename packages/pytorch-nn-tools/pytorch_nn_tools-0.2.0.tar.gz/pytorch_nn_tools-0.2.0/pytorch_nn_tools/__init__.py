a5f4dabd2476eba54ccb350b8f9609aedcc2d8e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:21 +0000

    Add package pytorch-nn-tools/0.2.0/pytorch_nn_tools-0.2.0.tar.gz

---
+++ packages/pytorch-nn-tools/pytorch_nn_tools-0.2.0.tar.gz/pytorch_nn_tools-0.2.0/pytorch_nn_tools/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for pytorch-nn-tools."""
+
+__author__ = """SergeM"""
+__email__ = 'serge-m@users.noreply.github.com'
+
+from .version import __version__
+
+from .hook import Hook, _hook_inner
+from .structure import get_output_sizes, LayerSize, NameGetter
+from .memory import dummy_batch
