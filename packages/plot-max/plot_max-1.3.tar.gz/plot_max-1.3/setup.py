3fe3e4590fb1fa659d2d55ec702ff4bd7701bb34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:40 +0000

    Add package plot-max/1.3/plot_max-1.3.tar.gz

---
+++ packages/plot-max/plot_max-1.3.tar.gz/plot_max-1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+files = ['plot_max/msyh.ttf']
+
+setup(name='plot_max',
+      version='1.3',
+      description='high level to use matplotlib',
+      url='http://github.com/pringwong',
+      author='pring',
+      author_email='huppid@qq.com',
+      packages=['plot_max'],
+      license='None',
+      zip_safe=False)
