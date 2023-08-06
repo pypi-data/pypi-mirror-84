615eb3ba5131a02dd7b497d1dfabe3dbd38ef4bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:39 +0000

    Add package rssarchive/0.5.2/rssarchive-0.5.2.tar.gz

---
+++ packages/rssarchive/rssarchive-0.5.2.tar.gz/rssarchive-0.5.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='rssarchive',
+      version='0.5.2',
+      keywords = 'RSS,SQlite, News',
+      description='Archive your RSS into SQLite:',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='http://github.com/suatatan/rssarchive',
+      author='Suat ATAN',
+      author_email='suatatan@gmail.com',
+      license='MIT',
+      packages=['rssarchive'],
+      test_suite='nose.collector',
+      scripts=['bin/rssarchive'],
+      tests_require=['nose'],
+      install_requires = ['newspaper3k','feedparser'],
+      zip_safe=False)
