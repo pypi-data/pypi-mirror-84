ac132fb7d177a03d0047fb43613a31b3f66b5d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:06 +0000

    Add package vehiclesfortest/0.1/vehiclesfortest-0.1.tar.gz

---
+++ packages/vehiclesfortest/vehiclesfortest-0.1.tar.gz/vehiclesfortest-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vehiclesfortest',
+version='0.1',
+description='Testing usage of packaging',
+packages=['vehiclesfortest'],
+author='Sufian',
+author_email='sufian@things.school',
+zip_safe=False)
---
+++ packages/vehiclesfortest/vehiclesfortest-0.1.tar.gz/vehiclesfortest-0.1/vehiclesfortest/app.py
@@ -0,0 +1,16 @@
+class vehicle:
+	wheels = 0
+	color = 'White'
+	
+	def set_wheels(self,value):
+		self.wheels = value
+
+	def set_color(self, value):
+		self.color = value
+
+'''
+car = vehicle()
+print("No. of wheels: " + str(car.wheels))
+car.set_wheels(4)
+print("New no. of wheels: " + str(car.wheels))
+'''
