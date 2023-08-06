5af58e72af192601c1b1cad4556833680ea0f96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:05 +0000

    Add package xiaobaiauto2/0.1.4.6/xiaobaiauto2-0.1.4.6-py3-none-any.whl

---
+++ packages/xiaobaiauto2/xiaobaiauto2-0.1.4.6-py3-none-any.whl/xiaobaiauto2/test/APITest2/__init__.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+__author__ = 'Tser'
+__email__ = '807447312@qq.com'
+__project__ = 'xiaobaiauto2'
+__script__ = '__init__.py.py'
+__create_time__ = '2020/10/4 21:43'
+
+
+'''
+
+@Request(title='测试百度搜索接口')
+def baidu_search_api():
+    pass
+
+if __name__ == '__main__':
+    baidu_search_api()
+'''
+print([1,2]  [2,1])
