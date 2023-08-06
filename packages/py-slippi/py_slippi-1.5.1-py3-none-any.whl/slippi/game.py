8fd082561609963567f1c017efad5dbb7595a8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:39 +0000

    Add package py-slippi/1.5.1/py_slippi-1.5.1-py3-none-any.whl

---
+++ packages/py-slippi/py_slippi-1.5.1-py3-none-any.whl/slippi/game.py
@@ -0,0 +1,56 @@
+from logging import debug
+
+from .event import FIRST_FRAME_INDEX
+from .parse import parse, ParseEvent
+from .util import *
+
+
+class Game(Base):
+    """Replay data from a game of Super Smash Brothers Melee."""
+
+    def __init__(self, input):
+        """Parses Slippi replay data from `input` (stream or path)."""
+
+        self.start = None
+        """:py:class:`slippi.event.Start`: Information about the start of the game"""
+
+        self.frames = []
+        """list(:py:class:`slippi.event.Frame`): Every frame of the game, indexed by frame number"""
+
+        self.end = None
+        """:py:class:`slippi.event.End`: Information about the end of the game"""
+
+        self.metadata = None
+        """:py:class:`slippi.metadata.Metadata`: Miscellaneous data not directly provided by Melee"""
+
+        self.metadata_raw = None
+        """dict: Raw JSON metadata, for debugging and forward-compatibility"""
+
+        handlers = {
+            ParseEvent.START: lambda x: setattr(self, 'start', x),
+            ParseEvent.FRAME: self._add_frame,
+            ParseEvent.END: lambda x: setattr(self, 'end', x),
+            ParseEvent.METADATA: lambda x: setattr(self, 'metadata', x),
+            ParseEvent.METADATA_RAW: lambda x: setattr(self, 'metadata_raw', x)}
+
+        parse(input, handlers)
+
+    def _add_frame(self, f):
+        idx = f.index - FIRST_FRAME_INDEX
+        count = len(self.frames)
+        if idx == count:
+            self.frames.append(f)
+        elif idx < count: # rollback
+            debug(f"rollback: {count-1} -> {idx}")
+            self.frames[idx] = f
+        else:
+            raise Exception(f'missing frames: {count-1} -> {idx}')
+
+    def _attr_repr(self, attr):
+        self_attr = getattr(self, attr)
+        if isinstance(self_attr, list):
+            return '%s=[...](%d)' % (attr, len(self_attr))
+        elif attr == 'metadata_raw':
+            return None
+        else:
+            return super()._attr_repr(attr)
---
+++ packages/py-slippi/py_slippi-1.5.1-py3-none-any.whl/slippi/metadata.py
@@ -0,0 +1,77 @@
+import re
+from datetime import datetime, timedelta, timezone
+
+from . import event as evt
+from . import id as sid
+from .util import *
+
+
+class Metadata(Base):
+    """Miscellaneous data not directly provided by Melee."""
+    def __init__(self, date, duration, platform, players, console_name=None):
+        self.date = date #: datetime: Game start date & time
+        self.duration = duration #: int: Duration of game, in frames
+        self.platform = platform #: :py:class:`Platform`: Platform the game was played on (console/dolphin)
+        self.players = players #: tuple(:py:class:`Player` | None): Player metadata by port (port 1 is at index 0; empty ports will contain None)
+        self.console_name = console_name #: str | None: Name of the console the game was played on, if any
+
+    @classmethod
+    def _parse(cls, json):
+        d = json['startAt'].rstrip('\x00') # workaround for Nintendont/Slippi<1.5 bug
+        # timezone & fractional seconds aren't always provided, so parse the date manually (strptime lacks support for optional components)
+        m = [int(g or '0') for g in re.search(r'(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(?:\.(\d+))?(?:Z|\+(\d{2})(\d{2}))?$', d).groups()]
+        date = datetime(*m[:7], timezone(timedelta(hours=m[7], minutes=m[8])))
+        try: duration = 1 + json['lastFrame'] - evt.FIRST_FRAME_INDEX
+        except KeyError: duration = None
+        platform = cls.Platform(json['playedOn'])
+        try: console_name = json['consoleNick']
+        except KeyError: console_name = None
+        players = [None, None, None, None]
+        for i in PORTS:
+            try: players[i] = cls.Player._parse(json['players'][str(i)])
+            except KeyError: pass
+        return cls(date=date, duration=duration, platform=platform, players=tuple(players), console_name=console_name)
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+        return self.date == other.date and self.duration == other.duration and self.platform == other.platform and self.players == other.players and self.console_name == other.console_name
+
+
+    class Player(Base):
+        def __init__(self, characters, netplay=None):
+            self.characters = characters #: dict(:py:class:`slippi.id.InGameCharacter`, int): Character(s) used, with usage duration in frames (for Zelda/Sheik)
+            self.netplay = netplay #: :py:class:`Netplay` | None: Netplay info (Dolphin-only)
+
+        @classmethod
+        def _parse(cls, json):
+            characters = {}
+            for char_id, duration in json['characters'].items():
+                characters[sid.InGameCharacter(int(char_id))] = duration
+            try:
+                netplay = cls.Netplay(code=json['names']['code'], name=json['names']['netplay'])
+            except KeyError: netplay = None
+            return cls(characters, netplay)
+
+        def __eq__(self, other):
+            if not isinstance(other, self.__class__):
+                return NotImplemented
+            return self.characters == other.characters and self.netplay == other.netplay
+
+
+        class Netplay(Base):
+            def __init__(self, code, name):
+                self.code = code #: str: Netplay code (e.g. "ABCD#123")
+                self.name = name #: str: Netplay nickname
+
+            def __eq__(self, other):
+                if not isinstance(other, self.__class__):
+                    return NotImplemented
+                return self.code == other.code and self.name == other.name
+
+
+    class Platform(Enum):
+        CONSOLE = 'console'
+        DOLPHIN = 'dolphin'
+        NETWORK = 'network'
+        NINTENDONT = 'nintendont'
