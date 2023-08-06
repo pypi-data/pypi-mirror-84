491135360167f57093a2eea209b7b012cbcdf044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:10 +0000

    Add package Hey-World/0.0.6/Hey_World-0.0.6-py3-none-any.whl

---
+++ packages/Hey-World/Hey_World-0.0.6-py3-none-any.whl/HiWorld/HiWorld.py
@@ -0,0 +1,10 @@
+# class HelloWorld:
+#
+#     def __init__(self):
+#         pass
+#
+#     def hello(self, name):
+#         print(f"Hey, {name}. \n I'm Rahul. \n Nice to meet you.")
+
+def hey():
+    print(' 👋 Hi there, Rahul here 🤗 ')
