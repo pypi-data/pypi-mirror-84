698e16522188f5336da1af8103878fe812918b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:28 +0000

    Add package snappi/0.0.5/snappi-0.0.5.tar.gz

---
+++ packages/snappi/snappi-0.0.5.tar.gz/snappi-0.0.5/snappi/configoptions.py
@@ -0,0 +1,27 @@
+from .snappicommon import SnappiObject
+
+
+class ConfigOptions(SnappiObject):
+    _TYPES = {
+        'port_options': '.portoptions.PortOptions',
+    }
+
+    def __init__(self):
+        super().__init__()
+        self.set()
+
+    @property
+    def port_options(self):
+        """config_options.port_options getter
+
+        TBD  
+
+        Returns: obj(snappi.PortOptions)
+        """
+        from .portoptions import PortOptions
+        if 'port_options' not in self._properties or self._properties['port_options'] is None:
+            self._properties['port_options'] = PortOptions()
+        return self._properties['port_options']
+
+    def set(self):
+        return self
