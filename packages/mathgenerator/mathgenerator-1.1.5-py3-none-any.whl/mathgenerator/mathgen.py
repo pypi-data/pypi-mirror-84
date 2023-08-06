32a9789ce66b57f372394a3fb819dc79d4bd601b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:31 +0000

    Add package mathgenerator/1.1.5/mathgenerator-1.1.5-py3-none-any.whl

---
+++ packages/mathgenerator/mathgenerator-1.1.5-py3-none-any.whl/mathgenerator/mathgen.py
@@ -0,0 +1,10 @@
+from .funcs import *
+from .__init__ import getGenList
+
+genList = getGenList()
+
+
+# || Non-generator Functions
+def genById(id):
+    generator = genList[id][2]
+    return (generator())
