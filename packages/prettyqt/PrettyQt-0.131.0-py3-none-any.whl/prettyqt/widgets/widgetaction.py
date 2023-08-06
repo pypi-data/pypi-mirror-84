66af5491605ba9f07aff36fa8e4b217217a0b8ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:10 +0000

    Add package prettyqt/0.131.0/PrettyQt-0.131.0-py3-none-any.whl

---
+++ packages/prettyqt/PrettyQt-0.131.0-py3-none-any.whl/prettyqt/widgets/widgetaction.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from qtpy import QtWidgets
+
+from prettyqt import widgets
+from typing import Optional
+
+
+QtWidgets.QWidgetAction.__bases__ = (widgets.Action,)
+
+
+class WidgetAction(QtWidgets.QWidgetAction):
+    def __init__(self, parent: Optional[QtWidgets.QWidget] = None) -> None:
+        super().__init__(parent)
+
+    #     self.set_text(text)
+    #     self.set_icon(icon)
+    #     self.set_shortcut(shortcut)
+    #     self.set_tooltip(tooltip)
