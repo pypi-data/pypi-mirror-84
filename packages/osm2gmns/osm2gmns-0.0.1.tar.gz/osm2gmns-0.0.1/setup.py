854c538be4154601888c015b685f3a6a320b0f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:09 +0000

    Add package osm2gmns/0.0.1/osm2gmns-0.0.1.tar.gz

---
+++ packages/osm2gmns/osm2gmns-0.0.1.tar.gz/osm2gmns-0.0.1/setup.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+# @author       Jiawei Lu (jiaweil9@asu.edu)
+# @time         2020/11/4 23:03
+# @desc         [script description]
+
+
+import setuptools
+
+setuptools.setup(
+    name='osm2gmns',
+    version='0.0.1',
+    author='Jiawei Lu, Xuesong Zhou',
+    author_email='jiaweil9@asu.edu, xzhou74@asu.edu',
+    url='https://github.com/jiawei92/OSM2GMNS',
+    description='build network with gmns format using .osm file from openstreetmap',
+    packages=['osm2gmns'],
+    install_requires=[]
+)
