6ce52090c00fabff0dbd56f1053cba8d24a510d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:05 +0000

    Add package magic-toolkit/0.0.3/magic_toolkit-0.0.3-py3-none-any.whl

---
+++ packages/magic-toolkit/magic_toolkit-0.0.3-py3-none-any.whl/magic/common/estimator.py
@@ -0,0 +1,29 @@
+"""abstract class"""
+
+class Estimator(object):
+
+    def fit(self):
+        """ train model """
+        raise NotImplementedError
+
+    def predict(self, *inputs):
+        """ do inference to predict """
+        raise NotImplementedError
+
+    def eval(self):
+        """ do inference to evaluate model performance """
+        raise NotImplementedError
+
+    def summary(self):
+        """model summary"""
+
+    def load(self):
+        """ load pre-trained model """
+
+    def save(self):
+        """ save trained model to local """
+
+    def export_model(self):
+        """export onnx, protobuf"""
+
+
