8fbb870fbacf0315912973855cad334e2308dbf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:31 +0000

    Add package enaml/0.12.0/enaml-0.12.0-cp39-cp39-manylinux2010_x86_64.whl

---
+++ packages/enaml/enaml-0.12.0-cp39-cp39-manylinux2010_x86_64.whl/enaml/qt/qt_radio_button.py
@@ -0,0 +1,28 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2013-2017, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+#------------------------------------------------------------------------------
+from atom.api import Typed
+
+from enaml.widgets.radio_button import ProxyRadioButton
+
+from .QtWidgets import QRadioButton
+
+from .qt_abstract_button import QtAbstractButton
+
+
+class QtRadioButton(QtAbstractButton, ProxyRadioButton):
+    """ A Qt implementation of an Enaml RadioButton.
+
+    """
+    #: A reference to the widget created by the proxy.
+    widget = Typed(QRadioButton)
+
+    def create_widget(self):
+        """ Create the underlying radio button widget.
+
+        """
+        self.widget = QRadioButton(self.parent_widget())
